a1e4c20771831d0a2bfe0fd6ee098818dd2b7d47
francis <francis>
2007-08-23 14:06:27 +0000

    Make it HTML 4.0 strict.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+    # This was the best solution I could find to this, yeuch.
+    # http://www.ruby-forum.com/topic/88857
+    def stylesheet_link_tag_html4( _n )
+        return stylesheet_link_tag( _n ).gsub( ' />', '>' )
+    end
+
 end
