550b119415eab54b92a586f924700f95f8cfb58d
francis <francis>
2007-10-08 14:50:27 +0000

    Use UTC as time storage

--- config/environment.rb
+++ config/environment.rb
@@ -39,7 +39,7 @@ Rails::Initializer.run do |config|
   # config.active_record.observers = :cacher, :garbage_collector
 
   # Make Active Record use UTC-base instead of local time
-  # config.active_record.default_timezone = :utc
+  config.active_record.default_timezone = :utc
   
   # See Rails::Configuration for more options
 end
