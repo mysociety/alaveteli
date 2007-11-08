06252081cd58732c76c14801c3d67c1b3be97dcc
francis <francis>
2007-11-08 12:51:04 +0000

    Some test stubs to force them into code coverage.

---
+++ spec/controllers/application_spec.rb
@@ -0,0 +1,9 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe ApplicationController, "when authenticating user" do
+    fixtures :users
+  
+#    it "blah" do
+#    end
+end
+
--- spec/controllers/request_controller_spec.rb
+++ spec/controllers/request_controller_spec.rb
@@ -1,5 +1,28 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+describe RequestController, "when showing the front page" do
+    integrate_views
+    fixtures :users
+
+    it "should be successful" do
+        get :frontpage
+        response.should be_success
+    end
+
+    it "should have sign in/up link when not signed in" do
+        get :frontpage
+        response.should have_tag('a', "Sign in or sign up")
+    end
+
+    it "should have sign out link when signed in" do
+        session[:user] = users(:bob_smith_user)
+        get :frontpage
+        response.should have_tag('a', "Sign out")
+    end
+        
+
+end
+
 describe RequestController, "when listing all requests" do
     fixtures :info_requests
   
