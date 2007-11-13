1da2acc5a2b9cd518b57f13ec9927333d6e7cfa8
francis <francis>
2007-11-13 12:15:46 +0000

    Test that you can use old short name for public bodies in URLs.

--- spec/controllers/body_controller_spec.rb
+++ spec/controllers/body_controller_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe BodyController, "when showing a body" do
-    fixtures :public_bodies
+    fixtures :public_bodies, :public_body_versions
   
     it "should be successful" do
         get :show, :simple_short_name => "dfh"
@@ -16,6 +16,11 @@ describe BodyController, "when showing a body" do
     it "should assign the body" do
         get :show, :simple_short_name => "dfh"
         assigns[:public_bodies].should == [ public_bodies(:humpadink_public_body) ]
+    end
+
+    it "should redirect to newest name if you use historic name of public body in URL" do
+        get :show, :simple_short_name => "hdink"
+        response.should redirect_to(:controller => 'body', :action => 'show', :simple_short_name => "dfh")
     end
     
     # XXX test for 404s when don't give valid name
