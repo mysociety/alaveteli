8ad28163639f7d1d4806ee5df000fdde43fc350d
francis <francis>
2007-11-27 15:46:12 +0000

    Various things deploying on live servers.

--- db/migrate/021_remove_contains_information_default.rb
+++ db/migrate/021_remove_contains_information_default.rb
@@ -1,9 +1,16 @@
 class RemoveContainsInformationDefault < ActiveRecord::Migration
   def self.up
         change_column :incoming_messages, :contains_information, :boolean, :default => nil
+        drop_table :rejection_reasons
   end
 
   def self.down
         change_column :incoming_messages, :contains_information, :boolean, :default => false
+        create_table :rejection_reasons do |t|
+          t.column :incoming_message_id, :integer
+          t.column :reason, :string
+          t.column :created_at, :datetime
+          t.column :updated_at, :datetime
+        end
   end
 end
