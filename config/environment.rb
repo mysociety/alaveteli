e7ca138e93cce427fb69dc1b9eca5099aa52ea17
francis <francis>
2007-11-27 11:22:07 +0000

    We're using rails 1.2.5, as that is what is in Debian Etch.

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,12 @@
 # Be sure to restart your web server when you modify this file.
 
+
 # Uncomment below to force Rails into production mode when 
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.2.1' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '1.2.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
