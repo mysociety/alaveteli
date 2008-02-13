dfa29061fd7a528487d4ab1fc72c3ff34d175aeb
francis <francis>
2008-02-13 08:12:10 +0000

    Can't believe ActionMailer defaults to returning errors, rather than throwing exceptions. This feels like the early 90s again.

--- app/models/application_mailer.rb
+++ app/models/application_mailer.rb
@@ -4,11 +4,13 @@
 # Copyright (c) 2008 UK Citizens Online Democracy. All rights reserved.
 # Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: application_mailer.rb,v 1.2 2008-01-14 12:27:56 francis Exp $
+# $Id: application_mailer.rb,v 1.3 2008-02-13 08:12:10 francis Exp $
 
 class ApplicationMailer < ActionMailer::Base
     # Include all the functions views get, as emails call similar things.
     helper :application
+
+    self.raise_delivery_errors = true
 
     def contact_from_name_and_email
         "GovernmentSpy <"+MySociety::Config.get("CONTACT_EMAIL", 'contact@localhost')+">"
