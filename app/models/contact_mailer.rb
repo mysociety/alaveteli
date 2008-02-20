c70d54942ab8d789c82ef9740d6c1c711f3632ae
francis <francis>
2008-02-20 07:40:43 +0000

    Tweaks to contact form.

--- app/models/contact_mailer.rb
+++ app/models/contact_mailer.rb
@@ -4,7 +4,7 @@
 # Copyright (c) 2008 UK Citizens Online Democracy. All rights reserved.
 # Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: contact_mailer.rb,v 1.1 2008-02-19 17:41:58 francis Exp $
+# $Id: contact_mailer.rb,v 1.2 2008-02-20 07:40:43 francis Exp $
 
 class ContactMailer < ApplicationMailer
  
@@ -13,7 +13,7 @@ class ContactMailer < ApplicationMailer
         @recipients = contact_from_name_and_email
         @subject = subject
         @body = { :message => message,
-            :request_details => "IP " + request_details 
+            :request_details => request_details 
         }
     end
 
