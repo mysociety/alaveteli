f3c4eb0a94502c072efec4acbe80c89416586aba
francis <francis>
2008-03-06 10:33:45 +0000

    Index the solr freshness boolean

--- db/migrate/041_index_requests_with_solr.rb
+++ db/migrate/041_index_requests_with_solr.rb
@@ -1,9 +1,11 @@
 class IndexRequestsWithSolr < ActiveRecord::Migration
     def self.up
         add_column :info_requests, :solr_up_to_date, :boolean, :default => false, :null => false
+        add_index :info_requests, :solr_up_to_date
     end
 
     def self.down
+        remove_index :info_requests, :solr_up_to_date
         remove_column :info_requests, :solr_up_to_date
     end
 end
