583b0b8348449c1849c2e0af5196396c35bff0bf
francis <francis>
2008-03-12 23:39:36 +0000

    Link to the Rails bug tracker for this fix.

--- vendor/rails-2.0.2/railties/lib/fcgi_handler.rb
+++ vendor/rails-2.0.2/railties/lib/fcgi_handler.rb
@@ -8,6 +8,7 @@ class RailsFCGIHandler
     'HUP'     => :reload,
     'INT'     => :exit_now,
     'TERM'    => :exit, # XXX was exit_now, changed by mySociety 2008-03-10
+                        # see also http://dev.rubyonrails.org/ticket/5399 - gah!
     'USR1'    => :exit,
     'USR2'    => :restart
   }
