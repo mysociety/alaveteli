270e7c1ab28ef161bf39628db1d33b049941c2c5
francis <francis>
2008-03-17 10:54:41 +0000

    Show requests in requires_admin state prominently on admin interface

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,16 +4,21 @@
 # Copyright (c) 2007 UK Citizens Online Democracy. All rights reserved.
 # Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: admin_controller.rb,v 1.3 2008-03-14 11:08:41 francis Exp $
+# $Id: admin_controller.rb,v 1.4 2008-03-17 10:54:42 francis Exp $
 
 class AdminController < ApplicationController
     layout "admin"
 
     def index
+        # Overview counts of things
         @user_count = User.count
         @public_body_count = PublicBody.count
         @info_request_count = InfoRequest.count
 
+        # Tasks to do
+        @requires_admin_requests = InfoRequest.find(:all, :conditions => ["described_state = 'requires_admin'"])
+
+        # Recent events
         @events_title = "Events in last week"
         date_back_to = Time.now - 1.week
         if params[:month]
@@ -24,7 +29,6 @@ class AdminController < ApplicationController
             @events_title = "Events, all time"
             date_back_to = Time.now - 1000.years
         end
-
         @events = InfoRequestEvent.find(:all, :order => "created_at desc, id desc",
                 :conditions => ["created_at > ? ", date_back_to])
     end
