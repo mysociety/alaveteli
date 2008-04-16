8fa715e5df5e388ef15581b93c8f16e3e94beede
francis <francis>
2008-04-16 10:27:58 +0000

    A note

--- db/migrate/051_add_track_things_unique_indices.rb
+++ db/migrate/051_add_track_things_unique_indices.rb
@@ -6,6 +6,9 @@ class AddTrackThingsUniqueIndices < ActiveRecord::Migration
         # I guess the query in ./activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
         # needs improving to not detect indices with complex components, rather than detect part of them.
         #execute "create unique index track_things_sent_emails_unique_index on track_things_sent_emails(track_thing_id, coalesce(info_request_event_id, -1), coalesce(user_id, -1), coalesce(public_body_id, -1))"
+        #
+        # I tried altering config.active_record.schema_format to :sql in config/environment.rb, but
+        # got all sorts of other problems with the test user not being a database super user, so gave up again.
     end
 
     def self.down
