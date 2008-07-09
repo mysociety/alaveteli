29576f0bb98ba403aa5874409dc32ec82d6fb241
francis <francis>
2008-07-09 07:39:08 +0000

    Fix terrible wording on contact form errors.

--- app/models/contact_validator.rb
+++ app/models/contact_validator.rb
@@ -15,7 +15,7 @@
 # Copyright (c) 2008 UK Citizens Online Democracy. All rights reserved.
 # Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: contact_validator.rb,v 1.15 2008-05-21 10:51:24 francis Exp $
+# $Id: contact_validator.rb,v 1.16 2008-07-09 07:39:08 francis Exp $
 
 class ContactValidator < ActiveRecord::BaseWithoutTable
     column :name, :string
@@ -23,7 +23,10 @@ class ContactValidator < ActiveRecord::BaseWithoutTable
     column :subject, :text
     column :message, :text
 
-    validates_presence_of :name, :email, :subject, :message
+    validates_presence_of :name, :message => "^Please enter your name"
+    validates_presence_of :email, :message => "^Please enter your email address"
+    validates_presence_of :subject, :message => "^Please enter a subject"
+    validates_presence_of :message, :message => "^Please enter the message you want to send"
 
     def validate
         errors.add(:email, "doesn't look like a valid address") unless MySociety::Validate.is_valid_email(self.email)
