1a36d38bb490d99e480e00e2f22e9a614830fe61
francis <francis>
2008-07-28 19:03:45 +0000

    Better error message.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,7 +4,7 @@
 # Copyright (c) 2007 UK Citizens Online Democracy. All rights reserved.
 # Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: user_controller.rb,v 1.49 2008-06-12 14:35:09 francis Exp $
+# $Id: user_controller.rb,v 1.50 2008-07-28 19:03:45 francis Exp $
 
 class UserController < ApplicationController
     # Show page about a set of users with same url name
@@ -16,7 +16,7 @@ class UserController < ApplicationController
 
         @display_user = User.find(:first, :conditions => [ "url_name = ? and email_confirmed", params[:url_name] ])
         if not @display_user
-            raise "user not found"
+            raise "user not found, url_name=" + params[:url_name]
         end
         @same_name_users = User.find(:all, :conditions => [ "name = ? and email_confirmed and id <> ?", @display_user.name, @display_user.id ], :order => "created_at")
 
