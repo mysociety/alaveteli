343ef95df5500537ccf7385d22a732e21bce6ffa
francis <francis>
2008-08-29 16:57:56 +0000

    Indices

---
+++ db/migrate/064_indices_for_annotations.rb
@@ -0,0 +1,11 @@
+class IndicesForAnnotations < ActiveRecord::Migration
+    def self.up
+        add_index :info_request_events, :created_at
+        add_index :info_request_events, :info_request_id
+    end
+
+    def self.down
+        remove_index :info_request_events, :created_at
+        remove_index :info_request_events, :info_request_id
+    end
+end
