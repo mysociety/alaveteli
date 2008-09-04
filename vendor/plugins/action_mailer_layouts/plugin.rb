481936b26741b33ff842406e6af161aba4b2d9b1
francis <francis>
2008-09-04 06:04:33 +0000

    Upgrade to new version for Rails 2.1

--- vendor/plugins/action_mailer_layouts/plugin.rb
+++ vendor/plugins/action_mailer_layouts/plugin.rb
@@ -7,19 +7,24 @@ module ActionMailer
     alias_method :render_message_without_layouts, :render_message
 
     def render_message(method_name, body)
-      layout = "./" + (@layout ? @layout.to_s : self.class.to_s.underscore)
-      md = /^([^\.]+)\.([^\.]+\.[^\.]+)\.(rhtml|rxml)$/.match(method_name)
+      layout = (@layout ? @layout.to_s.clone : self.class.to_s.underscore)
+      md = /^([^\.]+)\.([^\.]+\.[^\.]+)\.(erb|rhtml|rxml)$/.match(method_name)
       layout << ".#{md.captures[1]}" if md && md.captures[1]
-      layout << ".rhtml"
+      layout << ".#{md.captures[2]}" if md && md.captures[2]
+      layout << ".rhtml" # if Rails::VERSION::MAJOR < 2
       if File.exists?(File.join(layouts_path, layout))
         body[:content_for_layout] = render_message_without_layouts(method_name, body)
-        initialize_layout_template_class(body).render(:file => layout)
+        initialize_layout_template_class(body).render(:file => "/#{layout}")
       else
         render_message_without_layouts(method_name, body)
       end
     end
     
     def initialize_layout_template_class(assigns)
+      # for Rails 2.1 (and greater), we have to process view paths first!
+      if Rails::VERSION::MAJOR >= 2 and Rails::VERSION::MINOR >= 1
+        ActionView::TemplateFinder.process_view_paths(layouts_path)
+      end
       returning(template = ActionView::Base.new(layouts_path, assigns, self)) do
         template.extend self.class.master_helper_module
       end
