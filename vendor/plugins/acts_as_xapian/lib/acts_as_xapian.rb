a327ea1d0b6f6d74ceab7a90bf022d6c95735fe7
francis <francis>
2008-09-13 21:31:46 +0000

    From github

--- vendor/plugins/acts_as_xapian/lib/acts_as_xapian.rb
+++ vendor/plugins/acts_as_xapian/lib/acts_as_xapian.rb
@@ -573,7 +573,7 @@ module ActsAsXapian
             if type == :date
                 if value.kind_of?(Time)
                     value.utc.strftime("%Y%m%d")
-                elsif value.kind_f?(Date)
+                elsif value.kind_of?(Date)
                     value.to_time.utc.strftime("%Y%m%d")
                 else
                     raise "Only Time or Date types supported by acts_as_xapian for :date fields, got " + value.class.to_s
