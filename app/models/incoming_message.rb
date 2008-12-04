837c1685d3020b9fc72bfd2f904cbb79e0e0897a
francis <francis>
2008-12-04 19:01:47 +0000

    Doesn't work on server.

--- app/models/incoming_message.rb
+++ app/models/incoming_message.rb
@@ -19,7 +19,7 @@
 # Copyright (c) 2007 UK Citizens Online Democracy. All rights reserved.
 # Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: incoming_message.rb,v 1.172 2008-12-04 18:10:05 francis Exp $
+# $Id: incoming_message.rb,v 1.173 2008-12-04 19:01:47 francis Exp $
 
 # TODO
 # Move some of the (e.g. quoting) functions here into rblib, as they feel
@@ -88,11 +88,6 @@ def filename_and_content_to_mimetype(filename, content)
     mahoro_type.match(/^(.*);/)
     if $1
         mahoro_type = $1
-    end
-    # special cases
-    if mahoro_type == "\\012- application/msword"
-        # e.g. for http://www.whatdotheyknow.com/request/gifted_and_talented_programme#incoming-9365
-        return 'application/vnd.ms-excel'
     end
     # see if looks like a content type, or has something in it that does
     # and return that
