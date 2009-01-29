46b0df9cd7ae21466f66dc0f4c1c118dd78b766a
francis <francis>
2009-01-29 12:23:25 +0000

    Derive from right class

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,10 +4,10 @@
 # Copyright (c) 2009 UK Citizens Online Democracy. All rights reserved.
 # Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: admin_controller.rb,v 1.23 2009-01-29 12:10:10 francis Exp $
+# $Id: admin_controller.rb,v 1.24 2009-01-29 12:23:25 francis Exp $
 
 
-class AdminController < ActionController::Base
+class AdminController < ApplicationController
     layout "admin"
     before_filter :assign_http_auth_user
 
