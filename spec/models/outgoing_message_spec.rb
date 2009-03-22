10aa2a93c978974ddf32fe2032c12dc4a28ebbf5
tony <tony>
2009-03-22 14:50:16 +0000

    split into distinct named tests

--- spec/models/outgoing_message_spec.rb
+++ spec/models/outgoing_message_spec.rb
@@ -2,25 +2,26 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe OutgoingMessage, " when making an outgoing message" do
     before do
-    end
-
-    it "should not show email addresses for outgoing messages, except when mailing" do
-        outgoing_message = OutgoingMessage.new({
+        @outgoing_message = OutgoingMessage.new({
             :status => 'ready',
             :message_type => 'initial_request',
             :body => 'This request contains a foo@bar.com email address',
             :last_sent_at => Time.now(),
             :what_doing => 'normal_sort'
         })
+    end
 
-        # used for index, but also for track emails
-        outgoing_message.get_text_for_indexing.should_not include("foo@bar.com")
+    it "should not index the email addresses" do
+        # also used for track emails
+        @outgoing_message.get_text_for_indexing.should_not include("foo@bar.com")
+    end 
 
-        # used for normal display on page
-        outgoing_message.get_body_for_html_display.should_not include("foo@bar.com")
+    it "should not display email addresses on page" do
+        @outgoing_message.get_body_for_html_display.should_not include("foo@bar.com")
+    end
 
-        # called from the request sending email templates
-        outgoing_message.body.should include("foo@bar.com")
+    it "should include email addresses in outgoing messages" do
+        @outgoing_message.body.should include("foo@bar.com")
     end
 end
 
