4108357ef0e4d5880145cfe7ae853f2188facc10
tony <tony>
2009-04-14 08:41:04 +0000

    Migration for charity numbers

---
+++ db/migrate/075_add_charity_number.rb
@@ -0,0 +1,12 @@
+class AddCharityNumber < ActiveRecord::Migration
+    def self.up
+        add_column :public_bodies, :charity_number, :text, :null => false, :default => ""
+        add_column :public_body_versions, :charity_number, :text, :null => false, :default => ""
+    end
+
+    def self.down
+        remove_column :public_bodies, :charity_number
+        remove_column :public_body_versions, :charity_number
+    end
+end
+
