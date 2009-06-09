cb5cc0dfa56807a680135f07768940c4f99db051
francis <francis>
2009-06-09 10:04:45 +0000

    Add an index Alex found was missing.

---
+++ db/migrate/076_add_indices.rb
@@ -0,0 +1,9 @@
+class AddIndices < ActiveRecord::Migration
+  def self.up
+    add_index :track_things_sent_emails, :track_thing_id
+  end
+
+  def self.down
+    remove_index :track_things_sent_emails, :track_thing_id
+  end
+end
