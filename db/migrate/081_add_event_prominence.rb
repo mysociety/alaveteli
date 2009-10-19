7000c15527b0ba5ae5e8369a1b806df0427d9c09
francis <francis>
2009-10-19 19:25:06 +0000

    (not yet used) column for hiding individual responses

---
+++ db/migrate/081_add_event_prominence.rb
@@ -0,0 +1,9 @@
+class AddEventProminence < ActiveRecord::Migration
+    def self.up
+        add_column :info_request_events, :prominence, :string, :null => false, :default => 'normal'
+    end
+
+    def self.down
+        raise "safer not to have reverse migration scripts, and we never use them"
+    end
+end
