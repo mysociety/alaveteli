dfe62a251903f49bc7c1fee68efff1511b12e374
Francis Irving <francis@mysociety.org>
2009-12-02 23:05:26 +0000

    Test for TMail fix for a particular message. Monkey patch doesn't quite work yet.

---
+++ spec/libs/tmail_extensions.rb
@@ -0,0 +1,20 @@
+# This is a test of the monkey patches in lib/fcgi_fixes.rb
+
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe "when using TMail" do
+
+    it "should load an email with funny MIME settings" do
+        # just send it to the holding pen
+        InfoRequest.holding_pen_request.incoming_messages.size.should == 0
+        receive_incoming_mail("humberside-police-odd-mime-type.email", 'dummy')
+        InfoRequest.holding_pen_request.incoming_messages.size.should == 1
+
+        incoming_message = InfoRequest.holding_pen_request.incoming_messages[0]
+
+        # This will raise an error if the bug in TMail hasn't been fixed
+        incoming_message.get_body_for_html_display()
+    end
+
+end
+
