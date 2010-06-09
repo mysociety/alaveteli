d238c53445933b7abc9b31304b353dcd2a922e4f
Francis Irving <francis@mysociety.org>
2010-06-09 08:09:00 +0100

    Add index to create_at for track sent, so can remove old ones / only use new ones

---
+++ db/migrate/083_add_indices_track_sent.rb
@@ -0,0 +1,9 @@
+class AddIndicesTrackSent < ActiveRecord::Migration
+    def self.up
+        add_index :track_things_sent_emails, :created_at
+    end
+
+    def self.down
+        remove_index :track_things_sent_emails, :created_at
+    end
+end
