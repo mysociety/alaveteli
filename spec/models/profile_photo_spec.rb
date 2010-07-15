4686732f95698eaa3c92fd8540236d35802d374f
Francis Irving <francis@mysociety.org>
2010-07-15 14:56:24 +0100

    Fix tests to use mock user.

--- spec/models/profile_photo_spec.rb
+++ spec/models/profile_photo_spec.rb
@@ -3,21 +3,22 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe ProfilePhoto, "when constructing a new photo" do 
 
     before do 
+        @mock_user = mock_model(User)
     end
     
     it 'should take no image as invalid' do
-        profile_photo = ProfilePhoto.new(:data => nil)
+        profile_photo = ProfilePhoto.new(:data => nil, :user => @mock_user)
         profile_photo.valid?.should == false
     end
 
     it 'should take bad binary data as invalid' do
-        profile_photo = ProfilePhoto.new(:data => 'blahblahblah')
+        profile_photo = ProfilePhoto.new(:data => 'blahblahblah', :user => @mock_user)
         profile_photo.valid?.should == false
     end
 
     it 'should accept and convert a PNG to right size' do 
         data = load_file_fixture("parrot.png")
-        profile_photo = ProfilePhoto.new(:data => data)
+        profile_photo = ProfilePhoto.new(:data => data, :user => @mock_user)
         profile_photo.valid?.should == true
         profile_photo.image.format.should == 'PNG'
         profile_photo.image.columns.should == 96
@@ -26,7 +27,7 @@ describe ProfilePhoto, "when constructing a new photo" do
 
     it 'should accept and convert a JPEG to right format and size' do 
         data = load_file_fixture("parrot.jpg")
-        profile_photo = ProfilePhoto.new(:data => data)
+        profile_photo = ProfilePhoto.new(:data => data, :user => @mock_user)
         profile_photo.valid?.should == true
         profile_photo.image.format.should == 'PNG'
         profile_photo.image.columns.should == 96
