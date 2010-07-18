57aa8cc12e47efd6f79acd7349d4805cd4c986b9
Francis Irving <francis@mysociety.org>
2010-07-18 03:20:29 +0100

    Forgot this part of migration

---
+++ db/migrate/086_allow_null_profile_photo_user.rb
@@ -0,0 +1,9 @@
+class AllowNullProfilePhotoUser < ActiveRecord::Migration
+    def self.up
+        change_column :profile_photos, :user_id, :integer, :null => true
+    end
+
+    def self.down
+        raise "No reverse migration"
+    end
+end
