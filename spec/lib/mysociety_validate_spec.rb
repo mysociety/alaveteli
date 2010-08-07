2af2d1f3892801e5f04126b23fd41da591a24817
Francis Irving <francis@mysociety.org>
2010-08-07 01:15:20 +0100

    Test code for mixed case detection. Update to new code for that from commonlib.

--- spec/lib/mysociety_validate_spec.rb
+++ spec/lib/mysociety_validate_spec.rb
@@ -22,5 +22,26 @@ describe "when checking text uses mixed capitals" do
         MySociety::Validate.uses_mixed_capitals("This is a normal sentence. It is followed by another, and overall it is quite a long chunk of text so it exceeds the minimum limit.").should == true
     end
 
+    it "should allow lots of URLs, when the rest of casing is fine" do
+        MySociety::Validate.uses_mixed_capitals("
+The public authority appears to have aggregated this request with the following requests on this site:
+
+http://www.whatdotheyknow.com/request/financial_value_of_post_dismissa_2
+
+http://www.whatdotheyknow.com/request/number_of_post_dismissal_compens_2
+
+http://www.whatdotheyknow.com/request/number_of_post_dismissal_compens_3
+
+...and has given one response to all four of these requests, available here:
+
+http://www.whatdotheyknow.com/request/financial_value_of_post_dismissa_2#incoming-105717
+
+The information requested in this request was not provided, however the information requested in the following request was provided:
+
+http://www.whatdotheyknow.com/request/number_of_post_dismissal_compens_3
+        ").should == true
+    end
+
+
 end
 
