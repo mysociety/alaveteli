4859bbcaa8a105e8600ec1fa363789b86d116b04
Francis Irving <francis@mysociety.org>
2010-09-16 02:10:58 +0100

    Fix up path for assets.

--- config/environment.rb
+++ config/environment.rb
@@ -97,7 +97,7 @@ ActionMailer::Base.default_url_options[:host] = MySociety::Config.get("DOMAIN", 
 # So that javascript assets use full URL, so proxied admin URLs read javascript OK
 if (MySociety::Config.get("DOMAIN", "") != "")
     ActionController::Base.asset_host = Proc.new { |source, request|
-        if request.ssl? # for mySociety proxying
+        if request.fullpath.match(/^\/admin\//)
             MySociety::Config.get("ADMIN_PUBLIC_URL", "/")
         else
             MySociety::Config.get("DOMAIN", 'localhost:3000')
