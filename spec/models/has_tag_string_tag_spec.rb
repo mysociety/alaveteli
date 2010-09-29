4c7d4bd7ff63b83b03308050995fdcc9cd266ab9
Francis Irving <francis@mysociety.org>
2010-09-29 01:17:56 +0100

    Fix test of HasTagStringTag

--- spec/models/has_tag_string_tag_spec.rb
+++ spec/models/has_tag_string_tag_spec.rb
@@ -1,11 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe PublicBodyTag, " when fiddling with tag strings " do
+describe HasTagString::HasTagStringTag, " when fiddling with tag strings " do
     fixtures :public_bodies
 
     it "should be able to make a new tag and save it" do
-        @tag = PublicBodyTag.new 
-        @tag.public_body = public_bodies(:geraldine_public_body)
+        @tag = HasTagString::HasTagStringTag.new 
+        @tag.model = 'PublicBody'
+        @tag.model_id = public_bodies(:geraldine_public_body).id
         @tag.name = "moo"
         @tag.save
     end
