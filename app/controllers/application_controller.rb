ee067d86eea2ae66595bffa3abd6c3c0a2c4753c
Seb Bacon <seb@seb-laptop-acer.(none)>
2010-11-23 09:58:05 +0000

    Make default value an empty string, not nil (which is assumed to be an unset value)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -213,7 +213,7 @@ class ApplicationController < ActionController::Base
 
     # 
     def check_read_only
-        read_only = MySociety::Config.get('READ_ONLY', nil)
+        read_only = MySociety::Config.get('READ_ONLY', '')
         if !read_only.empty?
             flash[:notice] = "<p>WhatDoTheyKnow is currently in maintenance. You can only view existing requests. You cannot make new ones, add followups or annotations, or otherwise change the database.</p> <p>" + read_only + "</p>"
             redirect_to frontpage_url
