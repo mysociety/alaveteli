bc894f8e1b950950f3c682487139a8d5a11ae6d9
Louise Crow <louise.crow@gmail.com>
2011-02-23 14:11:24 +0000

    Add rspec-rails as a dependency for the test env.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,3 +17,5 @@ config.action_controller.perform_caching             = false
 # The :test delivery method accumulates sent emails in the
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
+
+  config.gem 'rspec-rails', :version => '>= 1.3.3', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
