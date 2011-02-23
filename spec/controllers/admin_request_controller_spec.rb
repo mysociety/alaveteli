3bcf8a8e793e791c44cb92442a05a73abe81de5d
Louise Crow <louise.crow@gmail.com>
2011-02-23 15:23:07 +0000

    Adding required info_request_event fixture.

--- spec/controllers/admin_request_controller_spec.rb
+++ spec/controllers/admin_request_controller_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe AdminRequestController, "when administering requests" do
     integrate_views
-    fixtures :info_requests, :outgoing_messages, :users
+    fixtures :info_requests, :outgoing_messages, :users, :info_request_events
   
     it "shows the index/list page" do
         get :index
