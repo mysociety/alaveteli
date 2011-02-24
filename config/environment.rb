8386f37a45c4e3e62555c57d067a04db4b2d4204
Louise Crow <louise.crow@gmail.com>
2011-02-24 16:47:20 +0000

    Adding dependency on rspec.

--- config/environment.rb
+++ config/environment.rb
@@ -17,7 +17,6 @@ $:.push(File.join(File.dirname(__FILE__), '../commonlib/rblib'))
 # (type "git submodule update --init" in the whatdotheyknow directory)
 
 # ruby-ole and ruby-msg.  We use a custom ruby-msg to avoid a name conflict
-$:.unshift(File.join(File.dirname(__FILE__), '../vendor/ruby-ole/lib'))
 $:.unshift(File.join(File.dirname(__FILE__), '../vendor/ruby-msg/lib'))
 
 require 'memcache'
@@ -53,6 +52,7 @@ Rails::Initializer.run do |config|
   config.gem "fast_gettext", :version => '>=0.4.8'
   config.gem "rack", :version => '1.1.0'
   config.gem "recaptcha", :lib => "recaptcha/rails"
+  config.gem 'rspec', :lib => false, :version => '1.3.1'
   config.gem 'rspec-rails', :lib => false, :version => '1.3.3'
   config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
 
