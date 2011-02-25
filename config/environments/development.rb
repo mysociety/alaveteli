224b8a4ba3a24af91068505c7907724448a4096d
Seb Bacon <seb.bacon@gmail.com>
2011-02-25 11:47:14 +0000

    ensure tests run with test environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,5 +21,3 @@ config.action_mailer.delivery_method = :sendmail # so is queued, rather than giv
 # unintentionally kept references to objects, especially strings.
 # require 'memory_profiler'
 # MemoryProfiler.start :string_debug => true, :delay => 10
-
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
 ENV["RAILS_ENV"] = "test"
+RAILS_ENV = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'spec/rails'
 
