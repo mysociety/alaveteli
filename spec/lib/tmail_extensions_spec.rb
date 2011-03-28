6e36729b44673a0e77f03e100d094ebf020e8e32
Louise Crow <louise.crow@gmail.com>
2011-03-28 15:16:10 +0100

    Monkeypatching TMail parser to fix case where multiple addresses in the to: field of an incoming email have unquoted display parts - with this patch, each address is returned by the TMail to() function, without it only the last one is returned.

--- spec/lib/tmail_extensions_spec.rb
+++ spec/lib/tmail_extensions_spec.rb
@@ -22,5 +22,11 @@ describe "when using TMail" do
         incoming_message.get_body_for_html_display()
     end
 
+    it 'should parse multiple to addresses with unqoted display names' do
+        example_file = File.join(Spec::Runner.configuration.fixture_path, 'multiple-unquoted-display-names.email')
+        mail = TMail::Mail.parse(File.read(example_file))
+        mail.to.should == ["request-66666-caa77777@whatdotheyknow.com", "foi@example.com"]
+    end
+
 end
 
