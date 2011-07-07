bd0fab680a361b93e27b8abef54387c89964715e
Seb Bacon <seb.bacon@gmail.com>
2011-07-07 09:59:19 +0100

    remove Kosovan theme from core install

--- vendor/plugins/alavetelitheme/lib/alavetelitheme.rb
+++
@@ -1,7 +0,0 @@
-class ActionController::Base
-    before_filter :set_view_paths
-
-    def set_view_paths         
-        self.prepend_view_path File.join(File.dirname(__FILE__), "views")
-    end
-end
--- vendor/plugins/alavetelitheme/tasks/alavetelitheme_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :alavetelitheme do
-#   # Task goes here
-# end
