178e780fc066eb5b7b978020086fd46518754a41
David Cabo <david@calibea.com>
2011-08-12 11:01:25 +0200

    Put back the gettext fuzzy/obsolete warnings
    Be careful: turns out that the :ignore_fuzzy flag changes the behaviour of gettext. The warnings dont show up, but the fuzzy strings get used, and theyre most often wrong!

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,2 +1,2 @@
-FastGettext.add_text_domain 'app', :path => 'locale', :type => :po, :ignore_fuzzy => true, :ignore_obsolete => true
+FastGettext.add_text_domain 'app', :path => 'locale', :type => :po
 FastGettext.default_text_domain = 'app'
