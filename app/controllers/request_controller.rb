6f46c15f6290e286ffa7b12ace40e4983c6641a3
David Cabo <david@calibea.com>
2011-08-23 02:32:40 +0200

    Move compulsory sign-in step after Preview page, where it was originally

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -287,15 +287,6 @@ class RequestController < ApplicationController
             return
         end
 
-        if !authenticated?(
-                :web => _("To send your FOI request"),
-                :email => _("Then your FOI request to {{public_body_name}} will be sent.",:public_body_name=>@info_request.public_body.name),
-                :email_subject => _("Confirm your FOI request to ") + @info_request.public_body.name
-            )
-            # do nothing - as "authenticated?" has done the redirect to signin page for us
-            return
-        end
-
         # Show preview page, if it is a preview
         if params[:preview].to_i == 1
             message = ""
@@ -315,6 +306,15 @@ class RequestController < ApplicationController
                 flash.now[:error] = message
             end
             render :action => 'preview'
+            return
+        end
+
+        if !authenticated?(
+                :web => _("To send your FOI request"),
+                :email => _("Then your FOI request to {{public_body_name}} will be sent.",:public_body_name=>@info_request.public_body.name),
+                :email_subject => _("Confirm your FOI request to ") + @info_request.public_body.name
+            )
+            # do nothing - as "authenticated?" has done the redirect to signin page for us
             return
         end
 
