f6a9a1949e3430f16f69672bf429141afd08efdf
Robin Houston <robin@lenny.robin>
2011-09-08 19:52:02 +0100

    Extend the detection of DNR addresses
    Extend the detection of do-not-reply addresses to detect
    <do_not_reply@> <do-not-reply@>, etc.
    
    Example: http://www.whatdotheyknow.com/request/the_number_of_complaints_for_ace#incoming-207024

--- app/models/incoming_message.rb
+++ app/models/incoming_message.rb
@@ -1305,7 +1305,7 @@ class IncomingMessage < ActiveRecord::Base
         prefix = email
         prefix =~ /^(.*)@/
         prefix = $1
-        if !prefix.nil? && prefix.downcase.match(/^(postmaster|mailer-daemon|auto_reply|donotreply|no.reply)$/)
+        if !prefix.nil? && prefix.downcase.match(/^(postmaster|mailer-daemon|auto_reply|do.?not.?reply|no.reply)$/)
             return false
         end
 
