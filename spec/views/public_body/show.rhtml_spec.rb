e275b42cb4bdbdc3556a09874c4251d4586759d2
James McKinney <james@slashpoundbang.com>
2011-10-16 14:27:13 -0400

    add encoding magic comments and fix paths

--- spec/views/public_body/show.rhtml_spec.rb
+++ spec/views/public_body/show.rhtml_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
+require File.expand_path(File.join('..', '..', '..', 'spec_helper'), __FILE__)
 
 describe "when viewing a body" do
     before do
