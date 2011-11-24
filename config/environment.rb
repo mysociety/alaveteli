0f39e8201a6541a5cea8a057b40d1977be7f0ddb
Seb Bacon <seb.bacon@gmail.com>
2011-11-24 18:29:27 +0000

    Use `test.yml` file to derive test default settings, rather than hard coding in `environment.rb`.  Fixes issue #294.

--- config/environment.rb
+++ config/environment.rb
@@ -37,7 +37,11 @@ require File.join(File.dirname(__FILE__), '../lib/old_rubygems_patch')
 
 Rails::Initializer.run do |config|
   # Load intial mySociety config
-  MySociety::Config.set_file(File.join(config.root_path, 'config', 'general'), true)
+  if ENV["RAILS_ENV"] == "test" 
+      MySociety::Config.set_file(File.join(config.root_path, 'config', 'test'), true)
+  else
+      MySociety::Config.set_file(File.join(config.root_path, 'config', 'general'), true)
+  end
   MySociety::Config.load_default
 
   # Settings in config/environments/* take precedence over those specified here
@@ -119,14 +123,8 @@ if (MySociety::Config.get("DOMAIN", "") != "")
 end
 
 # fallback locale and available locales
-if ENV["RAILS_ENV"] == "test"
-    # The tests assume that the "en" and "es" locales are available
-    available_locales = ["en", "es"]
-    default_locale = "en"
-else
-    available_locales = MySociety::Config.get('AVAILABLE_LOCALES', 'en es').split(/ /)
-    default_locale = MySociety::Config.get('DEFAULT_LOCALE', 'en')
-end
+available_locales = MySociety::Config.get('AVAILABLE_LOCALES', '').split(/ /)
+default_locale = MySociety::Config.get('DEFAULT_LOCALE', '')
 
 FastGettext.default_available_locales = available_locales
 I18n.locale = default_locale
