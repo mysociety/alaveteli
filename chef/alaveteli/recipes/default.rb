4b09d1b5698476871859f485cc1f06c63cdf4717
Henare Degan <henare.degan@gmail.com>
2012-01-18 15:02:30 +1100

    Install packages in the Vagrant VM

--- chef/alaveteli/recipes/default.rb
+++ chef/alaveteli/recipes/default.rb
@@ -1,1 +1,11 @@
+require_recipe "apt"
 require_recipe "postgresql::server"
+
+
+# Install package dependencies as per the readme
+packages = `cut -d " " -f 1 /vagrant/config/packages | grep -v "^#"`.split
+packages.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
