0ef7953fc193ee4bc148a3c22d5b44a6b61027d2
Henare Degan <henare.degan@gmail.com>
2012-01-30 16:58:17 +1100

    More changes for Debian

--- chef/alaveteli/recipes/default.rb
+++ chef/alaveteli/recipes/default.rb
@@ -1,10 +1,19 @@
+require_recipe "apt"
+
+apt_repository "mysociety" do
+  uri "http://debian.mysociety.org"
+  distribution "squeeze"
+  components ["main"]
+  action :add
+end
+
 require_recipe "postgresql::server"
 
 # Install package dependencies as per the readme
 packages = `cut -d " " -f 1 /vagrant/config/packages | grep -v "^#"`.split
 packages.each do |pkg|
   package pkg do
-    action :install
+    options "--allow-unauthenticated" if pkg == "wkhtmltopdf-static"
   end
 end
 
