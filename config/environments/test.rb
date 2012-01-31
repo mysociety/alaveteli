7c6eb09daf0ad55c0286995529a82076d29fdbc8
Robin Houston <robin.houston@gmail.com>
2012-01-31 10:04:47 +0000

    Avoid need to be db superuser to run tests
    Add a patch from louisecrow (https://github.com/mysociety/fixmytransport/blob/master/lib/patches/fixtures_constraint_disabling.rb)
    that makes it possible to run the tests without being a database superuser.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,7 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
+require 'lib/patches/fixtures_constraint_disabling'
+
 # The test environment is used exclusively to run your application's
 # test suite.  You never need to work with it otherwise.  Remember that
 # your test database is "scratch space" for the test suite and is wiped
