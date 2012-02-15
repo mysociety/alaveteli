506af7a640f63b17000ccfc5e1344bbc3039c913
Seb Bacon <seb.bacon@gmail.com>
2012-02-15 09:45:20 +0000

    Set a "Content-Disposition: attachment" header when downloading files.  Closes #428

--- spec/controllers/request_controller_spec.rb
+++ spec/controllers/request_controller_spec.rb
@@ -222,7 +222,6 @@ describe RequestController, "when showing one request" do
             get :get_attachment, :incoming_message_id => ir.incoming_messages[1].id, :id => ir.id, :part => 2, :file_name => ['hello.txt'], :skip_cache => 1
             response.content_type.should == "text/plain"
             response.should have_text(/Second hello/)
-            
             get :get_attachment, :incoming_message_id => ir.incoming_messages[1].id, :id => ir.id, :part => 3, :file_name => ['hello.txt'], :skip_cache => 1
             response.content_type.should == "text/plain"
             response.should have_text(/First hello/)
@@ -349,6 +348,15 @@ describe RequestController, "when showing one request" do
             get :get_attachment, :incoming_message_id => ir.incoming_messages[1].id, :id => ir.id, :part => 2, :file_name => ['hello.qwglhm'], :skip_cache => 1
             response.content_type.should == "application/octet-stream"
             response.should have_text(/an unusual sort of file/)
+        end
+
+        it "should apply a content-disposition header" do
+            ir = info_requests(:fancy_dog_request)
+            receive_incoming_mail('incoming-request-attachment-unknown-extension.email', ir.incoming_email)
+            ir.reload            
+            get :get_attachment, :incoming_message_id => ir.incoming_messages[1].id, :id => ir.id, :part => 2, :file_name => ['hello.qwglhm'], :skip_cache => 1
+            response.headers.should include("Content-Disposition")
+            response.headers["Content-Disposition"].should include('hello.qwglhm')
         end
 
         it "should not download attachments with wrong file name" do
