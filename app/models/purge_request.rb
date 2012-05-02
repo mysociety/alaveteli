e987fc36b4c9d22c6cb084595c176ca4114f5730
Seb Bacon <seb.bacon@gmail.com>
2012-05-02 08:14:00 +0100

    Add a method to run the purge process in a continuous loop

--- app/models/purge_request.rb
+++ app/models/purge_request.rb
@@ -6,10 +6,24 @@
 #
 
 class PurgeRequest < ActiveRecord::Base
-    require 'open-uri'
     def self.purge_all
+        done_something = false
         for item in PurgeRequest.all()
             item.purge
+            done_something = true
+        end
+        return done_something
+    end
+
+    def self.purge_all_loop
+        # Run purge_all in an endless loop, sleeping when there is nothing to do
+        while true
+            sleep_seconds = 1
+            while !purge_all
+                sleep sleep_seconds
+                sleep_seconds *= 2
+                sleep_seconds = 300 if sleep_seconds > 300
+            end
         end
     end
 
