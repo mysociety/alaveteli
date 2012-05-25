6e31257065dca23b60eb6e129d61434d75fda149
Seb Bacon <seb.bacon@gmail.com>
2012-05-25 13:07:06 +0100

    Don't send emails in development; output them to the console instead.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,7 @@ config.action_view.debug_rjs                         = true
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+config.action_mailer.perform_deliveries = false
 config.action_mailer.delivery_method = :sendmail # so is queued, rather than giving immediate errors
 
 # Writes useful log files to debug memory leaks, of the sort where have
