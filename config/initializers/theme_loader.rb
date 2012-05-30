fe09c8da70967ea591a3b01d7a70fc80490f3d8f
Seb Bacon <seb.bacon@gmail.com>
2012-05-30 13:04:49 +0100

    When loading themes, only attempt to do so if it exists.

--- config/initializers/theme_loader.rb
+++ config/initializers/theme_loader.rb
@@ -2,6 +2,9 @@ theme_urls = MySociety::Config.get("THEME_URLS", [])
 if ENV["RAILS_ENV"] != "test" # Don't let the theme interfere with Alaveteli specs
     for url in theme_urls.reverse
         theme_name = url.sub(/.*\/(.*).git/, "\\1")
-        require File.expand_path "../../../vendor/plugins/#{theme_name}/lib/alavetelitheme.rb", __FILE__
+        theme_main_include = File.expand_path "../../../vendor/plugins/#{theme_name}/lib/alavetelitheme.rb", __FILE__
+        if File.exists? theme_main_include
+            require theme_main_include
+        end
     end
 end
