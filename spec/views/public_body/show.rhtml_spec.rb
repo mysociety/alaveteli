a728386bd5875414c9d97d9456ce57d5aa599be4
Robin Houston <robin.houston@gmail.com>
2012-06-07 17:10:29 +0100

    Beef up mocks with new method

--- spec/views/public_body/show.rhtml_spec.rb
+++ spec/views/public_body/show.rhtml_spec.rb
@@ -105,6 +105,7 @@ def mock_event
             :display_status => 'waiting_response',
             :calculate_status => 'waiting_response',
             :public_body => @pb,
+            :is_external? => false,
             :user => mock_model(User, :name => 'Test User', :url_name => 'testuser')
         ),
         :incoming_message => nil, :is_incoming_message? => false,
