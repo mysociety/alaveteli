cef2b1074811567561f01f20fbffebb6baaee4f2
Robin Houston <robin.houston@gmail.com>
2012-07-02 13:58:04 +0100

    Do not pre-populate the API key
    It takes too long, and the mySociety deployment system runs
    these migrations while the site is down.

--- db/migrate/112_add_api_key_to_public_bodies.rb
+++ db/migrate/112_add_api_key_to_public_bodies.rb
@@ -4,12 +4,12 @@ class AddApiKeyToPublicBodies < ActiveRecord::Migration
   def self.up
     add_column :public_bodies, :api_key, :string
     
-    PublicBody.find_each do |pb|
-        pb.api_key = SecureRandom.base64(32)
-        pb.save!
-    end
-    
-    change_column_null :public_bodies, :api_key, false
+    # PublicBody.find_each do |pb|
+    #     pb.api_key = SecureRandom.base64(32)
+    #     pb.save!
+    # end
+    # 
+    # change_column_null :public_bodies, :api_key, false
   end
 
   def self.down
