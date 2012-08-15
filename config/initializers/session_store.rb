781e6e2b33a31038a4f3aa62265f3042c108cae1
Louise Crow <louise.crow@gmail.com>
2012-08-15 11:13:31 +0100

    Rollback change to activerecord session store - whilst activerecord store has some advantages (notably avoiding permanent versus transient session hijack possibilities), I think it needs some more work/testing (we'd need to have good code for cleaning out stale activerecord sessions periodically - with activerecord store this is now our responsibility). We'd also need to reset the session key to kill all in progress sessions to prevent errors as rails tries to store long cookie keys in the limited key field of the activerecord store. For this release, it'll be more straightforward to fix the csv_upload file issue #526 in a more localized way.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,14 +2,14 @@
 
 # Your secret key for verifying cookie session data integrity.
 # If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
 ActionController::Base.session = {
   :key => '_wdtk_cookie_session',
   :secret => MySociety::Config.get("COOKIE_STORE_SESSION_SECRET", 'this default is insecure as code is open source, please override for live sites in config/general; this will do for local development')
 }
-ActionController::Base.session_store = :active_record_store
+ActionController::Base.session_store = :cookie_store
 
 # Insert a bit of middleware code to prevent uneeded cookie setting.
 require "#{Rails.root}/lib/whatdotheyknow/strip_empty_sessions"
