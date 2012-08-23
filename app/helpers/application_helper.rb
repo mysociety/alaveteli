5b2a7579b66d45a042eb2510bf58cbdb1e502236
James McKinney <james@slashpoundbang.com>
2012-08-23 17:09:40 -0400

    Fix bug where errors were rendered like: <ul>["<li>Please enter a subject</li>"]</ul>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,7 +44,7 @@ module ApplicationHelper
           end
 
           content_tag(:div,
-              content_tag(:ul, error_messages),
+              content_tag(:ul, error_messages.join),
             html
           )
         else
