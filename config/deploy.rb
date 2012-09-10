bf7cec08bcc37a2725e64cfe19e060b78cabda5b
Henare Degan <henare.degan@gmail.com>
2012-09-10 15:46:16 +1000

    Remove comments

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,12 +8,6 @@ role :app, "your app-server here"                          # This may be the sam
 role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 role :db,  "your slave db-server here"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
 # If you are using Passenger mod_rails uncomment this:
 # namespace :deploy do
 #   task :start do ; end
