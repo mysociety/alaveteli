6d3d9d9846fa905e606f0a2aa6f1ec32122f4850
Louise Crow <louise.crow@gmail.com>
2012-09-12 12:48:50 +0100

    Add some indexes based on queries appearing in the SQL slow queries log.

---
+++ db/migrate/20120912112655_add_incoming_message_id_index_to_foi_attachments.rb
@@ -0,0 +1,9 @@
+class AddIncomingMessageIdIndexToFoiAttachments < ActiveRecord::Migration
+  def self.up
+      add_index :foi_attachments, :incoming_message_id
+  end
+
+  def self.down
+      remove_index :foi_attachments, :incoming_message_id
+  end
+end
---
+++ db/migrate/20120912114022_add_user_index_to_info_requests.rb
@@ -0,0 +1,9 @@
+class AddUserIndexToInfoRequests < ActiveRecord::Migration
+  def self.up
+      add_index :info_requests, :user_id
+  end
+
+  def self.down
+      remove_index :info_requests, :user_id
+  end
+end
