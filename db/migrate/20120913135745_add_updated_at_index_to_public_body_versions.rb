76b4697ae2490fc227ac0f150d07b74bb3ca5fa3
Louise Crow <louise.crow@gmail.com>
2012-09-13 15:47:57 +0100

    Add index on updated_at to public body versions as this is how they are retrieved in the admin timeline view.

---
+++ db/migrate/20120913135745_add_updated_at_index_to_public_body_versions.rb
@@ -0,0 +1,9 @@
+class AddUpdatedAtIndexToPublicBodyVersions < ActiveRecord::Migration
+  def self.up
+      add_index :public_body_versions, :updated_at
+  end
+
+  def self.down
+      remove_index :public_body_versions, :updated_at
+  end
+end
