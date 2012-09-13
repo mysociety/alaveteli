65b1d387a135ff027d2dfc2972ad97d2b3d85288
Louise Crow <louise.crow@gmail.com>
2012-09-13 09:13:33 +0100

    Add some indexes to help IncomingMessage.fully_destroy find records more efficiently by info_request_event_id

---
+++ db/migrate/20120913080807_add_info_request_event_index_to_track_things_sent_emails.rb
@@ -0,0 +1,9 @@
+class AddInfoRequestEventIndexToTrackThingsSentEmails < ActiveRecord::Migration
+  def self.up
+      add_index :track_things_sent_emails, :info_request_event_id
+  end
+
+  def self.down
+      remove_index :track_things_sent_emails, :info_request_event_id
+  end
+end
