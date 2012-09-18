6d80a29ceefe2bd96f71ff739f0640e07d7016c7
Henare Degan <henare.degan@gmail.com>
2012-09-18 15:08:11 +1000

    Refactor task so it can be reused

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,11 @@
 # Use this file to easily define all of your cron jobs.
 
-job_type :command_in_current_release, "cd :path && RAILS_ENV=:environment :task :output"
+# TODO: Replace run-with-lockfile command with something more Rubyesque
+job_type :run_with_lockfile, "cd :path && RAILS_ENV=:environment run-with-lockfile -n ./:lockfile_name.lock :task || echo \"stalled?\""
 
 every 5.minutes do
-  # TODO: Replace run-with-lockfile command with something more Rubyesque
   # TODO: Replace with Raketask xapian:rebuild_index
-  command_in_current_release "run-with-lockfile -n ./change-xapian-database.lock \"./script/update-xapian-index verbose=true\" >> ./log/update-xapian-index.log || echo \"stalled?\""
+  run_with_lockfile "\"./script/update-xapian-index verbose=true\" >> ./log/update-xapian-index.log", :lockfile_name => 'change-xapian-database'
 end
 
 # TODO:
