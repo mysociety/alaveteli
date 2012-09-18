1bcedb2b3e5b6986eba0b72c9ee8e989ca361e3d
Henare Degan <henare.degan@gmail.com>
2012-09-18 13:56:22 +1000

    Change to the standard Rails log path

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,5 +5,5 @@ job_type :command_in_current_release, "cd :path && :task :output"
 every 5.minutes do
   # TODO: Replace run-with-lockfile command with something more Rubyesque
   # TODO: Replace with Raketask xapian:rebuild_index
-  command_in_current_release "run-with-lockfile -n ./change-xapian-database.lock \"./script/update-xapian-index verbose=true\" >> ./logs/update-xapian-index.log || echo \"stalled?\""
+  command_in_current_release "run-with-lockfile -n ./change-xapian-database.lock \"./script/update-xapian-index verbose=true\" >> ./log/update-xapian-index.log || echo \"stalled?\""
 end
