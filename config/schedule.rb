f74548620495aea17edd5933a772c8244600b0ed
Henare Degan <henare.degan@gmail.com>
2012-09-18 13:52:18 +1000

    Run the cronjob in the current release so it can access logs, etc.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,9 @@
 # Use this file to easily define all of your cron jobs.
 
+job_type :command_in_current_release, "cd :path && :task :output"
+
 every 5.minutes do
   # TODO: Replace run-with-lockfile command with something more Rubyesque
   # TODO: Replace with Raketask xapian:rebuild_index
-  command "run-with-lockfile -n #{Whenever.path}/change-xapian-database.lock \"#{Whenever.path}/script/update-xapian-index verbose=true\" >> #{Whenever.path}/logs/update-xapian-index.log || echo \"stalled?\""
+  command_in_current_release "run-with-lockfile -n ./change-xapian-database.lock \"./script/update-xapian-index verbose=true\" >> ./logs/update-xapian-index.log || echo \"stalled?\""
 end
