f9426909e1ac5b95f14ddd7e10da5499744443bf
Henare Degan <henare.degan@gmail.com>
2012-09-18 14:10:41 +1000

    Add the rails environment so that Alaveteli scripts work

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 # Use this file to easily define all of your cron jobs.
 
-job_type :command_in_current_release, "cd :path && :task :output"
+job_type :command_in_current_release, "cd :path && RAILS_ENV=:environment :task :output"
 
 every 5.minutes do
   # TODO: Replace run-with-lockfile command with something more Rubyesque
