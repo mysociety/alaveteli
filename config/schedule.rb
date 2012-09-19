228d0110a1ef545db2bc3596276c60c00deb97b7
Henare Degan <henare.degan@gmail.com>
2012-09-19 15:27:59 +1000

    Refactor job type so we don't need lockfile_name

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,13 @@
 # Use this file to easily define all of your cron jobs.
 
 # TODO: Replace run-with-lockfile command with something more Rubyesque
-job_type :run_with_lockfile, "cd :path && RAILS_ENV=:environment run-with-lockfile -n ./:lockfile_name.lock :task || echo \"stalled?\""
+job_type :run_script_with_lockfile, "cd :path && RAILS_ENV=:environment run-with-lockfile -n ./:task ./script/:task || echo \"stalled?\""
 
 job_type :command_in_current_release, "cd :path && RAILS_ENV=:environment :task :output"
 
 every 5.minutes do
   # TODO: Replace with Raketask xapian:rebuild_index
-  run_with_lockfile "\"./script/update-xapian-index verbose=true\" >> ./log/update-xapian-index.log", :lockfile_name => 'change-xapian-database'
+  command_in_current_release "run-with-lockfile -n ./update-xapian-index.lock \"./script/update-xapian-index verbose=true\" >> ./log/update-xapian-index.log || echo \"stalled?\""
 end
 
 every 10.minutes do
@@ -16,44 +16,37 @@ every 10.minutes do
 end
 
 every :hour, :at => 9 do
-  # TODO: Replace script with runner task that Whenever natively supports
-  run_with_lockfile './script/alert-comment-on-request', :lockfile_name => 'alert-comment-on-request'
+  run_script_with_lockfile 'alert-comment-on-request'
 end
 
 every :hour, :at => 31 do
   # TODO: Add instructions about needing permissions to do this. The original
   # crontab specified running as root which does not sounds like A Good Idea
-  run_with_lockfile './script/load-exim-logs', :lockfile_name => 'load-exim-logs'
+  run_script_with_lockfile 'load-exim-logs'
 end
 
 every :day, :at => '04:23' do
-  # TODO: Replace script with runner task that Whenever natively supports
-  run_with_lockfile './script/delete-old-things', :lockfile_name => 'delete-old-things'
+  run_script_with_lockfile 'delete-old-things'
 end
 
 every :day, :at => '06:00' do
-  # TODO: Replace script with runner task that Whenever natively supports
-  run_with_lockfile './script/alert-overdue-requests', :lockfile_name => 'alert-overdue-requests'
+  run_script_with_lockfile 'alert-overdue-requests'
 end
 
 every :day, :at => '07:00' do
-  # TODO: Replace script with runner task that Whenever natively supports
-  run_with_lockfile './script/alert-new-response-reminders', :lockfile_name => 'alert-new-response-reminders'
+  run_script_with_lockfile 'alert-new-response-reminders'
 end
 
 every :day, :at => '08:00' do
-  # TODO: Replace script with runner task that Whenever natively supports
-  run_with_lockfile './script/alert-not-clarified-request', :lockfile_name => 'alert-not-clarified-request'
+  run_script_with_lockfile 'alert-not-clarified-request'
 end
 
 every :day, :at => '04:02' do
-  # TODO: Replace script with runner task that Whenever natively supports
-  run_with_lockfile './script/check-recent-requests-sent', :lockfile_name => 'check-recent-requests-sent'
+  run_script_with_lockfile 'check-recent-requests-sent'
 end
 
 every :day, :at => '03:45' do
-  # TODO: Replace script with runner task that Whenever natively supports
-  run_with_lockfile './script/stop-new-responses-on-old-requests', :lockfile_name => 'stop-new-responses-on-old-requests'
+  run_script_with_lockfile 'stop-new-responses-on-old-requests'
 end
 
 # # Once a day, early morning
