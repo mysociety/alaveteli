a6b67cab18b124b908a3613d6232163b00c74244
Louise Crow <louise.crow@gmail.com>
2012-10-24 17:10:44 +0100

    Remove redundant line.

--- lib/timezone_fixes.rb
+++ lib/timezone_fixes.rb
@@ -1,4 +1,4 @@
-# Taken from 
+# Taken from
 # https://rails.lighthouseapp.com/projects/8994/tickets/2946
 # http://github.com/rails/rails/commit/6f97ad07ded847f29159baf71050c63f04282170
 
@@ -11,7 +11,6 @@ module ActiveRecord
   module ConnectionAdapters # :nodoc:
     module Quoting
        def quoted_date(value)
-        value.to_s(:db)
         if value.acts_like?(:time)
           zone_conversion_method = ActiveRecord::Base.default_timezone == :utc ? :getutc : :getlocal
           value.respond_to?(zone_conversion_method) ? value.send(zone_conversion_method) : value
