3421afdb9eb79effa562ded447923b529ef8b714
Louise Crow <louise.crow@gmail.com>
2012-11-28 15:57:58 +0000

    Give the implicit default locale in a URL without locale precedence over the session in the case where the default locale is not being included in URLs. This allows the user to return to the default locale.

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -3,6 +3,4 @@ FastGettext.default_text_domain = 'app'
 
 I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 
-if Configuration::include_default_locale_in_urls == false
-    RoutingFilter::Locale.include_default_locale = false
-end
+RoutingFilter::Locale.include_default_locale = Configuration::include_default_locale_in_urls
