a92d95908bdc8b1f2a5111a6d36b6de210ab5c55
Henare Degan <henare.degan@gmail.com>
2012-12-09 19:08:59 +1100

    Remove interlock as it's not compatible with Rails 3 and is unmaintained

--- vendor/plugins/interlock/test/integration/app/app/models/book.rb
+++
@@ -1,3 +0,0 @@
-class Book < ActiveRecord::Base
-  set_primary_key :guid
-end
--- vendor/plugins/interlock/test/integration/app/test/test_helper.rb
+++
@@ -1,9 +0,0 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-require 'test_help'
-
-class Test::Unit::TestCase
-  self.use_transactional_fixtures = true
-  self.use_instantiated_fixtures  = false
-  fixtures :all
-end
