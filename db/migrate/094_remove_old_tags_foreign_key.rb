e11acad45d13024c396b679b866e761417079232
Louise Crow <louise.crow@gmail.com>
2013-01-17 17:46:13 +0000

    Drop the obsolete indexes explicitly by name - the 'remove_index' calls don't work as the table has been renamed, and they automatically generate the index name from the table name. Apply custom index name from a5e90e2b7ea6adedbf80c05bbb84425a02c63da0.

--- db/migrate/094_remove_old_tags_foreign_key.rb
+++ db/migrate/094_remove_old_tags_foreign_key.rb
@@ -2,11 +2,9 @@ class RemoveOldTagsForeignKey < ActiveRecord::Migration
     def self.up
         if ActiveRecord::Base.connection.adapter_name == "PostgreSQL"
             execute "ALTER TABLE has_tag_string_tags DROP CONSTRAINT fk_public_body_tags_public_body"
+            execute 'DROP INDEX index_public_body_tags_on_public_body_id_and_name_and_value'
+            execute 'DROP INDEX index_public_body_tags_on_name'
         end
-
-        # This table was already removed in the previous migration
-        # remove_index :public_body_tags, [:public_body_id, :name, :value]
-        # remove_index :public_body_tags, :name
 
         add_index :has_tag_string_tags, [:model, :model_id, :name, :value], :name => 'by_model_and_model_id_and_name_and_value'
         add_index :has_tag_string_tags, :name
