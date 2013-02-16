e195121efe1caa0ea845bec7be12b6ed2cd2812c
Louise Crow <louise.crow@gmail.com>
2013-02-16 19:30:10 +0000

    Clear purge requests in the setup of tests that count them.

--- spec/controllers/admin_censor_rule_controller_spec.rb
+++ spec/controllers/admin_censor_rule_controller_spec.rb
@@ -2,10 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe AdminCensorRuleController, "when making censor rules from the admin interface" do
     integrate_views
-    before { basic_auth_login @request }
-  
+    before do
+        basic_auth_login @request
+        PurgeRequest.destroy_all
+    end
+
+
     it "should create a censor rule and purge the corresponding request from varnish" do
-        ir = info_requests(:fancy_dog_request) 
+        ir = info_requests(:fancy_dog_request)
         post :create, :censor_rule => {
                          :text => "meat",
                          :replacement => "tofu",
