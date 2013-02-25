c8c0ec7f49a33458ac2fe4ef3484a171a65e6955
Henare Degan <henare.degan@gmail.com>
2013-02-26 10:03:09 +1100

    Upgrade ContactMailer to new ActionMailer API

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,17 +5,13 @@
 # Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 
 class ContactMailer < ApplicationMailer
-
     # Send message to administrator
     def to_admin_message(name, email, subject, message, logged_in_user, last_request, last_body)
-        @from = name + " <" + email + ">"
-        @recipients = contact_from_name_and_email
-        @subject = subject
-        @body = { :message => message,
-            :logged_in_user => logged_in_user ,
-            :last_request => last_request,
-            :last_body => last_body
-        }
+        @message, @logged_in_user, @last_request, @last_body = message, logged_in_user, last_request, last_body
+
+        mail(:from => name + " <" + email + ">",
+             :to => contact_from_name_and_email,
+             :subject => subject)
     end
 
     # We always set Reply-To when we set Return-Path to be different from From,
@@ -26,31 +22,24 @@ class ContactMailer < ApplicationMailer
 
     # Send message to another user
     def user_message(from_user, recipient_user, from_user_url, subject, message)
-        @from = from_user.name_and_email
+        @message, @from_user, @recipient_user, @from_user_url = message, from_user, recipient_user, from_user_url
+
         # Do not set envelope from address to the from_user, so they can't get
         # someone's email addresses from transitory bounce messages.
-        headers 'Return-Path' => blackhole_email, 'Reply-To' => @from
-        @recipients = recipient_user.name_and_email
-        @subject = subject
-        @body = {
-            :message => message,
-            :from_user => from_user,
-            :recipient_user => recipient_user,
-            :from_user_url => from_user_url
-        }
+        headers('Return-Path' => blackhole_email, 'Reply-To' => @from)
+
+        mail(:from => from_user.name_and_email,
+             :to => recipient_user.name_and_email,
+             :subject => subject)
     end
 
     # Send message to a user from the administrator
     def from_admin_message(recipient_user, subject, message)
-        @from = contact_from_name_and_email
-        @recipients = recipient_user.name_and_email
-        @subject = subject
-        @body = {
-            :message => message,
-            :from_user => @from,
-            :recipient_user => recipient_user,
-        }
-        bcc Configuration::contact_email
-    end
+        @message, @from_user, @recipient_user = message, @from, recipient_user
 
+        mail(:from => contact_from_name_and_email,
+             :to => recipient_user.name_and_email,
+             :bcc => Configuration::contact_email,
+             :subject => subject)
+    end
 end
