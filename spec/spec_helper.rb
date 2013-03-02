4fd235d470c7e0055044d3926736c1afaa9cfb40
Matthew Landauer <matthew@openaustralia.org>
2013-03-02 13:25:32 +1100

    Run local coverage tests as well as those on coveralls.io and exclude plugins and commonlib directories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+
+# Generate coverage locally in html as well as in coveralls.io
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start('rails') do
+  add_filter  'commonlib'
+  add_filter  'vendor/plugins'
+end
 
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
