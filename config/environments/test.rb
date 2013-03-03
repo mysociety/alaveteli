b5c51ce57dbabf7638f354c91f75ea52d75d2347
Henare Degan <henare.degan@gmail.com>
2013-03-03 15:01:51 +1100

    See how we go not disabling this now

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,8 +1,6 @@
 Alaveteli::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
-  require 'patches/fixtures_constraint_disabling'
-
   # The test environment is used exclusively to run your application's
   # test suite.  You never need to work with it otherwise.  Remember that
   # your test database is "scratch space" for the test suite and is wiped
