3d2b9e778e44fe7d4dab00880475dd414119dbe9
Henare Degan <henare.degan@gmail.com>
2013-03-05 12:14:15 +1100

    Remove debugging output that was causing migration to fail

--- db/migrate/101_add_hash_to_info_request.rb
+++ db/migrate/101_add_hash_to_info_request.rb
@@ -8,10 +8,8 @@ class AddHashToInfoRequest < ActiveRecord::Migration
         InfoRequest.find(:all).each do |info_request|
             info_request.idhash = Digest::SHA1.hexdigest(info_request.id.to_s + Configuration::incoming_email_secret)[0,8]
             info_request.save!
-            puts info_request.idhash
         end
         change_column :info_requests, :idhash, :string, :null => false
-        puts InfoRequest.find_by_idhash
     end
     def self.down
         remove_column :info_requests, :idhash
