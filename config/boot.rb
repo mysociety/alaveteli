777b6a0ae0cd3c4f288d2cd20da3cab2ea814781
Henare Degan <henare.degan@gmail.com>
2013-03-05 17:53:49 +1100

    Reinstate the hacky default environment thing for the moment

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,12 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# TODO: Remove this. This is a hacky system for having a default environment.
+# It looks for a config/rails_env.rb file, and reads stuff from there if
+# it exists. Put just a line like this in there:
+#   ENV['RAILS_ENV'] = 'production'
+rails_env_file = File.expand_path(File.join(File.dirname(__FILE__), 'rails_env.rb'))
+if File.exists?(rails_env_file)
+    require rails_env_file
+end
