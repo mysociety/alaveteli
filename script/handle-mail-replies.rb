6124624f4035e74eb79f1e93f26d5584ed3e2fa1
Louise Crow <louise.crow@gmail.com>
2013-04-30 15:40:37 +0100

    Remove setting of internal and external encodings for ruby 1.9 - as per 512d8e3d1845e327740acc2f7326e72b23d0d604, this is already the default in Rails 3.

--- script/handle-mail-replies.rb
+++ script/handle-mail-replies.rb
@@ -17,10 +17,6 @@ $:.push(File.join($alaveteli_dir, "commonlib", "rblib"))
 $:.push(File.join($alaveteli_dir, "lib"))
 $:.push(File.join($alaveteli_dir, "lib", "mail_handler"))
 require 'mail_handler'
-if RUBY_VERSION.to_f >= 1.9
-    # the default encoding for IO is utf-8, and we use utf-8 internally
-    Encoding.default_external = Encoding.default_internal = Encoding::UTF_8
-end
 
 def main(in_test_mode)
     Dir.chdir($alaveteli_dir) do
