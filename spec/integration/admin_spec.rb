e53e0a0ccbb4695dbbb01a56598b7c832a9c5a0c
Louise Crow <louise.crow@gmail.com>
2013-08-01 14:53:10 +0100

    Refactor some common setup steps in integration tests into a DSL.
    Add a failing test for what should happen on request hiding.

--- spec/integration/admin_spec.rb
+++ spec/integration/admin_spec.rb
@@ -1,21 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-
-require "base64"
+require File.expand_path(File.dirname(__FILE__) + '/alaveteli_dsl')
 
 describe "When administering the site" do
+
+    before do
+        AlaveteliConfiguration.stub!(:skip_admin_auth).and_return(false)
+    end
+
     it "allows an admin to log in as another user" do
         # First log in as Joe Admin
-        admin_user = users(:admin_user)
-        admin_user.email_confirmed = true
-        admin_user.save!
-        post_via_redirect "/profile/sign_in", :user_signin => {:email => admin_user.email, :password => "jonespassword"}
-        response.should be_success
-        
+        confirm(:admin_user)
+        admin = login(:admin_user)
+
         # Now fetch the "log in as" link to log in as Bob
-        get_via_redirect "/admin/user/login_as/#{users(:bob_smith_user).id}", nil, {
-          "Authorization" => "Basic " + Base64.encode64("#{AlaveteliConfiguration::admin_username}:#{AlaveteliConfiguration::admin_password}").strip
-        }
-        response.should be_success
-        session[:user_id].should == users(:bob_smith_user).id
+        admin.get_via_redirect "/admin/user/login_as/#{users(:bob_smith_user).id}"
+        admin.response.should be_success
+        admin.session[:user_id].should == users(:bob_smith_user).id
+    end
+
+    it 'does not allow a non-admin user to login as another user' do
+        robin = login(:robin_user)
+        robin.get_via_redirect "/admin/user/login_as/#{users(:bob_smith_user).id}"
+        robin.response.should be_success
+        robin.session[:user_id].should_not == users(:bob_smith_user).id
     end
 end
--- spec/integration/view_request_spec.rb
+++ spec/integration/view_request_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/alaveteli_dsl')
 
 describe "When viewing requests" do
 
@@ -7,18 +8,32 @@ describe "When viewing requests" do
     end
 
     it "should not make endlessly recursive JSON <link>s" do
-        @dog_request = info_requests(:fancy_dog_request)
-        get "request/#{@dog_request.url_title}?unfold=1"
-        response.body.should_not include("dog?unfold=1.json")
-        response.body.should include("dog.json?unfold=1")
+        unregistered = without_login
+        unregistered.browses_request('why_do_you_have_such_a_fancy_dog?unfold=1')
+        unregistered.response.body.should_not include("dog?unfold=1.json")
+        unregistered.response.body.should include("dog.json?unfold=1")
     end
 
     it 'should not raise a routing error when making a json link for a request with an
        "action" querystring param' do
-       @dog_request = info_requests(:fancy_dog_request)
-       get "request/#{@dog_request.url_title}?action=add"
-       response.should be_success
+       unregistered = without_login
+       unregistered.browses_request('why_do_you_have_such_a_fancy_dog?action=add')
     end
 
+    context 'when a response is hidden' do
+
+        before do
+            useless_message = incoming_messages(:useless_incoming_message)
+            useless_message.prominence = 'hidden'
+            useless_message.save!
+        end
+
+        it 'should show a hidden notice to an unregistered user' do
+            unregistered = without_login
+            response = unregistered.browses_request('why_do_you_have_such_a_fancy_dog')
+            response.body.should include("This message has been hidden.")
+        end
+
+    end
 end
 
