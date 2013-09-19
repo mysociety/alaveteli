881d393aff2598c4a0c4a23fee85ebe9fe478b03
Louise Crow <louise.crow@gmail.com>
2013-09-19 16:51:21 +0100

    Add a can_make_batch_requests flag to user model
    Make it updatable via the user admin page.

--- spec/factories.rb
+++ spec/factories.rb
@@ -123,6 +123,7 @@ FactoryGirl.define do
         salt "-6116981980.392287733335677"
         hashed_password '6b7cd45a5f35fd83febc0452a799530398bfb6e8' # jonespassword
         email_confirmed true
+        ban_text ""
         factory :admin_user do
             name 'Admin User'
             admin_level 'super'
