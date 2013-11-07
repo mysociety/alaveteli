c0c79a024d6cbfdea18fd87d620c856d8dfc43ef
Louise Crow <louise.crow@gmail.com>
2013-11-07 16:10:25 +0000

    Disambiguate File from ActiveRecord::Fixture::File

--- lib/no_constraint_disabling.rb
+++ lib/no_constraint_disabling.rb
@@ -47,7 +47,7 @@ module ActiveRecord
               connection,
               table_name,
               class_names[table_name.to_sym] || table_name.classify,
-              File.join(fixtures_directory, path))
+              ::File.join(fixtures_directory, path))
           end
 
           all_loaded_fixtures.update(fixtures_map)
