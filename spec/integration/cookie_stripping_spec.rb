2ca432a86623266c7f11a067a0dc87be6f509d6e
Louise Crow <louise.crow@gmail.com>
2013-11-13 16:40:11 +0000

    Insert cookie stripping at correct point in middleware stack.

---
+++ spec/integration/cookie_stripping_spec.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/alaveteli_dsl')
+
+describe 'when making stripping cookies' do
+
+    it 'should not set a cookie when no significant session data is set' do
+        get 'country_message'
+        response.headers['Set-Cookie'].should be_blank
+    end
+
+end
+
