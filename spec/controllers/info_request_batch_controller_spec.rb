dd25406c2c96eb1b59a825f866b22ee34365c06c
Louise Crow <louise.crow@gmail.com>
2013-11-27 17:55:03 +0000

    Show public body list when batch has not been sent.

--- spec/controllers/info_request_batch_controller_spec.rb
+++ spec/controllers/info_request_batch_controller_spec.rb
@@ -4,10 +4,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe InfoRequestBatchController, "when showing a request" do
 
     before do
+        @first_public_body = FactoryGirl.create(:public_body)
+        @second_public_body = FactoryGirl.create(:public_body)
         @info_request_batch = FactoryGirl.create(:info_request_batch, :title => 'Matched title',
-                                                                      :body => 'Matched body')
-        @first_request = FactoryGirl.create(:info_request, :info_request_batch => @info_request_batch)
-        @second_request = FactoryGirl.create(:info_request, :info_request_batch => @info_request_batch)
+                                                                      :body => 'Matched body',
+                                                                      :public_bodies => [@first_public_body,
+                                                                                         @second_public_body])
+        @first_request = FactoryGirl.create(:info_request, :info_request_batch => @info_request_batch,
+                                                           :public_body => @first_public_body)
+        @second_request = FactoryGirl.create(:info_request, :info_request_batch => @info_request_batch,
+                                                            :public_body => @second_public_body)
         @default_params = {:id => @info_request_batch.id}
     end
 
@@ -20,13 +26,28 @@ describe InfoRequestBatchController, "when showing a request" do
         response.should be_success
     end
 
-    it 'should assign info_requests to the view' do
-        make_request
-        assigns[:info_requests].should == [@first_request, @second_request]
-    end
-
     it 'should assign an info_request_batch to the view' do
         make_request
         assigns[:info_request_batch].should == @info_request_batch
     end
+
+    context 'when the batch has not been sent' do
+
+        it 'should assign public_bodies to the view' do
+            make_request
+            assigns[:public_bodies].should == [@first_public_body, @second_public_body]
+        end
+    end
+
+    context 'when the batch has been sent' do
+
+        it 'should assign info_requests to the view' do
+            @info_request_batch.sent_at = Time.now
+            @info_request_batch.save!
+            make_request
+            assigns[:info_requests].should == [@first_request, @second_request]
+        end
+
+    end
+
 end
