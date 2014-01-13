a16a1592278e036420c65894d86862cf0ef8b74f
Stanislav Gatev <s2gatev@gmail.com>
2014-01-14 00:25:32 +0200

    Temporary enable asset compilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Alaveteli::Application.configure do
   # config.assets.css_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
