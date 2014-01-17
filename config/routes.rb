8d95de46477ac164c99ca33eafd10115ba51db2a
Andrew Black <andrewdblack@googlemail.com>
2014-01-17 15:21:21 +0000

    Controller code to hide/unhide annotations
    The UI provides a list of comment_ids and a newstate="visible" or "hide"

--- config/routes.rb
+++ config/routes.rb
@@ -233,6 +233,7 @@ Alaveteli::Application.routes.draw do
     match '/admin/user/destroy_track' => 'admin_user#destroy_track', :as => :admin_user_destroy_track
     match '/admin/user/login_as/:id' => 'admin_user#login_as', :as => :admin_user_login_as
     match '/admin/user/clear_profile_photo/:id' => 'admin_user#clear_profile_photo', :as => :admin_clear_profile_photo
+    match '/admin/user/modify_comment_visibility/:id' => 'admin_user#modify_comment_visibility', :as => 'admin_user_modify_comment_visibility'
     ####
 
     #### AdminTrack controller
