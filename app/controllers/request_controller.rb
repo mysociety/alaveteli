54670c8fb3755a2de768b96edbba449a45402151
Louise Crow <louise.crow@gmail.com>
2014-01-28 10:41:04 +0000

    Preserve whitespace in strings for translation.
    Otherwise they get marked as fuzzy in .po files and lose their existing
    translations.

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1068,7 +1068,7 @@ class RequestController < ApplicationController
                 message += _("<p>You do not need to include your email in the request in order to get a reply (<a href=\"{{url}}\">details</a>).</p>", :url => (help_privacy_path+"#email_address").html_safe);
             end
             message += _("<p>We recommend that you edit your request and remove the email address.
-            If you leave it, the email address will be sent to the authority, but will not be displayed on the site.</p>")
+                If you leave it, the email address will be sent to the authority, but will not be displayed on the site.</p>")
         end
         if @outgoing_message.contains_postcode?
             message += _("<p>Your request contains a <strong>postcode</strong>. Unless it directly relates to the subject of your request, please remove any address as it will <strong>appear publicly on the Internet</strong>.</p>");
