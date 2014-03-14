eff755504d45bfeff415786dcd0dbbb4c47e8dd0
Gareth Rees <gareth@mysociety.org>
2014-03-14 11:47:52 +0000

    Add AdminHelper#comment_visibility
    Get the canonical human-readable status of a comment

---
+++ spec/helpers/admin_helper_spec.rb
@@ -0,0 +1,21 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe AdminHelper do
+
+    include AdminHelper
+
+    describe :comment_visibility do
+
+        it 'shows the status of a visible comment' do
+            comment = Factory.build(:visible_comment)
+            comment_visibility(comment).should == 'Visible'
+        end
+
+        it 'shows the status of a hidden comment' do
+            comment = Factory.build(:hidden_comment)
+            comment_visibility(comment).should == 'Hidden'
+        end
+
+    end
+    
+end
