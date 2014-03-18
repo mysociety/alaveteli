4bde7f5d1dce36ffeb47826b146b5d53a155d123
Louise Crow <louise.crow@gmail.com>
2014-03-18 10:15:49 +0000

    Make track factory name a bit more specific.

--- spec/factories.rb
+++ spec/factories.rb
@@ -140,9 +140,11 @@ FactoryGirl.define do
 
     factory :track_thing do
         association :tracking_user, :factory => :user
-        track_medium 'email_daily'
-        track_type 'search_query'
-        track_query 'Example Query'
+        factory :search_track do
+            track_medium 'email_daily'
+            track_type 'search_query'
+            track_query 'Example Query'
+        end
     end
 
     factory :public_body_change_request do
