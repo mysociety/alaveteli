254264c1f56c3e145ba61e94afebd6698d43c5e9
Rowan Crawford <wombleton@gmail.com>
2014-04-11 11:20:39 +1200

    Add some stop words as they appear to be being passed through by xapian.

--- spec/helpers/highlight_helper_spec.rb
+++ spec/helpers/highlight_helper_spec.rb
@@ -6,22 +6,25 @@ describe HighlightHelper do
 
     describe 'when highlighting text' do
       it 'should not highlight the middle of a word' do
-        do_highlight("quack", %W(a), :highlighter => '*\1*').should == 'quack'
+        do_highlight("quack", %W(ack), :highlighter => '*\1*').should == 'quack'
       end
       it 'should highlight a complete word' do
-        do_highlight("quack a doodle", %W(a), :highlighter => '*\1*').should == 'quack *a* doodle'
+        do_highlight("quack ack doodle", %W(ack), :highlighter => '*\1*').should == 'quack *ack* doodle'
       end
       it 'should work the same from highlight_words' do
-        highlight_words("quack a doodle", %W(a), false).should == 'quack *a* doodle'
+        highlight_words("quack ack doodle", %W(ack), false).should == 'quack *ack* doodle'
+      end
+      it 'should ignore stop works' do
+        highlight_words("a about above moo across after", %W(a about above moo across after), false).should == "a about above *moo* across after"
       end
     end
 
     describe 'when highlighting html' do
       it 'should not highlight the middle of a word' do
-        highlight_words("quack", %W(a)).should == "quack"
+        highlight_words("quack", %W(ack)).should == "quack"
       end
       it 'should highlight a complete word' do
-        highlight_words("quack a doodle", %W(a)).should == 'quack <span class="highlight">a</span> doodle'
+        highlight_words("quack ack doodle", %W(ack)).should == 'quack <span class="highlight">ack</span> doodle'
       end
     end
 end
