295e0aa13b5c575a89757c7be34fa8ae04d66895
Rowan Crawford <wombleton@gmail.com>
2014-04-11 11:33:16 +1200

    Lower case w word array — no interpolation needed.

--- spec/helpers/highlight_helper_spec.rb
+++ spec/helpers/highlight_helper_spec.rb
@@ -6,25 +6,25 @@ describe HighlightHelper do
 
     describe 'when highlighting text' do
       it 'should not highlight the middle of a word' do
-        do_highlight("quack", %W(ack), :highlighter => '*\1*').should == 'quack'
+        do_highlight("quack", %w(ack), :highlighter => '*\1*').should == 'quack'
       end
       it 'should highlight a complete word' do
-        do_highlight("quack ack doodle", %W(ack), :highlighter => '*\1*').should == 'quack *ack* doodle'
+        do_highlight("quack ack doodle", %w(ack), :highlighter => '*\1*').should == 'quack *ack* doodle'
       end
       it 'should work the same from highlight_words' do
-        highlight_words("quack ack doodle", %W(ack), false).should == 'quack *ack* doodle'
+        highlight_words("quack ack doodle", %w(ack), false).should == 'quack *ack* doodle'
       end
       it 'should ignore stop works' do
-        highlight_words("a about above moo across after", %W(a about above moo across after), false).should == "a about above *moo* across after"
+        highlight_words("a about above moo across after", %w(a about above moo across after), false).should == "a about above *moo* across after"
       end
     end
 
     describe 'when highlighting html' do
       it 'should not highlight the middle of a word' do
-        highlight_words("quack", %W(ack)).should == "quack"
+        highlight_words("quack", %w(ack)).should == "quack"
       end
       it 'should highlight a complete word' do
-        highlight_words("quack ack doodle", %W(ack)).should == 'quack <span class="highlight">ack</span> doodle'
+        highlight_words("quack ack doodle", %w(ack)).should == 'quack <span class="highlight">ack</span> doodle'
       end
     end
 end
