cc846cb49d8e99e0ca183cef8c73df8b8af6b39b
Louise Crow <louise.crow@gmail.com>
2014-04-25 09:02:12 +0100

    Fix for badly-formed content-disposition header.
    Taken from https://github.com/mikel/mail/pull/602

--- lib/mail_handler/backends/mail_extensions.rb
+++ lib/mail_handler/backends/mail_extensions.rb
@@ -9,6 +9,12 @@ module Mail
         attr_accessor :count_first_uudecode_count
     end
 
+    class Part < Message
+        def inline?
+          header[:content_disposition].disposition_type == 'inline' if header[:content_disposition] rescue false
+        end
+    end
+
     # A patched version of the parameter hash that handles nil values without throwing
     # an error.
     class ParameterHash < IndifferentHash
