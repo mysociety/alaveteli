e5a73815f580d296572e11b71b5f3ed320bbe912
Gareth Rees <gareth@mysociety.org>
2014-05-30 12:42:48 +0100

    Add helper to highlight and excerpt by regex
    Backport of https://github.com/rails/rails/pull/11793/
    
    Contains integration tests to check that it works
    as expected with ActsAsXapian.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,6 +22,9 @@ module ApplicationHelper
     # Useful for sending emails
     include MailerHelper
 
+    # Extra highlight helpers
+    include HighlightHelper
+
     # Copied from error_messages_for in active_record_helper.rb
     def foi_error_messages_for(*params)
         options = params.last.is_a?(Hash) ? params.pop.symbolize_keys : {}
@@ -52,25 +55,6 @@ module ApplicationHelper
         else
             ''
         end
-    end
-
-    # Highlight words, also escapes HTML (other than spans that we add)
-    def highlight_words(t, words, html = true)
-        if html
-            highlight(h(t), words, :highlighter => '<span class="highlight">\1</span>').html_safe
-        else
-            highlight(t, words, :highlighter => '*\1*')
-        end
-    end
-
-    def highlight_and_excerpt(t, words, excount, html = true)
-        newt = excerpt(t, words[0], :radius => excount)
-        if not newt
-            newt = excerpt(t, '', :radius => excount)
-        end
-        t = newt
-        t = highlight_words(t, words, html)
-        return t
     end
 
     def locale_name(locale)
