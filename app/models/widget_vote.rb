002e7d7f132d773cb3a5c1c46314f625a9aaeec3
Jody McIntyre <scjody@modernduck.com>
2014-09-17 15:27:42 -0400

    Add model to track "votes" for a request

---
+++ app/models/widget_vote.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: widget_votes
+#
+#  id              :integer          not null, primary key
+#  cookie          :string(255)
+#  info_request_id :integer          not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class WidgetVote < ActiveRecord::Base
+  belongs_to :info_request
+  validates :info_request, :presence => true
+
+  attr_accessible :cookie
+  validates :cookie, :length => { :is => 20 }
+end
