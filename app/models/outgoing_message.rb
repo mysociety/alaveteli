131c8d01834c07337ec5a10cafe4663e33165c26
Gareth Rees <gareth@mysociety.org>
2014-09-19 10:42:31 +0100

    Remove uneeded code / Tidy up

--- app/models/outgoing_message.rb
+++ app/models/outgoing_message.rb
@@ -61,14 +61,19 @@ class OutgoingMessage < ActiveRecord::Base
 
     # How the default letter starts and ends
     def get_salutation
-        if self.info_request.is_batch_request_template?
+        if info_request.is_batch_request_template?
             return OutgoingMessage.placeholder_salutation
         end
+
         ret = ""
-        if self.message_type == 'followup' && !self.incoming_message_followup.nil? && !self.incoming_message_followup.safe_mail_from.nil? && self.incoming_message_followup.valid_to_reply_to?
-            ret = ret + OutgoingMailer.name_for_followup(self.info_request, self.incoming_message_followup)
+        if message_type == 'followup' &&
+            !incoming_message_followup.nil? &&
+            !incoming_message_followup.safe_mail_from.nil? &&
+            incoming_message_followup.valid_to_reply_to?
+
+            ret += OutgoingMailer.name_for_followup(info_request, incoming_message_followup)
         else
-            return OutgoingMessage.default_salutation(self.info_request.public_body)
+            return OutgoingMessage.default_salutation(info_request.public_body)
         end
         salutation = _("Dear {{public_body_name}},", :public_body_name => ret)
     end
@@ -86,41 +91,52 @@ class OutgoingMessage < ActiveRecord::Base
     end
 
     def get_signoff
-        if self.message_type == 'followup' && !self.incoming_message_followup.nil? && !self.incoming_message_followup.safe_mail_from.nil? && self.incoming_message_followup.valid_to_reply_to?
-            return _("Yours sincerely,")
-        else
-            return _("Yours faithfully,")
-        end
-    end
-    def get_internal_review_insert_here_note
-        return _("GIVE DETAILS ABOUT YOUR COMPLAINT HERE")
-    end
-    def get_default_letter
-        if self.default_letter
-            return self.default_letter
-        end
+        if message_type == 'followup' &&
+            !incoming_message_followup.nil? &&
+            !incoming_message_followup.safe_mail_from.nil? &&
+            incoming_message_followup.valid_to_reply_to?
 
-        if self.what_doing == 'internal_review'
-            _("Please pass this on to the person who conducts Freedom of Information reviews.") +
-            "\n\n" +
-            _("I am writing to request an internal review of {{public_body_name}}'s handling of my FOI request '{{info_request_title}}'.",
-              :public_body_name => self.info_request.public_body.name,
-              :info_request_title => self.info_request.title) +
-            "\n\n\n\n [ " + self.get_internal_review_insert_here_note + " ] \n\n\n\n" +
-            _("A full history of my FOI request and all correspondence is available on the Internet at this address: {{url}}",
-            :url => request_url(self.info_request)) +
-            "\n"
+            _("Yours sincerely,")
+        else
+            _("Yours faithfully,")
+        end
+    end
+
+    def get_internal_review_insert_here_note
+        _("GIVE DETAILS ABOUT YOUR COMPLAINT HERE")
+    end
+
+    def get_default_letter
+        return default_letter if default_letter
+
+        if what_doing == 'internal_review'
+            letter = _("Please pass this on to the person who conducts Freedom of Information reviews.")
+            letter += "\n\n"
+            letter += _("I am writing to request an internal review of {{public_body_name}}'s handling of my FOI request '{{info_request_title}}'.",
+                          :public_body_name => info_request.public_body.name,
+                          :info_request_title => info_request.title)
+            letter += "\n\n\n\n [ #{ get_internal_review_insert_here_note } ] \n\n\n\n"
+            letter += _("A full history of my FOI request and all correspondence is available on the Internet at this address: {{url}}",
+                          :url => request_url(info_request))
+            letter += "\n"
         else
             ""
         end
     end
+
     def get_default_message
-        get_salutation + "\n\n" + get_default_letter + "\n\n" + get_signoff + "\n\n"
+        msg = get_salutation
+        msg += "\n\n"
+        msg += get_default_letter
+        msg += "\n\n"
+        msg += get_signoff
+        msg += "\n\n"
     end
+
     def set_signature_name(name)
         # TODO: We use raw_body here to get unstripped one
-        if self.raw_body == self.get_default_message
-            self.body = self.raw_body + name
+        if raw_body == get_default_message
+            self.body = raw_body + name
         end
     end
 
@@ -135,84 +151,88 @@ class OutgoingMessage < ActiveRecord::Base
         ret.gsub!(/(?:\n\s*){2,}/, "\n\n") # remove excess linebreaks that unnecessarily space it out
 
         # Remove things from censor rules
-        if !self.info_request.nil?
+        unless info_request.nil?
             self.info_request.apply_censor_rules_to_text!(ret)
         end
 
         ret
     end
+
     def raw_body
         read_attribute(:body)
     end
 
     # Used to give warnings when writing new messages
     def contains_email?
-        MySociety::Validate.email_find_regexp.match(self.body)
+        MySociety::Validate.email_find_regexp.match(body)
     end
+
     def contains_postcode?
-        MySociety::Validate.contains_postcode?(self.body)
+        MySociety::Validate.contains_postcode?(body)
     end
 
     # Deliver outgoing message
     # Note: You can test this from script/console with, say:
     # InfoRequest.find(1).outgoing_messages[0].send_message
     def send_message(log_event_type = 'sent')
-        if self.status == 'ready'
-            if self.message_type == 'initial_request'
+        if status == 'ready'
+            if message_type == 'initial_request'
                 self.last_sent_at = Time.now
                 self.status = 'sent'
                 self.save!
 
-                mail_message = OutgoingMailer.initial_request(self.info_request, self).deliver
+                mail_message = OutgoingMailer.initial_request(info_request, self).deliver
                 self.info_request.log_event(log_event_type, {
                     :email => mail_message.to_addrs.join(", "),
                     :outgoing_message_id => self.id,
                     :smtp_message_id => mail_message.message_id
                 })
                 self.info_request.set_described_state('waiting_response')
-            elsif self.message_type == 'followup'
+            elsif message_type == 'followup'
                 self.last_sent_at = Time.now
                 self.status = 'sent'
                 self.save!
 
-                mail_message = OutgoingMailer.followup(self.info_request, self, self.incoming_message_followup).deliver
+                mail_message = OutgoingMailer.followup(info_request, self, incoming_message_followup).deliver
                 self.info_request.log_event('followup_' + log_event_type, {
                     :email => mail_message.to_addrs.join(", "),
                     :outgoing_message_id => self.id,
                     :smtp_message_id => mail_message.message_id
                 })
-                if self.info_request.described_state == 'waiting_clarification'
+                if info_request.described_state == 'waiting_clarification'
                     self.info_request.set_described_state('waiting_response')
                 end
-                if self.what_doing == 'internal_review'
+                if what_doing == 'internal_review'
                     self.info_request.set_described_state('internal_review')
                 end
             else
-                raise "Message id #{self.id} has type '#{self.message_type}' which send_message can't handle"
+                raise "Message id #{id} has type '#{message_type}' which send_message can't handle"
             end
-        elsif self.status == 'sent'
-            raise "Message id #{self.id} has already been sent"
+        elsif status == 'sent'
+            raise "Message id #{id} has already been sent"
         else
-            raise "Message id #{self.id} not in state for send_message"
+            raise "Message id #{id} not in state for send_message"
         end
     end
 
     # An admin function
     def resend_message
-        if ['initial_request', 'followup'].include?(self.message_type) and self.status == 'sent'
+        if ['initial_request', 'followup'].include?(message_type) and status == 'sent'
             self.status = 'ready'
             send_message('resent')
         else
-            raise "Message id #{self.id} has type '#{self.message_type}' status '#{self.status}' which resend_message can't handle"
+            raise "Message id #{id} has type '#{message_type}' status '#{status}' which resend_message can't handle"
         end
     end
 
     # Returns the text to quote the original message when sending this one
     def quoted_part_to_append_to_email
-        if self.message_type == 'followup' && !self.incoming_message_followup.nil?
-            return "\n\n-----Original Message-----\n\n" + self.incoming_message_followup.get_body_for_quoting + "\n"
+        if message_type == 'followup' && !incoming_message_followup.nil?
+            quoted = "\n\n-----Original Message-----\n\n"
+            quoted += incoming_message_followup.get_body_for_quoting
+            quoted += "\n"
         else
-            return ""
+            ""
         end
     end
 
@@ -222,8 +242,8 @@ class OutgoingMessage < ActiveRecord::Base
     end
 
     # Returns text for indexing / text display
-    def get_text_for_indexing(strip_salutation=true)
-        text = self.body.strip
+    def get_text_for_indexing(strip_salutation = true)
+        text = body.strip
 
         # Remove salutation
         text.sub!(/Dear .+,/, "") if strip_salutation
@@ -231,19 +251,20 @@ class OutgoingMessage < ActiveRecord::Base
         # Remove email addresses from display/index etc.
         self.remove_privacy_sensitive_things!(text)
 
-        return text
+        text
     end
 
     # Return body for display as HTML
     def get_body_for_html_display
-        text = self.body.strip
+        text = body.strip
         self.remove_privacy_sensitive_things!(text)
-        text = MySociety::Format.wrap_email_body_by_lines(text) # reparagraph and wrap it so is good preview of emails
+        # reparagraph and wrap it so is good preview of emails
+        text = MySociety::Format.wrap_email_body_by_lines(text)
         text = CGI.escapeHTML(text)
         text = MySociety::Format.make_clickable(text, :contract => 1)
         text.gsub!(/\[(email address|mobile number)\]/, '[<a href="/help/officers#mobiles">\1</a>]')
         text = text.gsub(/\n/, '<br>')
-        return text.html_safe
+        text.html_safe
     end
 
     # Return body for display as text
@@ -254,16 +275,16 @@ class OutgoingMessage < ActiveRecord::Base
 
     def fully_destroy
         ActiveRecord::Base.transaction do
-            info_request_event = InfoRequestEvent.find_by_outgoing_message_id(self.id)
+            info_request_event = InfoRequestEvent.find_by_outgoing_message_id(id)
             info_request_event.track_things_sent_emails.each { |a| a.destroy }
             info_request_event.user_info_request_sent_alerts.each { |a| a.destroy }
             info_request_event.destroy
-            self.destroy
+            destroy
         end
     end
 
     def purge_in_cache
-        self.info_request.purge_in_cache
+        info_request.purge_in_cache
     end
 
     def for_admin_column
@@ -273,9 +294,9 @@ class OutgoingMessage < ActiveRecord::Base
     end
 
     def xapian_reindex_after_update
-        if self.changes.include?('body')
-            for info_request_event in self.info_request_events
-                info_request_event.xapian_mark_needs_index
+        if changes.include?('body')
+            info_request_events.each do |event|
+                event.xapian_mark_needs_index
             end
         end
     end
@@ -283,15 +304,13 @@ class OutgoingMessage < ActiveRecord::Base
     private
 
     def set_default_letter
-        if self.body.nil?
-            self.body = get_default_message
-        end
+        self.body = get_default_message if body.nil?
     end
 
     def format_of_body
-        if self.body.empty? || self.body =~ /\A#{Regexp.escape(get_salutation)}\s+#{Regexp.escape(get_signoff)}/ || self.body =~ /#{Regexp.escape(get_internal_review_insert_here_note)}/
-            if self.message_type == 'followup'
-                if self.what_doing == 'internal_review'
+        if body.empty? || body =~ /\A#{Regexp.escape(get_salutation)}\s+#{Regexp.escape(get_signoff)}/ || body =~ /#{Regexp.escape(get_internal_review_insert_here_note)}/
+            if message_type == 'followup'
+                if what_doing == 'internal_review'
                     errors.add(:body, _("Please give details explaining why you want a review"))
                 else
                     errors.add(:body, _("Please enter your follow up message"))
@@ -299,16 +318,19 @@ class OutgoingMessage < ActiveRecord::Base
             elsif
                 errors.add(:body, _("Please enter your letter requesting information"))
             else
-                raise "Message id #{self.id} has type '#{self.message_type}' which validate can't handle"
+                raise "Message id #{id} has type '#{message_type}' which validate can't handle"
             end
         end
-        if self.body =~ /#{get_signoff}\s*\Z/m
+
+        if body =~ /#{get_signoff}\s*\Z/m
             errors.add(:body, _("Please sign at the bottom with your name, or alter the \"{{signoff}}\" signature", :signoff => get_signoff))
         end
-        if !MySociety::Validate.uses_mixed_capitals(self.body)
+
+        unless MySociety::Validate.uses_mixed_capitals(body)
             errors.add(:body, _('Please write your message using a mixture of capital and lower case letters. This makes it easier for others to read.'))
         end
-        if self.what_doing.nil? || !['new_information', 'internal_review', 'normal_sort'].include?(self.what_doing)
+
+        if what_doing.nil? || !['new_information', 'internal_review', 'normal_sort'].include?(what_doing)
             errors.add(:what_doing_dummy, _('Please choose what sort of reply you are making.'))
         end
     end
