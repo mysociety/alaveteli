874021b45093861733bdab1a26ba6e41a3df6e89
Valentin Aitken <bostko@gmail.com>
2014-10-28 11:50:31 +0200

    Update libs to support rails 4.1.6

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 // ...
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.datepicker
-//= require jquery.ui.position
+//= require jquery-ui/datepicker
+//= require jquery-ui/position
 //= require jquery.cookie
 //= require general
 //= require ba-throttle-debounce
