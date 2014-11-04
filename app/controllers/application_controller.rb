88d7cb6d85662dde55ea1d9cd0eb0d02a84cd858
Ian Chard <ian@mysociety.org>
2014-11-04 15:18:58 +0000

    Always try Gaze if the geoip database can't be used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -424,10 +424,8 @@ class ApplicationController < ActionController::Base
 
     def country_from_ip
         country = ""
-        if !AlaveteliConfiguration::geoip_database.empty?
-            if File.file?(AlaveteliConfiguration::geoip_database)
-                country = GeoIP.new(AlaveteliConfiguration::geoip_database).country(request.remote_ip).country_code2
-            end
+        if File.file?(AlaveteliConfiguration::geoip_database)
+            country = GeoIP.new(AlaveteliConfiguration::geoip_database).country(request.remote_ip).country_code2
         elsif !AlaveteliConfiguration::gaze_url.empty?
             begin
                 country = quietly_try_to_open("#{AlaveteliConfiguration::gaze_url}/gaze-rest?f=get_country_from_ip;ip=#{request.remote_ip}")
