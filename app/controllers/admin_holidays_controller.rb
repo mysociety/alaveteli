276c819a58a619a543a9c8292e5776587846cfeb
Louise Crow <louise.crow@gmail.com>
2014-11-28 15:49:22 +0000

    Add index method for listing existing holidays

---
+++ app/controllers/admin_holidays_controller.rb
@@ -0,0 +1,14 @@
+class AdminHolidaysController < AdminController
+
+    def index
+        get_all_holidays
+    end
+
+    private
+
+    def get_all_holidays
+        @holidays_by_year = Holiday.all.group_by { |holiday| holiday.day.year }
+        @years = @holidays_by_year.keys.sort.reverse
+    end
+
+end
---
+++ spec/controllers/admin_holidays_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe AdminHolidaysController do
+
+    describe :index do
+
+        before do
+            @holiday_one = FactoryGirl.create(:holiday, :day => Date.new(2010, 1, 1))
+            @holiday_two = FactoryGirl.create(:holiday, :day => Date.new(2011, 2, 2))
+            @holiday_three = FactoryGirl.create(:holiday, :day => Date.new(2011, 3, 3))
+        end
+
+        it 'gets a hash of holidays keyed by year' do
+           get :index
+           assigns(:holidays_by_year)[2010].should include(@holiday_one)
+           assigns(:holidays_by_year)[2011].should include(@holiday_two)
+           assigns(:holidays_by_year)[2011].should include(@holiday_three)
+        end
+
+        it 'gets a list of years with holidays' do
+            get :index
+            assigns(:years).should include(2010)
+            assigns(:years).should include(2011)
+        end
+
+        it 'renders the index template' do
+           get :index
+           expect(response).to render_template('index')
+        end
+
+    end
+end
