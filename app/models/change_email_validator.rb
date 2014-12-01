mysociety/alaveteli

commit 7a3b462f41321034cbdd2c83707f739a442e83c6

Remove duplication in ChangeEmailValidator

app/models/change_email_validator.rb
@@ -38,9 +38,7 @@ class ChangeEmailValidator
     private
 
     def password_and_format_of_email
-        if !old_email.blank? && !MySociety::Validate.is_valid_email(old_email)
-            errors.add(:old_email, _("Old email doesn't look like a valid address"))
-        end
+        check_email_is_present_and_valid(:old_email)
 
         if errors[:old_email].blank?
             if old_email.downcase != logged_in_user.email.downcase
@@ -52,8 +50,13 @@ class ChangeEmailValidator
             end
         end
 
-        if !new_email.blank? && !MySociety::Validate.is_valid_email(new_email)
-            errors.add(:new_email, _("New email doesn't look like a valid address"))
+        check_email_is_present_and_valid(:new_email)
+    end
+
+    def check_email_is_present_and_valid(email)
+        if !send(email).blank? && !MySociety::Validate.is_valid_email(send(email))
+            errors.add(email, _("#{ email.to_s.humanize } doesn't look like a valid address"))
         end
     end
+
 end

commit 04616522ace7f777dadf209b86d7e97ea97379ed

Fix for interpolation bug introduced in 7a3b462f41321034cbdd2c83707f739a442e83c6.
The string with the unsubstituted variable pattern was being made
available for translation, instead of the translatable version.

app/models/change_email_validator.rb
@@ -55,8 +55,18 @@ class ChangeEmailValidator
 
     def check_email_is_present_and_valid(email)
         if !send(email).blank? && !MySociety::Validate.is_valid_email(send(email))
-            errors.add(email, _("#{ email.to_s.humanize } doesn't look like a valid address"))
+            msg_string = check_email_is_present_and_valid_msg_string(email)
+            errors.add(email, msg_string)
         end
+    end
+
+    def check_email_is_present_and_valid_msg_string(email)
+      case email.to_sym
+      when :old_email then _("Old email doesn't look like a valid address")
+      when :new_email then _("New email doesn't look like a valid address")
+      else
+        raise "Unsupported email type #{ email }"
+      end
     end
 
     def email_belongs_to_user?(email)