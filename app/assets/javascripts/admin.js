cb53e9a0cfbb53c848c1b9c30ccd696e86f0ef36
Louise Crow <louise.crow@gmail.com>
2014-12-11 15:01:01 +0000

    Remove bootstrap libs in favour of using the bootstrap-sass gem.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -3,8 +3,8 @@
 //= require jquery.ui.tabs
 //= require jquery.ui.sortable
 //= require jquery.ui.effect-highlight
-//= require admin/bootstrap-collapse
-//= require admin/bootstrap-tab
+//= require bootstrap-collapse
+//= require bootstrap-tab
 //= require admin/admin
 //= require admin/category-order
 //= require admin/holidays
