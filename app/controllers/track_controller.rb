5b99495ef39feda47da098f896a801beafc892c5
Louise Crow <louise.crow@gmail.com>
2014-12-23 15:55:57 +0000

    Add some specs for the vote tracking action.

--- app/controllers/track_controller.rb
+++ app/controllers/track_controller.rb
@@ -216,12 +216,12 @@ class TrackController < ApplicationController
 
     # Track interest in a request from a non-logged in user
     def widget_vote
-        info_request = InfoRequest.find(params[:info_request_id])
+        @info_request = InfoRequest.find(params[:info_request_id])
         if not @user and cookies[:widget_vote]
-            wv = info_request.widget_votes.where(:cookie => cookies[:widget_vote]).first_or_create
+            wv = @info_request.widget_votes.where(:cookie => cookies[:widget_vote]).first_or_create
         end
 
-        track_thing = TrackThing.create_track_for_request(info_request)
+        track_thing = TrackThing.create_track_for_request(@info_request)
         redirect_to do_track_path(track_thing), status => :temporary_redirect
     end
 end
