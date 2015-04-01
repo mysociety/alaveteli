2e6683ca30fe12f8e6ced9fe1c88a29181f62c86
Gareth Rees <gareth@mysociety.org>
2015-04-01 09:37:35 +0100

    Bump ALAVETELI_VERSION
    Bumped two versions as 823e58dc missed this out

--- config/initializers/alaveteli.rb
+++ config/initializers/alaveteli.rb
@@ -10,7 +10,7 @@ load "debug_helpers.rb"
 load "util.rb"
 
 # Application version
-ALAVETELI_VERSION = '0.20.0.14'
+ALAVETELI_VERSION = '0.20.0.16'
 
 # Add new inflection rules using the following format
 # (all these examples are active by default):
