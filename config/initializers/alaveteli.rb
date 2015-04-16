190f8acca6e365f0369370ab1921af507281a6ea
Gareth Rees <gareth@mysociety.org>
2015-04-16 09:25:32 +0100

    Bump alaveteli version

--- config/initializers/alaveteli.rb
+++ config/initializers/alaveteli.rb
@@ -10,7 +10,7 @@ load "debug_helpers.rb"
 load "util.rb"
 
 # Application version
-ALAVETELI_VERSION = '0.21.0.16'
+ALAVETELI_VERSION = '0.21.0.17'
 
 # Add new inflection rules using the following format
 # (all these examples are active by default):
