871a3a7214ca8381bf29e47d1c79c7736f871798
Louise Crow <louise.crow@gmail.com>
2015-04-23 14:35:39 +0100

    Move existing track logic to controller

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -16,6 +16,10 @@ class WidgetsController < ApplicationController
         @track_thing = TrackThing.create_track_for_request(@info_request)
         @status = @info_request.calculate_status
         @count = @info_request.track_things.count + @info_request.widget_votes.count + 1
+
+        if @user
+            @existing_track = TrackThing.find_existing(@user, @track_thing)
+        end
         unless @user or cookies[:widget_vote]
           cookies.permanent[:widget_vote] = SecureRandom.hex(10)
         end
