c9ac70159fc4c84c601c90be45554706f83681d9
Henare Degan <henare.degan@gmail.com>
2015-04-27 15:29:44 +1000

    Add a note about tidying up the spec file

--- spec/mailers/request_mailer_spec.rb
+++ spec/mailers/request_mailer_spec.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+# TODO: Combine all these separate "describe" blocks to tidy things up
+
 describe RequestMailer, " when receiving incoming mail" do
     before(:each) do
         load_raw_emails_data
