a687b48c0ae1fe6286e5055d7c2041df191170c8
Gareth Rees <gareth@mysociety.org>
2015-04-29 11:38:43 +0100

    InfoRequest#initial_request_text minor improvement
           system      total        real
    old   0.810000   0.300000   1.110000 (  1.358172)
    new   0.970000   0.090000   1.060000 (  1.292340)

--- app/models/info_request.rb
+++ app/models/info_request.rb
@@ -804,8 +804,7 @@ public
 
     # Text from the the initial request, for use in summary display
     def initial_request_text
-        return '' if outgoing_messages.empty? # mainly for use with incomplete fixtures
-        outgoing_messages.first.get_text_for_indexing
+        outgoing_messages.first.try(:get_text_for_indexing) or ''
     end
 
     # Returns index of last event which is described or nil if none described.
