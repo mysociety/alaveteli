0c915e5e1a982513f9ded38b11f8b7b570c518e8
Louise Crow <louise.crow@gmail.com>
2015-05-22 15:54:40 +0100

    Cleanup spec.
    Move setup to spec - not in common with other specs.

--- spec/models/raw_email_spec.rb
+++ spec/models/raw_email_spec.rb
@@ -7,21 +7,22 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe User, "manipulating a raw email" do
+describe RawEmail do
 
-    before do
-        @raw_email = RawEmail.new
-        incoming_message = mock_model(IncomingMessage)
-        info_request = mock_model(InfoRequest)
-        incoming_message.stub!(:info_request).and_return(info_request)
-        @raw_email.stub!(:incoming_message).and_return(incoming_message)
-    end
+    describe :data do
 
-    it 'putting data in comes back out' do
-        @raw_email.data = "Hello, world!"
-        @raw_email.save!
-        @raw_email.reload
-        @raw_email.data.should == "Hello, world!"
+        it 'roundtrips data unchanged' do
+            raw_email = RawEmail.new
+            incoming_message = mock_model(IncomingMessage)
+            info_request = mock_model(InfoRequest)
+            incoming_message.stub!(:info_request).and_return(info_request)
+            raw_email.stub!(:incoming_message).and_return(incoming_message)
+            raw_email.data = "Hello, world!"
+            raw_email.save!
+            raw_email.reload
+            raw_email.data.should == "Hello, world!"
+        end
+
     end
 
 end
