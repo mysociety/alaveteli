c442a780f98bb698ce0d2d8202d36ced7cf48af4
Gareth Rees <gareth@mysociety.org>
2015-06-03 17:55:00 +0100

    Privatise PublicBody#reindex_requested_from
    Only used in a callback

--- app/models/public_body.rb
+++ app/models/public_body.rb
@@ -269,19 +269,6 @@ class PublicBody < ActiveRecord::Base
         "authority"
     end
 
-    # if the URL name has changed, then all requested_from: queries
-    # will break unless we update index for every event for every
-    # request linked to it
-    def reindex_requested_from
-        if changes.include?('url_name')
-            info_requests.each do |info_request|
-                info_request.info_request_events.each do |info_request_event|
-                    info_request_event.xapian_mark_needs_index
-                end
-            end
-        end
-    end
-
     # Guess home page from the request email, or use explicit override, or nil
     # if not known.
     def calculated_home_page
@@ -714,6 +701,25 @@ class PublicBody < ActiveRecord::Base
       # (This would work automatically, were publication_scheme not a
       # translated attribute)
       self.publication_scheme = "" if publication_scheme.nil?
+    end
+
+    # if the URL name has changed, then all requested_from: queries
+    # will break unless we update index for every event for every
+    # request linked to it
+    #
+    # TODO: Can't actually deprecate this because spec/script/mailin_spec.rb:28
+    # fails due to the deprecation notice output
+    def reindex_requested_from
+        # warn %q([DEPRECATION] PublicBody#reindex_requested_from will become a
+        # private method in 0.23).squish
+
+        if changes.include?('url_name')
+            info_requests.each do |info_request|
+                info_request.info_request_events.each do |info_request_event|
+                    info_request_event.xapian_mark_needs_index
+                end
+            end
+        end
     end
 
     # Methods to remove
