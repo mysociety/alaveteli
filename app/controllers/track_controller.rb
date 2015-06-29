5bf5fb7a52f83b155b5f88a3b45a354a9899e76d
Gareth Rees <gareth@mysociety.org>
2015-06-29 13:09:32 +0100

    Clear old WidgetVotes when converted to TrackThing
    When a User signs in to confirm their WidgetVote, a TrackThing is
    created. This leads to the vote being double-counted as the WidgetVote
    is not cleared.
    
    Fixes https://github.com/mysociety/alaveteli/issues/2601

--- app/controllers/track_controller.rb
+++ app/controllers/track_controller.rb
@@ -17,6 +17,13 @@ class TrackController < ApplicationController
         return atom_feed_internal if params[:feed] == 'feed'
 
         if self.track_set
+            if AlaveteliConfiguration.enable_widgets && cookies[:widget_vote]
+                @info_request.
+                    widget_votes.
+                        where(:cookie => cookies[:widget_vote]).
+                            destroy_all
+            end
+
             redirect_to request_url(@info_request)
         end
     end
