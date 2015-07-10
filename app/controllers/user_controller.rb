a5500202a69cbff0bb7cbbba43018df492f10343
Louise Crow <louise.crow@gmail.com>
2015-07-10 12:31:20 +0100

    fixup! Use unless and #present? over if ! ...#nil?

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -59,8 +59,8 @@ class UserController < ApplicationController
             @xapian_comments = nil
         end
 
-        feed_results += @xapian_requests.results.map {|x| x[:model]} unless @xapian_requests.nil?
-        feed_results += @xapian_comments.results.map {|x| x[:model]} unless @xapian_comments.nil?
+        feed_results += @xapian_requests.results.map { |x| x[:model] } if @xapian_requests
+        feed_results += @xapian_comments.results.map { |x| x[:model] } if @xapian_comments
 
         # All tracks for the user
         if @is_you
@@ -167,7 +167,7 @@ class UserController < ApplicationController
             return
         end
 
-        unless User.stay_logged_in_on_redirect?(@user) || post_redirect.circumstance == "login_as"
+        if !User.stay_logged_in_on_redirect?(@user) || post_redirect.circumstance == "login_as"
             @user = post_redirect.user
             @user.email_confirmed = true
             @user.save!
@@ -329,7 +329,7 @@ class UserController < ApplicationController
         @recipient_user = User.find(params[:id])
 
         # Banned from messaging users?
-        unless authenticated_user.nil? && !authenticated_user.can_contact_other_users?
+        if authenticated_user && !authenticated_user.can_contact_other_users?
             @details = authenticated_user.can_fail_html
             render :template => 'user/banned'
             return
@@ -636,7 +636,7 @@ class UserController < ApplicationController
 
         requests_query = 'requested_by:' + @display_user.url_name
         comments_query = 'commented_by:' + @display_user.url_name
-        unless params[:user_query].nil?
+        if params[:user_query]
           requests_query += " " + params[:user_query]
           comments_query += " " + params[:user_query]
           @match_phrase = _("{{search_results}} matching '{{query}}'", :search_results => "", :query => params[:user_query])
