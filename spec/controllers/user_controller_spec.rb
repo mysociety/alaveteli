114931c7be1bfd1aa9be9fb0fbc6c45880add61c
Louise Crow <louise.crow@gmail.com>
2015-07-10 11:58:42 +0100

    fixup! Simplifying ActiveRecord queries

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -76,12 +76,9 @@ describe UserController, "when redirecting a show request to a canonical url" do
   end
 
   it 'should not redirect a long canonical name that has a numerical suffix' do
-    User.stub!(:find).with(:first, anything).and_return(mock_model(User,
-                                                                   :url_name => 'bob_smithbob_smithbob_smithbob_s_2',
-                                                                   :name => 'Bob Smith Bob Smith Bob Smith Bob Smith',
-                                                                   :info_requests => []))
-    User.stub!(:find).with(:all, anything).and_return([])
-    get :show, :url_name => 'bob_smithbob_smithbob_smithbob_s_2'
+    user = FactoryGirl.create(:user, :name => 'Bob Smith Bob Smith Bob Smith Bob Smith')
+    second_user = FactoryGirl.create(:user, :name => 'Bob Smith Bob Smith Bob Smith Bob Smith')
+    get :show, :url_name => 'bob_smith_bob_smith_bob_smith_bo_2'
     response.should be_success
   end
 
