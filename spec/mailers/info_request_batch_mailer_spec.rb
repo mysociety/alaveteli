cd703f18b10693f3f34cd65f416b6a02f2f4a372
Gareth Rees <gareth@mysociety.org>
2015-07-15 17:53:20 +0100

    2-space mailer specs

--- spec/mailers/info_request_batch_mailer_spec.rb
+++ spec/mailers/info_request_batch_mailer_spec.rb
@@ -5,13 +5,13 @@ describe InfoRequestBatchMailer do
 
   describe 'when sending batch sent notification' do
 
-        before do
-            @user = FactoryGirl.create(:user)
-            @info_request_batch = FactoryGirl.create(:info_request_batch)
-            @public_body = FactoryGirl.create(:public_body)
-            @unrequestable = [@public_body]
-            @mail = InfoRequestBatchMailer.batch_sent(@info_request_batch, @unrequestable, @user)
-        end
+    before do
+      @user = FactoryGirl.create(:user)
+      @info_request_batch = FactoryGirl.create(:info_request_batch)
+      @public_body = FactoryGirl.create(:public_body)
+      @unrequestable = [@public_body]
+      @mail = InfoRequestBatchMailer.batch_sent(@info_request_batch, @unrequestable, @user)
+    end
 
     it 'renders the subject' do
       @mail.subject.should == 'Your batch request "Example title" has been sent'
