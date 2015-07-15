6691f97656f1aff03b3da8cdf42c94eb05d9fc07
Gareth Rees <gareth@mysociety.org>
2015-07-15 17:51:21 +0100

    2-space mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -7,25 +7,24 @@
 
 require 'action_mailer/version'
 class ApplicationMailer < ActionMailer::Base
-    # Include all the functions views get, as emails call similar things.
-    helper :application
-    include MailerHelper
+  # Include all the functions views get, as emails call similar things.
+  helper :application
+  include MailerHelper
 
-    # This really should be the default - otherwise you lose any information
-    # about the errors, and have to do error checking on return codes.
-    self.raise_delivery_errors = true
+  # This really should be the default - otherwise you lose any information
+  # about the errors, and have to do error checking on return codes.
+  self.raise_delivery_errors = true
 
-    def blackhole_email
-        AlaveteliConfiguration::blackhole_prefix+"@"+AlaveteliConfiguration::incoming_email_domain
-    end
+  def blackhole_email
+    AlaveteliConfiguration::blackhole_prefix+"@"+AlaveteliConfiguration::incoming_email_domain
+  end
 
-    # URL generating functions are needed by all controllers (for redirects),
-    # views (for links) and mailers (for use in emails), so include them into
-    # all of all.
-    include LinkToHelper
+  # URL generating functions are needed by all controllers (for redirects),
+  # views (for links) and mailers (for use in emails), so include them into
+  # all of all.
+  include LinkToHelper
 
-    # Site-wide access to configuration settings
-    include ConfigHelper
+  # Site-wide access to configuration settings
+  include ConfigHelper
 
 end
-
