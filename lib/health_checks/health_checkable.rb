fdbdfd942b459614a8dd2f50cc1368b8f976348a
Gareth Rees <gareth@mysociety.org>
2015-07-17 12:49:21 +0100

    2-space lib/health_checks

--- lib/health_checks/health_checkable.rb
+++ lib/health_checks/health_checkable.rb
@@ -1,25 +1,25 @@
 # -*- encoding : utf-8 -*-
 module HealthChecks
-    module HealthCheckable
+  module HealthCheckable
 
-        attr_accessor :failure_message, :success_message
+    attr_accessor :failure_message, :success_message
 
-        def initialize(args = {})
-            self.failure_message = args.fetch(:failure_message) { _('Failed') }
-            self.success_message = args.fetch(:success_message) { _('Success') }
-        end
-
-        def name
-            self.class.to_s
-        end
-
-        def ok?
-            raise NotImplementedError
-        end
-
-        def message
-            ok? ? success_message : failure_message
-        end
-
+    def initialize(args = {})
+      self.failure_message = args.fetch(:failure_message) { _('Failed') }
+      self.success_message = args.fetch(:success_message) { _('Success') }
     end
+
+    def name
+      self.class.to_s
+    end
+
+    def ok?
+      raise NotImplementedError
+    end
+
+    def message
+      ok? ? success_message : failure_message
+    end
+
+  end
 end
--- lib/health_checks/health_checks.rb
+++ lib/health_checks/health_checks.rb
@@ -2,37 +2,37 @@
 require 'health_checkable'
 
 Dir[File.dirname(__FILE__) + '/checks/*.rb'].each do |file|
-    require file
+  require file
 end
 
 module HealthChecks
-    extend self
+  extend self
 
-    def all
-        @checks ||= []
+  def all
+    @checks ||= []
+  end
+
+  def add(check)
+    if assert_valid_check(check)
+      all << check
+      check
+    else
+      false
     end
+  end
 
-    def add(check)
-        if assert_valid_check(check)
-            all << check
-            check
-        else
-            false
-        end
-    end
+  def each(&block)
+    all.each(&block)
+  end
 
-    def each(&block)
-      all.each(&block)
-    end
+  def ok?
+    all.all? { |check| check.ok? }
+  end
 
-    def ok?
-       all.all? { |check| check.ok? }
-    end
+  private
 
-    private
-
-    def assert_valid_check(check)
-        check.respond_to?(:ok?)
-    end
+  def assert_valid_check(check)
+    check.respond_to?(:ok?)
+  end
 
 end
