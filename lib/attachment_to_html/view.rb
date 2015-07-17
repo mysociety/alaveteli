694a6beeccc76c8a1813fbd0ffa1bfb80aa3ad64
Gareth Rees <gareth@mysociety.org>
2015-07-17 12:48:10 +0100

    2-space lib/attachment_to_html

--- lib/attachment_to_html/view.rb
+++ lib/attachment_to_html/view.rb
@@ -1,40 +1,40 @@
 # -*- encoding : utf-8 -*-
 module AttachmentToHTML
-    class View < ERB
+  class View < ERB
 
-        def self.template
-            @template || "#{ File.dirname(__FILE__) }/template.html.erb"
-        end
-
-        def self.template=(path)
-            @template = path
-        end
-
-        attr_accessor :title, :body, :template, :wrapper
-
-        def initialize(adapter, opts = {})
-            self.title    = adapter.title
-            self.body     = adapter.body
-            self.template = opts.fetch(:template, self.class.template)
-            self.wrapper  = opts.fetch(:wrapper, 'wrapper')
-            super(File.read(template))
-        end
-
-        def render(&block)
-            instance_eval(&block) if block_given?
-            result(binding)
-        end
-
-        def content_for(area)
-            send(area) if respond_to?(area)
-        end
-
-        private
-
-        def inject_content(area, &block)
-          instance_variable_set("@#{ area }".to_sym, block.call)
-          self.class.send(:attr_accessor, area)
-        end
-
+    def self.template
+      @template || "#{ File.dirname(__FILE__) }/template.html.erb"
     end
+
+    def self.template=(path)
+      @template = path
+    end
+
+    attr_accessor :title, :body, :template, :wrapper
+
+    def initialize(adapter, opts = {})
+      self.title    = adapter.title
+      self.body     = adapter.body
+      self.template = opts.fetch(:template, self.class.template)
+      self.wrapper  = opts.fetch(:wrapper, 'wrapper')
+      super(File.read(template))
+    end
+
+    def render(&block)
+      instance_eval(&block) if block_given?
+      result(binding)
+    end
+
+    def content_for(area)
+      send(area) if respond_to?(area)
+    end
+
+    private
+
+    def inject_content(area, &block)
+      instance_variable_set("@#{ area }".to_sym, block.call)
+      self.class.send(:attr_accessor, area)
+    end
+
+  end
 end
