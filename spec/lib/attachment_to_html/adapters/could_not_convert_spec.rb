ecf9be077aca205fcfd2c42ce52751102f0d3300
Gareth Rees <gareth@mysociety.org>
2015-07-17 13:04:04 +0100

    2-space spec/lib

--- spec/lib/attachment_to_html/adapters/could_not_convert_spec.rb
+++ spec/lib/attachment_to_html/adapters/could_not_convert_spec.rb
@@ -3,35 +3,35 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
 
 describe AttachmentToHTML::Adapters::CouldNotConvert do
 
-    let(:attachment) { FactoryGirl.build(:pdf_attachment) }
-    let(:adapter) do
-        AttachmentToHTML::Adapters::CouldNotConvert.new(attachment)
+  let(:attachment) { FactoryGirl.build(:pdf_attachment) }
+  let(:adapter) do
+    AttachmentToHTML::Adapters::CouldNotConvert.new(attachment)
+  end
+
+  describe :title do
+
+    it 'uses the attachment filename for the title' do
+      adapter.title.should == attachment.display_filename
     end
 
-    describe :title do
+  end
 
-        it 'uses the attachment filename for the title' do
-            adapter.title.should == attachment.display_filename
-        end
- 
+  describe :body do
+
+    it 'contains a message asking the user to download the file directly' do
+      expected = "<p>Sorry, we were unable to convert this file to HTML. " \
+        "Please use the download link at the top right.</p>"
+      adapter.body.should == expected
     end
 
-    describe :body do
+  end
 
-        it 'contains a message asking the user to download the file directly' do
-            expected = "<p>Sorry, we were unable to convert this file to HTML. " \
-                       "Please use the download link at the top right.</p>"
-            adapter.body.should == expected
-        end
+  describe :success? do
 
+    it 'is always true' do
+      adapter.success?.should be_true
     end
 
-    describe :success? do
-
-        it 'is always true' do
-            adapter.success?.should be_true
-        end
-
-    end
+  end
 
 end
