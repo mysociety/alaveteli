a2ff804d574fe6549f34b39862e6ebdbee0d79c7
Gareth Rees <gareth@mysociety.org>
2015-07-17 13:04:55 +0100

    2-space spec/support

--- spec/support/load_file_fixtures.rb
+++ spec/support/load_file_fixtures.rb
@@ -1,9 +1,9 @@
 # -*- encoding : utf-8 -*-
 def file_fixture_name(file_name)
-    return File.join(RSpec.configuration.fixture_path, "files", file_name)
+  return File.join(RSpec.configuration.fixture_path, "files", file_name)
 end
 
 def load_file_fixture(file_name)
-    file_name = file_fixture_name(file_name)
-    return File.open(file_name, 'rb') { |f| f.read }
+  file_name = file_fixture_name(file_name)
+  return File.open(file_name, 'rb') { |f| f.read }
 end
