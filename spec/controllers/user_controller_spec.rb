a2f0d949131ba0295b0c8a7092048217ad19893e
Louise Crow <louise.crow@gmail.com>
2015-08-07 13:46:24 +0100

    fixup! Add test for uniqueness, slightly more idiomatic conversion method.

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -779,11 +779,11 @@ describe UserController, "when viewing the wall" do
     user.receive_email_alerts.should_not == true
   end
 
-    it 'should not show duplicate feed results' do
-        user = users(:silly_name_user)
-        session[:user_id] = user.id
-        get :wall, :url_name => user.url_name
-        assigns[:feed_results].uniq.should == assigns[:feed_results]
-    end
+  it 'should not show duplicate feed results' do
+    user = users(:silly_name_user)
+    session[:user_id] = user.id
+    get :wall, :url_name => user.url_name
+    assigns[:feed_results].uniq.should == assigns[:feed_results]
+  end
 
 end
