ccc749bc45d4a19d820241a03a0a1df19845925b
Louise Crow <louise.crow@gmail.com>
2015-08-11 16:56:55 +0100

    Add some convenience methods for getting messages from the holding pen.

--- spec/integration/admin_spec.rb
+++ spec/integration/admin_spec.rb
@@ -30,13 +30,14 @@ describe "When administering the site" do
   it "allows redelivery of an incoming message to a closed request" do
     ir = info_requests(:fancy_dog_request)
     close_request(ir)
-    expect(InfoRequest.holding_pen_request.incoming_messages.length).to eq(0)
+    expect(holding_pen_messages.length).to eq(0)
     expect(ir.incoming_messages.length).to eq(1)
     receive_incoming_mail('incoming-request-plain.email', ir.incoming_email, "frob@nowhere.com")
-    expect(InfoRequest.holding_pen_request.incoming_messages.length).to eq(1)
-    new_im = InfoRequest.holding_pen_request.incoming_messages[0]
+    expect(holding_pen_messages.length).to eq(1)
+    new_im = holding_pen_messages[0]
     expect(ir.incoming_messages.length).to eq(1)
     post_params = { 'url_title' => ir.url_title }
+
     using_session(@admin) do
       visit edit_admin_incoming_message_path(new_im)
       fill_in('Redeliver message to one or more other requests',
@@ -47,7 +48,7 @@ describe "When administering the site" do
     ir = InfoRequest.find_by_url_title(ir.url_title)
     expect(ir.incoming_messages.length).to eq(2)
 
-    expect(InfoRequest.holding_pen_request.incoming_messages.length).to eq(0)
+    expect(holding_pen_messages.length).to eq(0)
   end
 
   it "allows redelivery of an incoming message to more than one request" do
@@ -59,9 +60,9 @@ describe "When administering the site" do
     expect(ir2.incoming_messages.length).to eq(1)
 
     receive_incoming_mail('incoming-request-plain.email', ir1.incoming_email, "frob@nowhere.com")
-    expect(InfoRequest.holding_pen_request.incoming_messages.length).to eq(1)
+    expect(holding_pen_messages.length).to eq(1)
 
-    new_im = InfoRequest.holding_pen_request.incoming_messages[0]
+    new_im = holding_pen_messages[0]
 
 
     using_session(@admin) do
@@ -76,7 +77,7 @@ describe "When administering the site" do
     expect(ir1.incoming_messages.length).to eq(2)
     ir2.reload
     expect(ir2.incoming_messages.length).to eq(2)
-    expect(InfoRequest.holding_pen_request.incoming_messages.length).to eq(0)
+    expect(holding_pen_messages.length).to eq(0)
   end
 
   describe 'when administering the holding pen' do
@@ -89,7 +90,7 @@ describe "When administering the site" do
                             info_request.incoming_email,
                             "frob@nowhere.com")
       using_session(@admin) do
-        visit admin_raw_email_path InfoRequest.holding_pen_request.get_last_public_response.raw_email
+        visit admin_raw_email_path last_holding_pen_mail
         expect(page).to have_content "Only the authority can reply to this request"
       end
     end
@@ -100,10 +101,10 @@ describe "When administering the site" do
                                         :handle_rejected_responses => 'holding_pen')
       mail_to = "request-#{info_request.id}-asdfg@example.com"
       receive_incoming_mail('incoming-request-plain.email', mail_to)
-      interesting_email = InfoRequest.holding_pen_request.get_last_public_response.raw_email
+      interesting_email = last_holding_pen_mail
       # now we add another message to the queue, which we're not interested in
       receive_incoming_mail('incoming-request-plain.email', ir.incoming_email, "")
-      expect(InfoRequest.holding_pen_request.incoming_messages.length).to eq(2)
+      expect(holding_pen_messages.length).to eq(2)
       using_session(@admin) do
         visit admin_raw_email_path interesting_email
         expect(page).to have_content "Could not identify the request"
