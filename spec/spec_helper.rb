d5333f08952fc1ac5bf258d2109b7d564a0d6982
Louise Crow <louise.crow@gmail.com>
2015-08-13 14:19:58 +0100

    Remove deprecated require.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,7 +31,6 @@ Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  require 'rspec/autorun'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
