68ba81598d9b622b63f520626ba00bf2605cf405
Louise Crow <louise.crow@gmail.com>
2015-09-01 14:47:05 +0100

    Don't allow the caching of a redirect with a token.
    The token needs to be unique to the current request as if the user
    signs in using it, it is used to retrieve their account on
    confirmation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -248,6 +248,13 @@ class ApplicationController < ActionController::Base
             post_redirect = PostRedirect.new(:uri => request.request_uri, :post_params => params,
                 :reason_params => reason_params)
             post_redirect.save!
+      # Make sure this redirect does not get cached - it only applies to this user.
+      # HTTP 1.1
+      headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+      # HTTP 1.0
+      headers['Pragma'] = 'no-cache'
+      # Proxies
+      headers['Expires'] = '0'
             # 'modal' controls whether the sign-in form will be displayed in the typical full-blown
             # page or on its own, useful for pop-ups
             redirect_to signin_url(:token => post_redirect.token, :modal => params[:modal])
