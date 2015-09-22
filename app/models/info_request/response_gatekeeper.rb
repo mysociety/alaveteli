6b1dcd108b9648e5eb6b7d7f524ba91569b9d3e6
Gareth Rees <gareth@mysociety.org>
2015-09-22 11:34:54 +0100

    Extract ResponseGatekeeper
    Extract from info_request.rb and add specs

---
+++ app/models/info_request/response_gatekeeper.rb
@@ -0,0 +1,17 @@
+# -*- encoding : utf-8 -*-
+class InfoRequest
+  module ResponseGatekeeper
+    class UnknownResponseGatekeeperError < ArgumentError ; end
+
+    SPECIALIZED_CLASSES = { 'nobody' => Nobody,
+                            'anybody' => Base,
+                            'authority_only' => AuthorityOnly }
+
+    def self.for(name, info_request)
+      SPECIALIZED_CLASSES.fetch(name).new(info_request)
+      rescue KeyError
+        raise UnknownResponseGatekeeperError,
+              "Unknown allow_new_responses_from '#{ name }'"
+    end
+  end
+end
