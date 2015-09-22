74e71e67d6d466c048f5cd252a47f24ec3e3d396
Gareth Rees <gareth@mysociety.org>
2015-09-22 14:06:38 +0100

    Extract ResponseRejection::HoldingPen

--- app/models/info_request.rb
+++ app/models/info_request.rb
@@ -1308,23 +1308,6 @@ class InfoRequest < ActiveRecord::Base
   module ResponseRejection
     class UnknownResponseRejectionError < ArgumentError ; end
 
-    class HoldingPen < Base
-      attr_reader :holding_pen
-
-      def initialize(info_request, email, raw_email_data)
-        super
-        @holding_pen = InfoRequest.holding_pen_request
-      end
-
-      def reject(reason = nil)
-        if info_request == holding_pen
-          false
-        else
-          holding_pen.receive(email, raw_email_data, false, reason)
-        end
-      end
-    end
-
     SPECIALIZED_CLASSES = { 'bounce' => Bounce,
                             'holding_pen' => HoldingPen,
                             'blackhole' => Base,
