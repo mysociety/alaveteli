386d2c1f8890b5b69705088d0cdd4dd1385622bb
Gareth Rees <gareth@mysociety.org>
2015-10-16 09:54:29 +0100

    Use SQL to reverse InfoRequestEvent#sibling_events

--- app/models/info_request_event.rb
+++ app/models/info_request_event.rb
@@ -120,7 +120,7 @@ class InfoRequestEvent < ActiveRecord::Base
   end
 
   def latest_variety
-    sibling_events.reverse.each do |event|
+    sibling_events(:reverse => true).each do |event|
       if !event.variety.nil? and !event.variety.empty?
         return event.variety
       end
@@ -128,7 +128,7 @@ class InfoRequestEvent < ActiveRecord::Base
   end
 
   def latest_status
-    sibling_events.reverse.each do |event|
+    sibling_events(:reverse => true).each do |event|
       if !event.calculated_state.nil? and !event.calculated_state.empty?
         return event.calculated_state
       end
@@ -440,8 +440,9 @@ class InfoRequestEvent < ActiveRecord::Base
 
   private
 
-  def sibling_events
-    self.class.where(:info_request_id => info_request_id).order('created_at')
+  def sibling_events(opts = {})
+    order = opts[:reverse] ? 'created_at DESC' : 'created_at'
+    events = self.class.where(:info_request_id => info_request_id).order(order)
   end
 
 end
