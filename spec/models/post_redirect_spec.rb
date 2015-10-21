b661c30c565f92954bd0704a46b1a4c6d6e6b4ff
Gareth Rees <gareth@mysociety.org>
2015-10-21 13:50:02 +0100

    Add validation to PostRedirect#circumstance

--- spec/models/post_redirect_spec.rb
+++ spec/models/post_redirect_spec.rb
@@ -17,6 +17,19 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+describe PostRedirect do
+
+  describe '#valid?' do
+
+    it 'is false if an invalid circumstance is provided' do
+      pr = PostRedirect.new(:circumstance => 'invalid')
+      expect(pr).to_not be_valid
+    end
+
+  end
+
+end
+
 describe PostRedirect, " when constructing" do
   before do
   end
