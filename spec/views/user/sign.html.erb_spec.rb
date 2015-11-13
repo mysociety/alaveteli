e4f0ace7a5a46707024e95f2cf369ebcd960924b
Louise Crow <louise.crow@gmail.com>
2015-11-13 14:20:41 +0000

    Implementation of @lizconlan's original fix for #2736.
    Looking at this again, I think I was wrong in pushing back on this
    original solution. The problem is that strings containing quotes etc,
    interpolated into translated strings, get html escaped and marked as
    `html_safe`. This is fine when they are immediately displayed in a
    template as the `html_safe` marker means they don't automatically get
    escaped again. However, when the resulting strings are stored in a `PostRedirect`
    `reason_params` field as YAML and then pulled out again for display
    on the signup page, the html_safe attribute is lost, so the string
    is double escaped.
    
    The `web` param of a track is only ever displayed via a `PostRedirect`
    on the `sign` template, I think it's better to mark interpolated values
    as safe originally, so that the whole string gets escaped in the template
    like other non-translated strings. This won't fix any existing `PostRedirects`,
    but these will be deleted in 3 months. It doesn't require us to make
    special use of `html_entities`, so is more consistent with what's going
    on in general.
    
    Sorry for the bad review @lizconlan!

--- spec/views/user/sign.html.erb_spec.rb
+++ spec/views/user/sign.html.erb_spec.rb
@@ -4,23 +4,22 @@ require File.expand_path(File.join('..', '..', '..', 'spec_helper'), __FILE__)
 describe 'user/sign' do
   describe 'when a not logged in user is redirected while trying to track a request' do
     before do
-      html_title = "test&#x27;s &quote;title&quote; of many HTML tags &amp;c"
-      @rendered_title = 'test\'s &quote;title&quote; of many HTML tags &amp;c'
-
+      html_title = "test's \"title\" of many HTML tags &c"
+      @rendered_title = 'test&#x27;s &quot;title&quot; of many HTML tags &amp;c'
       request = FactoryGirl.create(:info_request, :title => html_title)
       tracker = FactoryGirl.create(:request_update_track,
                                    :info_request => request,
                                    :track_medium => 'email_daily',
                                    :track_query => 'test')
       redirect = PostRedirect.create(:uri => '/request/test',
-                                      :post_params => {},
-                                      :reason_params => tracker.params)
+                                     :post_params => {},
+                                     :reason_params => tracker.params)
       assign :post_redirect, redirect
     end
 
     it 'should show the first form for describing the state of the request' do
       render
-      expect(response).to have_content("To follow the request '#{@rendered_title}'")
+      expect(response).to match("To follow the request &#x27;#{@rendered_title}&#x27;")
     end
   end
 
