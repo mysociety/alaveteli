b248af79eecc6a2ff52f603c2e7cd6961733fb23
lizconlan <liz@mysociety.org>
2015-12-10 19:06:24 +0000

    Move Request#show_response to new Response controller

--- spec/controllers/request_controller_spec.rb
+++ spec/controllers/request_controller_spec.rb
@@ -55,12 +55,18 @@ describe RequestController, "when changing things that appear on the request pag
     new_comment = info_requests(:fancy_dog_request).add_comment('I also love making annotations.', users(:bob_smith_user))
     expect(PurgeRequest.all.first.model_id).to eq(ir.id)
   end
+
   it "should purge the downstream cache when a followup is made" do
     session[:user_id] = users(:bob_smith_user).id
     ir = info_requests(:fancy_dog_request)
-    post :show_response, :outgoing_message => { :body => "What a useless response! You suck.", :what_doing => 'normal_sort' }, :id => ir.id, :submitted_followup => 1
+    outgoing = FactoryGirl.create(:outgoing_message, :info_request_id => ir.id,
+                                                     :status => 'ready',
+                                                     :message_type => 'followup',
+                                                     :body => "What a useless response! You suck.",
+                                                     :what_doing => 'normal_sort')
     expect(PurgeRequest.all.first.model_id).to eq(ir.id)
   end
+
   it "should purge the downstream cache when the request is categorised" do
     ir = info_requests(:fancy_dog_request)
     ir.set_described_state('waiting_clarification')
@@ -1236,82 +1242,7 @@ describe RequestController, "when making a new request" do
 
 end
 
-describe RequestController, "when viewing an individual response for reply/followup" do
-  render_views
 
-  before(:each) do
-    load_raw_emails_data
-  end
-
-  it "should ask for login if you are logged in as wrong person" do
-    session[:user_id] = users(:silly_name_user).id
-    get :show_response, :id => info_requests(:fancy_dog_request).id, :incoming_message_id => incoming_messages(:useless_incoming_message)
-    expect(response).to render_template('user/wrong_user')
-  end
-
-  it "should show the response if you are logged in as right person" do
-    session[:user_id] = users(:bob_smith_user).id
-    get :show_response, :id => info_requests(:fancy_dog_request).id, :incoming_message_id => incoming_messages(:useless_incoming_message)
-    expect(response).to render_template('show_response')
-  end
-
-  it "should offer the opportunity to reply to the main address" do
-    session[:user_id] = users(:bob_smith_user).id
-    get :show_response, :id => info_requests(:fancy_dog_request).id, :incoming_message_id => incoming_messages(:useless_incoming_message)
-    expect(response.body).to have_css("div#other_recipients ul li", :text => "the main FOI contact address for")
-  end
-
-  it "should offer an opportunity to reply to another address" do
-    session[:user_id] = users(:bob_smith_user).id
-    ir = info_requests(:fancy_dog_request)
-    ir.allow_new_responses_from = "anybody"
-    ir.save!
-    receive_incoming_mail('incoming-request-plain.email', ir.incoming_email, "Frob <frob@bonce.com>")
-    get :show_response, :id => ir.id, :incoming_message_id => incoming_messages(:useless_incoming_message)
-    expect(response.body).to have_css("div#other_recipients ul li", :text => "Frob")
-  end
-
-  context 'when a request is hidden' do
-
-    before do
-      ir = info_requests(:fancy_dog_request)
-      ir.prominence = 'hidden'
-      ir.save!
-
-      session[:user_id] = users(:bob_smith_user).id
-    end
-
-    it "should not show individual responses, even if request owner" do
-      get :show_response, :id => info_requests(:fancy_dog_request).id, :incoming_message_id => incoming_messages(:useless_incoming_message)
-      expect(response).to render_template('request/hidden')
-    end
-
-    it 'should respond to a json request for a hidden request with a 403 code and no body' do
-      get :show_response, :id => info_requests(:fancy_dog_request).id,
-        :incoming_message_id => incoming_messages(:useless_incoming_message),
-        :format => 'json'
-
-      expect(response.code).to eq('403')
-    end
-
-  end
-
-  describe 'when viewing a response for an external request' do
-
-    it 'should show a message saying that external requests cannot be followed up' do
-      get :show_response, :id => info_requests(:external_request).id
-      expect(response).to render_template('request/followup_bad')
-      expect(assigns[:reason]).to eq('external')
-    end
-
-    it 'should be successful' do
-      get :show_response, :id => info_requests(:external_request).id
-      expect(response).to be_success
-    end
-
-  end
-
-end
 
 describe RequestController, "when classifying an information request" do
 
