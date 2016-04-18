72a67affe5fa95861f1bf42428b07a211ba5b146
Gareth Rees <gareth@garethrees.co.uk>
2016-04-18 17:48:59 +0100

    Add MailServerLogsController#index

---
+++ app/controllers/mail_server_logs_controller.rb
@@ -0,0 +1,34 @@
+# -*- encoding : utf-8 -*-
+class MailServerLogsController < ApplicationController
+  before_filter :set_subject, :check_prominence
+
+  def index
+    @title =
+      case @subject.class.to_s
+      when 'OutgoingMessage'
+        _('Mail Server Logs for Outgoing Message #{{id}}', :id => @subject.id)
+      end
+    @mail_server_logs = @subject.mail_server_logs
+
+    respond_to do |format|
+      format.html
+      format.text { render :text => @mail_server_logs.to_s }
+    end
+  end
+
+  protected
+
+  def set_subject
+    @subject =
+      if params[:outgoing_message_id]
+        OutgoingMessage.find(params[:outgoing_message_id])
+      end
+  end
+
+  def check_prominence
+    unless @subject.user_can_view?(authenticated_user)
+      return render_hidden('request/_hidden_correspondence',
+                           :locals => { :message => @subject })
+    end
+  end
+end
