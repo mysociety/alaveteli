16330949fad27947c551c3d1d142c3e9a0bb57aa
Louise Crow <louise.crow@gmail.com>
2016-04-28 13:53:05 +0100

    Add specs for AdminRawEmailController#show

--- spec/controllers/admin_raw_email_controller_spec.rb
+++ spec/controllers/admin_raw_email_controller_spec.rb
@@ -15,6 +15,57 @@ describe AdminRawEmailController do
         get :show, :id => @raw_email.id
       end
 
+      context 'when showing a message with a "From" address in the holding pen' do
+
+        before do
+          @public_body = FactoryGirl.create(:public_body,
+                                            :request_email => 'body@example.uk')
+          @info_request = FactoryGirl.create(:info_request)
+          raw_email_data = <<-EOF.strip_heredoc
+          From: bob@example.uk
+          To: #{@info_request.incoming_email}
+          Subject: Basic Email
+          Hello, World
+          EOF
+          @incoming_message = FactoryGirl.create(
+            :plain_incoming_message,
+            :info_request => InfoRequest.holding_pen_request,
+          )
+          @incoming_message.raw_email.data = raw_email_data
+          @incoming_message.raw_email.save!
+          @info_request_event = FactoryGirl.create(
+            :info_request_event,
+            :event_type => 'response',
+            :info_request => InfoRequest.holding_pen_request,
+            :incoming_message => @incoming_message,
+            :params => {:rejected_reason => 'Too dull'}
+          )
+        end
+
+        it 'assigns public bodies that match the "From" domain' do
+          get :show, :id => @incoming_message.raw_email.id
+          expect(assigns[:public_bodies]).to eq [@public_body]
+        end
+
+        it 'assigns info requests based on the hash' do
+          get :show, :id => @incoming_message.raw_email.id
+          expect(assigns[:info_requests]).to eq [@info_request]
+        end
+
+        it 'assigns a reason why the message is in the holding pen' do
+          get :show, :id => @incoming_message.raw_email.id
+          expect(assigns[:rejected_reason]).to eq 'Too dull'
+        end
+
+        it 'assigns a default reason if no reason is given' do
+          @info_request_event.params_yaml = {}
+          @info_request_event.save!
+          get :show, :id => @incoming_message.raw_email.id
+          expect(assigns[:rejected_reason]).to eq 'unknown reason'
+        end
+
+      end
+
     end
 
     describe 'text version' do
