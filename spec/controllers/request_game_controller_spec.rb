4ef357a27ac492e4592461cf1ac0563444787e40
Louise Crow <louise.crow@gmail.com>
2016-05-03 19:14:12 +0100

    Remove unneeded setup.

--- spec/controllers/request_game_controller_spec.rb
+++ spec/controllers/request_game_controller_spec.rb
@@ -1,14 +1,14 @@
 # -*- encoding : utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe RequestGameController, "when playing the game" do
-  before(:each) do
-    load_raw_emails_data
-  end
+describe RequestGameController do
 
-  it "should show the game homepage" do
-    get :play
-    expect(response).to render_template('play')
+  describe "GET play" do
+
+    it "shows the game homepage" do
+      get :play
+      expect(response).to render_template('play')
+    end
   end
 end
 
