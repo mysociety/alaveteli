2884c3b5d9f9bfb79333c2f4243a1272c58ae84e
Louise Crow <louise.crow@gmail.com>
2016-05-05 14:19:24 +0100

    Replace hash-based finder.

--- db/migrate/065_add_comments_to_user_track.rb
+++ db/migrate/065_add_comments_to_user_track.rb
@@ -2,8 +2,7 @@
 class AddCommentsToUserTrack < ActiveRecord::Migration
   def self.up
     TrackThing.update_all "track_query = replace(track_query, 'variety:sent ', '') where track_type in ('public_body_updates', 'user_updates')"
-    track_things = TrackThing.find(:all, :conditions => [ "track_type = 'user_updates'" ])
-    for track_thing in track_things
+    TrackThing.where(:track_type => 'user_updates').each do |track_thing|
       track_thing.track_query = track_thing.track_query.gsub(/^requested_by:([^\s]+)$/, "requested_by:\\1 OR commented_by:\\1")
       track_thing.save!
     end
