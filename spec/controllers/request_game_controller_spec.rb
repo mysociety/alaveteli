ecc45ed3b8c3563ec685bacc4bb55c20a13c5407
Louise Crow <louise.crow@gmail.com>
2016-05-20 13:55:46 +0100

    Replace hash-based finders in request_game_controller.

--- spec/controllers/request_game_controller_spec.rb
+++ spec/controllers/request_game_controller_spec.rb
@@ -9,6 +9,24 @@ describe RequestGameController do
       get :play
       expect(response).to render_template('play')
     end
+
+    it 'assigns three old unclassified requests' do
+      InfoRequest.destroy_all
+      requests = []
+      3.times do
+        requests << FactoryGirl.create(:old_unclassified_request)
+      end
+      get :play
+      expect(assigns[:requests]).to match_array(requests)
+    end
+
+    it 'assigns the number of unclassified requests' do
+      InfoRequest.destroy_all
+      FactoryGirl.create(:old_unclassified_request)
+      get :play
+      expect(assigns[:missing]).to eq(1)
+    end
+
   end
 end
 
