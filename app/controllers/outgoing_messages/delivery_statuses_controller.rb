91b9473c429c2526e722af3e8b04642cc2633d33
Gareth Rees <gareth@garethrees.co.uk>
2016-06-10 14:59:15 +0100

    Set whether to show logs to a user
    Only request owners and admins should be able to see mail server logs

--- app/controllers/outgoing_messages/delivery_statuses_controller.rb
+++ app/controllers/outgoing_messages/delivery_statuses_controller.rb
@@ -6,8 +6,12 @@ class OutgoingMessages::DeliveryStatusesController < ApplicationController
     @title = _('Delivery Status for Outgoing Message #{{id}}',
                :id => @outgoing_message.id)
 
-    @mail_server_logs = @outgoing_message.mail_server_logs.map do |log|
-      log.line(:redact_idhash => !@user.super?)
+    @show_mail_server_logs = @outgoing_message.is_owning_user?(@user)
+
+    if @show_mail_server_logs
+      @mail_server_logs = @outgoing_message.mail_server_logs.map do |log|
+        log.line(:redact_idhash => !@user.super?)
+      end
     end
 
     @delivery_status = @outgoing_message.delivery_status
@@ -22,7 +26,7 @@ class OutgoingMessages::DeliveryStatusesController < ApplicationController
   end
 
   def check_prominence
-    unless @outgoing_message.is_owning_user?(@user)
+    unless @outgoing_message.user_can_view?(@user)
       return render_hidden('request/_hidden_correspondence',
                            :locals => { :message => @outgoing_message })
     end
