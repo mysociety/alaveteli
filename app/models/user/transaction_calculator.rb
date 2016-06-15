ec933a9ffdbece42db60d01e34859a244dc0ed78
Gareth Rees <gareth@garethrees.co.uk>
2016-06-15 16:28:24 +0100

    Add User::TransactionCalculator
    Decorates a User with additional methods to count transactions.

---
+++ app/models/user/transaction_calculator.rb
@@ -0,0 +1,77 @@
+# -*- encoding : utf-8 -*-
+class User
+  class TransactionCalculator
+    DEFAULT_TRANSACTION_ASSOCIATIONS = [
+      :comments,
+      :info_requests,
+      :public_body_change_requests,
+      :request_classifications,
+      :track_things
+    ]
+
+    attr_reader :transaction_associations
+    attr_reader :user
+
+    def initialize(user, options = {})
+      @user = user
+      @transaction_associations =
+        options.
+          fetch(:transaction_associations) { DEFAULT_TRANSACTION_ASSOCIATIONS }
+
+      transaction_associations.each do |assoc|
+        unless user.respond_to?(assoc)
+          raise NoMethodError, "#{ user } does not respond to `#{ assoc }'"
+        end
+      end
+    end
+
+    # Public: Sum of the total transactions made by the User
+    #
+    # filter - Optionally filter by created_at during the Symbol named range or
+    #          Range (default: :current)
+    #
+    # Returns an Integer
+    # Raises ArgumentError if the argument is an invalid named range or
+    # or otherwise invalid
+    def total(filter = :current)
+      case filter
+      when :current
+        transaction_associations.
+          reduce(0) { |accum, assoc| accum += user.send(assoc).count }
+      when Symbol
+        range_total_count(named_range(filter))
+      when Range
+        range_total_count(filter)
+      else
+        raise ArgumentError, "Invalid argument `#{ filter }'"
+      end
+    end
+
+    private
+
+    def range_total_count(range)
+      transaction_associations.
+        reduce(0) do |accum, assoc|
+          accum += user.send(assoc).where(:created_at => range).count
+        end
+    end
+
+    def named_range(symbol)
+      case symbol
+      when :last_7_days
+        7.days.ago.beginning_of_day..Time.zone.now.end_of_day
+      when :last_30_days
+        30.days.ago.beginning_of_day..Time.zone.now.end_of_day
+      when :last_30_days
+        30.days.ago.beginning_of_day..Time.zone.now.end_of_day
+      when :last_quarter
+        end_of_last = Time.zone.now.beginning_of_quarter - 1.day
+        qstart = end_of_last.beginning_of_quarter
+        qend = end_of_last.end_of_quarter
+        qstart..qend
+      else
+        raise ArgumentError, "Invalid range `:#{ symbol }'"
+      end
+    end
+  end
+end
---
+++ spec/models/user/transaction_calculator_spec.rb
@@ -0,0 +1,147 @@
+# -*- encoding : utf-8 -*-
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe User::TransactionCalculator do
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  subject { described_class.new(user) }
+
+  describe '.new' do
+
+    it 'requires a User' do
+      expect { described_class.new }.to raise_error(ArgumentError)
+    end
+
+    it 'sets a list of default transaction associations' do
+      list = described_class::DEFAULT_TRANSACTION_ASSOCIATIONS
+      expect(described_class.new(user).transaction_associations).to eq(list)
+    end
+
+    it 'allows a list of custom transaction associations' do
+      list = [:comments, :info_requests]
+      calc = described_class.new(user, :transaction_associations => list)
+      expect(calc.transaction_associations).to eq(list)
+    end
+
+    it 'raises an error if a transaction association is invalid' do
+      list = [:invalid_method, :info_requests]
+      expect {
+        described_class.new(user, :transaction_associations => list)
+      }.to raise_error(NoMethodError)
+    end
+
+  end
+
+  describe '#user' do
+
+    it 'returns the User' do
+      expect(subject.user).to eq(user)
+    end
+
+  end
+
+  describe '#total' do
+
+    context 'with no arguments' do
+
+      it 'sums the total transactions made by the user' do
+        3.times do
+          FactoryGirl.create(:comment, :user => user)
+          FactoryGirl.create(:info_request, :user => user)
+        end
+        expect(subject.total).to eq(6)
+      end
+
+    end
+
+    context 'with a Range argument' do
+
+      it 'sums the total transactions made by the user during the range' do
+        time_travel_to(1.year.ago) do
+          FactoryGirl.create(:comment, :user => user)
+          FactoryGirl.create(:info_request, :user => user)
+        end
+
+        time_travel_to(3.days.ago) do
+          FactoryGirl.create(:comment, :user => user)
+          FactoryGirl.create(:info_request, :user => user)
+        end
+
+        FactoryGirl.create(:comment, :user => user)
+
+        expect(subject.total(10.days.ago..1.day.ago)).to eq(2)
+      end
+
+    end
+
+    context 'with a Symbol argument' do
+
+      it ':last_7_days sums the total transactions made by the user in the last 7 days' do
+        time_travel_to(8.days.ago) do
+          FactoryGirl.create(:comment, :user => user)
+        end
+
+        time_travel_to(7.days.ago) do
+          FactoryGirl.create(:comment, :user => user)
+        end
+
+        time_travel_to(6.days.ago) do
+          FactoryGirl.create(:comment, :user => user)
+        end
+
+        expect(subject.total(:last_7_days)).to eq(2)
+      end
+
+      it ':last_30_days sums the total transactions made by the user in the last 30 days' do
+        time_travel_to(31.days.ago) do
+          FactoryGirl.create(:comment, :user => user)
+        end
+
+        time_travel_to(30.days.ago) do
+          FactoryGirl.create(:comment, :user => user)
+        end
+
+        time_travel_to(29.days.ago) do
+          FactoryGirl.create(:comment, :user => user)
+        end
+
+        expect(subject.total(:last_30_days)).to eq(2)
+      end
+
+      it ':last_quarter sums the total transactions made by the user in the last quarter' do
+        time_travel_to(Date.parse('2014-12-31')) do
+          FactoryGirl.create(:comment, :user => user)
+        end
+
+        time_travel_to(Date.parse('2015-01-01')) do
+          FactoryGirl.create(:comment, :user => user)
+        end
+
+        time_travel_to(Date.parse('2015-03-31')) do
+          FactoryGirl.create(:comment, :user => user)
+        end
+
+        time_travel_to(Date.parse('2015-04-01')) do
+          FactoryGirl.create(:comment, :user => user)
+        end
+
+        time_travel_to(Date.parse('2015-04-01')) do
+          expect(subject.total(:last_quarter)).to eq(2)
+        end
+      end
+
+      it 'raises an ArgumentError if the named range is invalid' do
+        expect { subject.total(:invalid_range) }.
+          to raise_error(ArgumentError, "Invalid range `:invalid_range'")
+      end
+    end
+
+    it 'raises an ArgumentError if the argument is invalid' do
+      expect { subject.total('invalid argument') }.
+        to raise_error(ArgumentError, "Invalid argument `invalid argument'")
+    end
+
+  end
+
+end
