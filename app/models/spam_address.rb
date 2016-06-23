190d0596a7ce21cd9cd9c78c9ea6a29b6a94656d
Gareth Rees <gareth@garethrees.co.uk>
2016-06-23 15:31:05 +0100

    Add strong params protection to SpamAddress

--- app/models/spam_address.rb
+++ app/models/spam_address.rb
@@ -10,7 +10,7 @@
 #
 
 class SpamAddress < ActiveRecord::Base
-  attr_accessible :email
+  include ActiveModel::ForbiddenAttributesProtection
 
   validates_presence_of :email, :message => 'Please enter the email address to mark as spam'
   validates_uniqueness_of :email, :message => 'This address is already marked as spam'
