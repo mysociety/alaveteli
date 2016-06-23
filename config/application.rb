619c8041b7bf47664a9335602298f04ac80ca4dc
Gareth Rees <gareth@garethrees.co.uk>
2016-06-23 13:08:06 +0100

    Add strong_parameters gem

--- config/application.rb
+++ config/application.rb
@@ -46,6 +46,8 @@ module Alaveteli
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    config.active_record.whitelist_attributes = false
+
     # Use SQL instead of Active Record's schema dumper when creating the test database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
