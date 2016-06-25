b36dee6c909a9012fa92166eef06d124f0c82768
Gareth Rees <gareth@garethrees.co.uk>
2016-06-25 15:05:30 +0100

    Add strong params protection to UserInfoRequestSentAlert

--- app/models/user_info_request_sent_alert.rb
+++ app/models/user_info_request_sent_alert.rb
@@ -18,6 +18,8 @@
 # Email: hello@mysociety.org; WWW: http://www.mysociety.org/
 
 class UserInfoRequestSentAlert < ActiveRecord::Base
+  include ActiveModel::ForbiddenAttributesProtection
+
   ALERT_TYPES = [
     'overdue_1', # tell user that info request has become overdue
     'very_overdue_1', # tell user that info request has become very overdue
