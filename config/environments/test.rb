22858923859a33b5a9cac2509bc49651721c9768
Gareth Rees <gareth@garethrees.co.uk>
2016-07-12 12:22:51 +0100

    rake rails:update config/environments/test.rb
    - Move custom config to bottom of file
    - Update comments
    - Remove config.whiny_nils
    - Set config.eager_load to non-default value of true because of our test
      preloader
    - Serve static assets through rails
    - Raise instead of rendering exception templates
    - Don’t set config.i18n.fallbacks (Not sure what the current value is)
    - Use . instead of :: for class/module method calls

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,38 +1,51 @@
 # -*- encoding : utf-8 -*-
 Alaveteli::Application.configure do
-  # Settings specified here will take precedence over those in config/environment.rb
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
-  # test suite.  You never need to work with it otherwise.  Remember that
+  # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs.  Don't rely on the data there!
+  # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+  # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = true
 
-  # Show full error reports and disable caching
-  config.consider_all_requests_local = true
+  # Configure static asset server for tests with Cache-Control for performance.
+  config.serve_static_assets  = true
+  config.static_cache_control = "public, max-age=3600"
+
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Tell ActionMailer not to deliver emails to the real world.
+  # Raise exceptions instead of rendering exception templates.
+  config.action_dispatch.show_exceptions = false
+
+  # Disable request forgery protection in test environment.
+  config.action_controller.allow_forgery_protection = false
+
+  # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Disable request forgery protection in test environment
-  config.action_controller.allow_forgery_protection = false
+  # Print deprecation notices to the stderr.
+  config.active_support.deprecation = :stderr
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 
   # Raise if unpermitted request params are detected
   config.action_controller.action_on_unpermitted_parameters = :raise
 
-  # Print deprecation notices to the stderr
-  config.active_support.deprecation = :stderr
   if !AlaveteliConfiguration.exception_notifications_from.blank? && !AlaveteliConfiguration.exception_notifications_to.blank?
     middleware.use ExceptionNotification::Rack,
       :email => {
-        :sender_address => AlaveteliConfiguration::exception_notifications_from,
-        :exception_recipients => AlaveteliConfiguration::exception_notifications_to
+        :sender_address => AlaveteliConfiguration.exception_notifications_from,
+        :exception_recipients => AlaveteliConfiguration.exception_notifications_to
       }
   end
 end
