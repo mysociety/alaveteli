958c0062f09dee39abbf858c5f53d7d46b161b24
Gareth Rees <gareth@garethrees.co.uk>
2016-07-12 11:48:15 +0100

    rake rails:update config/environments/development.rb
    - Move custom config to bottom of file
    - Update comments
    - Remove config.whiny_nils
    - Remove config.assets.compress. This is replaced by
      config.assets.js_compressor in environments where you want to compress
      assets
    - Add config.active_record.migration_error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,26 +1,43 @@
 # -*- encoding : utf-8 -*-
 Alaveteli::Application.configure do
-  # Settings specified here will take precedence over those in config/environment.rb
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
-  # every request.  This slows down response time but is perfect for development
-  # since you don't have to restart the webserver when you make code changes.
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+  # Do not eager load code on boot.
+  config.eager_load = false
 
-  # Show full error reports and disable caching
-  config.consider_all_requests_local = true
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
+  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_deliveries = true
 
-  if AlaveteliConfiguration::use_mailcatcher_in_development
-    # Use mailcatcher in development
-    config.action_mailer.delivery_method = :smtp # so is queued, rather than giving immediate errors
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations
+  config.active_record.migration_error = :page_load
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  if AlaveteliConfiguration.use_mailcatcher_in_development
+    # So is queued, rather than giving immediate errors
+    config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   else
     config.action_mailer.delivery_method = :sendmail
@@ -30,22 +47,6 @@ Alaveteli::Application.configure do
   # unintentionally kept references to objects, especially strings.
   # require 'memory_profiler'
   # MemoryProfiler.start :string_debug => true, :delay => 10
-
-  # Print deprecation notices to the Rails logger
-  config.active_support.deprecation = :log
-
-  # Do not compress assets
-  config.assets.compress = false
-
-  # Expands the lines which load the assets
-  config.assets.debug = true
-
-  # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   if AlaveteliConfiguration.use_bullet_in_development
     config.after_initialize do
