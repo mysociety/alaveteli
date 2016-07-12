e0e2fe06391ac3960a6d38ed375bd36b7534db34
Gareth Rees <gareth@garethrees.co.uk>
2016-07-12 11:41:38 +0100

    rake rails:update config/boot.rb
    - rubygems is already loaded, so don’t require it
    - Use File.exist?

--- config/boot.rb
+++ config/boot.rb
@@ -1,10 +1,9 @@
 # -*- encoding : utf-8 -*-
-require 'rubygems'
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 
 # TODO: Remove this. This is a hacky system for having a default environment.
 # It looks for a config/rails_env.rb file, and reads stuff from there if
