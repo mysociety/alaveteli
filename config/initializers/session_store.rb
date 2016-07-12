066a0148dd421527690bbd59cb80a0dbfce973f4
Gareth Rees <gareth@garethrees.co.uk>
2016-07-12 12:35:22 +0100

    rake rails:update config/initializers/session_store.rb
    - Use new namespace

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # -*- encoding : utf-8 -*-
 # Be sure to restart your server when you modify this file.
-Rails.application.config.session_store :cookie_store, :key => '_wdtk_cookie_session'
+
+Alaveteli::Application.config.session_store :cookie_store, :key => '_wdtk_cookie_session'
