c3bc7a7fd3b31e930264eae66b2b68f7708256fa
Gareth Rees <gareth@garethrees.co.uk>
2016-07-12 12:25:39 +0100

    rake rails:update config/initializers/inflections.rb
    - Update comments

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,9 +1,10 @@
 # -*- encoding : utf-8 -*-
 # Be sure to restart your server when you modify this file.
 
-# Add new inflection rules using the following format
-# (all these examples are active by default):
-ActiveSupport::Inflector.inflections do |inflect|
+# Add new inflection rules using the following format. Inflections
+# are locale specific, and you may define rules for as many different
+# locales as you wish. All of these examples are active by default:
+ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
@@ -11,3 +12,8 @@ ActiveSupport::Inflector.inflections do |inflect|
   inflect.plural /^(embargo)$/i, '\1es'
   inflect.singular /^(embargo)es/i, '\1'
 end
+
+# These inflection rules are supported but not enabled by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym 'RESTful'
+# end
