f2f48d0f3eda0fa83ccc226ee92586ff624a2f20
Gareth Rees <gareth@garethrees.co.uk>
2016-07-12 11:42:11 +0100

    rake rails:update config/environment.rb
    - Update comments

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
-# Load the rails application
+# Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
+# Initialize the Rails application.
 Alaveteli::Application.initialize!
