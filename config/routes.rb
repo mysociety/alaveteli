eb75c798d85821dda4cf0af2840edcdc24d9536f
Gareth Rees <gareth@garethrees.co.uk>
2016-07-19 10:02:43 +0100

    Fix broken categorisation game routes
    - `RequestGameController#stop` is only ever POSTed to
    - `RequestGameController#play` accepts GET (for user display) and POST
      (to shuffle requests)

--- config/routes.rb
+++ config/routes.rb
@@ -389,13 +389,13 @@ Alaveteli::Application.routes.draw do
   #### RequestGame controller
   match '/categorise/play' => 'request_game#play',
         :as => :categorise_play,
-        :via => :get
+        :via => [:get, :post]
   match '/categorise/request/:url_title' => 'request_game#show',
         :as => :categorise_request,
         :via => :get
   match '/categorise/stop' => 'request_game#stop',
         :as => :categorise_stop,
-        :via => :get
+        :via => :post
   ####
 
   #### AdminPublicBody controller
