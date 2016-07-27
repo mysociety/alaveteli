2ed82c0a0b7026d019b168bc5184f6dc16367cd6
Gareth Rees <gareth@garethrees.co.uk>
2016-07-27 13:07:41 +0100

    Expect raise_error rather than response status
    The controller raises an error – rescuing from that is handled
    elsewhere – so test the controller's behaviour.

--- spec/integration/parameter_stripping_spec.rb
+++ spec/integration/parameter_stripping_spec.rb
@@ -4,8 +4,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe "When handling bad requests" do
 
   it 'should return a 404 for GET requests to a malformed request URL' do
-    get 'request/228%85'
-    expect(response.status).to eq(404)
+    expect {
+      get 'request/228%85'
+    }.to raise_error(ActiveRecord::RecordNotFound)
   end
 
 end
