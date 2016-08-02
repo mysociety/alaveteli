d329d5e6abe1ce052b43aa8158d8b7b985beb644
Louise Crow <louise.crow@gmail.com>
2016-08-02 17:24:38 +0100

    Keep hyphenated version of locales in i18n.available_locales
    The problem here is that i18n wants hyphenated locales and
    FastGettext wants underscore separated ones. Now that i18n is
    checking the available locales, and converts the current locale
    to a hyphenated version, keep hyphenated versions in
    I18n.available locales and use FastGettext.default_available_locales
    everywhere where we explicitly use locale lists.

--- lib/alaveteli_localization.rb
+++ lib/alaveteli_localization.rb
@@ -4,10 +4,11 @@ class AlaveteliLocalization
     def set_locales(available_locales, default_locale)
       # fallback locale and available locales
       available_locales = available_locales.split(/ /)
-      FastGettext.default_available_locales = available_locales
-      I18n.locale = default_locale
-      I18n.available_locales = available_locales.map { |locale_name| locale_name.to_sym }
-      I18n.default_locale = default_locale
+      FastGettext.default_available_locales = available_locales.map{ |x| x.to_sym}
+      I18n.locale = default_locale.gsub("_", '-').to_sym
+      I18n.available_locales = available_locales.map { |locale_name| locale_name.gsub("_", '-').to_sym }
+      I18n.default_locale = default_locale.gsub("_", '-').to_sym
+      FastGettext.default_locale = default_locale
       RoutingFilter::Conditionallyprependlocale.locales = available_locales
     end
 
