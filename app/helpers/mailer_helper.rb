995f2a8638cef9904aa8e4e6748b2c2da1d1cc88
Gareth Rees <gareth@garethrees.co.uk>
2016-08-04 10:15:54 +0100

    Add basic styling to HTML email
    Looks like premailer or roadie might be a better way to do this:
    http://stackoverflow.com/a/10386962/387558

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -3,4 +3,8 @@ module MailerHelper
   def contact_from_name_and_email
     "#{AlaveteliConfiguration::contact_name} <#{AlaveteliConfiguration::contact_email}>"
   end
+
+  def link_color
+    '#a94ca6'
+  end
 end
