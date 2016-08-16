b61a814288382c97b7d13d8e226375749ee1bbb5
Gareth Rees <gareth@garethrees.co.uk>
2016-08-16 16:12:06 +0100

    Remove invalid add_index option
    Rails 4 changed the behaviour to raise on invalid keys. This was being
    ignored in Rails 3.

--- db/migrate/20140804120601_add_display_order_to_categories_and_headings.rb
+++ db/migrate/20140804120601_add_display_order_to_categories_and_headings.rb
@@ -4,7 +4,7 @@ class AddDisplayOrderToCategoriesAndHeadings < ActiveRecord::Migration
     add_column :public_body_categories_public_body_headings, :category_display_order, :integer
     rename_table :public_body_categories_public_body_headings, :public_body_category_links
     add_column :public_body_category_links, :id, :primary_key
-    add_index :public_body_category_links, [:public_body_category_id, :public_body_heading_id], :name => "index_public_body_category_links_on_join_ids", :primary => true
+    add_index :public_body_category_links, [:public_body_category_id, :public_body_heading_id], :name => "index_public_body_category_links_on_join_ids"
   end
 
   def down
