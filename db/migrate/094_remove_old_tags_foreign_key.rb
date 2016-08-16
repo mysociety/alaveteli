63f1227485e6e6074e2f3c80c74d21bdfe0e274b
Gareth Rees <gareth@garethrees.co.uk>
2016-08-16 17:10:24 +0100

    Remove manual drop index statements
    Looks like these get removed automatically as of Rails 4

--- db/migrate/094_remove_old_tags_foreign_key.rb
+++ db/migrate/094_remove_old_tags_foreign_key.rb
@@ -3,8 +3,6 @@ class RemoveOldTagsForeignKey < ActiveRecord::Migration
   def self.up
     if ActiveRecord::Base.connection.adapter_name == "PostgreSQL"
       execute "ALTER TABLE has_tag_string_tags DROP CONSTRAINT fk_public_body_tags_public_body"
-      execute 'DROP INDEX index_public_body_tags_on_public_body_id_and_name_and_value'
-      execute 'DROP INDEX index_public_body_tags_on_name'
     end
 
     add_index :has_tag_string_tags, [:model, :model_id, :name, :value], :name => 'by_model_and_model_id_and_name_and_value'
