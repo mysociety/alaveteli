3c2a379b077f277c87948093df4ea60f5a55489b
Gareth Rees <gareth@garethrees.co.uk>
2016-08-17 12:36:37 +0100

    Backport bugfix for dependent destroy FK constraint
    Temporarily backport
    https://github.com/rails/rails/commit/87d1aba3cba0d9b02490784b3090b0e5c94f56df
    for Rails 4. Looks like this is only fixed in 4.2.

--- config/initializers/alaveteli.rb
+++ config/initializers/alaveteli.rb
@@ -61,6 +61,7 @@ require 'alaveteli_gettext/fuzzy_cleaner'
 require 'user_spam_scorer'
 require 'alaveteli_rate_limiter'
 require 'alaveteli_spam_term_checker'
+require 'belongs_to_destroy_with_fk_constraint'
 
 AlaveteliLocalization.set_locales(AlaveteliConfiguration::available_locales,
                                   AlaveteliConfiguration::default_locale)
