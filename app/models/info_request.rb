02821d1512f112c294e77101a6f74fa0bbb3fad0
Gareth Rees <gareth@garethrees.co.uk>
2016-08-26 10:30:45 +0100

    Remove deprecated Relation#all

--- app/models/info_request.rb
+++ app/models/info_request.rb
@@ -1069,7 +1069,7 @@ class InfoRequest < ActiveRecord::Base
 
   # Get the list of censor rules that apply to this request
   def applicable_censor_rules
-    applicable_rules = [censor_rules, CensorRule.global.all]
+    applicable_rules = [censor_rules, CensorRule.global]
     unless is_batch_request_template?
       applicable_rules << public_body.censor_rules
     end
