974a40e67eb2d96ff4e6c952377482204991806b
Gareth Rees <gareth@garethrees.co.uk>
2016-08-26 10:55:41 +0100

    Remove deprecated finder conditions

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -47,12 +47,10 @@ describe ApiController, "when using the API" do
   # POST /api/v2/request.json
   describe 'creating a request' do
     it 'should create a new request from a POST' do
-      number_of_requests = InfoRequest.count(
-        :conditions => [
-          "public_body_id = ?",
-          public_bodies(:geraldine_public_body).id
-        ]
-      )
+      number_of_requests =
+        InfoRequest.
+          where(:public_body_id => public_bodies(:geraldine_public_body).id).
+            count
 
       request_data = {
         'title' => 'Tell me about your chickens',
@@ -71,10 +69,11 @@ describe ApiController, "when using the API" do
       expect(response_body['errors']).to be_nil
       expect(response_body['url']).to match(/^http/)
 
-      expect(InfoRequest.count(:conditions => [
-                          'public_body_id = ?',
-                        public_bodies(:geraldine_public_body).id]
-                        )).to eq(number_of_requests + 1)
+      updated_count =
+        InfoRequest.
+          where(:public_body_id => public_bodies(:geraldine_public_body).id).
+            count
+      expect(updated_count).to eq(number_of_requests + 1)
 
       new_request = InfoRequest.find(response_body['id'])
       expect(new_request.user_id).to be_nil
@@ -98,7 +97,9 @@ describe ApiController, "when using the API" do
       request_id = info_requests(:external_request).id
 
       # Initially it has no incoming messages
-      expect(IncomingMessage.count(:conditions => ["info_request_id = ?", request_id])).to eq(0)
+      initial_count =
+        IncomingMessage.where(:info_request_id => request_id).count
+      expect(initial_count).to eq(0)
 
       # Now add one
       sent_at = '2012-05-28T12:35:39+01:00'
@@ -128,7 +129,9 @@ describe ApiController, "when using the API" do
       request_id = info_requests(:external_request).id
 
       # Initially it has one outgoing message
-      expect(OutgoingMessage.count(:conditions => ['info_request_id = ?', request_id])).to eq(1)
+      outgoing_messages =
+        OutgoingMessage.where(:info_request_id => request_id).count
+      expect(outgoing_messages).to eq(1)
 
       # Add another, as a followup
       sent_at = '2012-05-29T12:35:39+01:00'
@@ -161,7 +164,8 @@ describe ApiController, "when using the API" do
       request_id = info_requests(:external_request).id
 
       # Initially it has no incoming messages
-      expect(IncomingMessage.count(:conditions => ['info_request_id = ?', request_id])).to eq(0)
+      actual1 = IncomingMessage.where(:info_request_id => request_id).count
+      expect(actual1).to eq(0)
 
       # Now add one
       sent_at = '2012-05-28T12:35:39+01:00'
@@ -191,7 +195,9 @@ describe ApiController, "when using the API" do
       request_id = info_requests(:external_request).id
 
       # Initially it has no incoming messages
-      expect(IncomingMessage.count(:conditions => ['info_request_id = ?', request_id])).to eq(0)
+      initial_count =
+        IncomingMessage.where(:info_request_id => request_id).count
+      expect(initial_count).to eq(0)
 
       # Now add one
       sent_at = '2012-05-28T12:35:39+01:00'
@@ -319,7 +325,8 @@ describe ApiController, "when using the API" do
       request_id = info_requests(:external_request).id
 
       # Initially it has no incoming messages
-      expect(IncomingMessage.count(:conditions => ['info_request_id = ?', request_id])).to eq(0)
+      actual1 = IncomingMessage.where(:info_request_id => request_id).count
+      expect(actual1).to eq(0)
 
       # Now add one
       sent_at = '2012-05-28T12:35:39+01:00'
