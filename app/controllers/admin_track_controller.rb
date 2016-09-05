3d4f61b33159a4e64762291408fe006810a6c49e
Gareth Rees <gareth@garethrees.co.uk>
2016-09-05 16:53:23 +0100

    Upgrade will_paginate to remove deprecated finders
    https://github.com/mislav/will_paginate/releases/tag/v3.1.0

--- app/controllers/admin_track_controller.rb
+++ app/controllers/admin_track_controller.rb
@@ -16,7 +16,9 @@ class AdminTrackController < AdminController
     else
       track_things = TrackThing
     end
-    @admin_tracks = track_things.paginate :order => "created_at desc", :page => params[:page], :per_page => 100
+    @admin_tracks =
+      track_things.order('created_at DESC').
+        paginate(:page => params[:page], :per_page => 100)
     @popular = ActiveRecord::Base.connection.select_all("select count(*) as count, title, info_request_id from track_things join info_requests on info_request_id = info_requests.id where info_request_id is not null group by info_request_id, title order by count desc limit 10;")
   end
 
