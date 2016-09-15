feab05a2be56bd4b248ccda2ed90c82f94019236
Steven Day <steve@mysociety.org>
2016-09-15 18:01:24 +0100

    Ensure right ApplicationController is used in pro engine

--- engines/alaveteli_pro/app/controllers/alaveteli_pro/home_controller.rb
+++ engines/alaveteli_pro/app/controllers/alaveteli_pro/home_controller.rb
@@ -1,5 +1,9 @@
+# Ensure we get our ApplicationController, not the main app's one
+# See notes below http://guides.rubyonrails.org/engines.html#inside-an-engine
+require_dependency "alaveteli_pro/application_controller"
+
 module AlaveteliPro
-  class HomeController < AlaveteliPro::ApplicationController
+  class HomeController < ApplicationController
     def index
     end
   end
