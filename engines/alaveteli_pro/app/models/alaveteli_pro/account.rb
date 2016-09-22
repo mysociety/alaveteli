6ad2f7ec2e7576998629a3f66144ba579ae31091
Steven Day <steve@mysociety.org>
2016-09-22 17:53:19 +0100

    Change Account <-> User relationship to a has_one

--- engines/alaveteli_pro/app/models/alaveteli_pro/account.rb
+++ engines/alaveteli_pro/app/models/alaveteli_pro/account.rb
@@ -1,6 +1,6 @@
 module AlaveteliPro
   class Account < ActiveRecord::Base
     attr_accessible :user
-    belongs_to :user, class_name: AlaveteliPro.user_class
+    has_one :user, class_name: AlaveteliPro.user_class
   end
 end
