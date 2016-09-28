948610af8990ae6656fb69436e653f4040b8f8c5
lizconlan <liz@mysociety.org>
2016-09-28 11:21:45 +0100

    Backport handling of empty comments from 0.25.0.0
    Copied from d62498d2

--- spec/controllers/help_controller_spec.rb
+++ spec/controllers/help_controller_spec.rb
@@ -82,6 +82,22 @@ describe HelpController do
       deliveries.clear
     end
 
+    it 'renders the form when no params are supplied' do
+      post :contact
+      expect(response).to be_success
+      expect(response).to render_template('help/contact')
+    end
+
+    it 'does not accept a form without a comment param' do
+      post :contact, { :contact => {
+                         :name => 'Vinny Vanilli',
+                         :email => 'vinny@localhost',
+                         :subject => 'Why do I have such an ace name?',
+                         :message => "You really should know!!!\n\nVinny" },
+                       :submitted_contact_form => 1 }
+      expect(response).to redirect_to(frontpage_path)
+    end
+
   end
 
 end
