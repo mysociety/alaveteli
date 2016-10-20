74a6cf2bd7b9cc81dd7bc78e0f3968acc1221035
Louise Crow <louise.crow@gmail.com>
2016-10-20 15:31:26 +0100

    Add more patterns.

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -386,12 +386,14 @@ class RequestController < ApplicationController
     end
 
     spam_patterns =
-      [/Sea,?s?o?n?[ -]\d+[ -]Epis?,?o?d?e?s?[ -]\d+/i,
+      [
+       /Freedom of Information request - [\{\[\|]/i,
+       /Sea?,?s?o?n?[ -.]\d+[ -]Epi?'?s?,?o?d?e?s?[ -.]\d+/i,
        /\[Full-Watch\]/i,
        /free-watch/i,
        /Online Full 2016/i,
        /HD | Special ".*?" Online/i,
-       /FULL MOVIE/i,
+       /FULL .O?N?_?L?I?N?E? ?\.?MOVIE/i,
        /\[Full 20x5\]/i,
        /Putlocker/i,
        /se?\d+ep?\d+/i,
@@ -404,7 +406,28 @@ class RequestController < ApplicationController
        /\[DVDscr\]/i,
        /W@tch/i,
        /720p/i,
-       /1080p/i
+       /1080p/i,
+       /MEGA.TV/i,
+       /1080.HD/i,
+       /\[Online-Free\]/i,
+       /\[HD\]/i,
+       /\{DOWNLOAD\}/i,
+       /\(\{Ganzer FIlm\}\)/i,
+       /\{leak\}/i,
+       /MEGASH[a|e]RE?/i,
+       /\[Official.HD\]/i,
+       /Completa.*?Ver Online Gratis/i,
+       /Assistir.*?Completa Film Portuguese/i,
+       /\[MP3\]/i,
+       /Watch.*?Movie Online/i,
+       /Gratuitement.*?TELECHARGER/i,
+       /full album/i,
+       /album complet/i,
+       /Album.*?Gratuit/i,
+       /Free.Download/i,
+       /\{FR\}/i,
+       /\[Album\]/i,
+       /watch.*?online free/i
        ]
 
     if spam_patterns.any?{ |spam_pattern| spam_pattern.match(@outgoing_message.subject) }
