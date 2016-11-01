46be9db26f429193ad772dc17df09429e3d05c19
Steven Day <steve@mysociety.org>
2016-11-01 12:46:27 +0000

    Add a ProAccount model

---
+++ db/migrate/20161101110656_create_pro_accounts.rb
@@ -0,0 +1,10 @@
+class CreateProAccounts < ActiveRecord::Migration
+  def change
+    create_table :pro_accounts do |t|
+      t.column :user_id, :integer, null: false
+      t.column :default_embargo_duration, :string
+
+      t.timestamps
+    end
+  end
+end
