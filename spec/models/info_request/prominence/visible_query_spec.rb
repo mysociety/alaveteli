2f54c66a2315e80f4fedd56aaa51e5471ae9d2a6
Louise Crow <louise.crow@gmail.com>
2016-11-04 11:53:28 +0000

    Visible scope includes not embargoed.

--- spec/models/info_request/prominence/visible_query_spec.rb
+++ spec/models/info_request/prominence/visible_query_spec.rb
@@ -11,6 +11,11 @@ describe InfoRequest::Prominence::VisibleQuery do
       expect(described_class.new.call).to include(normal_request)
       expect(described_class.new.call).not_to include(hidden_request)
     end
+
+    it 'does not return an embargoed request with normal prominence' do
+      embargoed_request = FactoryGirl.create(:embargoed_request)
+      expect(described_class.new.call).not_to include(embargoed_request)
+    end
   end
 
 end
