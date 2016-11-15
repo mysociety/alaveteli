402a1610ae39c674759e77d1ea8c1868463ddab9
Steven Day <steve@mysociety.org>
2016-11-15 21:49:34 +0000

    Add a routing constraint object to AlaveteliFeatures

--- gems/alaveteli_features/lib/alaveteli_features.rb
+++ gems/alaveteli_features/lib/alaveteli_features.rb
@@ -1,5 +1,6 @@
 require "alaveteli_features/version"
 require "alaveteli_features/helpers"
+require "alaveteli_features/constraints"
 require "alaveteli_features/railtie" if defined?(Rails)
 require "flipper"
 require "flipper-active_record"
@@ -7,14 +8,17 @@ require "flipper-active_record"
 module AlaveteliFeatures
   def self.backend
     return @backend if @backend
-    if ActiveRecord::Base.connection.table_exists? :flipper_features
+    if ActiveRecord::Base.connected? && \
+       ActiveRecord::Base.connection.table_exists?(:flipper_features)
       @backend = Flipper.new(Flipper::Adapters::ActiveRecord.new)
     else
-      Rails.logger.warn "No database tables found for feature flags, you " \
-                        "might need to set a backend explicitly if you " \
-                        "don't want them stored in a database, or run" \
-                        "rake db:migrate to create the table."
-      Rails.logger.warn "Using memory-based feature storage instead."
+      if defined?(Rails)
+        Rails.logger.warn "No database tables found for feature flags, you " \
+                          "might need to set a backend explicitly if you " \
+                          "don't want them stored in a database, or run" \
+                          "rake db:migrate to create the table."
+        Rails.logger.warn "Using memory-based feature storage instead."
+      end
       require 'flipper/adapters/memory'
       @backend = Flipper.new(Flipper::Adapters::Memory.new)
     end
