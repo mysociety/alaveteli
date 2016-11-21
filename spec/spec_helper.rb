fa93786355d450287516c2f1bf2b9630ec69443f
Louise Crow <louise.crow@gmail.com>
2016-11-21 13:35:38 +0000

    Add specs for show_request_event action.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,6 +48,7 @@ RSpec.configure do |config|
 
   config.include Capybara::DSL, :type => :request
   config.include Delorean
+  config.include LinkToHelper
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
