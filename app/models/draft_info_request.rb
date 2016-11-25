5857e45166bd37feabe6195ad1765ee011ce9fb1
Steven Day <steve@mysociety.org>
2016-11-25 20:39:31 +0000

    Add a draft info request model

---
+++ app/models/draft_info_request.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+# Schema version: 20161128095350
+#
+# Table name: draft_info_requests
+#
+#  id               :integer          not null, primary key
+#  title            :string(255)
+#  user_id          :integer
+#  public_body_id   :integer
+#  body             :text
+#  embargo_duration :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
+class DraftInfoRequest < ActiveRecord::Base
+  validates_presence_of :user
+
+  belongs_to :user
+  belongs_to :public_body
+
+  strip_attributes
+end
