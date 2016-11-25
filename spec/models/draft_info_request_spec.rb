60b6e3a80426f89f307e39bccc91f953965967c8
Steven Day <steve@mysociety.org>
2016-11-25 20:39:31 +0000

    Add a draft info request model

---
+++ spec/models/draft_info_request_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe DraftInfoRequest do
+  let(:draft) { FactoryGirl.create(:draft_info_request) }
+
+  it "belongs to a public body" do
+    expect(draft.public_body).to be_a(PublicBody)
+  end
+
+  it "belongs to a user" do
+    expect(draft.user).to be_a(User)
+  end
+
+  it "has a title" do
+    expect(draft.title).to be_a(String)
+  end
+
+  it "has a body" do
+    expect(draft.body).to be_a(String)
+  end
+
+  it "requires a user" do
+    draft_request = DraftInfoRequest.new
+    expect(draft_request.valid?).to be false
+    draft_request.user = FactoryGirl.create(:user)
+    expect(draft_request.valid?).to be true
+  end
+end
