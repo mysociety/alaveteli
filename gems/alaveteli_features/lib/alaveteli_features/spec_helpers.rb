6c6d8658d2b25787c99f45a80eb709763d627004
Steven Day <steve@mysociety.org>
2016-11-25 20:42:41 +0000

    Only override the named feature in spec helper
    Before, checking for other features would raise an error in rspec because they
    hadn't been explicitly allowed, now anything other feature will be passed
    through to the original method on the backend.

--- gems/alaveteli_features/lib/alaveteli_features/spec_helpers.rb
+++ gems/alaveteli_features/lib/alaveteli_features/spec_helpers.rb
@@ -1,21 +1,17 @@
 module AlaveteliFeatures
   module SpecHelpers
     def with_feature_enabled(feature)
+      allow(AlaveteliFeatures.backend).to receive(:enabled?).and_call_original
       allow(AlaveteliFeatures.backend).
         to receive(:enabled?).with(feature).and_return(true)
       yield
-    ensure
-      allow(AlaveteliFeatures.backend).
-        to receive(:enabled?).with(feature).and_call_original
     end
 
     def with_feature_disabled(feature)
+      allow(AlaveteliFeatures.backend).to receive(:enabled?).and_call_original
       allow(AlaveteliFeatures.backend).
         to receive(:enabled?).with(feature).and_return(false)
       yield
-    ensure
-      allow(AlaveteliFeatures.backend).
-        to receive(:enabled?).with(feature).and_call_original
     end
   end
 end
