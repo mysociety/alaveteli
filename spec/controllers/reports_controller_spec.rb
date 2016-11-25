861cc743cbd0de209024076f4278c8a7aaf3d8a0
Louise Crow <louise.crow@gmail.com>
2016-11-25 12:03:04 +0000

    Cleanup specs
    Use factories, not fixtures and mocks. Remove spec testing action
    internals in favour of testing results.

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -4,19 +4,23 @@ require 'spec_helper'
 describe ReportsController do
 
   describe 'POST #create' do
+    let(:info_request){ FactoryGirl.create(:info_request) }
+    let(:user){ FactoryGirl.create(:user) }
+
     context "when reporting a request when not logged in" do
       it "should only allow logged-in users to report requests" do
-        post :create, :request_id => info_requests(:badger_request).url_title, :reason => "my reason"
-
+        post :create, :request_id => info_request.url_title,
+                      :reason => "my reason"
         expect(flash[:notice]).to match(/You need to be logged in/)
-        expect(response).to redirect_to show_request_path(:url_title => info_requests(:badger_request).url_title)
+        expect(response)
+          .to redirect_to show_request_path(:url_title =>
+                                              info_request.url_title)
       end
     end
 
     context "when reporting a request (logged in)" do
       before do
-        @user = users(:robin_user)
-        session[:user_id] = @user.id
+        session[:user_id] = user.id
       end
 
       it "should 404 for non-existent requests" do
@@ -26,54 +30,51 @@ describe ReportsController do
       end
 
       it "should mark a request as having been reported" do
-        ir = info_requests(:badger_request)
-        title = ir.url_title
-        expect(ir.attention_requested).to eq(false)
+        expect(info_request.attention_requested).to eq(false)
 
-        post :create, :request_id => title, :reason => "my reason"
-        expect(response).to redirect_to show_request_path(:url_title => title)
+        post :create, :request_id => info_request.url_title,
+                      :reason => "my reason"
+        expect(response)
+          .to redirect_to show_request_path(:url_title =>
+                                              info_request.url_title)
 
-        ir.reload
-        expect(ir.attention_requested).to eq(true)
-        expect(ir.described_state).to eq("attention_requested")
-      end
-
-      it "should pass on the reason and message" do
-        info_request = mock_model(InfoRequest, :url_title => "foo", :attention_requested= => nil, :save! => nil)
-        expect(InfoRequest).to receive(:find_by_url_title!).with("foo").and_return(info_request)
-        expect(info_request).to receive(:report!).with("Not valid request", "It's just not", @user)
-        post :create, :request_id => "foo", :reason => "Not valid request", :message => "It's just not"
+        info_request.reload
+        expect(info_request.attention_requested).to eq(true)
+        expect(info_request.described_state).to eq("attention_requested")
       end
 
       it "should not allow a request to be reported twice" do
-        title = info_requests(:badger_request).url_title
+        post :create, :request_id => info_request.url_title,
+                      :reason => "my reason"
+        expect(response)
+          .to redirect_to show_request_url(:url_title =>
+                                             info_request.url_title)
 
-        post :create, :request_id => title, :reason => "my reason"
-        expect(response).to redirect_to show_request_url(:url_title => title)
-
-        post :create, :request_id => title, :reason => "my reason"
-        expect(response).to redirect_to show_request_url(:url_title => title)
+        post :create, :request_id => info_request.url_title,
+                      :reason => "my reason"
+        expect(response)
+          .to redirect_to show_request_url(:url_title =>
+                                             info_request.url_title)
         expect(flash[:notice]).to match(/has already been reported/)
       end
 
       it "should send an email from the reporter to admins" do
-        ir = info_requests(:badger_request)
-        title = ir.url_title
-        post :create, :request_id => title, :reason => "my reason"
+        post :create, :request_id => info_request.url_title,
+                      :reason => "my reason",
+                      :message => "It's just not"
         deliveries = ActionMailer::Base.deliveries
         expect(deliveries.size).to eq(1)
         mail = deliveries[0]
         expect(mail.subject).to match(/attention_requested/)
-        expect(mail.header['Reply-To'].to_s).to include(@user.email)
-        expect(mail.body).to include(@user.name)
+        expect(mail.header['Reply-To'].to_s).to include(user.email)
+        expect(mail.body).to include(user.name)
+        expect(mail.body)
+          .to include("Reason: my reason\n\nIt's just not")
       end
 
       it "should force the user to pick a reason" do
-        info_request = mock_model(InfoRequest, :report! => nil, :url_title => "foo",
-                                  :report_reasons => ["Not FOIish enough"])
-        expect(InfoRequest).to receive(:find_by_url_title!).with("foo").and_return(info_request)
-
-        post :create, :request_id => "foo", :reason => ""
+        post :create, :request_id => info_request.url_title,
+                      :reason => ""
         expect(response).to render_template("new")
         expect(flash[:error]).to eq("Please choose a reason")
       end
@@ -84,28 +85,28 @@ end
 describe ReportsController do
 
   describe "GET #new" do
-
-    let(:info_request) { mock_model(InfoRequest, :url_title => "foo") }
-
-    before :each do
-      expect(InfoRequest).to receive(:find_by_url_title!).with("foo").and_return(info_request)
-    end
+    let(:info_request){ FactoryGirl.create(:info_request) }
+    let(:user){ FactoryGirl.create(:user) }
 
     context "not logged in" do
       it "should require the user to be logged in" do
-        get :new, :request_id => "foo"
+        get :new, :request_id => info_request.url_title
         expect(response).not_to render_template("new")
       end
     end
 
     context "logged in" do
       before :each do
-        session[:user_id] = users(:bob_smith_user).id
+        session[:user_id] = user.id
       end
+
       it "should show the form" do
-        get :new, :request_id => "foo"
+        get :new, :request_id => info_request.url_title
         expect(response).to render_template("new")
       end
+
     end
+
   end
+
 end
