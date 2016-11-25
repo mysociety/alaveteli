2c16938bfa03db1f8544d71bfe973f3da63ecfa7
Louise Crow <louise.crow@gmail.com>
2016-11-25 14:55:40 +0000

    Cleanup specs, use standard descriptions.

--- spec/controllers/widget_votes_controller_spec.rb
+++ spec/controllers/widget_votes_controller_spec.rb
@@ -5,21 +5,21 @@ describe WidgetVotesController do
 
   include LinkToHelper
 
-  describe 'POST create' do
+  describe 'POST #create' do
+    let(:info_request){ FactoryGirl.create(:info_request) }
 
     before do
-      @info_request = FactoryGirl.create(:info_request)
       allow(AlaveteliConfiguration).to receive(:enable_widgets).and_return(true)
     end
 
     it 'should find the info request' do
-      post :create, :request_id => @info_request.id
-      expect(assigns[:info_request]).to eq(@info_request)
+      post :create, :request_id => info_request.id
+      expect(assigns[:info_request]).to eq(info_request)
     end
 
     it 'should redirect to the track path for the info request' do
-      post :create, :request_id => @info_request.id
-      track_thing = TrackThing.create_track_for_request(@info_request)
+      post :create, :request_id => info_request.id
+      track_thing = TrackThing.create_track_for_request(info_request)
       expect(response).to redirect_to(do_track_path(track_thing))
     end
 
@@ -27,17 +27,17 @@ describe WidgetVotesController do
 
       it 'sets a tracking cookie' do
         allow(SecureRandom).to receive(:hex).and_return(mock_cookie)
-        post :create, :request_id => @info_request.id
+        post :create, :request_id => info_request.id
         expect(cookies[:widget_vote]).to eq(mock_cookie)
       end
 
       it 'creates a widget vote' do
         allow(SecureRandom).to receive(:hex).and_return(mock_cookie)
-        votes = @info_request.
+        votes = info_request.
           widget_votes.
           where(:cookie => mock_cookie)
 
-        post :create, :request_id => @info_request.id
+        post :create, :request_id => info_request.id
 
         expect(votes.size).to eq(1)
       end
@@ -48,17 +48,17 @@ describe WidgetVotesController do
 
       it 'retains the existing tracking cookie' do
         request.cookies['widget_vote'] = mock_cookie
-        post :create, :request_id => @info_request.id
+        post :create, :request_id => info_request.id
         expect(cookies[:widget_vote]).to eq(mock_cookie)
       end
 
       it 'creates a widget vote' do
         request.cookies['widget_vote'] = mock_cookie
-        votes = @info_request.
+        votes = info_request.
           widget_votes.
           where(:cookie => mock_cookie)
 
-        post :create, :request_id => @info_request.id
+        post :create, :request_id => info_request.id
 
         expect(votes.size).to eq(1)
       end
@@ -69,7 +69,7 @@ describe WidgetVotesController do
 
       it 'raises ActiveRecord::RecordNotFound' do
         allow(AlaveteliConfiguration).to receive(:enable_widgets).and_return(false)
-        expect{ post :create, :request_id => @info_request.id }.
+        expect{ post :create, :request_id => info_request.id }.
           to raise_error(ActiveRecord::RecordNotFound)
       end
 
@@ -78,9 +78,9 @@ describe WidgetVotesController do
     context "when the request's prominence is not 'normal'" do
 
       it 'should return a 403' do
-        @info_request.prominence = 'hidden'
-        @info_request.save!
-        post :create, :request_id => @info_request.id
+        info_request.prominence = 'hidden'
+        info_request.save!
+        post :create, :request_id => info_request.id
         expect(response.code).to eq("403")
       end
 
