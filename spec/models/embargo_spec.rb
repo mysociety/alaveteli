fb11609ca3bc0d8373a54ae040ed66f2033370ef
Steven Day <steve@mysociety.org>
2016-11-28 18:18:00 +0000

    Add a duration field to Embargo

--- spec/models/embargo_spec.rb
+++ spec/models/embargo_spec.rb
@@ -23,4 +23,43 @@ describe Embargo, :type => :model do
     expect(embargo.publish_at).to be_a(ActiveSupport::TimeWithZone)
   end
 
+  it 'requires a publish_at field' do
+    embargo.publish_at = nil
+    expect(embargo).not_to be_valid
+  end
+
+  it 'has an embargo_duration field' do
+    expect(embargo.embargo_duration).to be_a(String)
+  end
+
+  it 'validates embargo_duration field is in list' do
+    embargo.allowed_durations.each do |duration|
+      embargo.embargo_duration = duration
+      expect(embargo).to be_valid
+    end
+    embargo.embargo_duration = "not_in_list"
+    expect(embargo).not_to be_valid
+  end
+
+  it 'allows embargo_duration to be nil' do
+    embargo.embargo_duration = nil
+    expect(embargo).to be_valid
+  end
+
+  describe 'setting publish_at' do
+    let(:info_request) { FactoryGirl.create(:info_request) }
+
+    it 'sets publish_at from duration during creation' do
+      embargo = Embargo.create(info_request: info_request,
+                               embargo_duration: "3_months")
+      expect(embargo.publish_at).to eq Time.zone.today + 3.months
+    end
+
+    it 'doesnt set publish_at from duration if its already set' do
+      embargo = Embargo.create(info_request: info_request,
+                               publish_at: Time.zone.today,
+                               embargo_duration: "3_months")
+      expect(embargo.publish_at).to eq Time.zone.today
+    end
+  end
 end
