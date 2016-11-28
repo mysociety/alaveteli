b45f9122a3d111b45e902662b34d80b09138b8f3
Louise Crow <louise.crow@gmail.com>
2016-11-28 14:43:04 +0000

    Add request_filter to handle request params.

---
+++ app/assets/javascripts/alaveteli_pro/request-index.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('#request_filter_order').change(function() {
+        this.form.submit();
+    });
+    $('#request_filter_filter').change(function() {
+        this.form.submit();
+    });
+});
--- config/application.rb
+++ config/application.rb
@@ -126,6 +126,7 @@ module Alaveteli
                                  'ie7.css',
                                  'bootstrap-dropdown.js',
                                  'widget.css',
+                                 'responsive/alaveteli_pro/request-index.js',
                                  'responsive/print.css',
                                  'responsive/application-lte-ie7.css',
                                  'responsive/application-ie8.css']
