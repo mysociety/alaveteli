f7453629c25ff79ef29bf021b62c725846476c8e
Steven Day <steve@mysociety.org>
2016-11-30 18:20:13 +0000

    Add helpers provide select options with data for selectize

--- app/helpers/alaveteli_pro/info_requests_helper.rb
+++ app/helpers/alaveteli_pro/info_requests_helper.rb
@@ -8,4 +8,23 @@ module AlaveteliPro::InfoRequestsHelper
   def embargo_extension_options
     Embargo::DURATION_LABELS.invert
   end
+
+  def body_for_selectize(body)
+    {
+      id: body.id,
+      name: body.name,
+      notes: ActionController::Base.helpers.truncate(
+        ActionController::Base.helpers.strip_tags(body.notes),
+        length: 150
+      ),
+      info_requests_visible_count: body.info_requests_visible_count
+    }.to_json
+  end
+
+  def public_body_options(selected = nil)
+    bodies = PublicBody.visible.map do |pb|
+      [pb.name, pb.id, { 'data-data' => body_for_selectize(pb) }]
+    end
+    options_for_select(bodies, selected)
+  end
 end
