a5cec3a05f4f4077821d2661c40ec28b6e07a088
Louise Crow <louise.crow@gmail.com>
2016-12-02 12:25:57 +0000

    Fix for bug introduced in 773d5776d1085928d81c2ef3700f8a04ff1df74a.

--- spec/integration/track_alerts_spec.rb
+++ spec/integration/track_alerts_spec.rb
@@ -4,6 +4,14 @@ require File.expand_path(File.dirname(__FILE__) + '/alaveteli_dsl')
 
 describe "When sending track alerts" do
 
+  before do
+    # TODO: required to make sure xapian index can find files for raw emails
+    # associated with fixtures - can be removed when fixtures no longer
+    # automatically loaded for all specs
+    load_raw_emails_data
+    get_fixtures_xapian_index
+  end
+
   it "should send alerts" do
 
     info_request = FactoryGirl.create(:info_request)
