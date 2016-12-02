4beabfdb5770116b3ee5984810edb64b50336bdc
Louise Crow <louise.crow@gmail.com>
2016-12-02 16:20:49 +0000

    Remove pro-specific js - no longer needed.

--- app/assets/javascripts/pro/menus.js
+++
@@ -1,9 +0,0 @@
-var $moreMenuLink = $('.js-more-menu-link > a');
-var $moreMenu = $('.js-more-menu');
-$(function(){
-  $moreMenuLink.click(function(e){
-    e.preventDefault();
-    $moreMenu.slideToggle(250);
-    return false;
-  });
-});
