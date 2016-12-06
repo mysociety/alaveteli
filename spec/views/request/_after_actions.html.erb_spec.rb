4a011be7b24c8293221900213451ee6c2e9e60ea
Steven Day <steve@mysociety.org>
2016-12-06 17:31:38 +0000

    Modernise request after_actions view spec

--- spec/views/request/_after_actions.html.erb_spec.rb
+++ spec/views/request/_after_actions.html.erb_spec.rb
@@ -2,107 +2,96 @@
 require File.expand_path(File.join('..', '..', '..', 'spec_helper'), __FILE__)
 
 describe 'when displaying actions that can be taken with regard to a request' do
+    let(:info_request) { FactoryGirl.create(:info_request) }
+    let(:track_thing) do
+      FactoryGirl.create(:request_update_track, info_request: info_request)
+    end
 
     before do
-        @mock_body = mock_model(PublicBody, :name => 'test public body',
-                                            :url_name => 'test_public_body')
-        @mock_user = mock_model(User, :name => 'test user',
-                                      :url_name => 'test_user')
-        @mock_request = mock_model(InfoRequest, :title => 'test request',
-                                                :user => @mock_user,
-                                                :user_name => @mock_user.name,
-                                                :is_external? => false,
-                                                :public_body => @mock_body,
-                                                :comments_allowed? => true,
-                                                :url_title => 'test_request',
-                                                :is_public? => true)
-
-        @mock_track = mock_model(TrackThing, :track_type => 'request_updates',
-                                             :info_request => @mock_request)
-        assign :info_request, @mock_request
-        assign :track_thing, @mock_track
+      assign :info_request, info_request
+      assign :track_thing, track_thing
     end
 
     describe 'if the request is old and unclassified' do
+      before do
+        assign :old_unclassified, true
+      end
 
-        before do
-            assign :old_unclassified, true
+      it 'should not display a link for the request owner to update the status of the request' do
+        render :partial => 'request/after_actions'
+        expect(response.body).to have_css('ul.owner_actions') do |div|
+          expect(div).not_to have_css('a', :text => 'Update the status of this request')
         end
+      end
 
-        it 'should not display a link for the request owner to update the status of the request' do
-            render :partial => 'request/after_actions'
-            expect(response.body).to have_css('ul.owner_actions') do |div|
-                expect(div).not_to have_css('a', :text => 'Update the status of this request')
-            end
+      it 'should display a link for anyone to update the status of the request' do
+        render :partial => 'request/after_actions'
+        expect(response.body).to have_css('ul.anyone_actions') do |div|
+          expect(div).to have_css('a', :text => 'Update the status of this request')
         end
-
-        it 'should display a link for anyone to update the status of the request' do
-            render :partial => 'request/after_actions'
-            expect(response.body).to have_css('ul.anyone_actions') do |div|
-                expect(div).to have_css('a', :text => 'Update the status of this request')
-            end
-        end
-
+      end
     end
 
     describe 'if the request is not old and unclassified' do
+      before do
+        assign :old_unclassified, false
+      end
 
-        before do
-            assign :old_unclassified, false
+      it 'should display a link for the request owner to update the status of the request' do
+        render :partial => 'request/after_actions'
+        expect(response.body).to have_css('ul.owner_actions') do |div|
+          expect(div).to have_css('a', :text => 'Update the status of this request')
         end
+      end
 
-        it 'should display a link for the request owner to update the status of the request' do
-            render :partial => 'request/after_actions'
-            expect(response.body).to have_css('ul.owner_actions') do |div|
-                expect(div).to have_css('a', :text => 'Update the status of this request')
-            end
+      it 'should not display a link for anyone to update the status of the request' do
+        render :partial => 'request/after_actions'
+        expect(response.body).to have_css('ul.anyone_actions') do |div|
+          expect(div).not_to have_css('a', :text => 'Update the status of this request')
         end
-
-        it 'should not display a link for anyone to update the status of the request' do
-            render :partial => 'request/after_actions'
-            expect(response.body).to have_css('ul.anyone_actions') do |div|
-                expect(div).not_to have_css('a', :text => 'Update the status of this request')
-            end
-        end
-
+      end
     end
 
     it 'should display a link for the request owner to request a review' do
-        render :partial => 'request/after_actions'
-        expect(response.body).to have_css('ul.owner_actions') do |div|
-            expect(div).to have_css('a', :text => 'Request an internal review')
-        end
+      render :partial => 'request/after_actions'
+      expect(response.body).to have_css('ul.owner_actions') do |div|
+        expect(div).to have_css('a', :text => 'Request an internal review')
+      end
     end
 
 
     it 'should display the link to download the entire request' do
-        render :partial => 'request/after_actions'
-        expect(response.body).to have_css('ul.anyone_actions') do |div|
-            expect(div).to have_css('a', :text => 'Download a zip file of all correspondence')
-        end
+      render :partial => 'request/after_actions'
+      expect(response.body).to have_css('ul.anyone_actions') do |div|
+        expect(div).to have_css('a', :text => 'Download a zip file of all correspondence')
+      end
     end
 
     it "should display a link to annotate the request" do
+      with_feature_enabled(:annotations) do
         render :partial => 'request/after_actions'
         expect(response.body).to have_css('ul.anyone_actions') do |div|
             expect(div).to have_css('a', :text => 'Add an annotation (to help the requester or others)')
         end
+      end
     end
 
     it "should not display a link to annotate the request if comments are disabled on it" do
-        allow(@mock_request).to receive(:comments_allowed).and_return(false)
+      with_feature_enabled(:annotations) do
+        info_request.comments_allowed = false
         render :partial => 'request/after_actions'
         expect(response.body).to have_css('ul.anyone_actions') do |div|
             expect(div).not_to have_css('a', :text => 'Add an annotation (to help the requester or others)')
         end
+      end
     end
 
     it "should not display a link to annotate the request if comments are disabled globally" do
-        allow(view).to receive(:feature_enabled?).with(:annotations).and_return(false)
+      with_feature_disabled(:annotations) do
         render :partial => 'request/after_actions'
         expect(response.body).to have_css('ul.anyone_actions') do |div|
-            expect(div).not_to have_css('a', :text => 'Add an annotation (to help the requester or others)')
+          expect(div).not_to have_css('a', :text => 'Add an annotation (to help the requester or others)')
         end
+      end
     end
-
 end
