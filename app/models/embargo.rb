6d6b62219c172fcbe4481d811947aaae3e46b606
Steven Day <steve@mysociety.org>
2016-12-07 21:32:16 +0000

    Add an EmbargoExtension model

--- app/models/embargo.rb
+++ app/models/embargo.rb
@@ -14,6 +14,7 @@
 
 class Embargo < ActiveRecord::Base
   belongs_to :info_request
+  has_many :embargo_extensions
   validates_presence_of :info_request
   validates_presence_of :publish_at
   validates_inclusion_of :embargo_duration,
---
+++ spec/models/embargo_extension_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+RSpec.describe EmbargoExtension do
+  let(:embargo_extension) { FactoryGirl.create(:embargo_extension) }
+
+  it "has an embargo" do
+    expect(embargo_extension.embargo).to be_a Embargo
+  end
+
+  it "has an extension_duration" do
+    expect(embargo_extension.extension_duration).to be_a String
+  end
+
+  it "requires an embargo" do
+    embargo_extension.embargo = nil
+    expect(embargo_extension).not_to be_valid
+  end
+
+  it "requires an extension duration" do
+    embargo_extension.extension_duration = nil
+    expect(embargo_extension).not_to be_valid
+  end
+
+  it 'validates extension_duration field is in list' do
+    embargo_extension.embargo.allowed_durations.each do |duration|
+      embargo_extension.extension_duration = duration
+      expect(embargo_extension).to be_valid
+    end
+    embargo_extension.extension_duration = "not_in_list"
+    expect(embargo_extension).not_to be_valid
+  end
+end
