42b36d595a22b21c674b702513534d6eede23c08
Louise Crow <louise.crow@gmail.com>
2016-12-16 16:51:29 +0000

    Add scope for overdue requests.

---
+++ app/models/info_request/state/overdue_query.rb
@@ -0,0 +1,16 @@
+class InfoRequest
+  module State
+    class OverdueQuery
+      def initialize(relation = InfoRequest)
+        @relation = relation
+      end
+
+      def call
+        @relation
+          .where(described_state: ['waiting_response'])
+            .where("date_response_required_by < ?", Time.now.to_date)
+              .where("date_very_overdue_after >= ?", Time.now.to_date)
+      end
+    end
+  end
+end
