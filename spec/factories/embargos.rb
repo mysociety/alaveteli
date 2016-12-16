b38c31d830140b88c48c6b5cbbe661c72e624781
Louise Crow <louise.crow@gmail.com>
2016-12-16 16:53:48 +0000

    Add to-do list.

--- spec/factories/embargos.rb
+++ spec/factories/embargos.rb
@@ -16,5 +16,9 @@ FactoryGirl.define do
     info_request
     publish_at Time.zone.today + 3.months
     embargo_duration "3_months"
+
+    factory :expiring_embargo do
+      publish_at Time.now + 3.days
+    end
   end
 end
