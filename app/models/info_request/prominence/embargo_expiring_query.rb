722393d0d250e320cca3de748a1468d9d08482be
Louise Crow <louise.crow@gmail.com>
2016-12-19 14:58:02 +0000

    Add expiring embargo as a hidden filter in the request filter.

---
+++ app/models/info_request/prominence/embargo_expiring_query.rb
@@ -0,0 +1,16 @@
+# -*- encoding : utf-8 -*-
+class InfoRequest
+  module Prominence
+    class EmbargoExpiringQuery
+      def initialize(relation = InfoRequest)
+        @relation = relation
+      end
+
+      def call
+        @relation.includes(:embargo)
+          .where('embargoes.id IS NOT NULL')
+            .where("embargoes.publish_at <= ?", Time.now + 1.week)
+      end
+    end
+  end
+end
