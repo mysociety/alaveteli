2b33eee971198a69440d4a0b2d849bad649f4784
Steven Day <steve@mysociety.org>
2016-12-20 15:51:35 +0000

    Enable InfoRequest::State::Calculator to calculate transitions

--- app/models/info_request/state/calculator.rb
+++ app/models/info_request/state/calculator.rb
@@ -34,6 +34,73 @@ class InfoRequest
           end
         end
       end
+
+      def transitions(opts = {})
+        cached_value_ok = opts.fetch(:cached_value_ok, false)
+        state = @info_request.calculate_status(cached_value_ok)
+        if complete_states(include_admin_states: true).include?(state)
+          return {
+            pending: {},
+            complete: {},
+            other: {}
+          }
+        end
+        opts.merge!(in_internal_review: state == 'internal_review')
+        build_transitions_hash(opts)
+      end
+
+      private
+
+      def build_transitions_hash(opts)
+        hash = {}
+        [:pending, :complete, :other].each do |group|
+          method = "#{group}_states"
+          states = send(method, opts)
+          hash[group] = Transitions.labelled_hash(states, opts)
+        end
+        hash
+      end
+
+      def pending_states(opts)
+        # Which pending states can we transition into
+        if opts.fetch(:in_internal_review, false)
+          states = ['internal_review', 'gone_postal']
+        else
+          states = [
+            'waiting_response',
+            'waiting_clarification',
+            'gone_postal'
+          ]
+          if opts.fetch(:user_asked_to_update_status, false)
+            states += ['internal_review']
+          end
+        end
+        states
+      end
+
+      def complete_states(opts = {})
+        # States from which a request can go no further, because it's complete
+        states = [
+          'not_held',
+          'partially_successful',
+          'successful',
+          'rejected'
+        ]
+        if opts.fetch(:include_admin_states, false)
+          states += ['not_foi', 'vexatious']
+        end
+        states
+      end
+
+      def other_states(opts = {})
+        is_owning_user = opts.fetch(:is_owning_user, false)
+        user_asked_to_update_status = opts.fetch(:user_asked_to_update_status, false)
+        states = ['error_message']
+        if user_asked_to_update_status && is_owning_user
+          states += ['requires_admin', 'user_withdrawn']
+        end
+        states
+      end
     end
   end
 end
