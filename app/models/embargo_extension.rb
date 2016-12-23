93800d62a94982cc55a22d7e31df6bab0baf6ecd
Louise Crow <louise.crow@gmail.com>
2016-12-23 11:36:52 +0000

    Update model annotations

--- app/models/embargo_extension.rb
+++ app/models/embargo_extension.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: embargo_extensions
+#
+#  id                 :integer          not null, primary key
+#  embargo_id         :integer
+#  extension_duration :string(255)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 class EmbargoExtension < ActiveRecord::Base
   belongs_to :embargo
   validates_presence_of :embargo_id
