e0c1c294a9c0cdffbd5897fa16faa25cb013ca2a
Steven Day <steve@mysociety.org>
2017-01-04 15:46:45 +0000

    Redirect pros who log in after requesting to draft request form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -340,11 +340,22 @@ class ApplicationController < ActionController::Base
   # if they start making a request, then we realise they're a pro when they
   # log in, so we want to send them into the pro system
   def override_post_redirect_for_pro(uri, post_redirect, user)
-    case uri
-    when "/new"
-      # TODO: Send this to the new 'draft' controller action for pros
-      # instead. I guess we could just update the uri, but we might need to
-      # tweak the saved params too.
+    # We could have a locale in the url, or we could not, e.g. /en/new or /new
+    if uri =~ /(\/[a-z]{2})?\/new/
+      # Create a draft for the new request, then send the user to the new form
+      # with their data prefilled and a message about creating an embargo.
+      params = post_redirect.post_params
+      draft = DraftInfoRequest.create(
+        user: user,
+        title: params["info_request"]["title"],
+        body: params["outgoing_message"]["body"],
+        public_body_id: params["info_request"]["public_body_id"])
+      flash[:notice] = _("Thanks for logging in. We've saved your " \
+                         "request as a draft, in case you wanted to " \
+                         "add an embargo before sending it. You can " \
+                         "set that (or just send it straight away) " \
+                         "using the form below.")
+      return "#{new_alaveteli_pro_info_request_path}?draft_id=#{draft.id}"
     end
     return uri
   end
