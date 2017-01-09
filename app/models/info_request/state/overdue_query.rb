8235bf2e611a5c2b190e61ea7ed18b5976ed318f
Louise Crow <louise.crow@gmail.com>
2017-01-09 13:39:26 +0000

    Use zones for time calculations.

--- app/models/info_request/state/overdue_query.rb
+++ app/models/info_request/state/overdue_query.rb
@@ -8,8 +8,8 @@ class InfoRequest
       def call
         @relation
           .where(described_state: ['waiting_response'])
-            .where("date_response_required_by < ?", Time.now.to_date)
-              .where("date_very_overdue_after >= ?", Time.now.to_date)
+            .where("date_response_required_by < ?", Time.zone.now.to_date)
+              .where("date_very_overdue_after >= ?", Time.zone.now.to_date)
       end
     end
   end
