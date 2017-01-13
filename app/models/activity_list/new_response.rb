56beeb3ccf260c7cf9f76c7a4669b62265cfdc45
Louise Crow <louise.crow@gmail.com>
2017-01-13 17:38:50 +0000

    Add ActivityList as a facade module for displaying a user's activity.

---
+++ app/models/activity_list/new_response.rb
@@ -0,0 +1,13 @@
+module ActivityList
+  class NewResponse < Item
+
+    def description
+      N_('Your request to {{public_body_name}} "{{info_request_title}}" received a new response.')
+    end
+
+    def call_to_action_url
+      incoming_message_path(event.incoming_message)
+    end
+
+  end
+end
