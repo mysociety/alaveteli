a1ded63e54ad590f8133c40bd478cbe7a995c5f8
Louise Crow <louise.crow@gmail.com>
2017-01-24 18:38:45 +0000

    Move Pro redirect handler to namespaced, included module.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,6 +28,7 @@ class ApplicationController < ActionController::Base
   # Standard headers, footers and navigation for whole site
   layout "default"
   include FastGettext::Translation # make functions like _, n_, N_ etc available)
+  include AlaveteliPro::PostRedirectHandler
 
   # Note: a filter stops the chain if it redirects or renders something
   before_filter :authentication_check
@@ -333,30 +334,6 @@ class ApplicationController < ActionController::Base
       else
         uri += "?post_redirect=1"
       end
-    end
-    return uri
-  end
-
-  # A hook for us to override certain post redirects for pro users, e.g.
-  # if they start making a request, then we realise they're a pro when they
-  # log in, so we want to send them into the pro system
-  def override_post_redirect_for_pro(uri, post_redirect, user)
-    # We could have a locale in the url, or we could not, e.g. /en/new or /new
-    if uri =~ /(\/[a-z]{2})?\/new/
-      # Create a draft for the new request, then send the user to the new form
-      # with their data prefilled and a message about creating an embargo.
-      params = post_redirect.post_params
-      draft = DraftInfoRequest.create(
-        user: user,
-        title: params["info_request"]["title"],
-        body: params["outgoing_message"]["body"],
-        public_body_id: params["info_request"]["public_body_id"])
-      flash[:notice] = _("Thanks for logging in. We've saved your " \
-                         "request as a draft, in case you wanted to " \
-                         "add an embargo before sending it. You can " \
-                         "set that (or just send it straight away) " \
-                         "using the form below.")
-      return "#{new_alaveteli_pro_info_request_path}?draft_id=#{draft.id}"
     end
     return uri
   end
