0eb386c5fcac66cb2ea029a57a0ba262c9429530
Steven Day <steve@mysociety.org>
2017-01-31 12:41:18 +0000

    Refactor reply-handling into a module in /lib

---
+++ spec/lib/reply_handler_spec.rb
@@ -0,0 +1,64 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+require 'reply_handler'
+
+describe ReplyHandler do
+  describe ".forward_on" do
+    describe "non-bounce messages" do
+      let(:raw_email) { load_file_fixture("normal-contact-reply.email") }
+      let(:message) { MailHandler.mail_from_raw_email(raw_email) }
+
+      it "should forward the message to sendmail" do
+        expect(IO).
+          to receive(:popen).
+          with("/usr/sbin/sendmail -i user-support@localhost", "wb")
+        ReplyHandler.forward_on(raw_email, message)
+      end
+    end
+  end
+
+  describe ".get_forward_to_address" do
+    let(:pro_message) do
+      raw_email = load_file_fixture("pro-contact-reply.email")
+      MailHandler.mail_from_raw_email(raw_email)
+    end
+
+    let(:normal_message) do
+      raw_email = load_file_fixture("normal-contact-reply.email")
+      MailHandler.mail_from_raw_email(raw_email)
+    end
+
+    context "if alaveteli pro is disabled" do
+      before do
+        allow(AlaveteliConfiguration).
+          to receive(:enable_alaveteli_pro).and_return(false)
+      end
+
+      it "returns the normal forwarding address" do
+        expect(ReplyHandler.get_forward_to_address(normal_message)).
+          to eq AlaveteliConfiguration.forward_nonbounce_responses_to
+      end
+    end
+
+    context "if alaveteli pro is enabled" do
+      before do
+        allow(AlaveteliConfiguration).
+          to receive(:enable_alaveteli_pro).and_return(true)
+      end
+
+      context "and the email is replying to the pro contact" do
+        it "returns the pro forwarding address" do
+          expect(ReplyHandler.get_forward_to_address(pro_message)).
+            to eq AlaveteliConfiguration.forward_pro_nonbounce_responses_to
+        end
+      end
+
+      context "and the email is replying to the normal contact" do
+        it "returns the normal contact address" do
+          expect(ReplyHandler.get_forward_to_address(normal_message)).
+            to eq AlaveteliConfiguration.forward_nonbounce_responses_to
+        end
+      end
+    end
+  end
+end
--- spec/script/handle-mail-replies_spec.rb
+++ spec/script/handle-mail-replies_spec.rb
@@ -1,7 +1,6 @@
 # -*- encoding : utf-8 -*-
 require "spec_helper"
 require "external_command"
-require File.expand_path(File.dirname(__FILE__) + "/../../script/handle-mail-replies.rb")
 
 def mail_reply_test(email_filename)
   Dir.chdir Rails.root do
@@ -95,64 +94,5 @@ describe "When filtering" do
   it "should detect an ABCMail-style out-of-office" do
     r = mail_reply_test("track-response-abcmail-oof.email")
     expect(r.status).to eq(2)
-  end
-end
-
-describe "#forward_on" do
-  describe "non-bounce messages" do
-    let(:raw_email) { load_file_fixture("normal-contact-reply.email") }
-    let(:message) { MailHandler.mail_from_raw_email(raw_email) }
-
-    it "should forward the message to sendmail" do
-      expect(IO).
-        to receive(:popen).
-        with("/usr/sbin/sendmail -i user-support@localhost", "wb")
-      forward_on(raw_email, message)
-    end
-  end
-end
-
-describe "#get_forward_to_address" do
-  let(:pro_message) do
-    raw_email = load_file_fixture("pro-contact-reply.email")
-    MailHandler.mail_from_raw_email(raw_email)
-  end
-
-  let(:normal_message) do
-    raw_email = load_file_fixture("normal-contact-reply.email")
-    MailHandler.mail_from_raw_email(raw_email)
-  end
-
-  context "if alaveteli pro is disabled" do
-    before do
-      allow(AlaveteliConfiguration).
-        to receive(:enable_alaveteli_pro).and_return(false)
-    end
-
-    it "returns the normal forwarding address" do
-      expect(get_forward_to_address(normal_message)).
-        to eq AlaveteliConfiguration.forward_nonbounce_responses_to
-    end
-  end
-
-  context "if alaveteli pro is enabled" do
-    before do
-      allow(AlaveteliConfiguration).
-        to receive(:enable_alaveteli_pro).and_return(true)
-    end
-
-    context "and the email is replying to the pro contact" do
-      it "returns the pro forwarding address" do
-        expect(get_forward_to_address(pro_message)).
-          to eq AlaveteliConfiguration.forward_pro_nonbounce_responses_to
-      end
-    end
-
-    context "and the email is replying to the normal contact" do
-      it "returns the normal contact address" do
-        expect(get_forward_to_address(normal_message)).
-          to eq AlaveteliConfiguration.forward_nonbounce_responses_to
-      end
-    end
   end
 end
