7905075b2e2ac11efdc78f975255f2704e308e99
Louise Crow <louise.crow@gmail.com>
2017-02-01 09:32:03 +0000

    Add and correct asset compilation path.

--- config/application.rb
+++ config/application.rb
@@ -126,7 +126,8 @@ module Alaveteli
                                  'ie7.css',
                                  'bootstrap-dropdown.js',
                                  'widget.css',
-                                 'responsive/alaveteli_pro/request-index.js',
+                                 'request-attachments.js',
+                                 'alaveteli_pro/request-index.js',
                                  'responsive/print.css',
                                  'responsive/application-lte-ie7.css',
                                  'responsive/application-ie8.css']
