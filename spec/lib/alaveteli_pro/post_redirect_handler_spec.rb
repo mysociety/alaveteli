5f2a1bd782700f9f0c1d61b5715622c9e804f6af
Steven Day <steve@mysociety.org>
2017-02-21 18:09:00 +0000

    Add some specs for the PostRedirectHandler lib

---
+++ spec/lib/alaveteli_pro/post_redirect_handler_spec.rb
@@ -0,0 +1,50 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe AlaveteliPro::PostRedirectHandler, type: :controller do
+  controller do
+    include AlaveteliPro::PostRedirectHandler
+  end
+
+  describe "#override_post_redirect_for_pro" do
+    context "when the uri matches /<locale>/new" do
+      let(:uri) { '/en/new' }
+      let(:user) { FactoryGirl.create(:pro_user) }
+      let(:post_redirect) do
+        FactoryGirl.create(:new_request_post_redirect, user: user, uri: uri)
+      end
+
+      it "creates a draft info request" do
+        expect {
+          controller.override_post_redirect_for_pro(uri, post_redirect, user)
+        }.to change { DraftInfoRequest.count }.by(1)
+        draft = DraftInfoRequest.last
+        params = post_redirect.post_params
+        expect(draft.user).to eq(user)
+        expect(draft.title).to eq(params["info_request"]["title"])
+        expect(draft.body).to eq(params["outgoing_message"]["body"])
+        expect(draft.public_body_id).to eq(params["info_request"]["public_body_id"].to_i)
+      end
+
+      it "overrides the uri" do
+        expect(
+          controller.override_post_redirect_for_pro(uri, post_redirect, user)
+        ).to match /#{new_alaveteli_pro_info_request_path}\?draft_id=\d+/
+      end
+    end
+
+    context "when the uri does not match /<locale>/new" do
+      let(:uri) { '/en/new/public_body' }
+      let(:user) { FactoryGirl.create(:pro_user) }
+      let(:post_redirect) do
+        FactoryGirl.create(:post_redirect, user: user, uri: uri)
+      end
+
+      it "does not override the uri" do
+        expect(
+          controller.override_post_redirect_for_pro(uri, post_redirect, user)
+        ).to eq uri
+      end
+    end
+  end
+end
