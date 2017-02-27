66b1b478a2767c566817cda425e008e03523d46f
Louise Crow <louise.crow@gmail.com>
2017-02-27 14:24:20 +0000

    Remove :optional hash key - only applies in Rails 5 and above.

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,8 +2,7 @@ class Role < ActiveRecord::Base
   has_and_belongs_to_many :users, :join_table => :users_roles
 
   belongs_to :resource,
-             :polymorphic => true,
-             :optional => true
+             :polymorphic => true
 
   validates :resource_type,
             :inclusion => { :in => Rolify.resource_types },
