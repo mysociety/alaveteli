9c62f56845324b068534bc22b6d256c50a03eeab
Louise Crow <louise.crow@gmail.com>
2017-02-27 14:13:29 +0000

    Results of running "rails g rolify Role User"
    Deleted empty spec file until we have some specs.

---
+++ app/models/role.rb
@@ -0,0 +1,14 @@
+class Role < ActiveRecord::Base
+  has_and_belongs_to_many :users, :join_table => :users_roles
+
+  belongs_to :resource,
+             :polymorphic => true,
+             :optional => true
+
+  validates :resource_type,
+            :inclusion => { :in => Rolify.resource_types },
+            :allow_nil => true
+
+  scopify
+  # attr_accessible :title, :body
+end
---
+++ spec/factories/roles.rb
@@ -0,0 +1,4 @@
+FactoryGirl.define do
+  factory :role do
+  end
+end
