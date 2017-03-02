dc78b3d1775f97ce8f7ddcd7e3e403a3851d4a6e
Steven Day <steve@mysociety.org>
2017-03-02 15:21:20 +0000

    Placeholder integration specs for pro batch requests

---
+++ spec/integration/alaveteli_pro/batch_request_spec.rb
@@ -0,0 +1,9 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../alaveteli_dsl')
+
+describe "creating batch requests in alaveteli_pro" do
+  it "allows the user to search for authorities"
+  it "allows the user to add authorities to a draft"
+  it "allows the user to remove authorities from a draft"
+end
