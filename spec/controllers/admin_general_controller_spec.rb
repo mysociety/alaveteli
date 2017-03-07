4137db84bd24abb58546d8cd6b28c94904cb5cdd
Louise Crow <louise.crow@gmail.com>
2017-03-07 16:41:17 +0000

    Add specs for assigns.

--- spec/controllers/admin_general_controller_spec.rb
+++ spec/controllers/admin_general_controller_spec.rb
@@ -5,16 +5,37 @@ describe AdminGeneralController do
 
   describe "GET #index" do
 
+    before do
+      InfoRequest.destroy_all
+    end
+
     it "should render the front page" do
       get :index
       expect(response).to render_template('index')
     end
 
     it 'assigns old unclassified requests' do
-      InfoRequest.destroy_all
       @old_request = FactoryGirl.create(:old_unclassified_request)
       get :index
       expect(assigns[:old_unclassified]).to eq([@old_request])
+    end
+
+    it 'assigns requests that require admin to the view' do
+      requires_admin_request = FactoryGirl.create(:requires_admin_request)
+      get :index
+      expect(assigns[:requires_admin_requests]).to eq([requires_admin_request])
+    end
+
+    it 'assigns requests that have error messages to the view' do
+      error_message_request = FactoryGirl.create(:error_message_request)
+      get :index
+      expect(assigns[:error_message_requests]).to eq([error_message_request])
+    end
+
+    it 'assigns requests flagged for admin attention to the view' do
+      attention_requested_request = FactoryGirl.create(:attention_requested_request)
+      get :index
+      expect(assigns[:attention_requests]).to eq([attention_requested_request])
     end
 
   end
