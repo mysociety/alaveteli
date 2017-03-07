b76ec76c3bbd1d5283237ffce874f8b139cf6ad5
lizconlan <liz@mysociety.org>
2017-03-07 09:52:30 +0000

    Include security fix for CVE-2016-6316
    And port TestUnit specs to RSpec style

---
+++ lib/patch_tag_helper.rb
@@ -0,0 +1,40 @@
+# -*- encoding : utf-8 -*-
+
+# Backported fix for CVE-2016-6316
+# https://groups.google.com/forum/#!msg/ruby-security-ann/8B2iV2tPRSE/JkjCJkSoCgAJ
+
+module ActionView::Helpers::TagHelper
+
+  private
+
+  def tag_option(key, value, escape)
+    if value.is_a?(Array)
+      value = escape ? safe_join(value, " ") : value.join(" ")
+    else
+      value = escape ? ERB::Util.unwrapped_html_escape(value) : value.to_s
+    end
+    %(#{key}="#{value.gsub('"'.freeze, '&quot;'.freeze)}")
+  end
+
+end
+
+
+module ERB::Util
+
+  # Copied in from Rails 4.2 as it doesn't yet exist in 4.0
+
+  HTML_ESCAPE_REGEXP = /[&"'><]/
+
+  # HTML escapes strings but doesn't wrap them with an ActiveSupport::SafeBuffer.
+  # This method is not for public consumption! Seriously!
+  def unwrapped_html_escape(s) # :nodoc:
+    s = s.to_s
+    if s.html_safe?
+      s
+    else
+      s.gsub(HTML_ESCAPE_REGEXP, HTML_ESCAPE)
+    end
+  end
+  module_function :unwrapped_html_escape
+
+end
