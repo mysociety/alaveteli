e8da4163f2f2d4e7d9e3b2211f9d1cfdab7c9b77
Steven Day <steve@mysociety.org>
2017-03-08 18:04:05 +0000

    Respond to AJAX requests to create/update a batch with partial html

--- app/controllers/alaveteli_pro/draft_info_request_batches_controller.rb
+++ app/controllers/alaveteli_pro/draft_info_request_batches_controller.rb
@@ -1,24 +1,45 @@
 class AlaveteliPro::DraftInfoRequestBatchesController < ApplicationController
   def create
     @draft = current_user.draft_info_request_batches.create(draft_params)
-    redirect_after_create_or_update(@draft)
+    @query = params[:query]
+    if request.xhr?
+      respond_with_partial(@draft, @query)
+    else
+      redirect_after_create_or_update(@draft, @query)
+    end
   end
 
   def update
     @draft = current_user.draft_info_request_batches.find(params[:id])
     @draft.update_attributes(draft_params)
-    redirect_after_create_or_update(@draft)
+    @query = params[:query]
+    if request.xhr?
+      respond_with_partial(@draft, @query)
+    else
+      redirect_after_create_or_update(@draft, @query)
+    end
   end
 
   private
 
-  def redirect_after_create_or_update(draft)
-    if params[:query]
-      path = alaveteli_pro_batch_request_authority_searches_path(draft_id: draft.id, query: params[:query])
+  def redirect_after_create_or_update(draft, query)
+    if query
+      path = alaveteli_pro_batch_request_authority_searches_path(
+        draft_id: draft.id,
+        query: query
+      )
     else
-      path = new_alaveteli_pro_batch_request_authority_search_path(draft_id: draft.id)
+      path = new_alaveteli_pro_batch_request_authority_search_path(
+        draft_id: draft.id
+      )
     end
     redirect_to path, notice: _('Your Batch Request has been saved!')
+  end
+
+  def respond_with_partial(draft, query)
+    render :partial => 'summary',
+           :layout => false,
+           :locals => { :draft => draft, :query => query }
   end
 
   def draft_params
