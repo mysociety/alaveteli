7732b06d9b45512c574dd30e045cb21d019a26f8
Steven Day <steve@mysociety.org>
2017-03-08 12:01:47 +0000

    Allow pro batch authority search to respond to AJAX requests
    The controller now responds with partial HTML, rather than a full page
    when a request is made via ajax. It also uses perform_search_typeahead
    rather than perform_search, in order to get the partial matching
    implied by the wireframes.

--- app/controllers/alaveteli_pro/batch_request_authority_searches_controller.rb
+++ app/controllers/alaveteli_pro/batch_request_authority_searches_controller.rb
@@ -4,25 +4,34 @@ class AlaveteliPro::BatchRequestAuthoritySearchesController < AlaveteliPro::Base
 
   def new
     @draft_batch_request = find_or_initialise_draft
-    render 'search_results'
   end
 
   def create
     @draft_batch_request = find_or_initialise_draft
-    @search = perform_search(params[:query])
+    @body_ids_added = @draft_batch_request.public_body_ids
+    # perform_seach sets @query but perform_search_typeahead doesn't
+    @query = params[:query]
+    @search = perform_search_typeahead(@query, PublicBody)
     @result_limit = calculate_result_limit(@search)
     check_page_limit!(@page, @per_page)
-    render 'search_results'
+    if request.xhr?
+      render :partial => 'search_results',
+             :layout => false,
+             :locals => {
+               :search => @search,
+               :draft_batch_request => @draft_batch_request,
+               :body_ids_added => @body_ids_added,
+               :query => @query,
+               :page => @page,
+               :per_page => @per_page,
+               :result_limit => @result_limit
+             }
+    else
+      render :template => 'alaveteli_pro/batch_request_authority_searches/new'
+    end
   end
 
   private
-
-  def perform_search(query)
-    sortby = 'relevant'
-    collapse = nil
-    models = [PublicBody]
-    super(models, query, sortby, collapse)
-  end
 
   # Limit the result count
   def calculate_result_limit(search)
