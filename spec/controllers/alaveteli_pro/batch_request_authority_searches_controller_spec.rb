0719608e5e059e412f0a8b50448cbb167ae57081
Steven Day <steve@mysociety.org>
2017-03-08 12:01:47 +0000

    Allow pro batch authority search to respond to AJAX requests
    The controller now responds with partial HTML, rather than a full page
    when a request is made via ajax. It also uses perform_search_typeahead
    rather than perform_search, in order to get the partial matching
    implied by the wireframes.

--- spec/controllers/alaveteli_pro/batch_request_authority_searches_controller_spec.rb
+++ spec/controllers/alaveteli_pro/batch_request_authority_searches_controller_spec.rb
@@ -1,6 +1,33 @@
 # -*- encoding : utf-8 -*-
 require 'spec_helper'
 
+shared_examples_for "creating a search" do
+  it "performs a search" do
+    puts assigns[:search].results.inspect
+    results = assigns[:search].results
+    expect(results.count).to eq 3
+    expect(results.first[:model].name).to eq authority_1.name
+    expect(results.second[:model].name).to eq authority_2.name
+    expect(results.third[:model].name).to eq authority_3.name
+  end
+
+  it "sets @query" do
+    expect(assigns[:query]).to eq 'Example Public Body'
+  end
+
+  it "sets @result_limit" do
+    expect(assigns[:result_limit]).to eq assigns[:search].matches_estimated
+  end
+
+  it "sets @page" do
+    expect(assigns[:page]).to eq 1
+  end
+
+  it "sets @per_page" do
+    expect(assigns[:per_page]).to eq 25
+  end
+end
+
 describe AlaveteliPro::BatchRequestAuthoritySearchesController do
   let(:pro_user) { FactoryGirl.create(:pro_user) }
 
@@ -9,10 +36,10 @@ describe AlaveteliPro::BatchRequestAuthoritySearchesController do
       session[:user_id] = pro_user.id
     end
 
-    it "renders search_results.html.erb" do
+    it "renders new.html.erb" do
       with_feature_enabled :alaveteli_pro do
         get :new
-        expect(response).to render_template('search_results')
+        expect(response).to render_template('new')
       end
     end
   end
@@ -27,38 +54,43 @@ describe AlaveteliPro::BatchRequestAuthoritySearchesController do
       update_xapian_index
     end
 
-    it "performs a search" do
-      get :create, query: 'Example Public Body'
-      results = assigns[:search].results
-      expect(results.count).to eq 3
-      expect(results.first[:model].name).to eq authority_1.name
-      expect(results.second[:model].name).to eq authority_2.name
-      expect(results.third[:model].name).to eq authority_3.name
+    context "when responding to a normal request" do
+      before do
+        with_feature_enabled(:alaveteli_pro) do
+          get :create, query: 'Example Public Body'
+        end
+      end
+
+      it_behaves_like "creating a search"
+
+      it "renders new.html.erb" do
+        expect(response).to render_template('new')
+      end
+
+      it "raises WillPaginate::InvalidPage error for pages beyond the limit" do
+        expect { get :create, query: 'Example Public Body', page: 21 }.
+          to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
 
-    it "sets @query" do
-      get :create, query: 'Example Public Body'
-      expect(assigns[:query]).to eq 'Example Public Body'
-    end
+    context "when responding to an ajax request" do
+      before do
+        with_feature_enabled :alaveteli_pro do
+          xhr :get, :create, query: 'Example Public Body'
+        end
+      end
 
-    it "sets @result_limit" do
-      get :create, query: 'Example Public Body'
-      expect(assigns[:result_limit]).to eq assigns[:search].matches_estimated
-    end
+      it_behaves_like "creating a search"
 
-    it "sets @page" do
-      get :create, query: 'Example Public Body'
-      expect(assigns[:page]).to eq 1
-    end
+      it "only renders _search_results.html.erb" do
+        expect(response).to render_template '_search_results'
+        expect(response).not_to render_template('new')
+      end
 
-    it "sets @per_page" do
-      get :create, query: 'Example Public Body'
-      expect(assigns[:per_page]).to eq 25
-    end
-
-    it "raises WillPaginate::InvalidPage error for pages beyond the limit" do
-      expect { get :create, query: 'Example Public Body', page: 21 }.
-        to raise_error(ActiveRecord::RecordNotFound)
+      it "raises WillPaginate::InvalidPage error for pages beyond the limit" do
+        expect { xhr :get, :create, query: 'Example Public Body', page: 21 }.
+          to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 end
