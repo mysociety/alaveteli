9beef3c0741e3aee5fcd527db4e9083d93b140e0
Steven Day <steve@mysociety.org>
2017-03-13 12:41:20 +0000

    Add javascript for submitting the 'Remove body' buttons
    This required some reworking of the load order of javascript files, because
    both the add and remove button components relied on their parent modules, so
    those both had to be initialised first. I could have solved this with
    something like RequireJS but I wanted to keep this as simple as possible for
    now.

--- app/assets/javascripts/alaveteli_pro/alaveteli_pro.js
+++ app/assets/javascripts/alaveteli_pro/alaveteli_pro.js
@@ -4,5 +4,11 @@
 //= require alaveteli_pro/authority_select
 //= require alaveteli_pro/embargo_dropdown
 //= require alaveteli_pro/status_dropdown
-//= require alaveteli_pro/draft_batch_summary/draft_batch_summary
-//= require alaveteli_pro/batch_authority_search/batch_authority_search
+//= require alaveteli_pro/draft_batch_summary/initialise
+//= require alaveteli_pro/batch_authority_search/initialise
+//= require alaveteli_pro/draft_batch_summary/body-list
+//= require alaveteli_pro/draft_batch_summary/body
+//= require alaveteli_pro/batch_authority_search/search-form
+//= require alaveteli_pro/batch_authority_search/results
+//= require alaveteli_pro/batch_authority_search/pagination
+//= require alaveteli_pro/batch_authority_search/result
--- app/assets/javascripts/alaveteli_pro/batch_authority_search/batch_authority_search.js
+++
@@ -1,6 +0,0 @@
-//...
-//= require alaveteli_pro/batch_authority_search/initialise
-//= require alaveteli_pro/batch_authority_search/search-form
-//= require alaveteli_pro/batch_authority_search/results
-//= require alaveteli_pro/batch_authority_search/result
-//= require alaveteli_pro/batch_authority_search/pagination
