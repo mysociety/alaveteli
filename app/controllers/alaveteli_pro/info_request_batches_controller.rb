f3b1e6c609bf92acf8f235cd2fa6a20f727b13a3
Steven Day <steve@mysociety.org>
2017-03-23 10:51:20 +0000

    Make pro batch controller's new method work with draft batches

--- app/controllers/alaveteli_pro/info_request_batches_controller.rb
+++ app/controllers/alaveteli_pro/info_request_batches_controller.rb
@@ -8,18 +8,13 @@
 class AlaveteliPro::InfoRequestBatchesController < AlaveteliPro::BaseController
   def new
     @draft_info_request_batch = load_draft
-    load_data_from_draft
+    @info_request_batch = InfoRequestBatch.from_draft(@draft_info_request_batch)
+    render :template => 'alaveteli_pro/info_requests/new'
   end
 
   private
 
   def load_draft
     current_user.draft_info_request_batches.find(params[:draft_id])
-  end
-
-  def load_data_from_draft
-    @info_request_batch = InfoRequestBatch.from_draft(@draft_info_request)
-    @outgoing_message = @info_request_batch.outgoing_messages.first
-    @embargo = @info_request_batch.embargo
   end
 end
