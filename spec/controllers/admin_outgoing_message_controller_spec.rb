aed4d90ecd82ba4c96cfba9e81bfac4fa69c95f0
Gareth Rees <gareth@garethrees.co.uk>
2017-03-23 17:27:42 +0000

    Use let instead of setting ivars in before block
        Note the use of the @user instance variable for the user. That certainly
        works, but has the problem of introducing subtle bugs since @instance
        variables spring into existence as nil when first referenced. This means
        that if I have a typo in my spec and say @usar.should be_nil, the spec
        will still pass even if the correctly-named @user variable is not nil.
        Not good.
    
    http://web.archive.org/web/20150307053129/http://bitfluxx.com/2011/05/23/some-rspec-tips-and-best-practices.html

--- spec/controllers/admin_outgoing_message_controller_spec.rb
+++ spec/controllers/admin_outgoing_message_controller_spec.rb
@@ -5,20 +5,20 @@ describe AdminOutgoingMessageController do
 
   describe 'GET #edit' do
 
-    before do
-      @info_request = FactoryGirl.create(:info_request)
-      @outgoing = @info_request.outgoing_messages.first
-    end
+    let(:info_request) { FactoryGirl.create(:info_request) }
+    let(:outgoing) { info_request.outgoing_messages.first }
 
     it 'should be successful' do
-      get :edit, :id => @outgoing.id
+      get :edit, :id => outgoing.id
       expect(response).to be_success
     end
 
     it 'should assign the outgoing message to the view' do
-      get :edit, :id => @outgoing.id
-      expect(assigns[:outgoing_message]).to eq(@outgoing)
+      get :edit, :id => outgoing.id
+      expect(assigns[:outgoing_message]).to eq(outgoing)
     end
+
+  end
 
   describe 'DELETE #destroy' do
 
@@ -59,44 +59,44 @@ describe AdminOutgoingMessageController do
 
   describe 'PUT #update' do
 
-    before do
-      @info_request = FactoryGirl.create(:info_request)
-      @outgoing = @info_request.outgoing_messages.first
-      @default_params = {:id => @outgoing.id,
-                         :outgoing_message => {:prominence => 'hidden',
-                                               :prominence_reason => 'dull',
-                                               :body => 'changed body'} }
+    let(:info_request) { FactoryGirl.create(:info_request) }
+    let(:outgoing) { info_request.outgoing_messages.first }
+    let(:default_params) do
+      { :id => outgoing.id,
+        :outgoing_message => { :prominence => 'hidden',
+                               :prominence_reason => 'dull',
+                               :body => 'changed body' } }
     end
 
-    def make_request(params=@default_params)
+    def make_request(params = default_params)
       post :update, params
     end
 
     it 'should save a change to the body of the message' do
       make_request
-      @outgoing.reload
-      expect(@outgoing.body).to eq('changed body')
+      outgoing.reload
+      expect(outgoing.body).to eq('changed body')
     end
 
     it 'should save the prominence of the message' do
       make_request
-      @outgoing.reload
-      expect(@outgoing.prominence).to eq('hidden')
+      outgoing.reload
+      expect(outgoing.prominence).to eq('hidden')
     end
 
     it 'should save a prominence reason for the message' do
       make_request
-      @outgoing.reload
-      expect(@outgoing.prominence_reason).to eq('dull')
+      outgoing.reload
+      expect(outgoing.prominence_reason).to eq('dull')
     end
 
     it 'should log an "edit_outgoing" event on the info_request' do
       allow(@controller).to receive(:admin_current_user).and_return("Admin user")
       make_request
-      @info_request.reload
-      last_event = @info_request.info_request_events.last
+      info_request.reload
+      last_event = info_request.info_request_events.last
       expect(last_event.event_type).to eq('edit_outgoing')
-      expect(last_event.params).to eq({ :outgoing_message_id => @outgoing.id,
+      expect(last_event.params).to eq({ :outgoing_message_id => outgoing.id,
                                     :editor => "Admin user",
                                     :old_prominence => "normal",
                                     :prominence => "hidden",
@@ -114,7 +114,7 @@ describe AdminOutgoingMessageController do
 
       expect(info_request).to receive(:expire)
 
-      params = @default_params.dup
+      params = default_params.dup
       params[:id] = outgoing.id
       make_request(params)
     end
@@ -123,7 +123,7 @@ describe AdminOutgoingMessageController do
 
       it 'should redirect to the admin info request view' do
         make_request
-        expect(response).to redirect_to admin_request_url(@info_request)
+        expect(response).to redirect_to admin_request_url(info_request)
       end
 
       it 'should show a message that the incoming message has been updated' do
@@ -136,7 +136,7 @@ describe AdminOutgoingMessageController do
     context 'if the incoming message is not valid' do
 
       it 'should render the edit template' do
-        make_request({:id => @outgoing.id,
+        make_request({:id => outgoing.id,
                       :outgoing_message => {:prominence => 'fantastic',
                                             :prominence_reason => 'dull',
                                             :body => 'Some information please'}})
