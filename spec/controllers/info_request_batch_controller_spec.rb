5fe9d7afa188a4de6fbe4f3c4cfd525b05710607
Steven Day <steve@mysociety.org>
2017-04-03 13:06:10 +0100

    Modernise info_request_batch_controller specs

--- spec/controllers/info_request_batch_controller_spec.rb
+++ spec/controllers/info_request_batch_controller_spec.rb
@@ -1,53 +1,55 @@
 # -*- encoding : utf-8 -*-
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
-describe InfoRequestBatchController, "when showing a request" do
-
-  before do
-    @first_public_body = FactoryGirl.create(:public_body)
-    @second_public_body = FactoryGirl.create(:public_body)
-    @info_request_batch = FactoryGirl.create(:info_request_batch, :title => 'Matched title',
-                                             :body => 'Matched body',
-                                             :public_bodies => [@first_public_body,
-                                                                @second_public_body])
-    @first_request = FactoryGirl.create(:info_request, :info_request_batch => @info_request_batch,
-                                        :public_body => @first_public_body)
-    @second_request = FactoryGirl.create(:info_request, :info_request_batch => @info_request_batch,
-                                         :public_body => @second_public_body)
-    @default_params = {:id => @info_request_batch.id}
-  end
-
-  def make_request(params=@default_params)
-    get :show, params
-  end
-
-  it 'should be successful' do
-    make_request
-    expect(response).to be_success
-  end
-
-  it 'should assign an info_request_batch to the view' do
-    make_request
-    expect(assigns[:info_request_batch]).to eq(@info_request_batch)
-  end
-
-  context 'when the batch has not been sent' do
-
-    it 'should assign public_bodies to the view' do
-      make_request
-      expect(assigns[:public_bodies]).to eq([@first_public_body, @second_public_body])
+describe InfoRequestBatchController do
+  describe "#show" do
+    let(:first_public_body) { FactoryGirl.create(:public_body) }
+    let(:second_public_body) { FactoryGirl.create(:public_body) }
+    let!(:info_request_batch) do
+      FactoryGirl.create(:info_request_batch, :title => 'Matched title',
+                                              :body => 'Matched body',
+                                              :public_bodies => [first_public_body,
+                                                                 second_public_body])
     end
-  end
+    let(:params) { {:id => info_request_batch.id} }
+    let(:action) { get :show, params }
 
-  context 'when the batch has been sent' do
-
-    it 'should assign info_requests to the view' do
-      @info_request_batch.sent_at = Time.zone.now
-      @info_request_batch.save!
-      make_request
-      expect(assigns[:info_requests].sort).to eq([@first_request, @second_request])
+    it 'should be successful' do
+      action
+      expect(response).to be_success
     end
 
-  end
+    it 'should assign an info_request_batch to the view' do
+      action
+      expect(assigns[:info_request_batch]).to eq(info_request_batch)
+    end
 
+    context 'when the batch has not been sent' do
+      it 'should assign public_bodies to the view' do
+        action
+        expect(assigns[:public_bodies]).to eq([first_public_body, second_public_body])
+      end
+    end
+
+    context 'when the batch has been sent' do
+      let!(:first_request) do
+        FactoryGirl.create(:info_request, :info_request_batch => info_request_batch,
+                                          :public_body => first_public_body)
+      end
+      let!(:second_request) do
+        FactoryGirl.create(:info_request, :info_request_batch => info_request_batch,
+                                          :public_body => second_public_body)
+      end
+
+      before do
+        info_request_batch.sent_at = Time.zone.now
+        info_request_batch.save!
+      end
+
+      it 'should assign info_requests to the view' do
+        action
+        expect(assigns[:info_requests].sort).to eq([first_request, second_request])
+      end
+    end
+  end
 end
