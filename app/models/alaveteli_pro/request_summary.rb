539587450f13f644fefb39b347c40d20a83f2d98
Steven Day <steve@mysociety.org>
2017-04-11 14:23:50 +0100

    Add a create_from method to RequestSummary

--- app/models/alaveteli_pro/request_summary.rb
+++ app/models/alaveteli_pro/request_summary.rb
@@ -15,4 +15,58 @@
 
 class AlaveteliPro::RequestSummary < ActiveRecord::Base
   belongs_to :summarisable, polymorphic: true
+
+  ALLOWED_REQUEST_CLASSES = ["InfoRequest",
+                             "DraftInfoRequest",
+                             "InfoRequestBatch",
+                             "AlaveteliPro::DraftInfoRequestBatch"].freeze
+
+  def self.create_or_update_from(request)
+    unless ALLOWED_REQUEST_CLASSES.include?(request.class.name)
+      raise ArgumentError.new("Can't create a RequestSummary from " \
+                              "#{request.class.name} instances. Only " \
+                              "#{ALLOWED_REQUEST_CLASSES} are allowed.")
+    end
+    if request.request_summary
+      request.request_summary.update_from_request
+      request.request_summary
+    else
+      self.create_from(request)
+    end
+  end
+
+  def update_from_request
+    update_attributes(self.class.attributes_from_request(self.summarisable))
+  end
+
+  private
+
+  def self.create_from(request)
+    self.create(attributes_from_request(request))
+  end
+
+  def self.attributes_from_request(request)
+    {
+      title: request.title,
+      body: extract_request_body(request),
+      public_body_names: extract_request_public_body_names(request),
+      summarisable: request
+    }
+  end
+
+  def self.extract_request_body(request)
+    if request.class == InfoRequest
+      request.outgoing_messages.any? ? request.outgoing_messages.first.body : ""
+    else
+      request.body
+    end
+  end
+
+  def self.extract_request_public_body_names(request)
+    if [InfoRequest, DraftInfoRequest].include?(request.class)
+      request.public_body.name unless request.public_body.blank?
+    else
+      request.public_bodies.pluck(:name).join(" ")
+    end
+  end
 end
--- spec/models/alaveteli_pro/request_summary_spec.rb
+++ spec/models/alaveteli_pro/request_summary_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 RSpec.describe AlaveteliPro::RequestSummary, type: :model do
+  let(:public_bodies) { FactoryGirl.create_list(:public_body, 3) }
+  let(:public_body_names) { public_bodies.map(&:name).join(" ") }
+
   it "can belong to an info_request" do
     info_request = FactoryGirl.create(:info_request)
     summary = FactoryGirl.create(:request_summary, summarisable: info_request)
@@ -27,5 +30,107 @@ RSpec.describe AlaveteliPro::RequestSummary, type: :model do
     summary = FactoryGirl.create(:request_summary, summarisable: draft)
     expect(summary.summarisable).to eq draft
     expect(draft.request_summary).to eq summary
+  end
+
+  describe ".create_or_update_from" do
+    it "raises an ArgumentError if the request is of the wrong class" do
+      event = FactoryGirl.create(:info_request_event)
+      expect { AlaveteliPro::RequestSummary.create_or_update_from(event) }.
+        to raise_error(ArgumentError)
+    end
+
+    context "when the request already has a summary" do
+      it "updates the existing summary from a request" do
+        request = FactoryGirl.create(:info_request)
+        summary = FactoryGirl.create(:request_summary, summarisable: request)
+        public_body = FactoryGirl.create(:public_body)
+        request.title = "Updated title"
+        request.public_body = public_body
+        request.save
+        updated_summary = AlaveteliPro::RequestSummary.create_or_update_from(request)
+        expect(updated_summary.id).to eq summary.id
+        expect(updated_summary.title).to eq request.title
+        expect(updated_summary.public_body_names).to eq public_body.name
+        expect(updated_summary.summarisable).to eq request
+      end
+
+      it "updates the existing summary from a batch request" do
+        batch = FactoryGirl.create(
+          :info_request_batch,
+          public_bodies: public_bodies
+        )
+        summary = FactoryGirl.create(:request_summary, summarisable: batch)
+        public_body = FactoryGirl.create(:public_body)
+        batch.title = "Updated title"
+        batch.body = "Updated body"
+        batch.public_bodies << public_body
+        batch.save
+        updated_summary = AlaveteliPro::RequestSummary.create_or_update_from(batch)
+        expect(updated_summary.id).to eq summary.id
+        expect(updated_summary.title).to eq batch.title
+        expect(updated_summary.body).to eq batch.body
+        expect(updated_summary.public_body_names).to match /.*#{public_body.name}.*/
+        expect(updated_summary.summarisable).to eq batch
+      end
+    end
+
+    context "when the request doesn't already have a summary" do
+      it "creates a summary from an info_request" do
+        request = FactoryGirl.create(:info_request)
+        summary = AlaveteliPro::RequestSummary.create_or_update_from(request)
+        expect(summary.title).to eq request.title
+        expect(summary.body).to eq request.outgoing_messages.first.body
+        expect(summary.public_body_names).to eq request.public_body.name
+        expect(summary.summarisable).to eq request
+      end
+
+      it "creates a summary from a draft_info_request" do
+        draft = FactoryGirl.create(:draft_info_request)
+        summary = AlaveteliPro::RequestSummary.create_or_update_from(draft)
+        expect(summary.title).to eq draft.title
+        expect(summary.body).to eq draft.body
+        expect(summary.public_body_names).to eq draft.public_body.name
+        expect(summary.summarisable).to eq draft
+      end
+
+      it "creates a summary from an info_request_batch" do
+        batch = FactoryGirl.create(
+          :info_request_batch,
+          public_bodies: public_bodies
+        )
+        summary = AlaveteliPro::RequestSummary.create_or_update_from(batch)
+        expect(summary.title).to eq batch.title
+        expect(summary.body).to eq batch.body
+        expect(summary.public_body_names).to eq public_body_names
+        expect(summary.summarisable).to eq batch
+      end
+
+      it "creates a summary from an draft_info_request_batch" do
+        draft = FactoryGirl.create(
+          :draft_info_request_batch,
+          public_bodies: public_bodies
+        )
+        summary = AlaveteliPro::RequestSummary.create_or_update_from(draft)
+        expect(summary.title).to eq draft.title
+        expect(summary.body).to eq draft.body
+        expect(summary.public_body_names).to eq public_body_names
+        expect(summary.summarisable).to eq draft
+      end
+    end
+
+    describe "setting public body names" do
+      context "when the request is a draft with no public body" do
+        let(:draft) do
+          FactoryGirl.create(:draft_info_request, public_body: nil)
+        end
+        let(:summary) do
+          AlaveteliPro::RequestSummary.create_or_update_from(draft)
+        end
+
+        it "sets the public body names to nil" do
+          expect(summary.public_body_names).to be_nil
+        end
+      end
+    end
   end
 end
