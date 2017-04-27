7d1be890477cbee4dbedaac728642d923474899e
lizconlan <liz@mysociety.org>
2017-04-27 14:31:41 +0100

    Use html_escape and remove unwrapped_html_escape definition
    4.1's html_escape is identical to unwrapped_html_escape

--- lib/patch_tag_helper.rb
+++ lib/patch_tag_helper.rb
@@ -11,30 +11,9 @@ module ActionView::Helpers::TagHelper
     if value.is_a?(Array)
       value = escape ? safe_join(value, " ") : value.join(" ")
     else
-      value = escape ? ERB::Util.unwrapped_html_escape(value) : value.to_s
+      value = escape ? ERB::Util.html_escape(value) : value.to_s
     end
     %(#{key}="#{value.gsub('"'.freeze, '&quot;'.freeze)}")
   end
-
-end
-
-
-module ERB::Util
-
-  # Copied in from Rails 4.2 as it doesn't yet exist in 4.0
-
-  HTML_ESCAPE_REGEXP = /[&"'><]/
-
-  # HTML escapes strings but doesn't wrap them with an ActiveSupport::SafeBuffer.
-  # This method is not for public consumption! Seriously!
-  def unwrapped_html_escape(s) # :nodoc:
-    s = s.to_s
-    if s.html_safe?
-      s
-    else
-      s.gsub(HTML_ESCAPE_REGEXP, HTML_ESCAPE)
-    end
-  end
-  module_function :unwrapped_html_escape
 
 end
