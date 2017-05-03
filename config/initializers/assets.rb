8d6b722f10d55e1dd5059a03caada79862cfabb4
lizconlan <liz@mysociety.org>
2017-05-03 17:37:14 +0100

    Move asset config from application.rb to assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,50 @@
 # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.configure do
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+  # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version = '1.0'
+
+  # Enable the asset pipeline
+  config.assets.enabled = true
+
+  # Change the path that assets are served from
+  # config.assets.prefix = "/assets"
+
+  # Precompile additional assets.
+  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+  # Rails.application.config.assets.precompile += %w( search.js )
+
+  # These additional precompiled asset files are actually
+  # manifests that require the real asset files:
+  config.assets.precompile += ['admin.js',
+                               'profile-photos.js',
+                               'stats.js',
+                               'fancybox.css',
+                               'fancybox.js']
+  # ... while these are individual files that can't easily be
+  # grouped:
+  config.assets.precompile += ['jquery.Jcrop.min.css',
+                               'excanvas.min.js',
+                               'select-authorities.js',
+                               'new-request.js',
+                               'time_series.js',
+                               'fonts.css',
+                               'print.css',
+                               'admin.css',
+                               'ie6.css',
+                               'ie7.css',
+                               'bootstrap-dropdown.js',
+                               'widget.css',
+                               'request-attachments.js',
+                               'alaveteli_pro/request-index.js',
+                               'responsive/print.css',
+                               'responsive/application-lte-ie7.css',
+                               'responsive/application-ie8.css']
+
+  config.sass.load_paths += [
+    "#{Gem.loaded_specs['foundation-rails'].full_gem_path}/vendor/assets/stylesheets/foundation/components",
+    "#{Gem.loaded_specs['foundation-rails'].full_gem_path}/vendor/assets/stylesheets/foundation/"
+  ]
+
+end
