2d7b425ce333d469dd3dfed38264295746aba1de
Louise Crow <louise.crow@gmail.com>
2017-05-12 16:54:32 +0100

    Send email with account request.

--- app/controllers/alaveteli_pro/account_request_controller.rb
+++ app/controllers/alaveteli_pro/account_request_controller.rb
@@ -6,6 +6,15 @@ class AlaveteliPro::AccountRequestController < ApplicationController
   end
 
   def create
+    @account_request = AlaveteliPro::AccountRequest.new(params[:account_request])
+    if @account_request.valid?
+      AlaveteliPro::AccountMailer.account_request(@account_request)
+      flash[:notice] = _("Thanks for your interest in {{pro_site_name}}, we'll get back to you soon!",
+                         pro_site_name: AlaveteliConfiguration.pro_site_name)
+      redirect_to frontpage_url
+    else
+      render 'new'
+    end
   end
 
 end
