622e85e45d43202524fb2bf67795e4accc58974f
Steven Day <steve@mysociety.org>
2017-05-16 16:27:00 +0100

    Set a send_after time on Notifications when they're created

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -8,4 +8,17 @@ class Notification < ActiveRecord::Base
   enum frequency: [ INSTANTLY, DAILY ]
 
   validates_presence_of :info_request_event, :user, :frequency, :send_after
+
+  before_validation :calculate_send_after
+
+  # Set the send_at timestamp based on the chosen frequency
+  def calculate_send_after
+    unless self.persisted? || self.send_after.present?
+      if self.daily?
+        self.send_after = self.user.next_daily_summary_time
+      else
+        self.send_after = Time.zone.now
+      end
+    end
+  end
 end
