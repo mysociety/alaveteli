e448ea4eebbe11e8959d3fc76b06273018327009
lizconlan <liz@mysociety.org>
2017-05-24 11:01:26 +0100

    squash! Make Conditionallyprependlocale use its own copy of the class variables
    Fix comment

--- lib/routing_filters.rb
+++ lib/routing_filters.rb
@@ -14,8 +14,8 @@ module RoutingFilter
     end
     # And override the generation logic to use FastGettext.locale
     # rather than I18n.locale (the latter is what rails uses
-    # internally and may look like `en-US`, whereas the latter is
-    # was FastGettext and other POSIX-based systems use, and will
+    # internally and may look like `en-US`, whereas the former is
+    # what FastGettext and other POSIX-based systems use, and will
     # look like `en_US`
     def around_generate(*args, &block)
       params = args.extract_options!                              # this is because we might get a call like forum_topics_path(forum, topic, :locale => :en)
