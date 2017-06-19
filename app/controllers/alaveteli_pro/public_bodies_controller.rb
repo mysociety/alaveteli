5e8b378ec23d1be9207dff8b9511f2edf73cfd5f
Steven Day <steve@mysociety.org>
2017-06-19 16:49:45 +0100

    Exclude bodies to which FOI doesn't apply from pro search results

--- app/controllers/alaveteli_pro/public_bodies_controller.rb
+++ app/controllers/alaveteli_pro/public_bodies_controller.rb
@@ -3,8 +3,12 @@ class AlaveteliPro::PublicBodiesController < AlaveteliPro::BaseController
   def search
     query = params[:query] || ""
     xapian_results = typeahead_search(query, :model => PublicBody,
-                                             :exclude_tags => [ 'defunct' ])
+                                             :exclude_tags => [ 'defunct',
+                                                                'not_apply' ])
     results = xapian_results.present? ? xapian_results.results : []
+    # Exclude any bodies we can't make a request to (in addition to the ones
+    # we've already filtered out by the excluded tags above)
+    results.select! { |result| result[:model].is_requestable? }
     # Xapian's results include things we don't want to publish, like the
     # request email and api_key, so we map these results into a simpler object
     # with only some whitelisted attributes.
@@ -20,9 +24,11 @@ class AlaveteliPro::PublicBodiesController < AlaveteliPro::BaseController
       }
       # Render the result for the JS, so that we can use Rail's pluralisation,
       # translation, etc
-      result[:html] = render_to_string(partial: 'alaveteli_pro/public_bodies/search_result',
-                                       layout: false,
-                                       locals: { result: result })
+      result[:html] = render_to_string(
+        partial: 'alaveteli_pro/public_bodies/search_result',
+        layout: false,
+        locals: { result: result }
+      )
       result
     end
 
