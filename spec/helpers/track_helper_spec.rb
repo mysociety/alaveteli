419edb59903f8d08643439df8befd408c09919c2
Gareth Rees <gareth@garethrees.co.uk>
2017-06-20 12:28:08 +0100

    Organise specs by method
    This is a precursor to adding additional tests to this method.

--- spec/helpers/track_helper_spec.rb
+++ spec/helpers/track_helper_spec.rb
@@ -6,6 +6,70 @@ describe TrackHelper do
   include TrackHelper
   include LinkToHelper
 
+  describe '#unsubscribe_notice' do
+
+    context 'with a search track' do
+      let(:track_thing) { FactoryGirl.build(:search_track) }
+
+      it 'should create an unsubscribe notice' do
+        expected = %Q(You are no longer following <a href="/search/Example%20Query/newest/advanced">this search</a>.)
+        expect(unsubscribe_notice(track_thing)).to eq(expected)
+      end
+
+    end
+
+    context 'with a user track' do
+      let(:track_thing) { FactoryGirl.build(:user_track) }
+
+      it 'should create an unsubscribe notice' do
+        expected = %Q(You are no longer following '#{user_link(track_thing.tracked_user)}', a person.)
+        expect(unsubscribe_notice(track_thing)).to eq(expected)
+      end
+
+    end
+
+    context 'with a public body track' do
+      let(:track_thing) { FactoryGirl.build(:public_body_track) }
+
+      it 'should create an unsubscribe notice' do
+        expected = %Q(You are no longer following '#{public_body_link(track_thing.public_body)}', a public authority.)
+        expect(unsubscribe_notice(track_thing)).to eq(expected)
+      end
+
+    end
+
+    context 'with a successful request track' do
+      let(:track_thing) { FactoryGirl.build(:successful_request_track) }
+
+      it 'should create an unsubscribe notice' do
+        expected = %Q(You are no longer following <a href="/list/successful">successful requests</a>.)
+        expect(unsubscribe_notice(track_thing)).to eq(expected)
+      end
+
+    end
+
+    context 'with a new request track' do
+      let(:track_thing) { FactoryGirl.build(:new_request_track) }
+
+      it 'should create an unsubscribe notice' do
+        expected = %Q(You are no longer following <a href="/list">new requests</a>.)
+        expect(unsubscribe_notice(track_thing)).to eq(expected)
+      end
+
+    end
+
+    context 'with a request update track' do
+      let(:track_thing) { FactoryGirl.build(:request_update_track) }
+
+      it 'should create an unsubscribe notice' do
+        expected = %Q(You are no longer following '#{request_link(track_thing.info_request)}', a request.)
+        expect(unsubscribe_notice(track_thing)).to eq(expected)
+      end
+
+    end
+
+  end
+
   describe 'when displaying notices for a search track' do
 
     before do
@@ -25,11 +89,6 @@ describe TrackHelper do
     it 'should create a following subscription notice' do
       expected = %Q(You are now <a href="#{show_user_wall_path(:url_name => @track_thing.tracking_user.url_name)}">following</a> updates about <a href="/search/Example%20Query/newest/advanced">this search</a>.)
       expect(subscribe_follow_notice(@track_thing)).to eq(expected)
-    end
-
-    it 'should create an unsubscribe notice' do
-      expected = %Q(You are no longer following <a href="/search/Example%20Query/newest/advanced">this search</a>.)
-      expect(unsubscribe_notice(@track_thing)).to eq(expected)
     end
 
     it 'should create a description of the track' do
@@ -60,11 +119,6 @@ describe TrackHelper do
       expect(subscribe_follow_notice(@track_thing)).to eq(expected)
     end
 
-    it 'should create an unsubscribe notice' do
-      expected = %Q(You are no longer following '#{user_link(@track_thing.tracked_user)}', a person.)
-      expect(unsubscribe_notice(@track_thing)).to eq(expected)
-    end
-
     it 'should create a description of the track' do
       expected = %Q('#{user_link(@track_thing.tracked_user)}', a person)
       expect(track_description(@track_thing)).to eq(expected)
@@ -91,11 +145,6 @@ describe TrackHelper do
     it 'should create a following subscription notice' do
       expected = %Q(You are now <a href="#{show_user_wall_path(:url_name => @track_thing.tracking_user.url_name)}">following</a> updates about '#{public_body_link(@track_thing.public_body)}', a public authority.)
       expect(subscribe_follow_notice(@track_thing)).to eq(expected)
-    end
-
-    it 'should create an unsubscribe notice' do
-      expected = %Q(You are no longer following '#{public_body_link(@track_thing.public_body)}', a public authority.)
-      expect(unsubscribe_notice(@track_thing)).to eq(expected)
     end
 
     it 'should create a description of the track' do
@@ -125,11 +174,6 @@ describe TrackHelper do
       expect(subscribe_follow_notice(@track_thing)).to eq(expected)
     end
 
-    it 'should create an unsubscribe notice' do
-      expected = %Q(You are no longer following <a href="/list/successful">successful requests</a>.)
-      expect(unsubscribe_notice(@track_thing)).to eq(expected)
-    end
-
     it 'should create a description of the track' do
       expected = %Q(<a href="/list/successful">successful requests</a>)
       expect(track_description(@track_thing)).to eq(expected)
@@ -155,11 +199,6 @@ describe TrackHelper do
     it 'should create a following subscription notice' do
       expected = %Q(You are now <a href="#{show_user_wall_path(:url_name => @track_thing.tracking_user.url_name)}">following</a> updates about <a href="/list">new requests</a>.)
       expect(subscribe_follow_notice(@track_thing)).to eq(expected)
-    end
-
-    it 'should create an unsubscribe notice' do
-      expected = %Q(You are no longer following <a href="/list">new requests</a>.)
-      expect(unsubscribe_notice(@track_thing)).to eq(expected)
     end
 
     it 'should create a description of the track' do
@@ -188,11 +227,6 @@ describe TrackHelper do
     it 'should create a following subscription notice' do
       expected = %Q(You are now <a href="#{show_user_wall_path(:url_name => @track_thing.tracking_user.url_name)}">following</a> updates about '#{request_link(@track_thing.info_request)}', a request.)
       expect(subscribe_follow_notice(@track_thing)).to eq(expected)
-    end
-
-    it 'should create an unsubscribe notice' do
-      expected = %Q(You are no longer following '#{request_link(@track_thing.info_request)}', a request.)
-      expect(unsubscribe_notice(@track_thing)).to eq(expected)
     end
 
     it 'should create a description of the track' do
