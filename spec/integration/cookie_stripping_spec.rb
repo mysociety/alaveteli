efd6fbc2188710cc8f90c4e097c12419ec5c205c
lizconlan <liz@mysociety.org>
2017-07-20 17:10:09 +0100

    Prepend paths with / when using get
    Fixes spec failure:
      URI::InvalidURIError: the scheme http does not accept registry part
    
    Not sure what's changed in 4.2 to cause this

--- spec/integration/cookie_stripping_spec.rb
+++ spec/integration/cookie_stripping_spec.rb
@@ -5,7 +5,7 @@ require File.expand_path(File.dirname(__FILE__) + '/alaveteli_dsl')
 describe 'when making stripping cookies' do
 
   it 'should not set a cookie when no significant session data is set' do
-    get 'country_message'
+    get '/country_message'
     expect(response.headers['Set-Cookie']).to be_blank
   end
 
