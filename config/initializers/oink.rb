ae9f8c77ca6c9fe169ffa71203513ad8af68f5d3
Louise Crow <louise.crow@gmail.com>
2017-07-24 11:28:41 +0100

    Add oink memory debugging setup
    Use `ALAVETELI_USE_OINK=1` ti produce output like the following:
    
        Jul 24 11:29:50 Louises-MacBook-Pro rails[5291]: Oink Action: alaveteli_pro/info_requests#index
        Jul 24 11:29:50 Louises-MacBook-Pro rails[5291]: Memory usage: 3584716 | PID: 5291
        Jul 24 11:29:50 Louises-MacBook-Pro rails[5291]: Instantiation Breakdown: Total: 584 | PublicBody::Translation: 181 | InfoRequest: 77 | AlaveteliPro::RequestSummaryCategory: 65 | PublicBody: 50 | ActsAsXapian::ActsAsXapianJob: 48 | InfoRequestEvent: 33 | User: 28 | AlaveteliPro::RequestSummary: 26 | InfoRequestBatch: 22 | OutgoingMessage: 20 | HABTM_PublicBodies: 12 | HABTM_RequestSummaryCategories: 10 | Role: 6 | PublicBody::Version: 4 | DraftInfoRequest: 1 | AlaveteliPro::DraftInfoRequestBatch: 1
    
    This is useful for checking object allocation in integration specs.

---
+++ config/initializers/oink.rb
@@ -0,0 +1,5 @@
+# -*- encoding : utf-8 -*-
+# This can be added to either:
+# config/environments/test.rb
+# config/initializers/oink.rb
+Rails.application.middleware.use Oink::Middleware if ENV['ALAVETELI_USE_OINK']
