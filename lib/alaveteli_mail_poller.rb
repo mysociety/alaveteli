c2623666ed21b50d581de88d5e35b042657837a3
Louise Crow <louise.crow@gmail.com>
2017-07-27 14:43:30 +0100

    Add a mail polling class
    This polls a POP mailbox for messages, backing off
    when there aren't any to retrieve.

---
+++ lib/alaveteli_mail_poller.rb
@@ -0,0 +1,115 @@
+# -*- encoding : utf-8 -*-
+require 'net/pop'
+
+class AlaveteliMailPoller
+  include ConfigHelper
+
+  attr_accessor :settings, :pop3
+
+  def initialize(values = {})
+    defaults = { address:      AlaveteliConfiguration.pop_mailer_address,
+                 port:         AlaveteliConfiguration.pop_mailer_port,
+                 user_name:    AlaveteliConfiguration.pop_mailer_user_name,
+                 password:     AlaveteliConfiguration.pop_mailer_password,
+                 enable_ssl:   AlaveteliConfiguration.pop_mailer_enable_ssl }
+    self.settings = defaults.merge(values)
+  end
+
+  def poll_for_incoming
+    found_mail = false
+    start do |pop3|
+      pop3.each_mail do |popmail|
+        received = get_mail(popmail)
+        found_mail = found_mail || received
+      end
+    end
+    found_mail
+  end
+
+  # Make a poller and run poll_for_incoming in an endless loop,
+  # sleeping when there is nothing to do
+  def self.poll_for_incoming_loop
+    if AlaveteliConfiguration.production_mailer_retriever_method == 'pop'
+      poller = new
+      while true
+        sleep_seconds = 1
+        while !poller.poll_for_incoming
+          sleep sleep_seconds
+          sleep_seconds *= 2
+          sleep_seconds = 300 if sleep_seconds > 300
+        end
+      end
+    end
+  end
+
+  private
+
+  def get_mail(popmail)
+    unique_id = nil
+    raw_email = nil
+    received = false
+    begin
+      unique_id = popmail.unique_id
+      if retrieve?(unique_id)
+        raw_email = popmail.pop
+        RequestMailer.receive(raw_email)
+        received = true
+        popmail.delete
+      end
+    rescue Net::POPError, StandardError => error
+      if send_exception_notifications?
+        ExceptionNotifier.notify_exception(
+          error,
+          :data => { mail: raw_email,
+                     unique_id: unique_id }
+        )
+      end
+      record_error(unique_id, received, error)
+    end
+    received
+  end
+
+  def record_error(unique_id, received, error)
+    if unique_id
+      retry_at = received ? nil : Time.zone.now + 30.minutes
+      ime = IncomingMessageError.find_or_create_by!(unique_id: unique_id)
+      ime.retry_at = retry_at
+      ime.backtrace = error.backtrace.join("\n")
+      ime.save!
+    end
+  end
+
+  def failed?(unique_id)
+    IncomingMessageError.exists?(unique_id: unique_id)
+  end
+
+  def retry?(unique_id)
+    incoming_message_error = IncomingMessageError.
+      where(unique_id: unique_id).take
+    incoming_message_error &&
+    incoming_message_error.retry_at &&
+    incoming_message_error.retry_at < Time.zone.now
+  end
+
+  def retrieve?(unique_id)
+    !failed?(unique_id) || retry?(unique_id)
+  end
+
+  def start(&block)
+    # Start a POP3 session and ensure that it will be closed in any case.
+    unless block_given?
+      raise ArgumentError.new("AlaveteliMailPoller#start takes a block")
+    end
+
+    pop3 = Net::POP3.new(settings[:address], settings[:port], false)
+    pop3.enable_ssl(OpenSSL::SSL::VERIFY_NONE) if settings[:enable_ssl]
+    pop3.start(settings[:user_name], settings[:password])
+
+    yield pop3
+  ensure
+    if defined?(pop3) && pop3 && pop3.started?
+      pop3.finish
+    end
+  end
+
+end
---
+++ spec/lib/alaveteli_mail_poller_spec.rb
@@ -0,0 +1,267 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+require 'net/pop'
+
+describe AlaveteliMailPoller do
+  let(:mockpop3) { MockPOP3.new }
+  let(:poller) { AlaveteliMailPoller.new }
+
+  before do
+    allow(Net::POP3).to receive(:new).and_return(mockpop3)
+  end
+
+  describe '.poll_for_incoming_loop' do
+
+    context 'if the mail retrieval config is not set to poll a POP server' do
+      before do
+        allow(AlaveteliConfiguration).
+        to receive(:production_mailer_retriever_method).and_return("passive")
+      end
+
+      it 'does not create a poller' do
+        expect(AlaveteliMailPoller).not_to receive(:new)
+        AlaveteliMailPoller.poll_for_incoming_loop
+      end
+
+    end
+
+  end
+
+  describe '#poll_for_incoming' do
+
+    it 'starts and ends a session with the POP server' do
+      expect(MockPOP3).not_to be_started
+      expect(MockPOP3).to receive(:start).and_call_original
+      expect(MockPOP3).to receive(:finish).and_call_original
+      poller.poll_for_incoming
+      expect(MockPOP3).not_to be_started
+    end
+
+    context 'if there is no mail on the POP server' do
+      let(:mockpop3) { MockPOP3.new(0) }
+
+      it 'returns false' do
+        expect(poller.poll_for_incoming).to be false
+      end
+
+    end
+
+    context 'if there is mail on the POP server' do
+      let(:mockpop3) { MockPOP3.new(1) }
+
+      it 'returns true' do
+        expect(poller.poll_for_incoming).to be true
+      end
+
+      it 'sends the mail to RequestMailer.receive' do
+        expect(RequestMailer).to receive(:receive).
+          with(mockpop3.mails.first.pop)
+        poller.poll_for_incoming
+      end
+
+      it 'deletes the mail' do
+        poller.poll_for_incoming
+        expect(mockpop3.mails.first.deleted?).to be true
+      end
+
+      context 'if there is an error getting the unique ID of a mail' do
+
+        before do
+          allow(mockpop3.mails.first).
+            to receive(:unique_id).
+              and_raise(Net::POPError.new("Error code"))
+        end
+
+        it 'sends an exception notification' do
+          poller.poll_for_incoming
+          notification =  ActionMailer::Base.deliveries.first
+          expect(notification.subject).
+            to eq('[ERROR] (Net::POPError) "Error code"')
+        end
+
+      end
+
+      context 'if there is an error getting the text of a mail' do
+
+        before do
+          allow(mockpop3.mails.first).
+            to receive(:pop).
+              and_raise(Net::POPError.new("Error code"))
+        end
+
+        it 'sends an exception notification' do
+          poller.poll_for_incoming
+          notification =  ActionMailer::Base.deliveries.first
+          expect(notification.subject).
+            to eq('[ERROR] (Net::POPError) "Error code"')
+        end
+
+        context 'if there is already an error for this mail' do
+
+          before do
+            IncomingMessageError.
+              create(unique_id: mockpop3.mails.first.unique_id,
+                     retry_at: Time.zone.now)
+          end
+          it <<-EOF do
+            updates the error for the unique ID with a time of
+            30 minutes from now
+          EOF
+            poller.poll_for_incoming
+            errors = IncomingMessageError.
+                       where(unique_id: mockpop3.mails.first.unique_id)
+            expect(errors.size).to eq(1)
+            incoming_message_error = errors.first
+            expect(incoming_message_error.retry_at).
+              to be_within(5.seconds).of(Time.zone.now + 30.minutes)
+          end
+
+        end
+
+        context 'if there is no error for this mail' do
+
+          it 'stores the error for the unique ID with a time of 30
+              minutes from now' do
+            poller.poll_for_incoming
+            errors = IncomingMessageError.
+                       where(unique_id: mockpop3.mails.first.unique_id)
+            expect(errors.size).to eq(1)
+            incoming_message_error = errors.first
+            expect(incoming_message_error.retry_at).
+              to be_within(5.seconds).of(Time.zone.now + 30.minutes)
+          end
+
+        end
+
+      end
+
+      context 'if there is an error receiving the mail' do
+
+        before do
+          allow(RequestMailer).to receive(:receive).
+            with(mockpop3.mails.first.pop).
+              and_raise(ActiveRecord::StatementInvalid.new("Deadlock"))
+        end
+
+        it 'stores the unique ID with a retry time of 30 minutes from now' do
+          poller.poll_for_incoming
+          errors = IncomingMessageError.
+                     where(unique_id: mockpop3.mails.first.unique_id)
+          expect(errors.size).to eq(1)
+          incoming_message_error = errors.first
+          expect(incoming_message_error.retry_at).
+            to be_within(5.seconds).of(Time.zone.now + 30.minutes)
+        end
+
+        it 'sends an exception notification' do
+          poller.poll_for_incoming
+          notification =  ActionMailer::Base.deliveries.first
+          expect(notification.subject).
+            to eq('[ERROR] (ActiveRecord::StatementInvalid) "Deadlock"')
+        end
+
+        it 'returns false' do
+          expect(poller.poll_for_incoming).to be false
+        end
+
+      end
+
+      context 'if there is an error deleting the mail' do
+
+        before do
+          allow(mockpop3.mails.first).
+            to receive(:delete).
+              and_raise(Net::POPError.new("Error code"))
+        end
+
+        it 'stores the unique ID with no retry time' do
+          poller.poll_for_incoming
+          errors = IncomingMessageError.
+                     where(unique_id: mockpop3.mails.first.unique_id)
+          expect(errors.size).to eq(1)
+          incoming_message_error = errors.first
+          expect(incoming_message_error.retry_at).
+            to be nil
+        end
+
+        it 'sends the response notification and an exception notification' do
+          poller.poll_for_incoming
+          response_notification =  ActionMailer::Base.deliveries.first
+          expect(response_notification.subject).
+            to eq('New response to your FOI request - Holding pen')
+          exception_notification = ActionMailer::Base.deliveries.second
+          expect(exception_notification.subject).
+            to eq('[ERROR] (Net::POPError) "Error code"')
+        end
+
+        it 'returns true' do
+          expect(poller.poll_for_incoming).to be true
+        end
+
+      end
+
+      context 'if mail has previously failed' do
+
+        context 'and the mail has no retry time' do
+
+          before do
+            IncomingMessageError.create!(
+              unique_id: mockpop3.mails.first.unique_id
+            )
+          end
+
+          it 'does not send it to RequestMailer.receive' do
+            expect(RequestMailer).not_to receive(:receive)
+            poller.poll_for_incoming
+          end
+
+          it 'returns false' do
+            expect(poller.poll_for_incoming).to be false
+          end
+
+        end
+
+        context 'and the mail has not reached its retry time' do
+
+          before do
+            IncomingMessageError.create!(
+              unique_id: mockpop3.mails.first.unique_id,
+              retry_at: Time.now + 30.minutes
+            )
+          end
+
+          it 'does not send it to RequestMailer.receive' do
+            expect(RequestMailer).not_to receive(:receive)
+            poller.poll_for_incoming
+          end
+
+          it 'returns false' do
+            expect(poller.poll_for_incoming).to be false
+          end
+
+        end
+
+        context 'and the mail has reached its retry time' do
+
+          before do
+            IncomingMessageError.create!(
+              unique_id: mockpop3.mails.first.unique_id,
+              retry_at: Time.now - 30.minutes
+            )
+          end
+
+          it 'sends it to RequestMailer.receive' do
+            expect(RequestMailer).to receive(:receive).
+              with mockpop3.mails.first.pop
+            poller.poll_for_incoming
+          end
+
+          it 'returns true' do
+            expect(poller.poll_for_incoming).to be true
+          end
+
+        end
+      end
+    end
+  end
+end
