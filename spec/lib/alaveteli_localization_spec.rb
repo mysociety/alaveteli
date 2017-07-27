3705f1d6cb8732f046d5a0db78d060b335650326
lizconlan <liz@mysociety.org>
2017-07-27 15:46:54 +0100

    Improve setting of available_locales and default_locale

--- spec/lib/alaveteli_localization_spec.rb
+++ spec/lib/alaveteli_localization_spec.rb
@@ -9,46 +9,70 @@ describe AlaveteliLocalization do
       AlaveteliLocalization.set_locales('en_GB es', 'en_GB')
     end
 
-    it 'sets FastGettext.locale' do
-      expect(FastGettext.locale).to eq("en_GB")
-    end
+    context 'when dealing with FastGettext' do
 
-    it 'sets FastGettext.default_locale' do
-      expect(FastGettext.default_locale).to eq("en_GB")
-    end
-
-    it 'sets FastGettext.default_available_locales' do
-      expect(FastGettext.default_available_locales).to eq([:en_GB, :es])
-    end
-
-    context 'when enforce_available_locales is true' do
-
-      before do
-        I18n.config.enforce_available_locales = true
+      it 'sets FastGettext.locale' do
+        expect(FastGettext.locale).to eq("en_GB")
       end
 
-      it 'allows a new locale to be set as the default' do
-        AlaveteliLocalization.set_locales('nl en', 'nl')
-        expect(I18n.default_locale).to eq(:nl)
+      it 'sets FastGettext.locale correctly if given a hypheanted locale' do
+        AlaveteliLocalization.set_locales('en-GB es', 'en-GB')
+        expect(FastGettext.locale).to eq('en_GB')
+      end
+
+      it 'sets FastGettext.default_locale' do
+        expect(FastGettext.default_locale).to eq("en_GB")
+      end
+
+      it 'sets FastGettext.default_available_locales' do
+        expect(FastGettext.default_available_locales).to eq([:en_GB, :es])
       end
 
     end
 
-    it 'sets I18n.locale' do
-      expect(I18n.locale).to eq(:"en-GB")
-    end
+    context 'when dealing with I18n' do
 
-    it 'sets I18n.default_locale' do
-      expect(I18n.default_locale).to eq(:"en-GB")
-    end
+      context 'when enforce_available_locales is true' do
 
-    it 'sets I18n.available_locales' do
-      expect(I18n.available_locales).to eq([:"en-GB", :es])
+        before do
+          I18n.config.enforce_available_locales = true
+        end
+
+        it 'allows a new locale to be set as the default' do
+          AlaveteliLocalization.set_locales('nl en', 'nl')
+          expect(I18n.default_locale).to eq(:nl)
+        end
+
+      end
+
+      it 'sets I18n.locale' do
+        expect(I18n.locale).to eq(:"en-GB")
+      end
+
+      it 'sets I18n.default_locale' do
+        expect(I18n.default_locale).to eq(:"en-GB")
+      end
+
+      it 'sets I18n.available_locales' do
+        expect(I18n.available_locales).to eq([:"en-GB", :es])
+      end
+
     end
 
     it 'sets the locales for the custom routing filter' do
       expect(RoutingFilter::Conditionallyprependlocale.locales).
         to eq([:en_GB, :es])
+    end
+
+    it 'handles being passed a symbol as available_locales' do
+      AlaveteliLocalization.set_locales(:es, :es)
+      expect(AlaveteliLocalization.available_locales).to eq([:es])
+    end
+
+    it 'handles being passed hyphenated strings as available_locales' do
+      AlaveteliLocalization.set_locales('en-GB nl-BE es', :es)
+      expect(AlaveteliLocalization.available_locales).
+        to eq([:"en_GB", :"nl_BE", :es])
     end
 
   end
