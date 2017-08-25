7af8d78501396a43423d8226afcb14ad979e0a2a
Gareth Rees <gareth@garethrees.co.uk>
2017-08-25 16:16:19 +0100

    Add MVP of SubscriptionsController#create
    Subscribes a Stripe::Customer to a Stripe::Plan.
    
    * Doesn’t handle stripe errors
    * Doesn’t handle failures in assigning roles or pro account
    * Most of the logic should be moved to a service class
    * Hard coded plan because we don’t have more than one plan yet. We’ll
      need to have some way of performing different operations on the User
      depending on the plan they subscribe to. Batch needs more than just
      a role set.

--- spec/controllers/alaveteli_pro/subscriptions_controller_spec.rb
+++ spec/controllers/alaveteli_pro/subscriptions_controller_spec.rb
@@ -1,7 +1,18 @@
 # -*- encoding : utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require 'stripe_mock'
 
 describe AlaveteliPro::SubscriptionsController do
+  let(:stripe_helper) { StripeMock.create_test_helper }
+
+  before do
+    StripeMock.start
+    stripe_helper.create_plan(id: 'pro', amount: 1000)
+  end
+
+  after do
+    StripeMock.stop
+  end
 
   describe 'POST #create' do
 
@@ -20,10 +31,40 @@ describe AlaveteliPro::SubscriptionsController do
 
     context 'with a signed-in user' do
       let(:user) { FactoryGirl.create(:user) }
+      let(:token) { stripe_helper.generate_card_token }
 
       before do
         session[:user_id] = user.id
-        post :create
+        session[:user_id] = user.id
+        post :create, 'stripeToken' => token,
+                      'stripeTokenType' => 'card',
+                      'stripeEmail' => user.email,
+                      'plan_id' => 'pro'
+      end
+
+      it 'finds the token' do
+        expect(assigns(:token).id).to eq(token)
+      end
+
+      it 'creates a new Stripe customer' do
+        expect(assigns(:customer).email).to eq(user.email)
+      end
+
+      it 'subscribes the user to the plan' do
+        expected = { user: assigns(:customer).id,
+                     plan: 'pro' }
+        actual = { user: assigns(:subscription).customer,
+                   plan: assigns(:subscription).plan.id }
+        expect(actual).to eq(expected)
+      end
+
+      it 'creates a pro account for the user' do
+        expect(user.pro_account).to be_present
+      end
+
+      it 'stores the stripe_customer_id in the pro_account' do
+        expect(user.pro_account.stripe_customer_id).
+          to eq(assigns(:customer).id)
       end
 
       it 'redirects to the pro dashboard' do
