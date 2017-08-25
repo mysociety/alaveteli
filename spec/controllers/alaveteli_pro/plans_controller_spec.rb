bf8311b16e3c19cce05bb63a82eb29a3f9560271
Gareth Rees <gareth@garethrees.co.uk>
2017-08-25 13:50:45 +0100

    Add authentication to PlansController
    For our MVP, we’re expecting an existing User to be signed in so that
    we can apply the subscription to their account.
    
    In the future it would be nice to create a User record and the related
    subscription in one form.

--- spec/controllers/alaveteli_pro/plans_controller_spec.rb
+++ spec/controllers/alaveteli_pro/plans_controller_spec.rb
@@ -5,16 +5,35 @@ describe AlaveteliPro::PlansController do
 
   describe 'GET #show' do
 
-    before do
-      get :show, id: 'pro'
+    context 'without a signed-in user' do
+
+      before do
+        get :show, id: 'pro'
+      end
+
+      it 'redirects to the login form' do
+        expect(response).
+          to redirect_to(signin_path(:token => PostRedirect.last.token))
+      end
+
     end
 
-    it 'renders the plan page' do
-      expect(response).to render_template(:show)
-    end
+    context 'with a signed-in user' do
+      let(:user) { FactoryGirl.create(:user) }
 
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
+      before do
+        session[:user_id] = user.id
+        get :show, id: 'pro'
+      end
+
+      it 'renders the plan page' do
+        expect(response).to render_template(:show)
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+
     end
 
   end
