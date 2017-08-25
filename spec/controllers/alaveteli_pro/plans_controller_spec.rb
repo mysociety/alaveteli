50ef40ab935b7996d2eeb325dc9f493c2805371f
Gareth Rees <gareth@garethrees.co.uk>
2017-08-25 14:57:07 +0100

    Add basic Stripe checkout
    * Finds the specified plan. This is pulled directly from Stripe, so in
      the future we’ll want to have a local cache of this.
    * Renders Stripe checkout with the plan amount and user email pre-set.
      I’ve just dumped the plan amount / 100 in the view for now, but how
      we convert to pounds (rather than pence) will depend what the page
      design ends up looking like.

--- spec/controllers/alaveteli_pro/plans_controller_spec.rb
+++ spec/controllers/alaveteli_pro/plans_controller_spec.rb
@@ -1,7 +1,12 @@
 # -*- encoding : utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require 'stripe_mock'
 
 describe AlaveteliPro::PlansController do
+  before { StripeMock.start }
+  after { StripeMock.stop }
+  let(:stripe_helper) { StripeMock.create_test_helper }
+  let!(:pro_plan) { stripe_helper.create_plan(id: 'pro', amount: 1000) }
 
   describe 'GET #show' do
 
@@ -24,6 +29,10 @@ describe AlaveteliPro::PlansController do
       before do
         session[:user_id] = user.id
         get :show, id: 'pro'
+      end
+
+      it 'finds the specified plan' do
+        expect(assigns(:plan)).to eq(pro_plan)
       end
 
       it 'renders the plan page' do
