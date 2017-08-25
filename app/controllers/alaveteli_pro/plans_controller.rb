37482305364833e71e40adff848ee851d8dba557
Gareth Rees <gareth@garethrees.co.uk>
2017-08-25 13:50:45 +0100

    Add authentication to PlansController
    For our MVP, we’re expecting an existing User to be signed in so that
    we can apply the subscription to their account.
    
    In the future it would be nice to create a User record and the related
    subscription in one form.

--- app/controllers/alaveteli_pro/plans_controller.rb
+++ app/controllers/alaveteli_pro/plans_controller.rb
@@ -1,6 +1,19 @@
 # -*- encoding : utf-8 -*-
 # Does not inherit from AlaveteliPro::BaseController as is pre-login
 class AlaveteliPro::PlansController < ApplicationController
+  before_filter :authenticate
+
   def show
+  end
+
+  private
+
+  def authenticate
+    post_redirect_params = {
+      :web => _('To upgrade your account'),
+      :email => _('To upgrade your account'),
+      :email_subject => _('To upgrade your account') }
+
+    authenticated?(post_redirect_params)
   end
 end
