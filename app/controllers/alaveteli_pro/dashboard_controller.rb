dab493ecb496c95d5fbac926865a64f3dbaf3474
lizconlan <liz@mysociety.org>
2017-08-25 13:46:54 +0100

    Remove N+1 query for phase counts
    Uses a single SELECT COUNT query with a GROUP BY clause to fetch a
    summary of the current user's requests per phase rather than running a
    COUNT SELECT for every possible phase from within the _projects partial.
    
    Should cut down the number of calls per transaction on
    AlaveteliPro::Request summary from 9.4 to 1 as I don't think we load the
    request summary data directly here.

--- app/controllers/alaveteli_pro/dashboard_controller.rb
+++ app/controllers/alaveteli_pro/dashboard_controller.rb
@@ -13,5 +13,13 @@ class AlaveteliPro::DashboardController < AlaveteliPro::BaseController
     @page = 1 if @page < 1
     @per_page = 10
     @activity_list = AlaveteliPro::ActivityList::List.new(@user, @page, @per_page)
+    @phase_counts = @user.request_summaries.
+                      joins(:request_summary_categories).
+                      references(:request_summary_categories).
+                      group("request_summary_categories.slug").
+                      count("request_summary_categories.id")
+    @phase_counts['total'] = @phase_counts.values.reduce(0, :+)
+    @phase_counts['not_drafts'] =
+      @phase_counts['total'] - @phase_counts['draft'].to_i
   end
 end
