2837f02ae6d62523600a25f3eb64f9eef9b7d35a
Gareth Rees <gareth@garethrees.co.uk>
2017-08-29 12:38:18 +0100

    Calculate price as price + vat
    Adds `AlaveteliPro::PlanWithTax` to add 20% tax to the
    `Stripe::Plan#amount`. We'll want to make this configurable (and ideally
    expose `AlaveteliPro::PlanWithTax.tax_percent`) for use elsewhere.
    
    See
    https://makandracards.com/makandra/1505-invoices-how-to-properly-round-and-calculate-totals

---
+++ spec/models/alaveteli_pro/plan_with_tax_spec.rb
@@ -0,0 +1,20 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe AlaveteliPro::PlanWithTax do
+  let(:plan) { OpenStruct.new(amount: 833) }
+  subject { described_class.new(plan) }
+
+  describe '#amount_with_tax' do
+
+    it 'adds 20% tax to the plan amount' do
+      expect(subject.amount_with_tax).to eq(1000)
+    end
+
+  end
+
+  it 'delegates to the stripe plan' do
+    expect(subject.amount).to eq(833)
+  end
+
+end
