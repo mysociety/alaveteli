1e013979fdbb91046f153bc2955cb6457e95a4fb
lizconlan <liz@mysociety.org>
2017-09-04 16:37:32 +0100

    List the user's payment options
    Most people will only have a single payment option as we will take a
    payment card when they sign up and we're not providing a payment
    management interface but it's technically possible and easy to support.

--- app/helpers/alaveteli_pro/account_helper.rb
+++ app/helpers/alaveteli_pro/account_helper.rb
@@ -14,4 +14,16 @@ module AlaveteliPro::AccountHelper
     end
   end
 
+  def card_expiry_message(month, year)
+    if month == Date.today.month && year == Date.today.year
+      _('Expires soon')
+    end
+  end
+
+  def card_default_message(source, default_id)
+    if source == default_id
+      _('Default')
+    end
+  end
+
 end
