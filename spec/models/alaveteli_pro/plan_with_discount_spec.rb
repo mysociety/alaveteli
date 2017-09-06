d8dd63e45c124b87dc79094b9baffcb97f7fc975
lizconlan <liz@mysociety.org>
2017-09-06 12:18:54 +0100

    Display applied discount coupons on account page
    If a user has an applied discount coupon, it should show up on their
    account page and the amount should be deducted from their next payment
    information so that it doesn't appear we are going to charge them the
    full amount.
    
    The assumption being made here is that discount coupons will be manually
    applied to subscriptions via the Stripe dashboard rather than redeemed
    through the site. Also that any coupon that's applied to the account
    and marked as valid is fine to use - observation suggest that one-off
    coupons are automatically deleted from subscriptions by the Stripe
    service so hopefully there are no other checks needed here.
    
    NB - this just calculates a for-display value, there is no effect on the
    amount charged by Stripe.

---
+++ spec/models/alaveteli_pro/plan_with_discount_spec.rb
@@ -0,0 +1,43 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe AlaveteliPro::PlanWithDiscount do
+  let(:plan) { OpenStruct.new(amount: 833) }
+  let(:subscription) { OpenStruct.new(plan: plan) }
+  subject { described_class.new(subscription) }
+
+  describe '#amount' do
+
+    context 'no discount is set' do
+      it 'returns the original stripe plan amount' do
+        expect(subject.amount).to eq(833)
+      end
+    end
+
+    it 'applies a percentage discount correctly' do
+      coupon = OpenStruct.new(id: "50_off", object: "coupon", amount_off: nil,
+                              duration: "forever", max_redemptions: nil,
+                              percent_off: 50, redeem_by: nil,
+                              times_redeemed: 1, valid: true)
+      discount = OpenStruct.new(coupon: coupon)
+      subscription = OpenStruct.new(plan: plan, discount: discount)
+      subject = described_class.new(subscription)
+
+      expect(subject.amount).to eq(416.5)
+    end
+
+    it 'applies an amount_off discount correctly' do
+      coupon = OpenStruct.new(id: "2_off", object: "coupon", amount_off: 200,
+                              duration: "forever", max_redemptions: nil,
+                              percent_off: nil, redeem_by: nil,
+                              times_redeemed: 1, valid: true)
+      discount = OpenStruct.new(coupon: coupon)
+      subscription = OpenStruct.new(plan: plan, discount: discount)
+      subject = described_class.new(subscription)
+
+      expect(subject.amount).to eq(633.0)
+    end
+
+  end
+
+end
