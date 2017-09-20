4f3eb986c525ba045e19ecb6c57c08aca6ac6e68
Gareth Rees <gareth@garethrees.co.uk>
2017-09-20 16:49:30 +0100

    Add SubscriptionsController#destroy
    Allow cancellation of a Subscription
    
    Also renames `subscription_path` to `profile_subscription_path` to avoid
    route conflict.
    
    Handle Stripe errors
    
    Handle user trying to cancel a subscription that doesn't belong to them.

--- app/controllers/alaveteli_pro/plans_controller.rb
+++ app/controllers/alaveteli_pro/plans_controller.rb
@@ -28,7 +28,7 @@ class AlaveteliPro::PlansController < ApplicationController
     # TODO: This doesn't take the plan in to account
     if @user.pro_account.try(:active?)
       flash[:error] = _('You are already subscribed to this plan')
-      redirect_to subscription_path
+      redirect_to profile_subscription_path
     end
   end
 end
