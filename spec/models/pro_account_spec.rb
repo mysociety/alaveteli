cc8f2007802de8d900c0a8fcc65ccf0709644d59
Gareth Rees <gareth@garethrees.co.uk>
2017-09-20 13:20:24 +0100

    Add ProAccount#active?
    Hides the details away from finding out whether a `ProAccount` has an
    active Stripe Subscription.
    
    A Subscription is still "active" if it is due to expire at the month
    end, as it should still be functional until the end date.

--- spec/models/pro_account_spec.rb
+++ spec/models/pro_account_spec.rb
@@ -45,4 +45,51 @@ describe ProAccount do
 
   end
 
+  describe '#active?' do
+
+    subject { FactoryGirl.create(:pro_account) }
+
+    let(:customer) do
+      Stripe::Customer.create(email: subject.user.email,
+                              source: stripe_helper.generate_card_token)
+    end
+
+    let(:plan) do
+      stripe_helper.create_plan(id: 'pro', amount: 1000)
+    end
+
+    before do
+      subject.update!(stripe_customer_id: customer.id)
+    end
+
+    it 'returns true if there is an active subscription' do
+      Stripe::Subscription.create(customer: customer,
+                                  plan: plan.id,
+                                  tax_percent: 20.0)
+      expect(subject.active?).to eq(true)
+    end
+
+    it 'returns true if there is an expiring subscription' do
+      subscription =
+        Stripe::Subscription.create(customer: customer,
+                                    plan: plan.id,
+                                    tax_percent: 20.0)
+      subscription.delete(at_period_end: true)
+      expect(subject.active?).to eq(true)
+    end
+
+    it 'returns false if an existing subscription is cancelled' do
+      subscription =
+        Stripe::Subscription.create(customer: customer,
+                                    plan: plan.id)
+      subscription.delete
+      expect(subject.active?).to eq(false)
+    end
+
+    it 'returns false if there are no active subscriptions' do
+      expect(subject.active?).to eq(false)
+    end
+
+  end
+
 end
