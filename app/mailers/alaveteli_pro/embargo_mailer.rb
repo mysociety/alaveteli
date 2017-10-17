a05307e2bcd153761ff1440988b03f9dfc5a36d3
lizconlan <liz@mysociety.org>
2017-10-17 17:45:10 +0100

    Add an embargo expired alert to EmbargoMailer
    Sends an alert email to all pro users who are not opted in to the
    summarised notifications email when an embargo on one or more of their
    requests has expired.
    
    Requires that the task to automatically expire embargoes that are past
    their `publish_at` date has completed.

--- app/mailers/alaveteli_pro/embargo_mailer.rb
+++ app/mailers/alaveteli_pro/embargo_mailer.rb
@@ -35,12 +35,54 @@ module AlaveteliPro
       end
     end
 
+    def self.alert_expired
+      expired_info_requests =
+        InfoRequest.
+          embargo_expired_today.
+            where(use_notifications: false).
+              group_by(&:user)
+
+      expired_info_requests.each do |user, info_requests|
+        info_requests.reject! do |info_request|
+          alert_event_id = info_request.last_embargo_expire_event.id
+          UserInfoRequestSentAlert.where(
+            info_request_id: info_request.id,
+            user_id: user.id,
+            alert_type: 'embargo_expired_1',
+            info_request_event_id: alert_event_id).exists?
+        end
+        next if info_requests.empty?
+        expired_alert(user, info_requests).deliver_now
+        info_requests.each do |info_request|
+          alert_event_id = info_request.last_embargo_expire_event.id
+          UserInfoRequestSentAlert.create(
+            user: user,
+            info_request: info_request,
+            alert_type: 'embargo_expired_1',
+            info_request_event_id: alert_event_id)
+        end
+      end
+    end
+
     def expiring_alert(user, info_requests)
       @user = user
       @info_requests = info_requests
       subject = n_(
         "{{count}} request will be made public on {{site_name}} this week",
         "{{count}} requests will be made public on {{site_name}} this week",
+        info_requests.count,
+        :site_name => AlaveteliConfiguration.site_name.html_safe,
+        :count => info_requests.count)
+      auto_generated_headers
+      mail_user(@user, subject)
+    end
+
+    def expired_alert(user, info_requests)
+      @user = user
+      @info_requests = info_requests
+      subject = n_(
+        "{{count}} request has been made public on {{site_name}}",
+        "{{count}} requests have been made public on {{site_name}}",
         info_requests.count,
         :site_name => AlaveteliConfiguration.site_name.html_safe,
         :count => info_requests.count)
