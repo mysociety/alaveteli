46fc05cd890539ff4b76cb6612610cfcfb482be8
Graeme Porteous <graeme@rgbp.co.uk>
2017-10-24 17:14:19 +0100

    Pass draft selected public body to selectize element

--- app/helpers/alaveteli_pro/public_bodies_helper.rb
+++ app/helpers/alaveteli_pro/public_bodies_helper.rb
@@ -10,9 +10,14 @@ module AlaveteliPro::PublicBodiesHelper
       about: _('About {{public_body_name}}', public_body_name: body.name)
     }
 
+    # Work out which render method we need to use so this can be called from
+    # controllers and views
+    render_method = respond_to?(:render_to_string) ? :render_to_string : :render
+
     # Render the result for the JS, so that we can use Rails's pluralisation,
     # translation, etc
-    result[:html] = render_to_string(
+    result[:html] = public_send(
+      render_method,
       partial: 'alaveteli_pro/public_bodies/search_result',
       layout: false,
       locals: { result: result }
