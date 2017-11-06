fc4542bc7bf508f0489456a6e9f87c6a1a928a7d
Graeme Porteous <graeme@rgbp.co.uk>
2017-11-06 13:18:50 +0000

    Refactor subscription discount specs
    Using contexts/lets to removing duplicate lines

--- spec/models/alaveteli_pro/subscription_with_discount_spec.rb
+++ spec/models/alaveteli_pro/subscription_with_discount_spec.rb
@@ -3,36 +3,41 @@ require 'spec_helper'
 
 describe AlaveteliPro::SubscriptionWithDiscount do
   let(:plan) { OpenStruct.new(amount: 833) }
-  let(:subscription) { OpenStruct.new(plan: plan, discount: nil) }
-  subject { described_class.new(subscription) }
-
-  def mock_subscription(coupon)
-    discount = OpenStruct.new(coupon: coupon)
+  let(:coupon) { nil }
+  let(:trial) { nil }
+  let(:subscription) do
+    discount = OpenStruct.new(coupon: coupon) if coupon
     OpenStruct.new(plan: plan, discount: discount)
   end
+
+  subject { described_class.new(subscription) }
 
   describe '#amount' do
 
     context 'no discount is set' do
-
       it 'returns the original stripe plan amount' do
         expect(subject.amount).to eq(833)
       end
-
     end
 
-    it 'applies a percentage discount correctly' do
-      coupon = OpenStruct.new(id: "50_off", percent_off: 50, valid: true)
-      subject = described_class.new(mock_subscription(coupon))
+    context 'with percentage coupon' do
+      let(:coupon) do
+        OpenStruct.new(id: '50_off', percent_off: 50, valid: true)
+      end
 
-      expect(subject.amount).to eq(416)
+      it 'applies a percentage discount correctly' do
+        expect(subject.amount).to eq(416)
+      end
     end
 
-    it 'applies an amount_off discount correctly' do
-      coupon = OpenStruct.new(id: "2_off", amount_off: 200, valid: true)
-      subject = described_class.new(mock_subscription(coupon))
+    context 'with fixed amount coupon' do
+      let(:coupon) do
+        OpenStruct.new(id: '2_off', amount_off: 200, valid: true)
+      end
 
-      expect(subject.amount).to eq(633)
+      it 'applies an amount_off discount correctly' do
+        expect(subject.amount).to eq(633)
+      end
     end
 
   end
@@ -40,33 +45,29 @@ describe AlaveteliPro::SubscriptionWithDiscount do
   describe '#discounted?' do
 
     context 'there is no discount' do
-
       it 'returns false' do
         expect(subject.discounted?).to be false
       end
-
     end
 
     context 'the discount is invalid' do
-
-      it 'returns false' do
-        coupon = OpenStruct.new(id: "50_off", percent_off: 50, valid: false)
-        subject = described_class.new(mock_subscription(coupon))
-
-        expect(subject.discounted?).to be false
+      let(:coupon) do
+        OpenStruct.new(id: '50_off', percent_off: 50, valid: false)
       end
 
+      it 'returns false' do
+        expect(subject.discounted?).to be false
+      end
     end
 
     context 'a valid discount applies' do
-
-      it 'returns true' do
-        coupon = OpenStruct.new(id: "50_off", percent_off: 50, valid: true)
-        subject = described_class.new(mock_subscription(coupon))
-
-        expect(subject.discounted?).to be true
+      let(:coupon) do
+        OpenStruct.new(id: '50_off', percent_off: 50, valid: true)
       end
 
+      it 'returns true' do
+        expect(subject.discounted?).to be true
+      end
     end
 
   end
@@ -74,36 +75,33 @@ describe AlaveteliPro::SubscriptionWithDiscount do
   describe '#free?' do
 
     context 'the price is > 0' do
-
-      it 'returns false' do
-        coupon = OpenStruct.new(id: "50_off", percent_off: 50, valid: true)
-        subject = described_class.new(mock_subscription(coupon))
-
-        expect(subject.free?).to be false
+      let(:coupon) do
+        OpenStruct.new(id: '50_off', percent_off: 50, valid: true)
       end
 
+      it 'returns false' do
+        expect(subject.free?).to be false
+      end
     end
 
     context 'there is a 100% discount' do
-
-      it 'returns true' do
-        coupon = OpenStruct.new(id: "100_off", percent_off: 100, valid: true)
-        subject = described_class.new(mock_subscription(coupon))
-
-        expect(subject.free?).to be true
+      let(:coupon) do
+        OpenStruct.new(id: '100_off', percent_off: 100, valid: true)
       end
 
+      it 'returns true' do
+        expect(subject.free?).to be true
+      end
     end
 
     context 'there is a discount that zeros the price' do
-
-      it 'returns true' do
-        coupon = OpenStruct.new(id: "833_off", amount_off: 833, valid: true)
-        subject = described_class.new(mock_subscription(coupon))
-
-        expect(subject.free?).to be true
+      let(:coupon) do
+        OpenStruct.new(id: '833_off', amount_off: 833, valid: true)
       end
 
+      it 'returns true' do
+        expect(subject.free?).to be true
+      end
     end
 
   end
