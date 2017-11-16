5426a44d479a82fe1617c814bc39cfec30b280ff
Graeme Porteous <graeme@rgbp.co.uk>
2017-11-16 09:09:19 +0000

    Fix GeoIP error if remote gaze host is blocked
    If host is blocked by a firewall it will result in
    an `Errno::EHOSTDOWN` exception being raised

--- lib/quiet_opener.rb
+++ lib/quiet_opener.rb
@@ -8,12 +8,14 @@ def quietly_try_to_open(url, timeout=60)
   begin
     result = open(url, :read_timeout => timeout).read.strip
   rescue OpenURI::HTTPError,
-      SocketError,
-      Errno::ETIMEDOUT,
-      Errno::ECONNREFUSED,
-      Errno::EHOSTUNREACH,
-      Errno::ECONNRESET,
-      Timeout::Error => exception
+         SocketError,
+         Errno::ETIMEDOUT,
+         Errno::ECONNREFUSED,
+         Errno::EHOSTDOWN,
+         Errno::ENETUNREACH,
+         Errno::EHOSTUNREACH,
+         Errno::ECONNRESET,
+         Timeout::Error => exception
     e = Exception.new("Unable to open third-party URL #{url}: #{exception.message}")
     e.set_backtrace(exception.backtrace)
     # Send a notification if in a request context
