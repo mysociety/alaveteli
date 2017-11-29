e8e725695da928da390a5c348099486060cef510
lizconlan <liz@mysociety.org>
2017-11-29 17:01:01 +0000

    Handle Stripe signature checking
    Stripe automatically takes care of the replay attack check

--- app/controllers/alaveteli_pro/stripe_webhooks_controller.rb
+++ app/controllers/alaveteli_pro/stripe_webhooks_controller.rb
@@ -2,10 +2,35 @@
 # Does not inherit from AlaveteliPro::BaseController because it doesn't need to
 class AlaveteliPro::StripeWebhooksController < ApplicationController
 
+  before_action :read_event_notification
+
   def receive
     # send a 200 ok to acknowlege receipt of the webhook
     # https://stripe.com/docs/webhooks#responding-to-a-webhook
-    head :ok
+    render json: {}, status: 200
+  end
+
+  private
+
+  def read_event_notification
+    payload = request.body.read
+    sig_header = request.headers['HTTP_STRIPE_SIGNATURE']
+    endpoint_secret = AlaveteliConfiguration.stripe_webhook_secret
+    @stripe_event = nil
+
+    begin
+      @stripe_event = Stripe::Webhook.construct_event(
+        payload, sig_header, endpoint_secret
+      )
+    rescue JSON::ParserError => e
+      # Invalid payload, reject the webhook
+      render json: { error: e.message }, status: 400
+      return
+    rescue Stripe::SignatureVerificationError => e
+      # Invalid signature, reject the webhook
+      render json: { error: e.message }, status: 401
+      return
+    end
   end
 
 end
--- spec/controllers/alaveteli_pro/stripe_webhooks_controller_spec.rb
+++ spec/controllers/alaveteli_pro/stripe_webhooks_controller_spec.rb
@@ -5,11 +5,89 @@ describe AlaveteliPro::StripeWebhooksController do
 
   describe '#receive' do
 
-    it 'returns a successful response' do
+    let(:config_secret) { 'whsec_secret' }
+    let(:signing_secret) { config_secret }
+
+    before do
+      config = MySociety::Config.load_default
+      config['STRIPE_WEBHOOK_SECRET'] = config_secret
+    end
+
+    def encode_hmac(key, value)
+      # this is how Stripe signed headers work, method borrowed from:
+      # https://github.com/stripe/stripe-ruby/blob/v3.4.1/lib/stripe/webhook.rb#L24-L26
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), key, value)
+    end
+
+    let(:payload) { '{"type": "invoice.payment_failed"}' }
+
+    def signed_headers
+      timestamp = Time.zone.now.to_i
+      secret = encode_hmac(signing_secret, "#{timestamp}.#{payload}")
+      {
+        'HTTP_STRIPE_SIGNATURE' => "t=#{timestamp},v1=#{secret}",
+        'CONTENT_TYPE' => 'application/json'
+      }
+    end
+
+    it 'returns a successful response for correctly signed headers' do
       with_feature_enabled(:alaveteli_pro) do
-        post :receive
+        request.headers.merge! signed_headers
+        post :receive, payload
         expect(response).to be_success
       end
+    end
+
+    context 'the secret is not in the request' do
+
+      it 'returns a 401 Unauthorized response' do
+        with_feature_enabled(:alaveteli_pro) do
+          post :receive, payload
+          expect(response.status).to eq(401)
+        end
+      end
+
+    end
+
+    context 'the secret_key does not match' do
+
+      let(:signing_secret) { 'whsec_fake' }
+
+      it 'returns 401 Unauthorized response' do
+        with_feature_enabled(:alaveteli_pro) do
+          request.headers.merge! signed_headers
+          post :receive, payload
+          expect(response.status).to eq(401)
+        end
+      end
+
+    end
+
+    context 'the timestamp is stale (possible replay attack)' do
+
+      let!(:stale_headers) do
+        time_travel_to(1.hour.ago) { signed_headers }
+      end
+
+      before do
+        request.headers.merge! stale_headers
+        post :receive, payload
+      end
+
+      it 'returns a 401 Unauthorized response' do
+        with_feature_enabled(:alaveteli_pro) do
+          expect(response.status).to eq(401)
+        end
+      end
+
+      it 'sends an exception email' do
+        expected = 'Timestamp outside the tolerance zone'
+        with_feature_enabled(:alaveteli_pro) do
+          mail = ActionMailer::Base.deliveries.first
+          expect(mail.subject).to include(expected)
+        end
+      end
+
     end
 
   end
