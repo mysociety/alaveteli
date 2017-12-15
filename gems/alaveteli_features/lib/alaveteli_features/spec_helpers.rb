b1f1a29cd897fa565f1f2a189e0f2b345a223fe3
Graeme Porteous <graeme@rgbp.co.uk>
2017-12-15 09:30:59 +0000

    Allow setting feature flags using RSpec metadata
    Can now do:
    `context 'with pro pricing enabled', feature: :pro_pricing do ...` or
    `describe 'with a and b', feature: [:a, :b] do ...` etc

--- gems/alaveteli_features/lib/alaveteli_features/spec_helpers.rb
+++ gems/alaveteli_features/lib/alaveteli_features/spec_helpers.rb
@@ -4,14 +4,28 @@ module AlaveteliFeatures
       allow(AlaveteliFeatures.backend).to receive(:enabled?).and_call_original
       allow(AlaveteliFeatures.backend).
         to receive(:enabled?).with(feature).and_return(true)
-      yield
+      yield if block_given?
     end
 
     def with_feature_disabled(feature)
       allow(AlaveteliFeatures.backend).to receive(:enabled?).and_call_original
       allow(AlaveteliFeatures.backend).
         to receive(:enabled?).with(feature).and_return(false)
-      yield
+      yield if block_given?
+    end
+
+    RSpec.configure do |config|
+      config.before(:each) do |example|
+        features = [example.metadata[:feature]].flatten
+        next if features.empty?
+        features.each { |f| with_feature_enabled(f) }
+      end
+
+      config.after(:each) do |example|
+        features = [example.metadata[:feature]].flatten
+        next if features.empty?
+        features.each { |f| with_feature_disabled(f) }
+      end
     end
   end
 end
