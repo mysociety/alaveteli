4cc901b08766871762a8850673af3eff4d214a6f
Gareth Rees <gareth@garethrees.co.uk>
2018-01-05 17:04:23 +0000

    Alert systemd to init.d daemons
    `systemctl enable`: Enable the daemon – this performs an init.d >
    systemd conversion and puts the systemd unit in
    `/run/systemd/generator.late`.
    
    `systemctl daemon-reload`: Updates systemd with new daemon config.
    
    Daemons can then be started with `systemctl start`.

--- script/site-specific-install.sh
+++ script/site-specific-install.sh
@@ -37,6 +37,12 @@ install_daemon() {
   (su -l -c "cd '$REPOSITORY' && bundle exec rake config_files:convert_init_script DEPLOY_USER='$UNIX_USER' VHOST_DIR='$DIRECTORY' RUBY_VERSION='$RUBY_VERSION' SCRIPT_FILE=config/$1-debian.example" "$UNIX_USER") > /etc/init.d/"$SITE-$1"
   chgrp "$UNIX_USER" /etc/init.d/"$SITE-$1"
   chmod 754 /etc/init.d/"$SITE-$1"
+
+  if which systemctl > /dev/null
+  then
+    systemctl enable "$SITE-$1"
+  fi
+
   echo $DONE_MSG
 }
 
@@ -272,6 +278,10 @@ do
   install_daemon $daemon
 done
 
+if which systemctl > /dev/null
+then
+  systemctl daemon-reload
+fi
 
 if [ $DEFAULT_SERVER = true ] && [ x != x$EC2_HOSTNAME ]
 then
