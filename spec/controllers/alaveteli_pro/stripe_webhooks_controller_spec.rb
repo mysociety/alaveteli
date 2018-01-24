329d99ae4f43b175d077f079fe658ea2f3aa5960
Gareth Rees <gareth@garethrees.co.uk>
2018-01-24 10:24:09 +0000

    Refactor specs
    * Remove `with_feature_enabled` blocks – the top-level `describe`
      already enables the feature.
    * Extract pre-assertion steps to a `before` block – reduces duplication.

--- spec/controllers/alaveteli_pro/stripe_webhooks_controller_spec.rb
+++ spec/controllers/alaveteli_pro/stripe_webhooks_controller_spec.rb
@@ -284,6 +284,11 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
 
     describe 'updating the Stripe charge description when a payment succeeds' do
 
+      before do
+        request.headers.merge!(signed_headers)
+        post :receive, payload
+      end
+
       context 'when there is a charge for an invoice' do
         let(:stripe_event) do
           StripeMock.mock_webhook_event('invoice.payment_succeeded',
@@ -291,13 +296,8 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
         end
 
         it 'updates the charge description with the site name' do
-          with_feature_enabled(:alaveteli_pro) do
-            expect(charge.description).to be nil
-            request.headers.merge! signed_headers
-            post :receive, payload
-            expect(Stripe::Charge.retrieve(charge.id).description).
-              to eq('Alaveteli Professional')
-          end
+          expect(Stripe::Charge.retrieve(charge.id).description).
+            to eq('Alaveteli Professional')
         end
 
       end
@@ -309,11 +309,7 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
         end
 
         it 'does not attempt to update the nil charge' do
-          with_feature_enabled(:alaveteli_pro) do
-            request.headers.merge! signed_headers
-            post :receive, payload
-            expect(response.status).to eq(200)
-          end
+          expect(response.status).to eq(200)
         end
 
       end
