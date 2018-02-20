4564bd81656f400993758cb21de1908494825088
Gareth Rees <gareth@garethrees.co.uk>
2018-02-20 14:22:44 +0000

    Add parsed headers to AdminRawEmailController#show
    Makes it easier to spot incoming messages with several request addresses
    in the Bcc field.
    
    Related to https://github.com/mysociety/alaveteli/issues/32.

--- app/models/raw_email.rb
+++ app/models/raw_email.rb
@@ -46,8 +46,7 @@ class RawEmail < ActiveRecord::Base
   end
 
   def mail!
-    #MailHandler.mail_from_raw_email(data)
-    Mail.new(data)
+    MailHandler.mail_from_raw_email(data)
   end
 
   def data=(d)
