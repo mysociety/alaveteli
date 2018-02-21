e4c7d510fffc9547eb41d7e9bc3e60d29f7dbe27
Gareth Rees <gareth@garethrees.co.uk>
2018-02-21 17:05:29 +0000

    Extract InfoRequest.find_by_incoming_email specs
    Group existing specs under a method description.
    
    Copies the `before` block for setup.

--- spec/models/info_request_spec.rb
+++ spec/models/info_request_spec.rb
@@ -104,6 +104,64 @@ describe InfoRequest do
 
   end
 
+  describe '.find_by_incoming_email' do
+
+    before do
+      @info_request = info_requests(:fancy_dog_request)
+    end
+
+    it "recognises its own incoming email" do
+      incoming_email = @info_request.incoming_email
+      found_info_request = InfoRequest.find_by_incoming_email(incoming_email)
+      expect(found_info_request).to eq(@info_request)
+    end
+
+    it "recognises its own incoming email with some capitalisation" do
+      incoming_email = @info_request.incoming_email.gsub(/request/, "Request")
+      found_info_request = InfoRequest.find_by_incoming_email(incoming_email)
+      expect(found_info_request).to eq(@info_request)
+    end
+
+    it "recognises its own incoming email with quotes" do
+      incoming_email = "'" + @info_request.incoming_email + "'"
+      found_info_request = InfoRequest.find_by_incoming_email(incoming_email)
+      expect(found_info_request).to eq(@info_request)
+    end
+
+    it "recognises l and 1 as the same in incoming emails" do
+      # Make info request with a 1 in it
+      while true
+        ir = InfoRequest.new(:title => "Testing", :public_body => public_bodies(:geraldine_public_body),
+                             :user => users(:bob_smith_user))
+        ir.save!
+        hash_part = ir.incoming_email.match(/-[0-9a-f]+@/)[0]
+        break if hash_part.match(/1/)
+      end
+
+      # Make email with a 1 in the hash part changed to l
+      test_email = ir.incoming_email
+      new_hash_part = hash_part.gsub(/1/, "l")
+      test_email.gsub!(hash_part, new_hash_part)
+
+      # Try and find with an l
+      found_info_request = InfoRequest.find_by_incoming_email(test_email)
+      expect(found_info_request).to eq(ir)
+    end
+
+    it "recognises old style request-bounce- addresses" do
+      incoming_email = @info_request.magic_email("request-bounce-")
+      found_info_request = InfoRequest.find_by_incoming_email(incoming_email)
+      expect(found_info_request).to eq(@info_request)
+    end
+
+    it "returns nil when receiving email for a deleted request" do
+      deleted_request_address = InfoRequest.magic_email_for_id("request-", 98765)
+      found_info_request = InfoRequest.find_by_incoming_email(deleted_request_address)
+      expect(found_info_request).to be_nil
+    end
+
+  end
+
   describe '.holding_pen_request' do
 
     context 'when the holding pen exists' do
@@ -1670,56 +1728,6 @@ describe InfoRequest do
 
     it "has a sensible recipient name and email" do
       expect(@info_request.recipient_name_and_email).to eq("FOI requests at TGQ <geraldine-requests@localhost>")
-    end
-
-    it "recognises its own incoming email" do
-      incoming_email = @info_request.incoming_email
-      found_info_request = InfoRequest.find_by_incoming_email(incoming_email)
-      expect(found_info_request).to eq(@info_request)
-    end
-
-    it "recognises its own incoming email with some capitalisation" do
-      incoming_email = @info_request.incoming_email.gsub(/request/, "Request")
-      found_info_request = InfoRequest.find_by_incoming_email(incoming_email)
-      expect(found_info_request).to eq(@info_request)
-    end
-
-    it "recognises its own incoming email with quotes" do
-      incoming_email = "'" + @info_request.incoming_email + "'"
-      found_info_request = InfoRequest.find_by_incoming_email(incoming_email)
-      expect(found_info_request).to eq(@info_request)
-    end
-
-    it "recognises l and 1 as the same in incoming emails" do
-      # Make info request with a 1 in it
-      while true
-        ir = InfoRequest.new(:title => "Testing", :public_body => public_bodies(:geraldine_public_body),
-                             :user => users(:bob_smith_user))
-        ir.save!
-        hash_part = ir.incoming_email.match(/-[0-9a-f]+@/)[0]
-        break if hash_part.match(/1/)
-      end
-
-      # Make email with a 1 in the hash part changed to l
-      test_email = ir.incoming_email
-      new_hash_part = hash_part.gsub(/1/, "l")
-      test_email.gsub!(hash_part, new_hash_part)
-
-      # Try and find with an l
-      found_info_request = InfoRequest.find_by_incoming_email(test_email)
-      expect(found_info_request).to eq(ir)
-    end
-
-    it "recognises old style request-bounce- addresses" do
-      incoming_email = @info_request.magic_email("request-bounce-")
-      found_info_request = InfoRequest.find_by_incoming_email(incoming_email)
-      expect(found_info_request).to eq(@info_request)
-    end
-
-    it "returns nil when receiving email for a deleted request" do
-      deleted_request_address = InfoRequest.magic_email_for_id("request-", 98765)
-      found_info_request = InfoRequest.find_by_incoming_email(deleted_request_address)
-      expect(found_info_request).to be_nil
     end
 
     it "copes with indexing after item is deleted" do
