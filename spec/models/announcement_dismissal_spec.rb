c5d3d4fcf490fb3d8f6004e0f26db189547d57b1
Gareth Rees <gareth@garethrees.co.uk>
2018-04-11 16:40:06 +0100

    Annotate models
        bundle exec annotate

--- spec/models/announcement_dismissal_spec.rb
+++ spec/models/announcement_dismissal_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: announcement_dismissals
+#
+#  id              :integer          not null, primary key
+#  announcement_id :integer          not null
+#  user_id         :integer          not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'spec_helper'
 
 describe AnnouncementDismissal do
