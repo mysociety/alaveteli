mysociety/alaveteli

commit 2df25d6e180f3f5f04c19fbdb2994bb79c0618a2

Add stripe_customer_id to ProAccount
This seems like the best place to store this for now. It doesn’t affect
core models here, and its tied to a single user.

We’ll later want to move this to a Subscription model that can be a
Subscription to the “free” plan.

db/migrate/2017082515044823_add_stripe_customer_id_to_pro_account.rb
@@ -1,0 +1,6 @@
+# -*- encoding: utf-8 -*-
+class AddStripeCustomerIdToProAccount < ActiveRecord::Migration
+  def change
+    add_column :pro_accounts, :stripe_customer_id, :string
+  end
+end

commit bbf0ad64fe3a538ceab3c84fdaf8c66eec4d81e2

Fix broken migrations
49fd980 and later 2df25d6 introduced migrations with a timestamp two
characters too long. This breaks `rake db:rollback` because these
migrations end up getting applied last – or indeed rolled back instead
of the actually most recent migration.

This commit:

* Fixes migration filenames
* Adds a migration to fix broken timestamps in the schema_migrations
  table

Fixing the filenames is trivial – the files just get renamed removing
the extra digits. Note that in `2017071826152436` we remove `26` rather
than the `36` at the end so that we don't get an invalid time of
`261524`.

The difficulty here is matching up the `schema_migrations` entries with
the new files.

In the case of the `up` migration, we know the old migrations with the
fixed filenames will get applied first. I've added conditionals here so
that if the columns already exist, the only thing that will happen is
that the new `schema_migrations` entry gets recorded. We can then simply
delete the old `version` entry as the file no longer exists, and we know
the migration has been applied.

If we want to rollback from _this_ migration, we only want to revert to
the old entries in `schema_migrations` if the old filename exists. We
need to do this because if we don't, the migration will get re-attempted
on `db:migrate` and fail because the columns already exist. I don't
think this case is likely, but its correct to include this safeguard.

Fixes https://github.com/mysociety/alaveteli/issues/4467.

db/migrate/2017071826152436_add_expiring_notification_at.rb
@@ -1,6 +1,0 @@
-# -*- encoding: utf-8 -*-
-class AddExpiringNotificationAt < ActiveRecord::Migration
-  def change
-    add_column :embargoes, :expiring_notification_at, :datetime
-  end
-end

db/migrate/20170718261524_add_expiring_notification_at.rb
@@ -1,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+class AddExpiringNotificationAt < ActiveRecord::Migration
+  def up
+    unless column_exists?(:embargoes, :expiring_notification_at)
+      add_column :embargoes, :expiring_notification_at, :datetime
+    end
+  end
+
+  def down
+    if column_exists?(:embargoes, :expiring_notification_at)
+      remove_column :embargoes, :expiring_notification_at
+    end
+  end
+
+  private
+
+  def column_exists?(table, column)
+    if table_exists?(table)
+      connection.column_exists?(table, column)
+    end
+  end
+
+  def table_exists?(table)
+    connection.table_exists?(table)
+  end
+end

db/migrate/2017082515044823_add_stripe_customer_id_to_pro_account.rb
@@ -1,6 +1,0 @@
-# -*- encoding: utf-8 -*-
-class AddStripeCustomerIdToProAccount < ActiveRecord::Migration
-  def change
-    add_column :pro_accounts, :stripe_customer_id, :string
-  end
-end

db/migrate/20170825150448_add_stripe_customer_id_to_pro_account.rb
@@ -1,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+class AddStripeCustomerIdToProAccount < ActiveRecord::Migration
+  def up
+    unless column_exists?(:pro_accounts, :stripe_customer_id)
+      add_column :pro_accounts, :stripe_customer_id, :string
+    end
+  end
+
+  def down
+    if column_exists?(:pro_accounts, :stripe_customer_id)
+      remove_column :pro_accounts, :stripe_customer_id
+    end
+  end
+
+  private
+
+  def column_exists?(table, column)
+    if table_exists?(table)
+      connection.column_exists?(table, column)
+    end
+  end
+
+  def table_exists?(table)
+    connection.table_exists?(table)
+  end
+end

db/migrate/20180412135329_fix_broken_migration_timestamps.rb
@@ -1,0 +1,50 @@
+# -*- encoding: utf-8 -*-
+class FixBrokenMigrationTimestamps < ActiveRecord::Migration
+  def up
+    # We can just delete the old migration version from the database beacuse:
+    # * We know the renamed migration has been applied, because the timestamp
+    #   is less than the timestamp of this migration.
+    # * The renamed migration will be a no-op, because the columns already exist
+    #   and we handle that in the renamed migration files.
+    # * The badly-named migration files no longer exist.
+    if migration_exist?('20170718261524_add_expiring_notification_at.rb')
+      execute(<<-SQL)
+      DELETE FROM schema_migrations
+      WHERE version = '2017071826152436'
+      SQL
+    end
+
+    if migration_exist?('20170825150448_add_stripe_customer_id_to_pro_account.rb')
+      execute(<<-SQL)
+      DELETE FROM schema_migrations
+      WHERE version = '2017082515044823'
+      SQL
+    end
+  end
+
+  def down
+    # Here, we need to revert to the badly-named migrations, because the files
+    # will still exist.
+    if migration_exist?('2017071826152436_add_expiring_notification_at.rb')
+      execute(<<-SQL)
+      UPDATE schema_migrations
+      SET version = '2017071826152436'
+      WHERE version = '20170718152436'
+      SQL
+    end
+
+    if migration_exist?('2017082515044823_add_stripe_customer_id_to_pro_account.rb')
+      execute(<<-SQL)
+      UPDATE schema_migrations
+      SET version = '2017082515044823'
+      WHERE version = '20170825150448'
+      SQL
+    end
+  end
+
+  private
+
+  def migration_exist?(migration_file)
+    File.exist?(Rails.root + "db/migrate/#{ migration_file }")
+  end
+end