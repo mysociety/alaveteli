6304128f9163226f51cfc98d9ca5082673746c53
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-18 12:43:37 +0100

    Extract `drop_xapian_index` helper method
    So it can be used without calling `destroy_and_rebuild_xapian_index`.

--- spec/support/xapian_index.rb
+++ spec/support/xapian_index.rb
@@ -1,20 +1,22 @@
 # -*- encoding : utf-8 -*-
 # Rebuild the current xapian index
 def destroy_and_rebuild_xapian_index(terms = true, values = true, texts = true, dropfirst = true)
-  if dropfirst
-    begin
-      ActsAsXapian.readable_init
-      FileUtils.rm_rf(ActsAsXapian.db_path)
-    rescue RuntimeError
-    end
-    ActsAsXapian.writable_init
-    ActsAsXapian.writable_db.close
-  end
+  drop_xapian_index if dropfirst
   parse_all_incoming_messages
   # safe_rebuild=true, which involves forking to avoid memory leaks, doesn't work well with rspec.
   # unsafe is significantly faster, and we can afford possible memory leaks while testing.
   models = [PublicBody, User, InfoRequestEvent]
   ActsAsXapian.destroy_and_rebuild_index(models, verbose=false, terms, values, texts, safe_rebuild=false)
+end
+
+def drop_xapian_index
+  begin
+    ActsAsXapian.readable_init
+    FileUtils.rm_rf(ActsAsXapian.db_path)
+  rescue RuntimeError
+  end
+  ActsAsXapian.writable_init
+  ActsAsXapian.writable_db.close
 end
 
 def update_xapian_index
