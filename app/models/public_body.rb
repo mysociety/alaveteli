0de563c426ecbaaf0a460582992b67a967e81cc4
Gareth Rees <gareth@garethrees.co.uk>
2018-04-26 13:12:27 +0100

    Add PublicBody#update_counter_cache
    InfoRequest shouldn’t care how PublicBody updates its caches.
    
    This commit duplicates InfoRequest#update_counter_cache over to
    PublicBody with some specs. The specs are a bit duplication heavy, and
    the setup is a bit gross, but they’re a start at least.

--- app/models/public_body.rb
+++ app/models/public_body.rb
@@ -855,6 +855,30 @@ class PublicBody < ActiveRecord::Base
     end
   end
 
+  # This method updates the count columns of the PublicBody that
+  # store the number of "not held", "to some extent successful" and
+  # "both visible and classified" requests when saving or destroying
+  # an InfoRequest associated with the body:
+  def update_counter_cache
+    success_states = ['successful', 'partially_successful']
+    basic_params = {
+      :public_body_id => id,
+    }
+    [['info_requests_not_held_count', { :awaiting_description => false,
+                                        :described_state => 'not_held' }],
+     ['info_requests_successful_count', { :awaiting_description => false,
+                                          :described_state => success_states }],
+     ['info_requests_visible_classified_count', { :awaiting_description => false }],
+     ['info_requests_visible_count', {}]].each do |column, extra_params|
+       params = basic_params.clone.update extra_params
+       send "#{column}=", InfoRequest.where(params).is_searchable.count
+     end
+     without_revision do
+       self.no_xapian_reindex = true
+       save(validate: false)
+     end
+  end
+
   private
 
   # if the URL name has changed, then all requested_from: queries
