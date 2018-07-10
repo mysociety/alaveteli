d9e0815f8465a98b4cdbc7a93f2ebf446548fb30
lizconlan <liz@mysociety.org>
2018-07-10 15:41:31 +0100

    Check that 'pro' is enabled before showing pro content in the menu bar
    Otherwise sites that have not enabled pro will attempt to show pro links
    and menu configurations for users with the pro role assigned.

--- spec/views/general/_responsive_topnav.html.erb_spec.rb
+++ spec/views/general/_responsive_topnav.html.erb_spec.rb
@@ -14,6 +14,7 @@ describe 'general/_responsive_topnav.html.erb' do
     context 'when a pro user is logged in' do
       before do
         assign :user, pro_user
+        allow(controller).to receive(:current_user).and_return(pro_user)
       end
 
       it 'shows a Dashboard link' do
@@ -21,11 +22,30 @@ describe 'general/_responsive_topnav.html.erb' do
         expect(rendered).to have_link('Dashboard')
       end
 
+      context 'and pro features are not enabled' do
+
+        it 'shows "pro" next to the user name' do
+          with_feature_disabled(:alaveteli_pro) do
+            render_view
+            expect(rendered).to_not have_css('.pro-pill')
+          end
+        end
+
+        it 'does not show a Dashboard link' do
+          with_feature_disabled(:alaveteli_pro) do
+            render_view
+            expect(rendered).to_not have_link('Dashboard')
+          end
+        end
+
+      end
+
     end
 
     context 'when a normal user is logged in' do
       before do
         assign :user, user
+        allow(controller).to receive(:current_user).and_return(user)
       end
 
       it 'does not show a Dashboard link' do
