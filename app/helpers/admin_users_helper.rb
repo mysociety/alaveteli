b7ee7b32656d58d2de7bc3bdc6c1ab32a57e2bd5
Graeme Porteous <graeme@rgbp.co.uk>
2018-08-02 12:23:28 +0100

    Add admin label for closed accounts

--- app/helpers/admin_users_helper.rb
+++ app/helpers/admin_users_helper.rb
@@ -3,6 +3,7 @@ module AdminUsersHelper
   def user_labels(user)
     html = ''
     html += banned_label if user.banned?
+    html += closed_label if user.closed?
     user.roles.each do |role|
       html += role_label(role)
     end
@@ -13,6 +14,10 @@ module AdminUsersHelper
 
   def banned_label
     content_tag(:span, 'banned', :class => 'label label-warning')
+  end
+
+  def closed_label
+    content_tag(:span, 'closed', :class => 'label label-warning')
   end
 
   def role_label(role)
