ef3b2e53f58b25fbfe76b00b351dd193c0bc2a9a
Martin Wright <martin@mysociety.org>
2018-08-09 14:59:27 +0100

    Add buttons to navigate through request correspondence

--- app/assets/javascripts/alaveteli_pro/request_navigation.js
+++ app/assets/javascripts/alaveteli_pro/request_navigation.js
@@ -3,10 +3,16 @@
 
     $('.js-request-navigation').each(function() {
         var $requestNav = $(this);
+
         var statusText = $(this).attr('data-status-text');
+        var nextText = $(this).attr('data-next-text');
+        var prevText = $(this).attr('data-prev-text');
+
         var $allCorrespondence = $('.correspondence');
         var currentCorrespondenceIndex = 0;
         var correspondenceIds = [];
+
+        var adminBarHeight = $('.admin .navbar-fixed-top').outerHeight() || 0;
         
         $allCorrespondence.each(function() {
             correspondenceIds.push($(this).attr('id'));
@@ -25,10 +31,31 @@
             );
         }
 
-        var $navStatus = $('<p>');
+        var scrollToCurrentCorrespondence = function scrollToCurrentCorrespondence() {
+            var $el = $('#' + correspondenceIds[currentCorrespondenceIndex]);
+            $('html, body').animate({
+                scrollTop: $el.offset().top - adminBarHeight
+            }, 150);
+
+        }
+
+        var nextCorrespondence = function nextCorrespondence() {
+            currentCorrespondenceIndex = Math.min(currentCorrespondenceIndex + 1, correspondenceIds.length - 1);
+            scrollToCurrentCorrespondence();
+        }
+
+        var prevCorrespondence = function prevCorrespondence() {
+            currentCorrespondenceIndex = Math.max(currentCorrespondenceIndex - 1, 0);
+            scrollToCurrentCorrespondence();
+        }
+
+        var $prevButton = $('<button>').text(prevText).on('click', prevCorrespondence);
+        var $nextButton = $('<button>').text(nextText).on('click', nextCorrespondence);
+
+        var $navStatus = $('<button>').on('click', scrollToCurrentCorrespondence);
         updateStatusText();
 
-        $requestNav.append($navStatus);
+        $requestNav.append($prevButton, $nextButton, $navStatus);
 
         window.addEventListener('hashchange', updateStatusText);
 
