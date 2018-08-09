21bd855948c88dda47b40af23e551774a4b41d10
Martin Wright <martin@mysociety.org>
2018-08-09 15:54:52 +0100

    Update URL fragment when using request navigation buttons
    Reflect URL fragment changes in user interface

--- app/assets/javascripts/alaveteli_pro/request_navigation.js
+++ app/assets/javascripts/alaveteli_pro/request_navigation.js
@@ -18,7 +18,7 @@
             correspondenceIds.push($(this).attr('id'));
         });
 
-        var updateStatusText = function updateStatusText() {
+        var updateCurrentCorrespondenceIndex = function updateCurrentCorrespondenceIndex() {
             if(window.location.hash) {
                 var i = correspondenceIds.indexOf(window.location.hash.substr(1)); 
                 // make sure the URL fragment refers to a correspondence element
@@ -26,6 +26,14 @@
                     currentCorrespondenceIndex = i;
                 }
             } 
+        }
+
+        var highlightCurrentCorrespondence = function highlightCurrentCorrespondence() {
+            $('.correspondence--current').removeClass('correspondence--current');
+            $('#' + correspondenceIds[currentCorrespondenceIndex]).addClass('correspondence--current');
+        }
+
+        var updateStatusText = function updateStatusText() {
             $navStatus.text(
                 statusText.replace('[[x]]', currentCorrespondenceIndex + 1).replace('[[y]]', $allCorrespondence.length)
             );
@@ -33,31 +41,50 @@
 
         var scrollToCurrentCorrespondence = function scrollToCurrentCorrespondence() {
             var $el = $('#' + correspondenceIds[currentCorrespondenceIndex]);
-            $('html, body').animate({
+            $('html, body').stop().animate({
                 scrollTop: $el.offset().top - adminBarHeight
             }, 150);
-
         }
 
         var nextCorrespondence = function nextCorrespondence() {
-            currentCorrespondenceIndex = Math.min(currentCorrespondenceIndex + 1, correspondenceIds.length - 1);
+            history.pushState({}, null, '#' + correspondenceIds[Math.min(currentCorrespondenceIndex + 1, correspondenceIds.length - 1)]);
+            updateCurrentCorrespondenceIndex();
+            updateStatusText();
             scrollToCurrentCorrespondence();
+            highlightCurrentCorrespondence();
         }
 
         var prevCorrespondence = function prevCorrespondence() {
-            currentCorrespondenceIndex = Math.max(currentCorrespondenceIndex - 1, 0);
+            history.pushState({}, null, '#' + correspondenceIds[Math.max(currentCorrespondenceIndex - 1, 0)]);
+            updateCurrentCorrespondenceIndex();
+            updateStatusText();
             scrollToCurrentCorrespondence();
+            highlightCurrentCorrespondence();
         }
 
         var $prevButton = $('<button>').text(prevText).on('click', prevCorrespondence);
         var $nextButton = $('<button>').text(nextText).on('click', nextCorrespondence);
 
         var $navStatus = $('<button>').on('click', scrollToCurrentCorrespondence);
+        updateCurrentCorrespondenceIndex();
         updateStatusText();
+        if(correspondenceIds.indexOf(window.location.hash.substr(1)) > -1) {
+            scrollToCurrentCorrespondence();
+            highlightCurrentCorrespondence();
+        }
+
 
         $requestNav.append($prevButton, $nextButton, $navStatus);
 
-        window.addEventListener('hashchange', updateStatusText);
+        window.addEventListener('hashchange', function(event) {
+            if(correspondenceIds.indexOf(window.location.hash.substr(1)) > -1) {
+                event.preventDefault();
+                updateCurrentCorrespondenceIndex();
+                updateStatusText();
+                scrollToCurrentCorrespondence();
+                highlightCurrentCorrespondence();
+            }
+        });
 
     });
 
