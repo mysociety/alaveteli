bd9d4aca50c0f22b046990e1707db47e18f48da6
lizconlan <liz@mysociety.org>
2018-08-10 10:24:56 +0100

    Pass in a custom spam scorer config when testing new signups
    We need to try to strike a balance between preventing spam signups based
    solely on the name and email address - the only information we currently
    have at this point - and creating too many false positives.
    
    The mappings for the about_me field have been given a weighting of 0
    as it will always be empty at signup.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -599,7 +599,7 @@ class UserController < ApplicationController
   end
 
   def spam_user?(user_signup)
-    UserSpamScorer.new.spam?(user_signup)
+    UserSpamScorer.new(spam_scorer_signup_config).spam?(user_signup)
   end
 
   def block_spam_signups?
@@ -631,6 +631,27 @@ class UserController < ApplicationController
 
       false
     end
+  end
+
+  def spam_scorer_signup_config
+    {
+      spam_score_threshold: 13,
+      score_mappings: {
+        name_is_all_lowercase?: 1,
+        name_is_one_word?: 1,
+        name_includes_non_alpha_characters?: 1,
+        name_is_garbled?: 1,
+        email_from_suspicious_domain?: 10,
+        email_from_spam_domain?: 13,
+        email_from_spam_tld?: 1,
+        name_is_spam_format?: 10,
+        about_me_includes_currency_symbol?: 0,
+        about_me_is_link_only?: 0,
+        about_me_is_spam_format?: 0,
+        about_me_includes_anchor_tag?: 0,
+        about_me_already_exists?: 0
+      }
+    }
   end
 
 end
