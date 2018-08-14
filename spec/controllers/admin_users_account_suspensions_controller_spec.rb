7c12d8b1337683ff3c64251679b013ba06f56f83
Graeme Porteous <graeme@rgbp.co.uk>
2018-08-14 10:19:13 +0100

    Add 'Close and anonymise' buttons to user forms

--- spec/controllers/admin_users_account_suspensions_controller_spec.rb
+++ spec/controllers/admin_users_account_suspensions_controller_spec.rb
@@ -34,6 +34,30 @@ describe AdminUsersAccountSuspensionsController do
       end
     end
 
+    context 'with valid params for closing' do
+      let(:valid_params) do
+        { user_id: user.id, close_and_anonymise: true }
+      end
+
+      before { post :create, valid_params }
+
+      it 'finds the user to suspend' do
+        expect(assigns[:suspended_user]).to eq(user)
+      end
+
+      it 'closes the user' do
+        expect { user.reload }.to change(user, :closed?).to(true)
+      end
+
+      it 'tells the admin that the user was banned' do
+        expect(flash[:notice]).to eq('The user was suspended.')
+      end
+
+      it 'redirects to the user page' do
+        expect(response).to redirect_to(admin_user_path(user))
+      end
+    end
+
     context 'with invalid params' do
       it 'renders a 404' do
         expect { post :create, {} }.to raise_error(ActiveRecord::RecordNotFound)
