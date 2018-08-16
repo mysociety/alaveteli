369f40d89996a9a9967d370abe0caf9c0bdf8b7c
Graeme Porteous <graeme@rgbp.co.uk>
2018-08-16 12:47:53 +0100

    Move user spam scorer settings into YAML file

---
+++ config/initializers/user_spam_scorer.rb
@@ -0,0 +1,15 @@
+require 'user_spam_scorer'
+
+path = Rails.root.join('config/user_spam_scorer.yml')
+
+if File.exists?(path)
+  settings = YAML.load(File.read(path))['user_spam_scorer']
+  settings.each do |key, value|
+    case key
+    when /_format/
+      raise "UserSpamScorer: Can't load Regexp from YAML file"
+    end
+
+    UserSpamScorer.public_send("#{key}=", value)
+  end
+end
