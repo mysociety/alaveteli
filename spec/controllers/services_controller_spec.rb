45da04c39cd49e07a03c243696577cfacbbdeae5
lizconlan <liz@mysociety.org>
2018-08-23 11:43:19 +0100

    Refactor ServicesController spec
    * Simplify `require` of spec helper; can just reference the file by name as its already in the load path
    * Make the top level `describe` block describe only the class we're testing
    * Group specs under `describe '#method_name' do` block
    * Remove the Ruby 1.9 encoding comment line
    * Remove line breaks from spec descriptions
    * Reduce line lengths

--- spec/controllers/services_controller_spec.rb
+++ spec/controllers/services_controller_spec.rb
@@ -1,83 +1,94 @@
-# -*- encoding : utf-8 -*-
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 require 'fakeweb'
 
-describe ServicesController, "when returning a message for people in other countries" do
+describe ServicesController do
 
   render_views
 
-  # store and restore the locale in the context of the test suite to isolate
-  # changes made in these tests
-  before do
-    @old_locale = FastGettext.locale
-  end
+  describe '#other_country_message' do
 
-  it 'keeps the flash' do
-    # Make two get requests to simulate the flash getting swept after the
-    # first response.
-    get :other_country_message, nil, nil, :some_flash_key => 'abc'
-    get :other_country_message
-    expect(flash[:some_flash_key]).to eq('abc')
-  end
+    # store and restore the locale in the context of the test suite to isolate
+    # changes made in these tests
+    before do
+      @old_locale = FastGettext.locale
+    end
 
-  it "shows no alaveteli message when user in same country as deployed alaveteli" do
-    allow(AlaveteliConfiguration).to receive(:iso_country_code).and_return("DE")
-    allow(controller).to receive(:country_from_ip).and_return('DE')
-    get :other_country_message
-    expect(response.body).to eq("")
-  end
+    after do
+      FastGettext.set_locale(@old_locale)
+    end
 
-  it "shows a message if user location has a deployed FOI website" do
-    allow(AlaveteliConfiguration).to receive(:iso_country_code).and_return("ZZ")
-    allow(controller).to receive(:country_from_ip).and_return('DE')
-    get :other_country_message
-    expect(response.body).
-      to match(/requests within Deutschland at/)
-  end
+    it 'keeps the flash' do
+      # Make two get requests to simulate the flash getting swept after the
+      # first response.
+      get :other_country_message, nil, nil, :some_flash_key => 'abc'
+      get :other_country_message
+      expect(flash[:some_flash_key]).to eq('abc')
+    end
 
-  it "shows a message when user not in same country as deployed alaveteli and user country has no FOI website" do
-    allow(AlaveteliConfiguration).to receive(:iso_country_code).and_return("DE")
-    allow(controller).to receive(:country_from_ip).and_return('ZZ')
-    get :other_country_message
-    expect(response.body).to match(/outside Deutschland/)
-  end
+    it "shows no alaveteli message when user in same country as deployed alaveteli" do
+      allow(AlaveteliConfiguration).
+        to receive(:iso_country_code).and_return("DE")
+      allow(controller).to receive(:country_from_ip).and_return('DE')
+      get :other_country_message
+      expect(response.body).to eq("")
+    end
 
-  it "shows a message when user not in same country as deployed alaveteli and user country has no FOI website
-      and WorldFOIWebsites has no data about the deployed alaveteli" do
-    allow(AlaveteliConfiguration).to receive(:iso_country_code).and_return("XY")
-    allow(controller).to receive(:country_from_ip).and_return('ZZ')
-    get :other_country_message
-    expect(response.body).to match(/in other countries/)
-  end
+    it "shows a message if user location has a deployed FOI website" do
+      allow(AlaveteliConfiguration).
+        to receive(:iso_country_code).and_return("ZZ")
+      allow(controller).to receive(:country_from_ip).and_return('DE')
+      get :other_country_message
+      expect(response.body).
+        to match(/requests within Deutschland at/)
+    end
 
-  it "shows an EU message if the user location has a deployed FOI website and is covered by AskTheEU" do
-    allow(AlaveteliConfiguration).to receive(:iso_country_code).and_return("DE")
-    allow(controller).to receive(:country_from_ip).and_return('GB')
-    get :other_country_message
-    expect(response.body).to match(/within United Kingdom at/)
-    expect(response.body).to match(/EU institutions/)
-  end
+    context 'when user not in the same country as site' do
 
-  it "shows a message when user not in same country as deployed alaveteli and user country has no FOI website
-      but user country is covered by EU" do
-    allow(AlaveteliConfiguration).to receive(:iso_country_code).and_return("DE")
-    allow(controller).to receive(:country_from_ip).and_return('MT')
-    get :other_country_message
-    expect(response.body).to match(/outside Deutschland/)
-    expect(response.body).to match(/EU institutions/)
-  end
+      it "shows a message when user country has no FOI website" do
+        allow(AlaveteliConfiguration).
+          to receive(:iso_country_code).and_return("DE")
+        allow(controller).to receive(:country_from_ip).and_return('ZZ')
+        get :other_country_message
+        expect(response.body).to match(/outside Deutschland/)
+      end
 
-  it "shows a message when user not in same country as deployed alaveteli and user country has no FOI website
-      and WorldFOIWebsites has no data about the deployed alaveteli but user is covered by EU" do
-    allow(AlaveteliConfiguration).to receive(:iso_country_code).and_return("XY")
-    allow(controller).to receive(:country_from_ip).and_return('MT')
-    get :other_country_message
-    expect(response.body).to match(/in other countries/)
-    expect(response.body).to match(/EU institutions/)
-  end
+      it "shows a message when user country has no FOI website and WorldFOIWebsites has no data about the deployed alaveteli" do
+        allow(AlaveteliConfiguration).
+          to receive(:iso_country_code).and_return("XY")
+        allow(controller).to receive(:country_from_ip).and_return('ZZ')
+        get :other_country_message
+        expect(response.body).to match(/in other countries/)
+      end
 
-  after do
-    FastGettext.set_locale(@old_locale)
+      it "shows an EU message if the user location has a deployed FOI website and is covered by AskTheEU" do
+        allow(AlaveteliConfiguration).
+          to receive(:iso_country_code).and_return("DE")
+        allow(controller).to receive(:country_from_ip).and_return('GB')
+        get :other_country_message
+        expect(response.body).to match(/within United Kingdom at/)
+        expect(response.body).to match(/EU institutions/)
+      end
+
+      it "shows a message when user country has no FOI website but user country is covered by EU" do
+        allow(AlaveteliConfiguration).
+          to receive(:iso_country_code).and_return("DE")
+        allow(controller).to receive(:country_from_ip).and_return('MT')
+        get :other_country_message
+        expect(response.body).to match(/outside Deutschland/)
+        expect(response.body).to match(/EU institutions/)
+      end
+
+      it "shows a message when and user country has no FOI website and WorldFOIWebsites has no data about the deployed alaveteli but user is covered by EU" do
+        allow(AlaveteliConfiguration).
+          to receive(:iso_country_code).and_return("XY")
+        allow(controller).to receive(:country_from_ip).and_return('MT')
+        get :other_country_message
+        expect(response.body).to match(/in other countries/)
+        expect(response.body).to match(/EU institutions/)
+      end
+
+    end
+
   end
 
 end
