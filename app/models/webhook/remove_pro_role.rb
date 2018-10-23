bffdbd8bcc2fec138c6fc5047f568b82bb990cfc
Graeme Porteous <graeme@rgbp.co.uk>
2018-10-23 08:45:22 +0100

    Rename Webhook handler classes

---
+++ app/models/webhook/remove_pro_role.rb
@@ -0,0 +1,22 @@
+class Webhook
+  class RemoveProRole
+    include Webhook::Base
+
+    register 'customer.subscription.deleted'
+
+    def process
+      return unless pro_account
+
+      pro_account.user.remove_role(:pro)
+    end
+
+    private
+
+    def pro_account
+      @pro_account ||= (
+        id = data.object.customer
+        ProAccount.find_by(stripe_customer_id: id) if id
+      )
+    end
+  end
+end
