137ead1bf489f5ef9a4dc2d9f911c2ed305a925f
Gareth Rees <gareth@garethrees.co.uk>
2018-10-25 15:15:27 +0100

    Extract stripe spec helpers
    Refactoring webhook handling will require these outside the single
    controller spec.
    
    Explicitly raise `ArgumentError` on empty keyword args because required
    keyword arguments are Ruby 2.1+ only [1].
    
    [1] https://makandracards.com/makandra/25739-ruby-2-0-introduces-keyword-arguments

--- spec/controllers/alaveteli_pro/stripe_webhooks_controller_spec.rb
+++ spec/controllers/alaveteli_pro/stripe_webhooks_controller_spec.rb
@@ -68,7 +68,8 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
     end
 
     it 'returns a successful response for correctly signed headers' do
-      request.headers.merge! signed_headers
+      signed = signed_headers(payload: payload, signing_secret: signing_secret)
+      request.headers.merge!(signed)
       post :receive, params: payload
       expect(response).to be_success
     end
@@ -102,7 +103,9 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
       let(:signing_secret) { 'whsec_fake' }
 
       before do
-        request.headers.merge! signed_headers
+        signed =
+          signed_headers(payload: payload, signing_secret: signing_secret)
+        request.headers.merge!(signed)
         post :receive, params: payload
       end
 
@@ -133,7 +136,9 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
       end
 
       it 'sends an exception email' do
-        request.headers.merge! signed_headers
+        signed =
+          signed_headers(payload: payload, signing_secret: signing_secret)
+        request.headers.merge!(signed)
         post :receive, params: payload
         mail = ActionMailer::Base.deliveries.first
         expect(mail.subject).to match(/UnhandledStripeWebhookError/)
@@ -144,7 +149,10 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
     context 'the timestamp is stale (possible replay attack)' do
 
       let!(:stale_headers) do
-        time_travel_to(1.hour.ago) { signed_headers }
+        signed = signed_headers(payload: payload,
+                                signing_secret: signing_secret,
+                                timestamp: 1.hour.ago)
+        request.headers.merge!(signed)
       end
 
       before do
@@ -169,7 +177,9 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
       let(:payload) { '{"id": "1234"}' }
 
       before do
-        request.headers.merge! signed_headers
+        signed =
+          signed_headers(payload: payload, signing_secret: signing_secret)
+        request.headers.merge!(signed)
         post :receive, params: payload
       end
 
@@ -197,7 +207,9 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
       context 'the webhook does not reference our plan namespace' do
 
         it 'returns a custom 200 response' do
-          request.headers.merge! signed_headers
+          signed =
+            signed_headers(payload: payload, signing_secret: signing_secret)
+          request.headers.merge!(signed)
           post :receive, params: payload
           expect(response.status).to eq(200)
           expect(response.body).
@@ -205,7 +217,9 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
         end
 
         it 'does not send an exception email' do
-          request.headers.merge! signed_headers
+          signed =
+            signed_headers(payload: payload, signing_secret: signing_secret)
+          request.headers.merge!(signed)
           post :receive, params: payload
           expect(ActionMailer::Base.deliveries.count).to eq(0)
         end
@@ -235,7 +249,9 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
         end
 
         it 'returns a 200 OK response' do
-          request.headers.merge! signed_headers
+          signed =
+            signed_headers(payload: payload, signing_secret: signing_secret)
+          request.headers.merge!(signed)
           post :receive, params: payload
           expect(response.status).to eq(200)
           expect(response.body).to match('OK')
@@ -250,7 +266,9 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
         end
 
         it 'does not raise an error when trying to filter on plan name' do
-          request.headers.merge! signed_headers
+          signed =
+            signed_headers(payload: payload, signing_secret: signing_secret)
+          request.headers.merge!(signed)
           expect {
             post :receive, params: payload
           }.not_to raise_error
@@ -271,7 +289,9 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
 
       it 'removes the pro role from the associated user' do
         expect(user.is_pro?).to be true
-        request.headers.merge! signed_headers
+        signed =
+          signed_headers(payload: payload, signing_secret: signing_secret)
+        request.headers.merge!(signed)
         post :receive, params: payload
         expect(user.reload.is_pro?).to be false
       end
@@ -281,7 +301,9 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
     describe 'updating the Stripe charge description when a payment succeeds' do
 
       before do
-        request.headers.merge!(signed_headers)
+        signed =
+          signed_headers(payload: payload, signing_secret: signing_secret)
+        request.headers.merge!(signed)
         post :receive, params: payload
       end
 
@@ -315,19 +337,4 @@ describe AlaveteliPro::StripeWebhooksController, feature: [:alaveteli_pro, :pro_
 
   end
 
-end
-
-def encode_hmac(key, value)
-  # this is how Stripe signed headers work, method borrowed from:
-  # https://github.com/stripe/stripe-ruby/blob/v3.4.1/lib/stripe/webhook.rb#L24-L26
-  OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha256"), key, value)
-end
-
-def signed_headers
-  timestamp = Time.zone.now.to_i
-  secret = encode_hmac(signing_secret, "#{timestamp}.#{payload}")
-  {
-    'HTTP_STRIPE_SIGNATURE' => "t=#{timestamp},v1=#{secret}",
-    'CONTENT_TYPE' => 'application/json'
-  }
 end
