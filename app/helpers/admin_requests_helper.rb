a597bd2aaac4eebac2c30cb37879cdc1f6be0f86
Liz Conlan <liz@mysociety.org>
2018-11-02 17:39:47 +0000

    Revert "Experimentally pass in the public body"
    This reverts commit f2f426e95ff3b96c4416d389f4d84eee1e3b17f0.

--- app/helpers/admin_requests_helper.rb
+++ app/helpers/admin_requests_helper.rb
@@ -1,21 +1,17 @@
 module AdminRequestsHelper
 
-  def reason_text(reason, public_body = nil)
+  def reason_text(reason)
     method = "reason_text_#{ reason }"
     if respond_to?(method, true)
-      send(method, public_body)
+      send(method)
     else
       reason_text_default
     end
   end
 
-  def supported_reasons
-    %w(not_foi vexatious immigration_correspondence)
-  end
-
   private
 
-  def reason_text_default(public_body = nil)
+  def reason_text_default
     [
       _("We consider it to be vexatious, and have therefore hidden it from " \
         "other users."),
@@ -25,12 +21,12 @@ module AdminRequestsHelper
     ].join(" ")
   end
 
-  def reason_text_not_foi(public_body = nil)
+  def reason_text_not_foi
     _("We consider it is not a valid FOI request, and have therefore hidden " \
       "it from other users.")
   end
 
-  def reason_text_immigration_correspondence(public_body = nil)
+  def reason_text_immigration_correspondence
     _("As it is personal correspondence which it wasn't appropriate to " \
       "send via our public website we have hidden it from other users. " \
       "You will still be able to view it while logged in to the site.")
