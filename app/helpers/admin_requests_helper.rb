f2f426e95ff3b96c4416d389f4d84eee1e3b17f0
Liz Conlan <liz@mysociety.org>
2018-11-02 17:36:15 +0000

    Experimentally pass in the public body
    So that we can have a theme override that goes something like...
    
    def reason_text_immigration_correspondence(public_body = nil)
      text = "standard text"
      if public_body && public_body.name == "The Home Office"
        text += "special extra text"
      end
    end
    
    (Not convinced by this!)

--- app/helpers/admin_requests_helper.rb
+++ app/helpers/admin_requests_helper.rb
@@ -1,17 +1,21 @@
 module AdminRequestsHelper
 
-  def reason_text(reason)
+  def reason_text(reason, public_body = nil)
     method = "reason_text_#{ reason }"
     if respond_to?(method, true)
-      send(method)
+      send(method, public_body)
     else
       reason_text_default
     end
   end
 
+  def supported_reasons
+    %w(not_foi vexatious immigration_correspondence)
+  end
+
   private
 
-  def reason_text_default
+  def reason_text_default(public_body = nil)
     [
       _("We consider it to be vexatious, and have therefore hidden it from " \
         "other users."),
@@ -21,12 +25,12 @@ module AdminRequestsHelper
     ].join(" ")
   end
 
-  def reason_text_not_foi
+  def reason_text_not_foi(public_body = nil)
     _("We consider it is not a valid FOI request, and have therefore hidden " \
       "it from other users.")
   end
 
-  def reason_text_immigration_correspondence
+  def reason_text_immigration_correspondence(public_body = nil)
     _("As it is personal correspondence which it wasn't appropriate to " \
       "send via our public website we have hidden it from other users. " \
       "You will still be able to view it while logged in to the site.")
