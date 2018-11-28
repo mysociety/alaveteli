7f1922fa740580a576180db830dc0856f36e4c21
Graeme Porteous <graeme@rgbp.co.uk>
2018-11-28 16:06:38 +0000

    Ruby 1.9 style hash

--- app/models/incoming_message.rb
+++ app/models/incoming_message.rb
@@ -43,7 +43,7 @@ class IncomingMessage < ActiveRecord::Base
   MAX_ATTACHMENT_TEXT_CLIPPED = 1000000 # 1Mb ish
 
   belongs_to :info_request,
-             :inverse_of => :incoming_messages
+             inverse_of: :incoming_messages
 
   validates_presence_of :info_request
 
