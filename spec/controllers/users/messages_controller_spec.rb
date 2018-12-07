501c96f64d5be8756b8a683b59429198feba56a2
Liz Conlan <liz@mysociety.org>
2018-12-07 16:49:15 +0000

    Set session vars in before block, stop using user fixtures

--- spec/controllers/users/messages_controller_spec.rb
+++ spec/controllers/users/messages_controller_spec.rb
@@ -5,22 +5,30 @@ describe Users::MessagesController do
 
   render_views
 
-  it 'redirects to signin page if you are not signed in' do
-    get :contact, params: { id: users(:silly_name_user) }
-    expect(response).
-      to redirect_to(signin_path(token: get_last_post_redirect.token))
+  let(:sender) { FactoryBot.create(:user, name: 'Bob Smith') }
+  let(:recipient) { FactoryBot.create(:user) }
+
+  before { session[:user_id] = sender.id }
+
+  context 'when not signed in' do
+
+    it 'redirects to signin page' do
+      session[:user_id] = nil
+      get :contact, params: { id: recipient.id }
+      expect(response).
+        to redirect_to(signin_path(token: get_last_post_redirect.token))
+    end
+
   end
 
-  it 'shows the contact form if you are signed in' do
-    session[:user_id] = users(:bob_smith_user).id
-    get :contact, params: { id: users(:silly_name_user) }
+  it 'shows the contact form' do
+    get :contact, params: { id: recipient.id }
     expect(response).to render_template('contact')
   end
 
   it 'shows an error if not given a subject line' do
-    session[:user_id] = users(:bob_smith_user).id
     post :contact, params: {
-                     id: users(:silly_name_user),
+                     id: recipient.id,
                      contact: {
                        subject: '',
                        message: 'Gah'
@@ -38,7 +46,6 @@ describe Users::MessagesController do
     end
 
     it 'does not send the message without the recaptcha being completed' do
-       session[:user_id] = users(:bob_smith_user).id
        post :contact, params: {
                         id: users(:silly_name_user).id,
                         contact: {
@@ -56,27 +63,26 @@ describe Users::MessagesController do
   end
 
   it 'sends the message' do
-    session[:user_id] = users(:bob_smith_user).id
     post :contact, params: {
-                     id: users(:silly_name_user),
+                     id: recipient.id,
                      contact: {
                        subject: 'Dearest you',
                        message: 'Just a test!'
                      },
                      submitted_contact_form: 1
                    }
-    expect(response).to redirect_to(user_url(users(:silly_name_user)))
+    expect(response).to redirect_to(user_url(recipient))
 
     deliveries = ActionMailer::Base.deliveries
-    expect(deliveries.size).to  eq(1)
+    expect(deliveries.size).to eq(1)
     mail = deliveries[0]
     expect(mail.body).
       to include("Bob Smith has used #{AlaveteliConfiguration.site_name} " \
                  "to send you the message below")
     expect(mail.body).to include('Just a test!')
     # TODO: fix some nastiness with quoting name_and_email
-    # mail.to_addrs.first.to_s.should == users(:silly_name_user).name_and_email
-    expect(mail.header['Reply-To'].to_s).to match(users(:bob_smith_user).email)
+    # mail.to_addrs.first.to_s.should == recipient.name_and_email
+    expect(mail.header['Reply-To'].to_s).to match(sender.email)
   end
 
 end
