dd7a4833e7d883286ab76f4ea178d4c1f6c21c06
Liz Conlan <liz@mysociety.org>
2018-12-10 13:33:38 +0000

    Set the contact info in a before filter

--- app/controllers/users/messages_controller.rb
+++ app/controllers/users/messages_controller.rb
@@ -3,7 +3,7 @@
 class Users::MessagesController < UserController
 
   before_action :set_recipient, :check_can_send_messages, :check_logged_in,
-                :set_recaptcha_required
+                :set_contact, :set_recaptcha_required
 
   # Send a message to another user
   def contact
@@ -12,9 +12,6 @@ class Users::MessagesController < UserController
         flash.now[:error] = _('There was an error with the reCAPTCHA. ' \
                               'Please try again.')
       else
-        params[:contact][:name] = @user.name
-        params[:contact][:email] = @user.email
-        @contact = ContactValidator.new(params[:contact])
         if @contact.valid?
           ContactMailer.user_message(
             @user,
@@ -30,13 +27,7 @@ class Users::MessagesController < UserController
           redirect_to user_url(@recipient_user)
         end
       end
-      return
-    else
-      @contact = ContactValidator.new(
-        { :message => "" + @recipient_user.name + _(",\n\n\n\nYours,\n\n{{user_name}}",:user_name=>@user.name) }
-      )
     end
-
   end
 
   private
@@ -68,6 +59,19 @@ class Users::MessagesController < UserController
       email_subject: _('Send a message to {{user_name}}',
                        user_name: @recipient_user.name)
     )
+  end
+
+  def set_contact
+    if params[:submitted_contact_form]
+      params[:contact][:name] = @user.name
+      params[:contact][:email] = @user.email
+      @contact = ContactValidator.new(params[:contact])
+    else
+      @contact = ContactValidator.new(
+        message: '' + @recipient_user.name +
+                 _(",\n\n\n\nYours,\n\n{{user_name}}", user_name: @user.name)
+      )
+    end
   end
 
   def set_recaptcha_required
