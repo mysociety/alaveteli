ef6a7f23d4e895b0c2f326c206b023dfbedc0465
Liz Conlan <liz@mysociety.org>
2019-01-31 18:42:37 +0000

    fixup! Move #params_to_hash to a controller concern

--- app/controllers/concerns/hashable_params.rb
+++ app/controllers/concerns/hashable_params.rb
@@ -8,7 +8,7 @@ module HashableParams
   def params_to_hash(input_params)
     return {} if input_params.blank?
     if rails5?
-      input_params.try(:to_unsafe_h) || input_params
+      input_params.try(:to_unsafe_h) || input_params.clone
     else
       input_params.clone
     end
