11653ba7cdbb16e2941526b05cf5ba0e98eac2d1
Liz Conlan <liz@mysociety.org>
2019-03-01 10:37:49 +0000

    Minor spec improvements
    * Hash style
    * Line length
    * Prefer single quoted strings
    * Use the initial message from the request that was just created rather
      than generating a fresh one

--- spec/views/outgoing_mailer/initial_request.text.erb_spec.rb
+++ spec/views/outgoing_mailer/initial_request.text.erb_spec.rb
@@ -1,27 +1,28 @@
 # -*- encoding : utf-8 -*-
 require File.expand_path(File.join('..', '..', '..', 'spec_helper'), __FILE__)
 
-describe "outgoing_mailer/initial_request" do
+describe 'outgoing_mailer/initial_request' do
   let(:body) do
     FactoryBot.create(:public_body, name: "Apostrophe's",
                                     request_email: "a'b@example.com")
   end
-  let(:request) { FactoryBot.create(:info_request, :public_body => body) }
-  let(:outgoing_message) { FactoryBot.create(:initial_request) }
+  let(:request) { FactoryBot.create(:info_request, public_body: body) }
+  let(:outgoing_message) { request.outgoing_messages.first }
 
-  it "does not add HTMLEntities to the public body name" do
+  it 'does not add HTMLEntities to the public body name' do
     assign(:info_request, request)
     assign(:outgoing_message, outgoing_message)
     render
     expect(response).to match("requests to Apostrophe's")
   end
 
-  it "does not add HTMLEntities to the FOI law title" do
+  it 'does not add HTMLEntities to the FOI law title' do
     allow(request).to receive(:law_used_human).and_return("Test's Law")
     assign(:info_request, request)
     assign(:outgoing_message, outgoing_message)
     render
-    expect(response).to match("the wrong address for Test's Law requests")
+    expect(response).
+      to match("the wrong address for Test's Law requests")
   end
 
   it 'does not add HTMLEntities to the public body email address' do
