101e2dae7ba741211399efc9a44906dab8a772c7
Liz Conlan <liz@mysociety.org>
2019-03-15 12:23:11 +0000

    Apply the Rails 4.2.11.1 patch
    Mimetype specs loosely adapted from:
    https://github.com/rails/rails/blob/406e2f4679835fc83dade66e71412ca8ec382602/actionpack/test/controller/new_base/content_negotiation_test.rb#L16-L35
    
    Proof of vulnerability fix inspiredby:
    https://github.com/mpgn/CVE-2019-5418#cve-2019-5418---file-content-disclosure-on-rails
    
    Fixes:
    
    CVE-2019-5418
    https://groups.google.com/forum/#!topic/rubyonrails-security/pFRKI96Sm8Q
    
    CVE-2019-5419
    https://groups.google.com/forum/#!topic/rubyonrails-security/GN7w9fFAQeI

--- config/initializers/alaveteli.rb
+++ config/initializers/alaveteli.rb
@@ -55,6 +55,7 @@ require 'date_quarter'
 require 'public_body_csv'
 require 'routing_filters'
 require 'alaveteli_text_masker'
+require 'mime_negotiation_patch'
 
 AlaveteliLocalization.set_locales(AlaveteliConfiguration::available_locales,
                                   AlaveteliConfiguration::default_locale)
