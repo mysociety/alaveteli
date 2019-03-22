1797b4403d88b072b2391b8d573cccda4e2594a5
Liz Conlan <liz@mysociety.org>
2019-03-22 18:09:33 +0000

    Controller specs for handling followup and request sending errors

--- spec/controllers/followups_controller_spec.rb
+++ spec/controllers/followups_controller_spec.rb
@@ -424,6 +424,22 @@ describe FollowupsController do
       expect(response).to render_template('new')
     end
 
+    context 'a network error occurs while sending a followup' do
+
+      def send_request
+        post :create, params: {
+               outgoing_message: dummy_message,
+               request_id: request.id,
+               incoming_message_id: message_id
+             }
+      end
+
+      let(:outgoing_message) { request.reload.outgoing_messages.last }
+
+      it_behaves_like 'NetworkSendErrors'
+
+    end
+
     it_behaves_like 'successful_followup_sent'
 
     it "updates the status for successful followup sends" do
