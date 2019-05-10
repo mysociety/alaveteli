b16ed95b96dc596e56b0fcfbe4919e018a864260
Gareth Rees <gareth@garethrees.co.uk>
2019-05-10 11:42:26 +0100

    Minor style fixes
    * Line length
    * Hash style
    * Quote style
    * Use parenthesis around arguments

--- spec/script/mailin_spec.rb
+++ spec/script/mailin_spec.rb
@@ -1,40 +1,37 @@
 # -*- encoding : utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-require "external_command"
+require 'external_command'
 
 def mailin_test(email_filename)
-  Dir.chdir Rails.root do
-
+  Dir.chdir(Rails.root)do
     mail = load_file_fixture(email_filename)
     ir = info_requests(:other_request)
     mail.gsub!('EMAIL_TO', ir.incoming_email)
     mail.gsub!('EMAIL_FROM', 'responder@localhost')
-    xc = ExternalCommand.new("script/mailin", :stdin_string => mail).run
-    expect(xc.err).to eq("")
+    xc = ExternalCommand.new('script/mailin', stdin_string: mail).run
+    expect(xc.err).to eq('')
     return xc
   end
 end
 
-describe "When importing mail into the application" do
-
-  # Turn off transactional fixtures for this suite - incoming message is imported
-  # outside the transaction via ExternalCommand, so needs to be destroyed outside the
-  # transaction
+describe 'When importing mail into the application' do
+  # Turn off transactional fixtures for this suite - incoming message is
+  # imported outside the transaction via ExternalCommand, so needs to be
+  # destroyed outside the transaction.
   if rails5?
     self.use_transactional_tests = false
   else
     self.use_transactional_fixtures = false
   end
 
-  it "should not produce any output and should return a 0 code on importing a plain email" do
-    r = mailin_test("incoming-request-empty.email")
+  it 'should not produce any output and should return a 0 code on importing a plain email' do
+    r = mailin_test('incoming-request-empty.email')
     expect(r.status).to eq(0)
-    expect(r.out).to eq("")
+    expect(r.out).to eq('')
   end
 
   # Destroy the incoming message so that it doesn't affect other tests
   after do
     info_requests(:other_request).incoming_messages.destroy_all
   end
-
 end
