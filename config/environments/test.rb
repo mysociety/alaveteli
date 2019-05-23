6519d4f89abed4d67f391601614abad47d82d7a9
Graeme Porteous <graeme@rgbp.co.uk>
2019-05-23 16:16:00 +0100

    Disable eager loading in test environment
    Should've been disabled when we removed Spring preloader.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = true
+  config.eager_load = false
 
   if rails5?
     config.public_file_server.headers =
