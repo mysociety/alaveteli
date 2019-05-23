76305b67e166aad8c3bee1ba357142fa4fc5e32c
Graeme Porteous <graeme@rgbp.co.uk>
2019-05-23 15:13:17 +0100

    Move asset precompile list to manifest
    Rails 5 requires Sprockets 4 which is moving to defining assets for
    pre-compilation in a manifest JS file. This also works for Rails 4 with
    the current version of Sprockets.
    
    See: https://eileencodes.com/posts/the-sprockets-4-manifest/
    
    The other config options have been removed from the default generated
    assets initialiser.

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,28 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+//
+// These additional precompiled asset files are actually manifests that require
+// the real asset files:
+//
+//= link admin.js
+//= link profile-photos.js
+//= link stats.js
+//= link fancybox.css
+//= link fancybox.js
+//
+// ... while these are individual files that can't easily be grouped:
+//
+//= link jquery.Jcrop.min.css
+//= link excanvas.min.js
+//= link select-authorities.js
+//= link new-request.js
+//= link time_series.js
+//= link admin.css
+//= link bootstrap-dropdown.js
+//= link widget.css
+//= link request-attachments.js
+//= link alaveteli_pro/request-index.js
+//= link responsive/print.css
+//= link responsive/application-lte-ie7.css
+//= link responsive/application-ie8.css
