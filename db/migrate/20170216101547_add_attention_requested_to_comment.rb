c6a8e4f9a475cb6a139a431ff2ba288c4d81ef86
Graeme Porteous <graeme@rgbp.co.uk>
2019-05-23 21:25:44 +0100

    Add migration version generated under Rails 4.0.x
    These migrations were added between 03abbd1...7ed5c55^

--- db/migrate/20170216101547_add_attention_requested_to_comment.rb
+++ db/migrate/20170216101547_add_attention_requested_to_comment.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
-class AddAttentionRequestedToComment < ActiveRecord::Migration
+class AddAttentionRequestedToComment < !rails5? ? ActiveRecord::Migration : ActiveRecord::Migration[4.2] # 4.0
   def change
     add_column :comments, :attention_requested, :boolean,
                           :null => false, :default => false
--- db/migrate/20170227140831_rolify_create_roles.rb
+++ db/migrate/20170227140831_rolify_create_roles.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
-class RolifyCreateRoles < ActiveRecord::Migration
+class RolifyCreateRoles < !rails5? ? ActiveRecord::Migration : ActiveRecord::Migration[4.2] # 4.0
   def change
     create_table(:roles) do |t|
       t.string :name
