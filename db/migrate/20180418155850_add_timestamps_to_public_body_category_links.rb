5b43fbd8977de050bef9974fa6554d64014ab8b6
Graeme Porteous <graeme@rgbp.co.uk>
2019-05-24 13:44:12 +0100

    Allow null timestamps for old migrations
    Fix deprecation warning for change in Rails 5.0:
    > DEPRECATION WARNING: `#add_timestamps` was called without specifying an option
    > for `null`. In Rails 5, this behavior will change to `null: false`. You should
    > manually specify `null: true` to prevent the behavior of your existing
    > migrations from changing.

--- db/migrate/20180418155850_add_timestamps_to_public_body_category_links.rb
+++ db/migrate/20180418155850_add_timestamps_to_public_body_category_links.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class AddTimestampsToPublicBodyCategoryLinks < !rails5? ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
   def change
-    add_timestamps(:public_body_category_links)
+    add_timestamps(:public_body_category_links, null: true)
   end
 end
--- db/migrate/20180418160204_add_timestamps_to_user_info_request_sent_alerts.rb
+++ db/migrate/20180418160204_add_timestamps_to_user_info_request_sent_alerts.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class AddTimestampsToUserInfoRequestSentAlerts < !rails5? ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
   def change
-    add_timestamps(:user_info_request_sent_alerts)
+    add_timestamps(:user_info_request_sent_alerts, null: true)
   end
 end
