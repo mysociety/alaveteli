582a380f8bc51aa6b0514aac48aad2673e9062ab
Graeme Porteous <graeme@rgbp.co.uk>
2019-06-05 12:57:12 +0100

    Annotate models
    - To ensure defaults values don't change I set `TIME_ZONE=London` in
      config/general.yml. This matches what is set in production for WDTK
    - Cleared the existing annotations with: `bundle exec annotate -d`
    - Finally ran: `bundle exec annotate`

--- spec/factories/alaveteli_pro/request_summary_categories.rb
+++ spec/factories/alaveteli_pro/request_summary_categories.rb
@@ -1,4 +1,15 @@
 # -*- encoding : utf-8 -*-
+
+# == Schema Information
+#
+# Table name: request_summary_categories
+#
+#  id         :integer          not null, primary key
+#  slug       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryBot.define do
   factory :request_summary_category, :class => 'AlaveteliPro::RequestSummaryCategory',
                                      :aliases => [:waiting_response_request_summary_category] do
