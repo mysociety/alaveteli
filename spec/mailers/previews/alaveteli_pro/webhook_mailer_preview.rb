1a9a7dc3474534b556a95aefbc7b9cb98c7ca049
Graeme Porteous <graeme@rgbp.co.uk>
2019-06-27 09:26:00 +0100

    Use Webhook fixtures on digest preview
    Better and more actuate previews are good.

--- spec/mailers/previews/alaveteli_pro/webhook_mailer_preview.rb
+++ spec/mailers/previews/alaveteli_pro/webhook_mailer_preview.rb
@@ -5,10 +5,26 @@ module AlaveteliPro
 
       customers = %w(cus_123 cus_456 cus_789)
 
+      fixtures = %w(
+        coupon-code-applied
+        coupon-code-revoked
+        plan-changed
+        subscription-cancelled
+        subscription-reactivated
+        subscription-renewal-failure
+        subscription-renewal-repeated-failure
+        subscription-renewed-after-failure
+        subscription-renewed
+        trial-cancelled
+        trial-ended-first-payment-failed
+        trial-extended
+      )
+
       10.times do |idx|
         webhooks << FactoryBot.build(:webhook,
                                      created: idx,
-                                     customer: customers.sample)
+                                     customer: customers.sample,
+                                     fixture: fixtures.sample)
       end
 
       WebhookMailer.digest(webhooks)
