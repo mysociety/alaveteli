d0216a2834ea394d93f26b4ddb7adffa4051f3cc
Liz Conlan <liz@mysociety.org>
2019-07-18 16:28:13 +0100

    Support plan names from older Stripe accounts
    Tries to use @plan.name and takes @plan.nickname instead if it's either
    not set or the `#name` method is not supported.
    
    Warning: for older accounts without a Plan nickname, @plan.name may
    return the product name instead.

--- app/controllers/alaveteli_pro/stripe_webhooks_controller.rb
+++ app/controllers/alaveteli_pro/stripe_webhooks_controller.rb
@@ -57,7 +57,8 @@ class AlaveteliPro::StripeWebhooksController < ApplicationController
 
       subscription_id = @stripe_event.data.object.subscription
       subscription = Stripe::Subscription.retrieve(subscription_id)
-      plan_name = subscription.plan.nickname
+      plan_name =
+        subscription.plan.try(:name) || subscription.plan.nickname
 
       charge.description =
         "#{ AlaveteliConfiguration.pro_site_name }: #{ plan_name }"
