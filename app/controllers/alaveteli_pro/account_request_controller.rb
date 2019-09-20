245611fd7e0fb586c7e1dfcaf9162caff1363833
Graeme Porteous <graeme@rgbp.co.uk>
2019-09-20 10:19:52 +0100

    Refactor account_request controller
    - Line length
    - Trailing blank lines

--- app/controllers/alaveteli_pro/account_request_controller.rb
+++ app/controllers/alaveteli_pro/account_request_controller.rb
@@ -11,11 +11,15 @@ class AlaveteliPro::AccountRequestController < ApplicationController
   end
 
   def create
-    @account_request = AlaveteliPro::AccountRequest.new(params[:account_request])
+    @account_request = AlaveteliPro::AccountRequest.new(
+      params[:account_request]
+    )
+
     if @account_request.valid?
       AlaveteliPro::AccountMailer.account_request(@account_request).deliver_now
-      flash[:notice] = _("Thanks for your interest in {{pro_site_name}}, we'll get back to you soon!",
-                         pro_site_name: AlaveteliConfiguration.pro_site_name)
+      flash[:notice] = _('Thanks for your interest in {{pro_site_name}}, ' \
+                         "we'll get back to you soon!",
+                         pro_site_name: pro_site_name)
       redirect_to frontpage_url
     else
       render 'index'
@@ -28,4 +32,3 @@ class AlaveteliPro::AccountRequestController < ApplicationController
     @in_pro_area = true
   end
 end
-
