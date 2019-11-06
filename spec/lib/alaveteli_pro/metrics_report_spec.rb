ef7fbd4c458efbab3fd352c1a64b7265408293ae
Graeme Porteous <graeme@rgbp.co.uk>
2019-11-06 11:42:26 +0000

    Update metric report to use date ranges
    This allows ActiveRecord queries to be improved to use pure Ruby
    instead of having SQL fragments. Specs setup have been updated to
    keep the specs passing and to be more obvious what is happening.
    
    Also removed a duplicate spec.

--- spec/lib/alaveteli_pro/metrics_report_spec.rb
+++ spec/lib/alaveteli_pro/metrics_report_spec.rb
@@ -28,18 +28,20 @@ describe AlaveteliPro::MetricsReport do
 
     let(:user) { FactoryBot.create(:user) }
     let(:pro_user) { FactoryBot.create(:pro_user) }
+    let(:other_pro_user) { FactoryBot.create(:pro_user) }
 
     before do
-      2.times { FactoryBot.create(:info_request, user: user) }
-      3.times { FactoryBot.create(:info_request, user: pro_user) }
-      FactoryBot.create(:info_request_batch,
-                        :sent,
-                        :embargoed,
-                        user: pro_user)
+      # created before the report and shouldn't be included
+      time_travel_to(2.week.ago) {
+        FactoryBot.create(:embargoed_request, user: other_pro_user)
+      }
 
-      time_travel_to(2.weeks.ago) {
-        pro = FactoryBot.create(:pro_user)
-        FactoryBot.create(:info_request, user: pro)
+      # created the week of the report and should be included
+      time_travel_to(1.week.ago) {
+        2.times { FactoryBot.create(:info_request, user: user) }
+        3.times { FactoryBot.create(:info_request, user: pro_user) }
+        FactoryBot.create(:info_request_batch,
+                          :sent, :embargoed, user: pro_user)
       }
     end
 
@@ -56,10 +58,6 @@ describe AlaveteliPro::MetricsReport do
     end
 
     it { is_expected.to be_a(Hash) }
-
-    it 'does not include non-pro requests in requests made count' do
-      expect(subject[:new_pro_requests]).to eq 4
-    end
 
     it 'does not include non-pro requests in requests made count' do
       expect(subject[:new_pro_requests]).to eq 4
