1d1c6aa20b82b1d82d86a1c99c1367afb18f2504
Gareth Rees <gareth@garethrees.co.uk>
2019-11-15 13:25:03 +0000

    Cleanup spec
    * Hash style
    * Quote style
    * Line length
    * Line spacing
    * Describe HTTP verb and action
    * Don't use should
    * spec_helper is already in load path

--- spec/controllers/admin_public_body_change_requests_controller_spec.rb
+++ spec/controllers/admin_public_body_change_requests_controller_spec.rb
@@ -1,52 +1,47 @@
 # -*- encoding : utf-8 -*-
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
-describe AdminPublicBodyChangeRequestsController, "editing a change request" do
+describe AdminPublicBodyChangeRequestsController do
 
-  it "should render the edit template" do
-    change_request = FactoryBot.create(:add_body_request)
-    get :edit, params: { :id => change_request.id }
-    expect(response).to render_template("edit")
-  end
-
-end
-
-describe AdminPublicBodyChangeRequestsController, 'updating a change request' do
-
-  before do
-    @change_request = FactoryBot.create(:add_body_request)
-  end
-
-  it 'should close the change request' do
-    post :update, params: { :id => @change_request.id }
-    expect(PublicBodyChangeRequest.find(@change_request.id).is_open).to eq(false)
-  end
-
-  context 'when a response and subject are passed' do
-
-    it 'should send a response email to the user who requested the change' do
-      post :update, params: {
-                      :id => @change_request.id,
-                      :response => 'Thanks but no',
-                      :subject => 'Your request'
-                    }
-      deliveries = ActionMailer::Base.deliveries
-      expect(deliveries.size).to eq(1)
-      mail = deliveries[0]
-      expect(mail.subject).to eq('Your request')
-      expect(mail.to).to eq([@change_request.get_user_email])
-      expect(mail.body).to match(/Thanks but no/)
-    end
-
-  end
-
-  context 'when no response or subject are passed' do
-
-    it 'should send a response email to the user who requested the change' do
-      post :update, params: { :id => @change_request.id }
-      deliveries = ActionMailer::Base.deliveries
-      expect(deliveries.size).to eq(0)
+  describe 'GET #edit' do
+    it 'renders the edit template' do
+      change_request = FactoryBot.create(:add_body_request)
+      get :edit, params: { id: change_request.id }
+      expect(response).to render_template('edit')
     end
   end
 
+  describe 'PUT #update' do
+    before do
+      @change_request = FactoryBot.create(:add_body_request)
+    end
+
+    it 'closes the change request' do
+      post :update, params: { id: @change_request.id }
+      expect(PublicBodyChangeRequest.find(@change_request.id).is_open).
+        to eq(false)
+    end
+
+    context 'when a response and subject are passed' do
+      it 'sends a response email to the user who requested the change' do
+        post :update, params: { id: @change_request.id,
+                                response: 'Thanks but no',
+                                subject: 'Your request' }
+        deliveries = ActionMailer::Base.deliveries
+        expect(deliveries.size).to eq(1)
+        mail = deliveries[0]
+        expect(mail.subject).to eq('Your request')
+        expect(mail.to).to eq([@change_request.get_user_email])
+        expect(mail.body).to match(/Thanks but no/)
+      end
+    end
+
+    context 'when no response or subject are passed' do
+      it 'sends a response email to the user who requested the change' do
+        post :update, params: { id: @change_request.id }
+        deliveries = ActionMailer::Base.deliveries
+        expect(deliveries.size).to eq(0)
+      end
+    end
+  end
 end
