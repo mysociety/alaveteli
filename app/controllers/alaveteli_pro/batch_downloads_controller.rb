b72ae84e0ac524d0207ac707559edb110b52726c
Graeme Porteous <graeme@rgbp.co.uk>
2019-11-18 20:06:42 +0000

    Add batch download route and controller action

---
+++ app/controllers/alaveteli_pro/batch_downloads_controller.rb
@@ -0,0 +1,8 @@
+##
+# Controller which manages Alaveteli Professional info request batch data
+# downloads.
+#
+class AlaveteliPro::BatchDownloadsController < AlaveteliPro::BaseController
+  def show
+  end
+end
---
+++ spec/controllers/alaveteli_pro/batch_downloads_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+require 'cancan/matchers'
+
+RSpec.describe AlaveteliPro::BatchDownloadsController, type: :controller do
+  describe 'GET #show' do
+  end
+end
