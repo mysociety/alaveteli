cfb3c41363bf5494916af278709d9631a6a28fd6
Gareth Rees <gareth@garethrees.co.uk>
2019-11-29 14:12:36 +0000

    Make sure xapian index is initialized before specs run
    Similar to c4e296753ff8c55385680601ae9aa51539351c5e.
    
    Fixes https://github.com/mysociety/alaveteli/issues/5478.

--- spec/integration/signin_spec.rb
+++ spec/integration/signin_spec.rb
@@ -19,6 +19,8 @@ describe "Signing in" do
     end
   end
 
+  before { get_fixtures_xapian_index }
+
   it "shows you an error if you get the password wrong" do
     try_login(user, { :password => 'badpassword' })
     expect(page).
