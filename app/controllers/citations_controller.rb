22126ef78d5d7b644a1f44cb8b106ed037d75b21
Graeme Porteous <graeme@rgbp.co.uk>
2019-12-15 23:18:07 +0000

    Add authentication to citations controller

--- app/controllers/citations_controller.rb
+++ app/controllers/citations_controller.rb
@@ -2,9 +2,22 @@
 # Controller to create a new Citation for an InfoRequest or an InfoRequestBatch.
 #
 class CitationsController < ApplicationController
+  before_action :authenticate
+
   def new
   end
 
   def create
+  end
+
+  private
+
+  def authenticate
+    authenticated?(
+      web: _('To add a citation'),
+      email: _('Then you can add citations'),
+      email_subject: _('Confirm your account on {{site_name}}',
+                       site_name: site_name)
+    )
   end
 end
--- spec/controllers/citations_controller_spec.rb
+++ spec/controllers/citations_controller_spec.rb
@@ -1,17 +1,69 @@
 require 'spec_helper'
 
 RSpec.describe CitationsController, type: :controller do
+  before do
+    allow(controller).to receive(:site_name).and_return('SITE')
+  end
+
   describe 'GET new' do
-    it 'should be successful' do
-      get :new, params: { url_title: 'request_title' }
-      expect(response).to be_successful
+    context 'logged in' do
+      let(:user) { FactoryBot.create(:user) }
+      before { session[:user_id] = user.id }
+
+      it 'should be successful' do
+        get :new, params: { url_title: 'request_title' }
+        expect(response).to be_successful
+      end
+    end
+
+    context 'logged out' do
+      it 'redirects to sign in form' do
+        get :new, params: { url_title: 'request_title' }
+        expect(response.status).to eq 302
+      end
+
+      it 'saves post redirect' do
+        get :new, params: { url_title: 'request_title' }
+
+        post_redirect = get_last_post_redirect
+        expect(post_redirect.uri).to eq '/request/request_title/citations/new'
+        expect(post_redirect.reason_params).to eq(
+          web: 'To add a citation',
+          email: 'Then you can add citations',
+          email_subject: 'Confirm your account on SITE'
+        )
+      end
     end
   end
 
   describe 'POST create' do
-    it 'should be successful' do
-      post :create, params: { url_title: 'request_title' }
-      expect(response).to be_successful
+    context 'logged in' do
+      let(:user) { FactoryBot.create(:user) }
+      before { session[:user_id] = user.id }
+
+      it 'should be successful' do
+        post :create, params: { url_title: 'request_title' }
+        expect(response).to be_successful
+      end
+    end
+
+    context 'logged out' do
+      it 'redirects to sign in form' do
+        post :create, params: { url_title: 'request_title' }
+        expect(response.status).to eq 302
+      end
+
+      it 'saves post redirect' do
+        post :create, params: { url_title: 'request_title' }
+
+        post_redirect = get_last_post_redirect
+        expect(post_redirect.uri).to eq '/request/request_title/citations'
+        expect(post_redirect.reason_params).to eq(
+          web: 'To add a citation',
+          email: 'Then you can add citations',
+          email_subject: 'Confirm your account on SITE'
+        )
+      end
     end
   end
 end
