f87bd822eefdb03337531f9e471ca7e53c25758b
Gareth Rees <gareth@garethrees.co.uk>
2020-01-07 17:40:32 +0000

    WIP All subs are "active"

--- app/models/pro_account.rb
+++ app/models/pro_account.rb
@@ -22,7 +22,7 @@ class ProAccount < ApplicationRecord
   validates :user, presence: true
 
   def active?
-    subscriptions.active.any? || subscriptions.past_due.any?
+    subscriptions.any?
   end
 
   def subscriptions
