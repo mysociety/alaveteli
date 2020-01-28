4bab4dd2be94904e0383afe9d596c24f183fb6f6
Gareth Rees <gareth@garethrees.co.uk>
2020-01-28 16:26:24 +0000

    Auto-correct db/seeds.rb
    Only a couple of issues here so just auto-corrected the lot.
    
    * Line length (manual)
    * Single line `if`
    * Hash syntax
    * Use `%w` for arrays of words
    
        $ be rubocop -a db/seeds.rb
        Inspecting 1 file
        C
    
        Offenses:
    
        db/seeds.rb:2:81: C: Line is too long. [103/80]
        # This file should contain all the record creation needed to seed the database with its default values.
    										    ^^^^^^^^^^^^^^^^^^^^^^^
        db/seeds.rb:3:81: C: Line is too long. [111/80]
        # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
    										    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
        db/seeds.rb:7:81: C: Line is too long. [81/80]
        #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
    										    ^
        db/seeds.rb:14:1: C: [Corrected] Favor modifier if usage when having a single-line body. Another good alternative is the usage of control flow &&/||.
        if Role.where(:name => 'admin').empty?
        ^^
        db/seeds.rb:14:15: C: [Corrected] Use the new Ruby 1.9 hash syntax.
        if Role.where(:name => 'admin').empty?
    		  ^^^^^^^^
        db/seeds.rb:15:15: C: [Corrected] Use the new Ruby 1.9 hash syntax.
          Role.create(:name => 'admin')
    		  ^^^^^^^^
        db/seeds.rb:19:3: C: [Corrected] Use %w or %W for an array of words.
          ['pro', 'pro_admin'].each do |role_name|
          ^^^^^^^^^^^^^^^^^^^^
        db/seeds.rb:20:5: C: [Corrected] Favor modifier if usage when having a single-line body. Another good alternative is the usage of control flow &&/||.
    	if Role.where(:name => role_name).empty?
    	^^
        db/seeds.rb:20:19: C: [Corrected] Use the new Ruby 1.9 hash syntax.
    	if Role.where(:name => role_name).empty?
    		      ^^^^^^^^
        db/seeds.rb:21:19: C: [Corrected] Use the new Ruby 1.9 hash syntax.
    	  Role.create(:name => role_name)
    		      ^^^^^^^^
        db/seeds.rb:26:1: C: [Corrected] Use %w or %W for an array of words.
        [ ...
        ^
        db/seeds.rb:30:49: C: [Corrected] Use the new Ruby 1.9 hash syntax.
          if AlaveteliPro::RequestSummaryCategory.where(:slug => category_slug).empty?
    						    ^^^^^^^^
        db/seeds.rb:31:49: C: [Corrected] Use the new Ruby 1.9 hash syntax.
    	AlaveteliPro::RequestSummaryCategory.create(:slug => category_slug)
    						    ^^^^^^^^
    
        1 file inspected, 13 offenses detected, 10 offenses corrected

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,33 @@
 # -*- encoding : utf-8 -*-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+#
+# The data can then be loaded with the rails db:seed command (or created
+# alongside the database with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies =
+#     Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
 # IMPORTANT! SEEDS MUST BE IDEMPOTENT. This generally means only creating seeds
 # when they don't exist.
 include AlaveteliFeatures::Helpers
 
-if Role.where(:name => 'admin').empty?
-  Role.create(:name => 'admin')
-end
+Role.create(name: 'admin') if Role.where(name: 'admin').empty?
 
 if feature_enabled?(:alaveteli_pro)
-  ['pro', 'pro_admin'].each do |role_name|
-    if Role.where(:name => role_name).empty?
-      Role.create(:name => role_name)
-    end
+  %w[pro pro_admin].each do |role_name|
+    Role.create(name: role_name) if Role.where(name: role_name).empty?
   end
 end
 
-[
-  'draft', 'complete', 'clarification_needed', 'awaiting_response',
-  'response_received', 'overdue', 'very_overdue', 'other', 'embargo_expiring'
+%w[
+  draft complete clarification_needed awaiting_response
+  response_received overdue very_overdue other embargo_expiring
 ].each do |category_slug|
-  if AlaveteliPro::RequestSummaryCategory.where(:slug => category_slug).empty?
-    AlaveteliPro::RequestSummaryCategory.create(:slug => category_slug)
+  if AlaveteliPro::RequestSummaryCategory.where(slug: category_slug).empty?
+    AlaveteliPro::RequestSummaryCategory.create(slug: category_slug)
   end
 end
