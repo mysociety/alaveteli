b830ebaa15a415ccddd8802e49ecdbf875f9cc52
Graeme Porteous <graeme@rgbp.co.uk>
2020-03-05 23:47:31 +0000

    Add public token controller
    Loads and ensures request is readable but don't yet render the request.

---
+++ app/controllers/public_tokens_controller.rb
@@ -0,0 +1,22 @@
+##
+# Controller responsible for rendering any InfoRequest by its public token
+#
+class PublicTokensController < ApplicationController
+  before_action :find_info_request, :can_view_info_request
+
+  def show
+    respond_to do |format|
+      format.html { render plain: 'Success' }
+    end
+  end
+
+  private
+
+  def find_info_request
+    @info_request = InfoRequest.find_by!(public_token: params[:id])
+  end
+
+  def can_view_info_request
+    render_hidden if cannot?(:read, @info_request)
+  end
+end
---
+++ spec/controllers/public_tokens_controller_spec.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+
+RSpec.describe PublicTokensController, type: :controller do
+
+  let(:ability) { Object.new.extend(CanCan::Ability) }
+  let(:info_request) { FactoryBot.build(:info_request) }
+
+  before do
+    allow(controller).to receive(:current_ability).and_return(ability)
+  end
+
+  describe 'GET #show' do
+    context 'when public token is valid and Info Request is readable' do
+      before do
+        allow(InfoRequest).to receive(:find_by!).and_return(info_request)
+        ability.can :read, info_request
+      end
+
+      it 'finds Info Request by public token' do
+        expect(InfoRequest).to receive(:find_by!).with(public_token: 'TOKEN')
+        get :show, params: { id: 'TOKEN' }
+      end
+
+      it 'assigns info_request' do
+        get :show, params: { id: 'TOKEN' }
+        expect(assigns(:info_request)).to eq info_request
+      end
+
+      it 'returns http success' do
+        get :show, params: { id: 'TOKEN' }
+        expect(response).to be_successful
+      end
+
+      it 'returns plain message' do
+        get :show, params: { id: 'TOKEN' }
+        expect(response.body).to eq 'Success'
+      end
+    end
+
+    context 'when public token is valid and Info Request is unreadable' do
+      before do
+        allow(InfoRequest).to receive(:find_by!).and_return(info_request)
+        ability.cannot :read, info_request
+      end
+
+      it 'finds Info Request by public token' do
+        expect(InfoRequest).to receive(:find_by!).with(public_token: 'TOKEN')
+        get :show, params: { id: 'TOKEN' }
+      end
+
+      it 'assigns info_request' do
+        get :show, params: { id: 'TOKEN' }
+        expect(assigns(:info_request)).to eq info_request
+      end
+
+      it 'renders hidden request template' do
+        get :show, params: { id: 'TOKEN' }
+        expect(response).to render_template('request/hidden')
+      end
+    end
+
+    context 'when public token is invalid' do
+      it 'raises not found error' do
+        expect { get :show, params: { id: 'NOT-FOUND' } }.to(
+          raise_error(ActiveRecord::RecordNotFound)
+        )
+      end
+    end
+  end
+
+end
