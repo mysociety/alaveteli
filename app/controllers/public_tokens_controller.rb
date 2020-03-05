5bf6313ff1af4ecd7d7bc0e8f3d852b562b34629
Graeme Porteous <graeme@rgbp.co.uk>
2020-03-05 23:47:31 +0000

    Add public token controller
    Loads and ensures request is readable but don't yet render the request.

---
+++ app/controllers/public_tokens_controller.rb
@@ -0,0 +1,30 @@
+##
+# Controller responsible for rendering any InfoRequest by its public token
+#
+class PublicTokensController < ApplicationController
+  before_action :find_info_request, :can_view_info_request
+
+  def show
+    respond_to do |format|
+      format.html { render plain: 'Success' }
+    end
+  end
+
+  private
+
+  def find_info_request
+    @info_request = InfoRequest.find_by!(public_token: params[:id])
+  end
+
+  def can_view_info_request
+    if guest.can?(:read, @info_request)
+      redirect_to show_request_path(@info_request.url_title)
+    elsif cannot?(:read, @info_request)
+      render_hidden
+    end
+  end
+
+  def guest
+    @guest ||= Ability.new(nil)
+  end
+end
