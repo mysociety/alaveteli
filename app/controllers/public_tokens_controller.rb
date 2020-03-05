681711f209901faf3b26a6974be2330e96497245
Graeme Porteous <graeme@rgbp.co.uk>
2020-03-05 23:55:20 +0000

    Add public token request rendering
    Prevent rendering of the action menus.

--- app/controllers/public_tokens_controller.rb
+++ app/controllers/public_tokens_controller.rb
@@ -5,8 +5,12 @@ class PublicTokensController < ApplicationController
   before_action :find_info_request, :can_view_info_request
 
   def show
+    @public_token_view = true
+
+    headers['X-Robots-Tag'] = 'noindex'
+
     respond_to do |format|
-      format.html { render plain: 'Success' }
+      format.html { render template: 'request/show' }
     end
   end
 
