b94ad232def5eb17b2f4c941489f90935fccab63
Graeme Porteous <graeme@rgbp.co.uk>
2020-03-27 16:03:00 +0000

    Refactor attachment controller
    Fix Rubocop warnings.

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,3 +1,6 @@
+##
+# Controller to serve info request message attachments in both raw and as HTML.
+#
 class AttachmentsController < ApplicationController
   include FragmentCachable
 
@@ -8,16 +11,18 @@ class AttachmentsController < ApplicationController
     get_attachment_internal(false)
     return unless @attachment
 
-
-    # we don't use @attachment.content_type here, as we want same mime type when cached in cache_attachments above
+    # we don't use @attachment.content_type here, as we want same mime type
+    # when cached in cache_attachments above
     content_type =
       AlaveteliFileTypes.filename_to_mimetype(params[:file_name]) ||
       'application/octet-stream'
 
     # Prevent spam to magic request address. Note that the binary
     # subsitution method used depends on the content type
-    body = @incoming_message.
-            apply_masks(@attachment.default_body, @attachment.content_type)
+    body = @incoming_message.apply_masks(
+      @attachment.default_body,
+      @attachment.content_type
+    )
 
     if content_type == 'text/html'
       body =
@@ -26,39 +31,44 @@ class AttachmentsController < ApplicationController
         try(:html_safe)
     end
 
-    render :body => body, :content_type => content_type
+    render body: body, content_type: content_type
   end
 
   def show_as_html
-    # The conversion process can generate files in the cache directory that can be served up
-    # directly by the webserver according to httpd.conf, so don't allow it unless that's OK.
+    # The conversion process can generate files in the cache directory that can
+    # be served up directly by the webserver according to httpd.conf, so don't
+    # allow it unless that's OK.
     if @files_can_be_cached != true
-      raise ActiveRecord::RecordNotFound.new("Attachment HTML not found.")
+      raise ActiveRecord::RecordNotFound, 'Attachment HTML not found.'
     end
     get_attachment_internal(true)
     return unless @attachment
 
-    # images made during conversion (e.g. images in PDF files) are put in the cache directory, so
-    # the same cache code in cache_attachments above will display them.
-    key = params.merge(:only_path => true)
+    # images made during conversion (e.g. images in PDF files) are put in the
+    # cache directory, so the same cache code in cache_attachments above will
+    # display them.
+    key = params.merge(only_path: true)
     key_path = foi_fragment_cache_path(key)
     image_dir = File.dirname(key_path)
     FileUtils.mkdir_p(image_dir)
 
     html = @attachment.body_as_html(
-             image_dir,
-             attachment_url: Rack::Utils.escape(@attachment_url),
-             content_for: {
-               head_suffix: render_to_string(
-                              partial: 'request/view_html_stylesheet',
-                              formats: [:html]),
-               body_prefix: render_to_string(
-                              partial: 'request/view_html_prefix')
-             })
+      image_dir,
+      attachment_url: Rack::Utils.escape(@attachment_url),
+      content_for: {
+        head_suffix: render_to_string(
+          partial: 'request/view_html_stylesheet',
+          formats: [:html]
+        ),
+        body_prefix: render_to_string(
+          partial: 'request/view_html_prefix'
+        )
+      }
+    )
 
     html = @incoming_message.apply_masks(html, response.content_type)
 
-    render :html => html.html_safe
+    render html: html.html_safe
   end
 
   private
@@ -66,7 +76,9 @@ class AttachmentsController < ApplicationController
   def authenticate_attachment
     # Test for hidden
     incoming_message = IncomingMessage.find(params[:incoming_message_id])
-    raise ActiveRecord::RecordNotFound.new("Message not found") if incoming_message.nil?
+    if incoming_message.nil?
+      raise ActiveRecord::RecordNotFound, "Message not found"
+    end
     if cannot?(:read, incoming_message.info_request)
       @info_request = incoming_message.info_request # used by view
       request.format = :html
@@ -79,7 +91,7 @@ class AttachmentsController < ApplicationController
     end
     # Is this a completely public request that we can cache attachments for
     # to be served up without authentication?
-    if incoming_message.info_request.prominence(:decorate => true).is_public? &&
+    if incoming_message.info_request.prominence(decorate: true).is_public? &&
        incoming_message.is_public?
       @files_can_be_cached = true
     end
@@ -90,20 +102,20 @@ class AttachmentsController < ApplicationController
     if !params[:skip_cache].nil?
       yield
     else
-      key = params.merge(:only_path => true)
+      key = params.merge(only_path: true)
       key_path = foi_fragment_cache_path(key)
       if foi_fragment_cache_exists?(key_path)
         logger.info("Reading cache for #{key_path}")
 
         if File.directory?(key_path)
-          render :plain => "Directory listing not allowed", :status => 403
+          render plain: 'Directory listing not allowed', status: 403
         else
           content_type =
             AlaveteliFileTypes.filename_to_mimetype(params[:file_name]) ||
             'application/octet-stream'
 
-          render :body => foi_fragment_cache_read(key_path),
-                 :content_type => content_type
+          render body: foi_fragment_cache_read(key_path),
+                 content_type: content_type
         end
         return
       end
@@ -123,8 +135,6 @@ class AttachmentsController < ApplicationController
     end
   end
 
-  private
-
   def get_attachment_internal(html_conversion)
     @incoming_message = IncomingMessage.find(params[:incoming_message_id])
     @requested_request = InfoRequest.find(params[:id])
@@ -134,38 +144,51 @@ class AttachmentsController < ApplicationController
       # Note that params[:id] might not be an integer, though
       # if we’ve got this far then it must begin with an integer
       # and that integer must be the id number of an actual request.
-      message = "Incoming message %d does not belong to request '%s'" % [@incoming_message.info_request_id, params[:id]]
-      raise ActiveRecord::RecordNotFound.new(message)
+      message = format("Incoming message %d does not belong to request '%s'",
+                       @incoming_message.info_request_id, params[:id])
+      raise ActiveRecord::RecordNotFound, message
     end
     @part_number = params[:part].to_i
     @filename = params[:file_name]
     if html_conversion
-      @original_filename = @filename.gsub(/\.html$/, "")
+      @original_filename = @filename.gsub(/\.html$/, '')
     else
       @original_filename = @filename
     end
 
     # check permissions
-    raise "internal error, pre-auth filter should have caught this" if cannot?(:read, @info_request)
-    @attachment = IncomingMessage.get_attachment_by_url_part_number_and_filename(@incoming_message.get_attachments_for_display, @part_number, @original_filename)
+    if cannot?(:read, @info_request)
+      raise "internal error, pre-auth filter should have caught this"
+    end
+    @attachment = IncomingMessage.
+      get_attachment_by_url_part_number_and_filename(
+        @incoming_message.get_attachments_for_display,
+        @part_number,
+        @original_filename
+      )
     # If we can't find the right attachment, redirect to the incoming message:
     unless @attachment
-      return redirect_to incoming_message_url(@incoming_message), :status => 303
+      return redirect_to incoming_message_url(@incoming_message), status: 303
     end
 
-    # check filename in URL matches that in database (use a censor rule if you want to change a filename)
-    if @attachment.display_filename != @original_filename && @attachment.old_display_filename != @original_filename
+    # check filename in URL matches that in database (use a censor rule if you
+    # want to change a filename)
+    if @attachment.display_filename != @original_filename &&
+       @attachment.old_display_filename != @original_filename
       msg = 'please use same filename as original file has, display: '
       msg += "'#{ @attachment.display_filename }' "
       msg += 'old_display: '
       msg += "'#{ @attachment.old_display_filename }' "
       msg += 'original: '
       msg += "'#{ @original_filename }'"
-      raise ActiveRecord::RecordNotFound.new(msg)
+      raise ActiveRecord::RecordNotFound, msg
     end
 
-    @attachment_url = get_attachment_url(:id => @incoming_message.info_request_id,
-                                         :incoming_message_id => @incoming_message.id, :part => @part_number,
-                                         :file_name => @original_filename )
+    @attachment_url = get_attachment_url(
+      id: @incoming_message.info_request_id,
+      incoming_message_id: @incoming_message.id,
+      part: @part_number,
+      file_name: @original_filename
+    )
   end
 end
