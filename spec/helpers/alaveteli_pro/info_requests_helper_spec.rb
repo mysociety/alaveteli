540b766469c63529ed031622c5d4abc418f6d77f
Gareth Rees <gareth@garethrees.co.uk>
2020-04-15 13:35:32 +0100

    Add default parameter to embargo_extension_options
    Set `embargo` to `nil` by default so that we can use
    `embargo_extension_options` without an argument.
    
    Slightly refactors the specs to account for the new default parameter.

--- spec/helpers/alaveteli_pro/info_requests_helper_spec.rb
+++ spec/helpers/alaveteli_pro/info_requests_helper_spec.rb
@@ -2,50 +2,52 @@ require 'spec_helper'
 
 RSpec.describe AlaveteliPro::InfoRequestsHelper, type: :helper do
   describe '#embargo_extension_options' do
-    subject { embargo_extension_options(embargo) }
+    context 'with an embargo' do
+      subject { embargo_extension_options(embargo) }
 
-    let(:embargo) do
-      FactoryBot.build(:embargo, publish_at: Date.new(2020, 1, 1))
-    end
-
-    context 'with existing embargo' do
-      it 'returns a list of expiry dates 3, 6 and 12 months after publish at' do
-        is_expected.to match_array(
-          [
-            ['Choose a duration', ''],
-            ['3 Months', '3_months',
-             { 'data-expiry-date' => '01 April 2020' }],
-            ['6 Months', '6_months',
-             { 'data-expiry-date' => '01 July 2020' }],
-            ['12 Months', '12_months', {
-              'data-expiry-date' => '30 December 2020'
-            }]
-          ]
-        )
+      let(:embargo) do
+        FactoryBot.build(:embargo, publish_at: Date.new(2020, 1, 1))
       end
-    end
 
-    context 'with a different locale' do
-      before { AlaveteliLocalization.set_locales(:es, :es) }
+      context 'with existing embargo' do
+        it 'returns a list of expiry dates 3, 6 and 12 months after publish at' do
+          is_expected.to match_array(
+            [
+              ['Choose a duration', ''],
+              ['3 Months', '3_months',
+               { 'data-expiry-date' => '01 April 2020' }],
+              ['6 Months', '6_months',
+               { 'data-expiry-date' => '01 July 2020' }],
+              ['12 Months', '12_months', {
+                'data-expiry-date' => '30 December 2020'
+              }]
+            ]
+          )
+        end
+      end
 
-      it 'returns a localised list of expiry dates' do
-        is_expected.to match_array(
-          [
-            ['Elija una duración', ''],
-            ['3 Meses', '3_months',
-             { 'data-expiry-date' => '01 abril 2020' }],
-            ['6 Meses', '6_months',
-             { 'data-expiry-date' => '01 julio 2020' }],
-            ['12 Meses', '12_months', {
-              'data-expiry-date' => '30 diciembre 2020'
-            }]
-          ]
-        )
+      context 'with a different locale' do
+        before { AlaveteliLocalization.set_locales(:es, :es) }
+
+        it 'returns a localised list of expiry dates' do
+          is_expected.to match_array(
+            [
+              ['Elija una duración', ''],
+              ['3 Meses', '3_months',
+               { 'data-expiry-date' => '01 abril 2020' }],
+              ['6 Meses', '6_months',
+               { 'data-expiry-date' => '01 julio 2020' }],
+              ['12 Meses', '12_months', {
+                'data-expiry-date' => '30 diciembre 2020'
+              }]
+            ]
+          )
+        end
       end
     end
 
     context 'without embargo' do
-      let(:embargo) { nil }
+      subject { embargo_extension_options }
 
       around do |example|
         time_travel_to Time.utc(2020, 1, 2)
