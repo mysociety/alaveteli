4601ef28b81a282689a11a2585c90afa72b4657d
Graeme Porteous <graeme@rgbp.co.uk>
2020-04-22 11:42:55 +0100

    Add some missing specs around Pro classifications

--- spec/controllers/alaveteli_pro/classifications_controller_spec.rb
+++ spec/controllers/alaveteli_pro/classifications_controller_spec.rb
@@ -2,19 +2,73 @@ require 'spec_helper'
 
 RSpec.describe AlaveteliPro::ClassificationsController, type: :controller do
   describe '#create' do
-    let(:pro_user) { FactoryBot.create(:pro_user) }
-    let(:other_pro_user) { FactoryBot.create(:pro_user) }
-    let(:info_request) { FactoryBot.create(:info_request, user: pro_user) }
+    let(:user) { FactoryBot.create(:pro_user) }
+    let(:ability) { Object.new.extend(CanCan::Ability) }
 
-    context 'when the user is not allowed to update the request' do
+    before do
+      session[:user_id] = user.id
+      allow(controller).to receive(:current_user).and_return(user)
+
+      allow(controller).to receive(:current_ability).and_return(ability)
+    end
+
+    context 'request to be classified can not be found' do
+      it'raises a ActiveRecord::RecordNotFound error' do
+        expect {
+          post :create, params: { url_title: 'invalid' }
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    shared_context 'request to be classified can be found' do
+      let(:info_request) { FactoryBot.create(:info_request, user: user) }
+
+      before do
+        allow(InfoRequest).to receive(:find_by!).with(url_title: 'url_title').
+          and_return(info_request)
+      end
+
+      def post_status(status, message: nil)
+        classification = { described_state: status }
+        classification[:message] = message if message
+
+        post :create, params: {
+          classification: classification,
+          url_title: 'url_title'
+        }
+      end
+    end
+
+    context 'user is not allow to update the request' do
+      include_context 'request to be classified can be found'
+
+      before { ability.cannot :update_request_state, info_request }
+
       it 'raises a CanCan::AccessDenied error' do
-        session[:user_id] = other_pro_user.id
-        expect do
-          post :create, params: {
-            url_title: info_request.url_title,
-            classification: { described_state: 'successful' }
-          }
-        end.to raise_error(CanCan::AccessDenied)
+        expect {
+          post_status('successful')
+        }.to raise_error(CanCan::AccessDenied)
+      end
+    end
+
+    shared_context 'user can classify request' do
+      include_context 'request to be classified can be found'
+      before { ability.can :update_request_state, info_request }
+    end
+
+    context 'user is allowed to update the request' do
+      include_context 'user can classify request'
+
+      it 'should call set_described_state on the request' do
+        expect(info_request).to receive(:set_described_state)
+        post_status('successful')
+      end
+
+      it 'should redirect back to the request' do
+        post_status('successful')
+        expect(response).to redirect_to(
+          show_alaveteli_pro_request_path(url_title: info_request.url_title)
+        )
       end
     end
   end
