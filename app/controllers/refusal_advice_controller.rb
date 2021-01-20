239233de8c4e998b4f636cf98cacf17a17f0cb62
Graeme Porteous <graeme@rgbp.co.uk>
2021-01-20 08:20:04 +0000

    Store refusal advice wizard answers and suggestion
    Create an `InfoRequestEvent` when an user submits the form and redirect
    to follow up action.

--- app/controllers/refusal_advice_controller.rb
+++ app/controllers/refusal_advice_controller.rb
@@ -3,11 +3,36 @@
 # after their Info Requests are marked as refused/rejected.
 #
 class RefusalAdviceController < ApplicationController
+  before_action :authenticate
+
   def create
-    @params = parsed_refusal_advice_params # TODO: move into log event method
+    log_event
   end
 
   private
+
+  def authenticate
+    authenticated_as_user?(info_request.user) if info_request
+  end
+
+  def log_event
+    return unless info_request && current_user
+
+    info_request.log_event(
+      'refusal_advice',
+      parsed_refusal_advice_params.merge(user_id: current_user.id).to_h
+    )
+  end
+
+  def info_request
+    return unless url_title_param
+
+    @info_request ||= InfoRequest.find_by!(url_title: url_title_param)
+  end
+
+  def url_title_param
+    params[:url_title]
+  end
 
   def refusal_advice_params
     params.require('refusal_advice').permit(:id, questions: {}, actions: {})
