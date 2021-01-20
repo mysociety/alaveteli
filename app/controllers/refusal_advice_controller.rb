2b0f40b1a92f14cd2ca378dba811e7694ac01363
Graeme Porteous <graeme@rgbp.co.uk>
2021-01-20 08:17:58 +0000

    Update refusal advice wizard HTML
    If there is a InfoRequest present then this will add a form which will
    submit wizard answers, suggestion and selected action to the refusal
    advice controller.

--- app/controllers/refusal_advice_controller.rb
+++ app/controllers/refusal_advice_controller.rb
@@ -4,5 +4,21 @@
 #
 class RefusalAdviceController < ApplicationController
   def create
+    @params = parsed_refusal_advice_params # TODO: move into log event method
+  end
+
+  private
+
+  def refusal_advice_params
+    params.require('refusal_advice').permit(:id, questions: {}, actions: {})
+  end
+
+  def parsed_refusal_advice_params
+    refusal_advice_params.merge(
+      actions: refusal_advice_params.fetch(:actions).
+        each_pair do |_, suggestions|
+          suggestions.transform_values! { |v| v == 'true' }
+        end
+    )
   end
 end
--- spec/helpers/refusal_advice_helper_spec.rb
+++ spec/helpers/refusal_advice_helper_spec.rb
@@ -4,17 +4,26 @@ describe RefusalAdviceHelper do
   include RefusalAdviceHelper
 
   describe '#refusal_advice_question' do
-    subject { refusal_advice_question(question, option) }
+    subject { refusal_advice_question(question, option, f: form_builder) }
 
+    let(:form_builder) { double(:form_builder, object_name: 'refusal-advice') }
     let(:question) { double(id: 'confirm-or-deny', options: options) }
     let(:options) { [option] }
     let(:option) { double(value: 'yes', label: 'Yes') }
 
-    it { is_expected.to match(/name="confirm-or-deny"/) }
     it { is_expected.to match(/id="confirm-or-deny_yes"/) }
     it { is_expected.to match(/value="yes"/) }
     it { is_expected.to match(/for="confirm-or-deny_yes"/) }
     it { is_expected.to match(/Yes/) }
+
+    context 'with form_builder' do
+      it { is_expected.to match(/name="refusal-advice"/) }
+    end
+
+    context 'without form_builder' do
+      subject { refusal_advice_question(question, option) }
+      it { is_expected.to match(/name="confirm-or-deny"/) }
+    end
 
     context 'two or fewer options' do
       let(:options) { [option, option] }
