0500907a0f890e3dcc98db07eabbdc802ce35cdd
Graeme Porteous <graeme@rgbp.co.uk>
2021-01-20 08:15:12 +0000

    Create refusal advice controller
    Allow us to setup HTML form to submit to the correct action.

---
+++ app/controllers/refusal_advice_controller.rb
@@ -0,0 +1,8 @@
+##
+# Controller to store advice, from the refusal advice wizard, given to users
+# after their Info Requests are marked as refused/rejected.
+#
+class RefusalAdviceController < ApplicationController
+  def create
+  end
+end
