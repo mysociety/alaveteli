7c5f92cbe66fc6b65dec77c876a21b303c907f74
Graeme Porteous <graeme@rgbp.co.uk>
2021-01-29 15:29:54 +0000

    Set refusal advice in FollowupsController
    `OutgoingMessage` snippets can then be rendered when creating a new
    followup message.

--- app/models/refusal_advice.rb
+++ app/models/refusal_advice.rb
@@ -2,14 +2,15 @@
 # A collection of Questions that help users challenge refusals.
 #
 class RefusalAdvice
-  def self.default(info_request = nil)
+  def self.default(info_request = nil, **options)
     files = Rails.configuration.paths['config/refusal_advice'].existent
-    new(Store.from_yaml(files), info_request: info_request)
+    new(Store.from_yaml(files), info_request: info_request, **options)
   end
 
-  def initialize(data, info_request: nil)
+  def initialize(data, info_request: nil, **options)
     @data = data
     @info_request = info_request
+    @options = options
   end
 
   def legislation
@@ -24,6 +25,17 @@ class RefusalAdvice
   def actions
     data[legislation.to_sym][:actions].
       map { |action| Action.new(action) }
+  end
+
+  ##
+  # Return any OutgoingMessage refusal advice snippets. Also can pass in an
+  # additional scope to further limit the snippets returned. EG if we want
+  # internal_review advice or not.
+  #
+  def snippets
+    scope = OutgoingMessage::Snippet.with_tag('refusal_advice')
+    return scope.with_tag('internal_review') if @options[:internal_review]
+    scope.without_tag('internal_review')
   end
 
   def ==(other)
