f86893996167b74ecd1fd86d85d1b897a692dbd1
Graeme Porteous <graeme@rgbp.co.uk>
2021-02-09 08:28:15 +0000

    Add RefusalAdvice::Suggestion#advice
    This replaces #label and improves the method name.

--- spec/models/refusal_advice/suggestion_spec.rb
+++ spec/models/refusal_advice/suggestion_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe RefusalAdvice::Suggestion do
   let(:data) do
     {
       action: 'reply',
+      advice: { plain: 'Refusing a request on cost grounds...' },
       response_template: 'i-only-need-some-of-the-information'
     }
   end
@@ -13,6 +14,35 @@ RSpec.describe RefusalAdvice::Suggestion do
   describe '#action' do
     subject { suggestion.action }
     it { is_expected.to eq('reply') }
+  end
+
+  describe '#advice' do
+    subject { suggestion.advice }
+
+    it 'returns hash with valid render options' do
+      is_expected.
+        to eq('plain' => 'Refusing a request on cost grounds...')
+    end
+
+    context 'with HTML render option' do
+      let(:data) { { advice: { html: '<h1>Hello World</h1>' } } }
+
+      it 'marks HTML as being safe' do
+        is_expected.to eq('html' => '<h1>Hello World</h1>')
+        expect(suggestion.advice['html']).to be_html_safe
+      end
+    end
+
+    context 'with invalid render option' do
+      let(:data) { { advice: { invalid: 'Boom' } } }
+
+      it 'raises unpermitted parameter error' do
+        expect { suggestion.advice }.to raise_error(
+          ActionController::UnpermittedParameters,
+          'found unpermitted parameter: :invalid'
+        )
+      end
+    end
   end
 
   describe '#response_template' do
