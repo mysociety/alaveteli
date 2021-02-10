cd2299149215724cef86934b268393cdedbed73a
Graeme Porteous <graeme@rgbp.co.uk>
2021-02-10 09:53:06 +0000

    Add refusal advice target redirection
    When an action has been click then we need to redirect to the target
    defined in the refusal advice YAML data.

--- spec/controllers/refusal_advice_controller_spec.rb
+++ spec/controllers/refusal_advice_controller_spec.rb
@@ -22,8 +22,149 @@ RSpec.describe RefusalAdviceController do
       }
     end
 
+    let(:target) { { internal: 'followup' } }
+    let(:action) { RefusalAdvice::Action.new(id: 'action_2', target: target) }
+
     before do
       allow(controller).to receive(:current_user).and_return(user)
+      allow(RefusalAdvice).to receive(:default).
+        and_return(double(actions: [action]))
+    end
+
+    shared_examples 'internal target redirection' do
+      context 'when internal followup target' do
+        let(:target) { { internal: 'followup' } }
+
+        it 'redirects to new followup action' do
+          post :create, params: params
+          expect(response).to redirect_to(
+            new_request_followup_path(
+              request_id: info_request.id,
+              anchor: 'followup'
+            )
+          )
+        end
+      end
+
+      context 'when internal followup target' do
+        let(:target) { { internal: 'internal_review' } }
+
+        it 'redirects to new internal review action' do
+          post :create, params: params
+          expect(response).to redirect_to(
+            new_request_followup_path(
+              request_id: info_request.id,
+              internal_review: '1',
+              anchor: 'followup'
+            )
+          )
+        end
+      end
+
+      context 'when new request target' do
+        let(:target) { { internal: 'new_request' } }
+
+        it 'redirects to new request action' do
+          post :create, params: params
+          expect(response).to redirect_to(
+            new_request_to_body_path(
+              url_name: info_request.public_body.url_name
+            )
+          )
+        end
+      end
+
+      context 'when unknown internal target' do
+        let(:target) { { internal: 'unknown' } }
+
+        it 'raises redirection error' do
+          expect { post :create, params: params }.to raise_error(
+            RefusalAdvice::Action::RedirectionError,
+            %q(Can't redirect to {:internal=>"unknown"})
+          )
+        end
+      end
+    end
+
+    shared_examples 'internal target exception' do
+      context 'when internal followup target' do
+        let(:target) { { internal: 'followup' } }
+
+        it 'raises redirection error' do
+          expect { post :create, params: params }.to raise_error(
+            RefusalAdvice::Action::RedirectionError,
+            %q(Can't redirect to {:internal=>"followup"})
+          )
+        end
+      end
+
+      context 'when internal followup target' do
+        let(:target) { { internal: 'internal_review' } }
+
+        it 'raises redirection error' do
+          expect { post :create, params: params }.to raise_error(
+            RefusalAdvice::Action::RedirectionError,
+            %q(Can't redirect to {:internal=>"internal_review"})
+          )
+        end
+      end
+
+      context 'when new request target' do
+        let(:target) { { internal: 'new_request' } }
+
+        it 'raises redirection error' do
+          expect { post :create, params: params }.to raise_error(
+            RefusalAdvice::Action::RedirectionError,
+            %q(Can't redirect to {:internal=>"new_request"})
+          )
+        end
+      end
+
+      context 'when unknown internal target' do
+        let(:target) { { internal: 'unknown' } }
+
+        it 'raises redirection error' do
+          expect { post :create, params: params }.to raise_error(
+            RefusalAdvice::Action::RedirectionError,
+            %q(Can't redirect to {:internal=>"unknown"})
+          )
+        end
+      end
+    end
+
+    shared_examples 'help page target redirection' do
+      context 'when help page target' do
+        let(:target) { { help_page: 'ico' } }
+
+        it 'redirects to help page' do
+          post :create, params: params
+          expect(response).to redirect_to(help_general_path(template: 'ico'))
+        end
+      end
+    end
+
+    shared_examples 'external target redirection' do
+      context 'when external target' do
+        let(:target) { { external: 'http://www.writetothem.com/' } }
+
+        it 'redirects to external page' do
+          post :create, params: params
+          expect(response).to redirect_to('http://www.writetothem.com/')
+        end
+      end
+    end
+
+    shared_examples 'invalid target exception' do
+      context 'when invalid target' do
+        let(:target) { { invalid: 'invalid' } }
+
+        it 'raises redirection error' do
+          expect { post :create, params: params }.to raise_error(
+            RefusalAdvice::Action::RedirectionError,
+            %q(Can't redirect to {:invalid=>"invalid"})
+          )
+        end
+      end
     end
 
     context 'when logged in as request owner' do
@@ -59,6 +200,11 @@ RSpec.describe RefusalAdviceController do
 
           post :create, params: params
         end
+
+        include_examples 'internal target redirection'
+        include_examples 'help page target redirection'
+        include_examples 'external target redirection'
+        include_examples 'invalid target exception'
       end
 
       context 'invalid refusal advice params' do
@@ -94,10 +240,10 @@ RSpec.describe RefusalAdviceController do
       context 'without url title param' do
         before { params.delete(:url_title) }
 
-        it 'returns no content' do
-          post :create, params: params
-          expect(response).to have_http_status(:no_content)
-        end
+        include_examples 'internal target exception'
+        include_examples 'help page target redirection'
+        include_examples 'external target redirection'
+        include_examples 'invalid target exception'
       end
     end
 
@@ -113,10 +259,10 @@ RSpec.describe RefusalAdviceController do
       context 'without url title param' do
         before { params.delete(:url_title) }
 
-        it 'returns no content' do
-          post :create, params: params
-          expect(response).to have_http_status(:no_content)
-        end
+        include_examples 'internal target exception'
+        include_examples 'help page target redirection'
+        include_examples 'external target redirection'
+        include_examples 'invalid target exception'
       end
     end
 
@@ -136,10 +282,10 @@ RSpec.describe RefusalAdviceController do
       context 'without url title param' do
         before { params.delete(:url_title) }
 
-        it 'returns no content' do
-          post :create, params: params
-          expect(response).to have_http_status(:no_content)
-        end
+        include_examples 'internal target exception'
+        include_examples 'help page target redirection'
+        include_examples 'external target redirection'
+        include_examples 'invalid target exception'
       end
     end
   end
