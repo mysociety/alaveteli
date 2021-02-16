f5899dd7077e182c2fe82f67156d167ef92b1c81
Graeme Porteous <graeme@rgbp.co.uk>
2021-02-16 09:32:56 +0000

    Update Xapian wildcard exception handling
    Use new ActsAsXapian exception instead of relying matching the exception
    message which has/may changed in the past/future.
    
    Adds a spec, using the new `max_wildcard_expansion` config to prove
    non-wildcard searches will happen.

--- lib/typeahead_search.rb
+++ lib/typeahead_search.rb
@@ -20,13 +20,11 @@ class TypeaheadSearch
     ActsAsXapian.query_parser.default_op = Xapian::Query::OP_OR
     begin
       xapian_search = run_query
-    rescue RuntimeError => e
-      if e.message =~ /^QueryParserError: Wildcard/
-        # Wildcard expands to too many terms
-        Rails.logger.info "Wildcard query '#{query}' caused: #{e.message.force_encoding('UTF-8')}"
-        @wildcard = false
-        xapian_search = run_query
-      end
+    rescue ActsAsXapian::UnhandledRuntimeError => e
+      # Wildcard expands to too many terms
+      Rails.logger.info "Wildcard query '#{query}' caused: #{e.message.force_encoding('UTF-8')}"
+      @wildcard = false
+      xapian_search = run_query
     end
     ActsAsXapian.query_parser.default_op = old_default_op
     xapian_search
