ceccd9d15937250edc2211a42d628c97ba5420b2
Graeme Porteous <graeme@rgbp.co.uk>
2021-02-16 09:25:26 +0000

    Allow Xapian wildcard expansion to be configurable
    Still defaults to 1000 expansions but this can now be overriden by
    adding a `config/xapian.yml` file or by calling
    `ActsAsXapian.max_wildcard_expansion = n` for example in a before spec
    callback hook.

--- lib/acts_as_xapian/acts_as_xapian.rb
+++ lib/acts_as_xapian/acts_as_xapian.rb
@@ -88,6 +88,12 @@ module ActsAsXapian
   def self.config
     @@config
   end
+  def self.max_wildcard_expansion=(max_wildcard_expansion)
+    @@max_wildcard_expansion = max_wildcard_expansion
+  end
+  def self.max_wildcard_expansion
+    @@max_wildcard_expansion
+  end
 
   ######################################################################
   # Initialisation
@@ -121,6 +127,8 @@ module ActsAsXapian
     Dir.mkdir(db_parent_path) unless File.exist?(db_parent_path)
 
     @@db_path = File.join(db_parent_path, environment)
+
+    @@max_wildcard_expansion = config.fetch('max_wildcard_expansion', 1000)
 
     # make some things that don't depend on the db
     # TODO: this gets made once for each acts_as_xapian. Oh well.
@@ -170,7 +178,9 @@ module ActsAsXapian
     # Large installations of Alaveteli should consider
     # upgrading, because uncontrolled wildcard expansion
     # can crash the whole server: see http://trac.xapian.org/ticket/350
-    @@query_parser.set_max_wildcard_expansion(1000) if @@query_parser.respond_to? :set_max_wildcard_expansion
+    if @@query_parser.respond_to? :set_max_wildcard_expansion
+      @@query_parser.set_max_wildcard_expansion(@@max_wildcard_expansion)
+    end
 
     @@stopper = Xapian::SimpleStopper.new
     @@stopper.add("and")
