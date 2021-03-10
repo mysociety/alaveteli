1c541a6141c55fccfa09dd5ba1e263eb22964706
Matthew Landauer <mlandauer@gmail.com>
2021-03-10 15:02:00 +1100

    Extract traits in factory for info_request
    This will make it easier to create info_requests with multiple 
    conditions set at the same time

--- spec/factories/info_requests.rb
+++ spec/factories/info_requests.rb
@@ -48,146 +48,35 @@ FactoryBot.define do
       initial_request.save
     end
 
-    factory :info_request_with_incoming do
+    trait :with_incoming do
       after(:create) do |info_request, evaluator|
         incoming_message = create(:incoming_message, :info_request => info_request)
         info_request.log_event("response", {:incoming_message_id => incoming_message.id})
       end
-
-      factory :waiting_clarification_info_request do
-        after(:create) do |info_request, evaluator|
-          info_request.set_described_state('waiting_clarification')
-        end
-      end
-
-      factory :successful_request do
-        after(:create) do |info_request, evaluator|
-          info_request.set_described_state('successful')
-        end
-      end
-
-      factory :requires_admin_request do
-        after(:create) do |info_request, evaluator|
-          info_request.log_event(
-            'status_update',
-            user_id: info_request.user.id,
-            old_described_state: info_request.described_state,
-            described_state: 'requires_admin',
-            message: 'Useful info')
-          info_request.set_described_state('requires_admin')
-        end
-      end
-
-      factory :error_message_request do
-        after(:create) do |info_request, evaluator|
-          info_request.log_event(
-            'status_update',
-            user_id: info_request.user.id,
-            old_described_state: info_request.described_state,
-            described_state: 'error_message',
-            message: 'Useful info')
-          info_request.set_described_state('error_message')
-        end
-      end
-
-      factory :blank_message_request do
-        after(:create) do |info_request, evaluator|
-          info_request.log_event(
-            'status_update',
-            user_id: info_request.user.id,
-            old_described_state: info_request.described_state,
-            described_state: 'error_message',
-            message: '')
-          info_request.set_described_state('error_message')
-        end
-      end
-
-      factory :attention_requested_request do
-        after(:create) do |info_request, evaluator|
-          info_request.log_event('report_request',
-                                 request_id: info_request.id,
-                                 editor: info_request.user,
-                                 reason: 'Not a valid request',
-                                 message: 'Useful info',
-                                 old_attention_requested: false,
-                                 attention_requested: true)
-          info_request.set_described_state('attention_requested')
-        end
-      end
-
-      factory :not_held_request do
-        after(:create) do |info_request, evaluator|
-          info_request.set_described_state('not_held')
-        end
-      end
-
     end
 
-    factory :info_request_with_plain_incoming do
+    trait :with_plain_incoming do
       after(:create) do |info_request, evaluator|
         incoming_message = create(:plain_incoming_message, :info_request => info_request)
         info_request.log_event("response", {:incoming_message_id => incoming_message.id})
       end
     end
 
-    factory :info_request_with_html_attachment do
+    trait :with_incoming_with_html_attachment do
       after(:create) do |info_request, evaluator|
         incoming_message = create(:incoming_message_with_html_attachment, :info_request => info_request)
         info_request.log_event("response", {:incoming_message_id => incoming_message.id})
       end
     end
 
-    factory :info_request_with_incoming_attachments do
+    trait :with_incoming_with_attachments do
       after(:create) do |info_request, evaluator|
         incoming_message = create(:incoming_message_with_attachments, :info_request => info_request)
         info_request.log_event("response", {:incoming_message_id => incoming_message.id})
       end
     end
 
-    factory :info_request_with_internal_review_request do
-      after(:create) do |info_request, evaluator|
-        outgoing_message = create(:internal_review_request, :info_request => info_request)
-      end
-    end
-
-    factory :embargoed_request do
-      after(:create) do |info_request, evaluator|
-        create(:embargo, :info_request => info_request)
-        info_request.reload
-        incoming_message = create(:incoming_message_with_attachments, :info_request => info_request)
-        info_request.log_event("response", {:incoming_message_id => incoming_message.id})
-      end
-    end
-
-    factory :embargo_expiring_request do
-      after(:create) do |info_request, evaluator|
-        create(:expiring_embargo, :info_request => info_request)
-        info_request.reload
-      end
-    end
-
-    factory :re_embargoed_request do
-      after(:create) do |info_request, evaluator|
-        info_request.log_event('expire_embargo', {})
-        create(:embargo, :info_request => info_request)
-        info_request
-      end
-    end
-
-    factory :embargo_expired_request do
-      after(:create) do |info_request, evaluator|
-        info_request.log_event("expire_embargo", info_request: info_request)
-        info_request.reload
-      end
-    end
-
-    factory :external_request do
-      user { nil }
-      external_user_name { 'External User' }
-      external_url { 'http://www.example.org/request/external' }
-    end
-
-    factory :old_unclassified_request do
+    trait :with_old_incoming do
       after(:create) do |info_request, evaluator|
         incoming_message = FactoryBot.create(
           :incoming_message,
@@ -204,12 +93,95 @@ FactoryBot.define do
           )
         ]
         info_request.last_public_response_at = Time.zone.now - 31.days
-        info_request.awaiting_description = true
         info_request.save!
       end
     end
 
-    factory :awaiting_description do
+    trait :requires_admin do
+      after(:create) do |info_request, evaluator|
+        info_request.log_event(
+          'status_update',
+          user_id: info_request.user.id,
+          old_described_state: info_request.described_state,
+          described_state: 'requires_admin',
+          message: 'Useful info')
+        info_request.set_described_state('requires_admin')
+      end
+    end
+
+    trait :error_message do
+      after(:create) do |info_request, evaluator|
+        info_request.log_event(
+          'status_update',
+          user_id: info_request.user.id,
+          old_described_state: info_request.described_state,
+          described_state: 'error_message',
+          message: 'Useful info')
+        info_request.set_described_state('error_message')
+      end
+    end
+
+    trait :error_message_blank do
+      after(:create) do |info_request, evaluator|
+        info_request.log_event(
+          'status_update',
+          user_id: info_request.user.id,
+          old_described_state: info_request.described_state,
+          described_state: 'error_message',
+          message: '')
+        info_request.set_described_state('error_message')
+      end
+    end
+
+    trait :attention_requested do
+      after(:create) do |info_request, evaluator|
+        info_request.log_event('report_request',
+                               request_id: info_request.id,
+                               editor: info_request.user,
+                               reason: 'Not a valid request',
+                               message: 'Useful info',
+                               old_attention_requested: false,
+                               attention_requested: true)
+        info_request.set_described_state('attention_requested')
+      end
+    end
+
+    trait :with_internal_review_request do
+      after(:create) do |info_request, evaluator|
+        outgoing_message = create(:internal_review_request, :info_request => info_request)
+      end
+    end
+
+    trait :embargoed do
+      after(:create) do |info_request, evaluator|
+        create(:embargo, :info_request => info_request)
+        info_request.reload
+      end
+    end
+
+    trait :embargo_expiring do
+      after(:create) do |info_request, evaluator|
+        create(:expiring_embargo, :info_request => info_request)
+        info_request.reload
+      end
+    end
+
+    trait :re_embargoed do
+      after(:create) do |info_request, evaluator|
+        info_request.log_event('expire_embargo', {})
+        create(:embargo, :info_request => info_request)
+        info_request
+      end
+    end
+
+    trait :embargo_expired do
+      after(:create) do |info_request, evaluator|
+        info_request.log_event("expire_embargo", info_request: info_request)
+        info_request.reload
+      end
+    end
+
+    trait :awaiting_description do
       awaiting_description { true }
       after(:create) do |info_request, evaluator|
         info_request.awaiting_description = true
@@ -217,15 +189,39 @@ FactoryBot.define do
       end
     end
 
-    factory :hidden_request do
+    trait :external do
+      user { nil }
+      external_user_name { 'External User' }
+      external_url { 'http://www.example.org/request/external' }
+    end
+
+    trait :hidden do
       prominence { 'hidden' }
     end
 
-    factory :backpage_request do
+    trait :backpage do
       prominence { 'backpage' }
     end
 
-    factory :overdue_request do
+    trait :waiting_clarification do
+      after(:create) do |info_request, evaluator|
+        info_request.set_described_state('waiting_clarification')
+      end
+    end
+
+    trait :successful do
+      after(:create) do |info_request, evaluator|
+        info_request.set_described_state('successful')
+      end
+    end
+
+    trait :not_held do
+      after(:create) do |info_request, evaluator|
+        info_request.set_described_state('not_held')
+      end
+    end
+
+    trait :overdue do
       date_response_required_by { Time.zone.now - 1.day }
       after(:create) do |info_request, evaluator|
         info_request.date_response_required_by = Time.zone.now - 1.day
@@ -233,7 +229,7 @@ FactoryBot.define do
       end
     end
 
-    factory :very_overdue_request do
+    trait :very_overdue do
       date_response_required_by { Time.zone.now - 21.days }
       date_very_overdue_after { Time.zone.now - 1.days }
       after(:create) do |info_request, evaluator|
@@ -243,9 +239,35 @@ FactoryBot.define do
       end
     end
 
-    factory :use_notifications_request do
+    trait :use_notifications do
       use_notifications { true }
     end
-  end
 
+    factory :info_request_with_incoming, traits: [:with_incoming] do
+      factory :waiting_clarification_info_request, traits: [:waiting_clarification]
+      factory :successful_request, traits: [:successful]
+      factory :requires_admin_request, traits: [:requires_admin]
+      factory :error_message_request, traits: [:error_message]
+      factory :blank_message_request, traits: [:error_message_blank]
+      factory :attention_requested_request, traits: [:attention_requested]
+      factory :not_held_request, traits: [:not_held]
+    end
+
+    factory :info_request_with_plain_incoming, traits: [:with_plain_incoming]
+    factory :info_request_with_html_attachment, traits: [:with_incoming_with_html_attachment]
+    factory :info_request_with_incoming_attachments, traits: [:with_incoming_with_attachments]
+    factory :info_request_with_internal_review_request, traits: [:with_internal_review_request]
+    factory :embargoed_request, traits: [:embargoed, :with_incoming_with_attachments]
+    factory :embargo_expiring_request, traits: [:embargo_expiring]
+    factory :re_embargoed_request, traits: [:re_embargoed]
+    factory :embargo_expired_request, traits: [:embargo_expired]
+    factory :external_request, traits: [:external]
+    factory :old_unclassified_request, traits: [:with_old_incoming, :awaiting_description]
+    factory :awaiting_description, traits: [:awaiting_description]
+    factory :hidden_request, traits: [:hidden]
+    factory :backpage_request, traits: [:backpage]
+    factory :overdue_request, traits: [:overdue]
+    factory :very_overdue_request, traits: [:very_overdue]
+    factory :use_notifications_request, traits: [:use_notifications]
+  end
 end
