9ca7d0362b716be66fa75c018b4ea33080aef105
Gareth Rees <gareth@garethrees.co.uk>
2021-03-24 18:01:29 +0000

    Move highlight_prominence to Admin::ProminenceHelper
    Moves `highlight_prominence` to the more appropriate helper.
    
    Minor refactoring to code and specs to bring more in-line with
    `prominence_icon`. No behaviour change other than allowing an argument
    that responds to `#prominence` as well as a `String`.

--- spec/helpers/admin/prominence_helper_spec.rb
+++ spec/helpers/admin/prominence_helper_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe Admin::ProminenceHelper, type: :helper do
+  %i[normal backpage requester_only hidden].each do |prominence|
+    let(prominence) { double(prominence: prominence.to_s) }
+  end
+
   describe '#prominence_icon' do
     subject { helper.prominence_icon(prominence) }
-
-    %i[normal backpage requester_only hidden].each do |prominence|
-      let(prominence) { double(prominence: prominence.to_s) }
-    end
 
     context 'when given a string' do
       let(:prominence) { 'normal' }
@@ -35,6 +35,43 @@ describe Admin::ProminenceHelper, type: :helper do
       let(:prominence) { hidden }
       it { is_expected.to include(%q(title="hidden")) }
       it { is_expected.to include('icon-prominence--hidden') }
+    end
+  end
+
+  describe '#highlight_prominence' do
+    subject { helper.highlight_prominence(prominence) }
+
+    context 'when given a string' do
+      let(:prominence) { 'backpage' }
+      it { is_expected.to eq(helper.highlight_prominence(backpage)) }
+    end
+
+    context 'normal' do
+      let(:prominence) { normal }
+      it { is_expected.to eq('normal') }
+    end
+
+    context 'backpage' do
+      let(:prominence) { backpage }
+      it { is_expected.to eq(%q(<span class="text-warning">backpage</span>)) }
+    end
+
+    context 'requester_only' do
+      let(:prominence) { requester_only }
+
+      it do
+        is_expected.to eq(%q(<span class="text-warning">requester_only</span>))
+      end
+    end
+
+    context 'hidden' do
+      let(:prominence) { hidden }
+      it { is_expected.to eq(%q(<span class="text-error">hidden</span>)) }
+    end
+
+    context 'an unhandled string' do
+      let(:prominence) { 'unhandled' }
+      it { is_expected.to eq('unhandled') }
     end
   end
 end
