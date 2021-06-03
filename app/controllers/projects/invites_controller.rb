8646c460b1a85e2bac001a7c6056583c57e3a713
Graeme Porteous <graeme@rgbp.co.uk>
2021-06-03 12:10:23 +0100

    Create and sign in demo users when someone joins a project

--- app/controllers/projects/invites_controller.rb
+++ app/controllers/projects/invites_controller.rb
@@ -20,11 +20,33 @@ class Projects::InvitesController < Projects::BaseController
   end
 
   def authenticate
+    sign_in_as_demo_user unless current_user
     authenticated?(
       web: _('To join this project'),
       email: _('Then you can join this project'),
       email_subject: _('Confirm your account on {{site_name}}',
                        site_name: site_name)
     )
+  end
+
+  def sign_in_as_demo_user
+    session[:user_id] = create_demo_user.id
+  end
+
+  def create_demo_user
+    user = nil
+
+    loop do
+      uuid = SecureRandom.hex(3)
+      user = User.new(
+        name: "Demo User #{uuid}",
+        email: "foi-demo-#{uuid}@localhost",
+        email_confirmed: true,
+        password: uuid * 3
+      )
+      break unless User.find_by(email: user.email)
+    end
+
+    user if user.save!
   end
 end
