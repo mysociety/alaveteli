mysociety/alaveteli

commit a90a1b87fa466eeb57d2d529012262269f3d4685

Add wizard option input class
Filter inputs using this class in the JS. This is better than finding
all inputs elements which could accidentally match other unexpected
elements.

Also removes handling for select/option elements in the JS as this
hasn't been tested.

app/assets/javascripts/wizard.js
@@ -8,6 +8,7 @@
       questionClass: "wizard__question",
       questionAnswerableClass: "wizard__question--answerable",
       questionAnsweredClass: "wizard__question--answered",
+      questionOptionClass: "wizard__question__option",
 
       suggestionClass: "wizard__suggestion",
       suggestionSuggestedClass: "wizard__suggestion--suggested",
@@ -108,8 +109,10 @@
   };
 
   RefusalWizard.prototype._valuesOf = function(question) {
+    var wizard = this;
+
     return $(question)
-      .find("input:checked, option:selected")
+      .find("input." + wizard.options.questionOptionClass + ":checked")
       .map(function() {
         return $(this).val();
       })
@@ -124,8 +127,7 @@
       var showIfArray = $block.data("show-if");
 
       // can't be a dependent if already answered
-      // FIXME: this assumes suggestions won't have input/option elements
-      if ($block.find("input:checked, option:selected").length) {
+      if ($block.find("input." + wizard.options.questionOptionClass + ":checked").length) {
         return false;
       }
 
@@ -212,7 +214,7 @@
 
     if ($next_question) {
       $next_question.addClass(wizard.options.questionAnswerableClass);
-      $next_question.find("input[value=yes]").focus();
+      $next_question.find("input." + wizard.options.questionOptionClass + "[value=yes]").focus();
     }
 
     if ($current_question && $next_question) {
@@ -238,8 +240,8 @@
         .addClass(wizard.options.nextStepSuggestedClass);
     });
 
-    wizard.$actions.find('input[name^="refusal_advice"]').val(false);
-    $suggestions.find('input[name^="refusal_advice"]').val(true);
+    wizard.$actions.find('input.' + wizard.options.questionOptionClass + '[name^="refusal_advice"]').val(false);
+    $suggestions.find('input.' + wizard.options.questionOptionClass + '[name^="refusal_advice"]').val(true);
   };
 
   RefusalWizard.prototype._resetQuestion = function($question) {
@@ -247,9 +249,8 @@
     $question.removeClass(wizard.options.questionAnswerableClass);
     $question.removeClass(wizard.options.questionAnsweredClass);
 
-    var $options = $question.find("input, option");
+    var $options = $question.find("input." + wizard.options.questionOptionClass);
     $options.prop("checked", false);
-    $options.prop("selected", false);
   };
 
   RefusalWizard.prototype.log = function() {

app/helpers/refusal_advice_question_form.rb
@@ -7,11 +7,15 @@ class RefusalAdviceQuestionForm < ActionView::Helpers::FormBuilder
     @template.tag.div do
       value = option.value
       id = "#{@object.id}_#{value}"
+      options = {
+        id: id,
+        class: 'wizard__question__option'
+      }
 
       if refusal_advice_grid?(@object.options)
-        input = @template.check_box_tag(object_name, value, false, id: id)
+        input = @template.check_box_tag(object_name, value, false, options)
       else
-        input = @template.radio_button_tag(object_name, value, false, id: id)
+        input = @template.radio_button_tag(object_name, value, false, options)
       end
 
       input + @template.label_tag(id, option.label)

commit 2bcfe584a7852599f3c3ff682f6838f765fbd1a6

Fix refusal advice suggestion hidden input value
The hidden form inputs which pass through which advice has been shown to
the user wasn't being updated. This means its not been stored and
snippets not shown.

Broken in a90a1b8

app/assets/javascripts/wizard.js
@@ -244,8 +244,8 @@
         .addClass(wizard.options.nextStepSuggestedClass);
     });
 
-    wizard.$actions.find('input.' + wizard.options.questionOptionClass + '[name^="refusal_advice"]').val(false);
-    $suggestions.find('input.' + wizard.options.questionOptionClass + '[name^="refusal_advice"]').val(true);
+    wizard.$actions.find('input[name^="refusal_advice"]').val(false);
+    $suggestions.find('input[name^="refusal_advice"]').val(true);
   };
 
   RefusalWizard.prototype._resetQuestion = function($question) {