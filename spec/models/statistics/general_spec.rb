mysociety/alaveteli

commit 71c57ad8d6d863c626b135eb150e68277436b48b

Extract Statistics::General from GeneralController
Reduce complexity of controller (and its tests) by extracting logic to a
more appropriate model.

Allows us to remove a non-controller method from ApplicationController.

Also allows easier access from the command line:

    bin/rails runner "pp Statistics::General.new.to_h"

app/models/statistics/general.rb
@@ -1,0 +1,33 @@
+class Statistics
+  # High-level site statistics and version information
+  class General
+    def to_h
+      {
+        alaveteli_git_commit: alaveteli_git_commit,
+        alaveteli_version: ALAVETELI_VERSION,
+        ruby_version: RUBY_VERSION,
+        visible_public_body_count: PublicBody.visible.count,
+        visible_request_count: InfoRequest.is_searchable.count,
+        private_request_count: InfoRequest.embargoed.count,
+        confirmed_user_count: User.active.where(email_confirmed: true).count,
+        visible_comment_count: Comment.visible.count,
+        track_thing_count: TrackThing.count,
+        widget_vote_count: WidgetVote.count,
+        public_body_change_request_count: PublicBodyChangeRequest.count,
+        request_classification_count: RequestClassification.count,
+        visible_followup_message_count: OutgoingMessage.
+          where(prominence: 'normal', message_type: 'followup').count
+      }
+    end
+
+    def to_json
+      to_h.to_json
+    end
+
+    protected
+
+    def alaveteli_git_commit
+      `git log -1 --format="%H"`.strip
+    end
+  end
+end

spec/controllers/general_controller_spec.rb
@@ -1,79 +1,21 @@
 require 'spec_helper'
 
 RSpec.describe GeneralController do
-
   describe 'GET version' do
-
-    it 'renders json stats about the install' do
-      # Clean up fixtures
-      InfoRequest.find_each(&:destroy)
-      Comment.find_each(&:destroy)
-      PublicBody.find_each(&:destroy)
-      TrackThing.find_each(&:destroy)
-      User.find_each(&:destroy)
-
-      # Create some constant God models for other factories
-      user = FactoryBot.create(:user)
-      body = FactoryBot.create(:public_body)
-      banned_user = FactoryBot.create(:user, :ban_text => 'banned')
-      info_request = FactoryBot.create(:info_request,
-                                       :user => user, :public_body => body)
-      default_args = { :info_request => info_request,
-                       :public_body => body,
-                       :user => user }
-
-      # Create the other data we're checking
-      FactoryBot.create(:embargoed_request, user: user, public_body: body)
-      FactoryBot.create(:info_request, :user => user,
-                                       :public_body => body,
-                                       :prominence => 'hidden')
-      FactoryBot.create(:user, :email_confirmed => false)
-      FactoryBot.create(:visible_comment,
-                        default_args.dup.slice!(:public_body))
-      FactoryBot.create(:hidden_comment,
-                        default_args.dup.slice!(:public_body))
-      FactoryBot.create(:search_track, :tracking_user => user)
-      FactoryBot.create(:widget_vote,
-                        default_args.dup.slice!(:user, :public_body))
-      FactoryBot.create(:internal_review_request,
-                        default_args.dup.slice!(:user, :public_body))
-      FactoryBot.create(:internal_review_request,
-                        :info_request => info_request, :prominence => 'hidden')
-      FactoryBot.create(:add_body_request,
-                        default_args.dup.slice!(:info_request))
-      event = FactoryBot.create(:info_request_event,
-                                default_args.dup.slice!(:user, :public_body))
-      FactoryBot.create(:request_classification, :user => user,
-                                                 :info_request_event => event)
-
-      mock_git_commit = Digest::SHA1.hexdigest(Time.now.to_s)
-
-      allow_any_instance_of(ApplicationController).
-        to receive(:alaveteli_git_commit).
-          and_return(mock_git_commit)
-
-      expected = { alaveteli_git_commit: mock_git_commit,
-                   alaveteli_version: ALAVETELI_VERSION,
-                   ruby_version: RUBY_VERSION,
-                   visible_public_body_count: 1,
-                   visible_request_count: 1,
-                   private_request_count: 1,
-                   confirmed_user_count: 1,
-                   visible_comment_count: 1,
-                   track_thing_count: 1,
-                   widget_vote_count: 1,
-                   public_body_change_request_count: 1,
-                   request_classification_count: 1,
-                   visible_followup_message_count: 1 }
-
-      get :version, params: { :format => :json }
-
-      parsed_body = JSON.parse(response.body).symbolize_keys
-      expect(parsed_body).to eq(expected)
+    let(:mock_stats) do
+      double(to_json: { foo: 'x', bar: 'y' }.to_json)
     end
 
-  end
+    before do
+      expect(Statistics::General).to receive(:new).and_return(mock_stats)
+    end
 
+    it 'renders json stats about the install' do
+      get :version, params: { format: :json }
+      parsed_body = JSON.parse(response.body).symbolize_keys
+      expect(parsed_body).to eq({ foo: 'x', bar: 'y' })
+    end
+  end
 end
 
 RSpec.describe GeneralController, "when trying to show the blog" do

spec/models/statistics/general_spec.rb
@@ -1,0 +1,78 @@
+require 'spec_helper'
+
+RSpec.describe Statistics::General do
+  let(:statistics) { described_class.new }
+
+  before do
+    # Clean up fixtures
+    InfoRequest.find_each(&:destroy)
+    Comment.find_each(&:destroy)
+    PublicBody.find_each(&:destroy)
+    TrackThing.find_each(&:destroy)
+    User.find_each(&:destroy)
+
+    # Create some constant God models for other factories
+    user = FactoryBot.create(:user)
+    body = FactoryBot.create(:public_body)
+    banned_user = FactoryBot.create(:user, ban_text: 'banned')
+    info_request = FactoryBot.create(:info_request,
+                                     user: user,
+                                     public_body: body)
+
+    default_args = { info_request: info_request,
+                     public_body: body,
+                     user: user }
+
+    # Create the other data we're checking
+    FactoryBot.create(:embargoed_request, user: user, public_body: body)
+    FactoryBot.create(:info_request, user: user,
+                                     public_body: body,
+                                     prominence: 'hidden')
+    FactoryBot.create(:user, email_confirmed: false)
+    FactoryBot.create(:visible_comment,
+                      default_args.dup.slice!(:public_body))
+    FactoryBot.create(:hidden_comment,
+                      default_args.dup.slice!(:public_body))
+    FactoryBot.create(:search_track, tracking_user: user)
+    FactoryBot.create(:widget_vote,
+                      default_args.dup.slice!(:user, :public_body))
+    FactoryBot.create(:internal_review_request,
+                      default_args.dup.slice!(:user, :public_body))
+    FactoryBot.create(:internal_review_request,
+                      info_request: info_request, prominence: 'hidden')
+    FactoryBot.create(:add_body_request,
+                      default_args.dup.slice!(:info_request))
+    event = FactoryBot.create(:info_request_event,
+                              default_args.dup.slice!(:user, :public_body))
+    FactoryBot.create(:request_classification, user: user,
+                                               info_request_event: event)
+
+    allow(statistics).to receive(:alaveteli_git_commit).and_return('SHA')
+  end
+
+  let(:expected) do
+    { alaveteli_git_commit: 'SHA',
+      alaveteli_version: ALAVETELI_VERSION,
+      ruby_version: RUBY_VERSION,
+      visible_public_body_count: 1,
+      visible_request_count: 1,
+      private_request_count: 1,
+      confirmed_user_count: 1,
+      visible_comment_count: 1,
+      track_thing_count: 1,
+      widget_vote_count: 1,
+      public_body_change_request_count: 1,
+      request_classification_count: 1,
+      visible_followup_message_count: 1 }
+  end
+
+  describe '#to_h' do
+    subject { statistics.to_h }
+    it { is_expected.to eq(expected) }
+  end
+
+  describe '#to_json' do
+    subject { statistics.to_json }
+    it { is_expected.to eq(expected.to_json) }
+  end
+end

app/controllers/admin_general_controller.rb
@@ -156,7 +156,7 @@ class AdminGeneralController < AdminController
 
   def debug
     @admin_current_user = admin_current_user
-    @current_commit = alaveteli_git_commit
+    @current_commit = Statistics::General.new.to_h[:alaveteli_git_commit]
     @current_branch = `git branch | perl -ne 'print $1 if /^\\* (.*)/'`
     @current_version = ALAVETELI_VERSION
     repo = `git remote show origin -n | perl -ne 'print $1 if m{Fetch URL: .*github\\.com[:/](.*)\\.git}'`

app/controllers/application_controller.rb
@@ -458,10 +458,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def alaveteli_git_commit
-    `git log -1 --format="%H"`.strip
-  end
-
   # URL Encode the path parameter for use in render_exception
   #
   # params - the params Hash

app/controllers/general_controller.rb
@@ -218,24 +218,7 @@ class GeneralController < ApplicationController
 
   def version
     respond_to do |format|
-      format.json {
-        render json: {
-          alaveteli_git_commit: alaveteli_git_commit,
-          alaveteli_version: ALAVETELI_VERSION,
-          ruby_version: RUBY_VERSION,
-          visible_public_body_count: PublicBody.visible.count,
-          visible_request_count: InfoRequest.is_searchable.count,
-          private_request_count: InfoRequest.embargoed.count,
-          confirmed_user_count: User.active.where(email_confirmed: true).count,
-          visible_comment_count: Comment.visible.count,
-          track_thing_count: TrackThing.count,
-          widget_vote_count: WidgetVote.count,
-          public_body_change_request_count: PublicBodyChangeRequest.count,
-          request_classification_count: RequestClassification.count,
-          visible_followup_message_count: OutgoingMessage.
-            where(prominence: 'normal', message_type: 'followup').count
-        }
-      }
+      format.json { render json: Statistics::General.new }
     end
   end
 

commit 84207a35dc8ab039e77eb5d004b7849ab2950690

Allow args given to Statistics::General#to_json
When Rails calls `to_json` under the hood [1] it also passes some layout
options in a `Hash`. This results in an `ArgumentError` when the current
code is called:

    From: /home/vagrant/alaveteli/app/models/statistics/general.rb @ line 25 Statistics::General#to_json:

        23: def to_json(args)
        24:   binding.pry
     => 25:   to_h.to_json
        26: end

    [1] pry(#<Statistics::General>)> args
    # => {:prefixes=>["general", "application"],
     :template=>"version",
     :layout=>#<Proc:0x000055eac710a448 /home/vagrant/bundle/ruby/2.7.0/gems/actionview-5.2.6/lib/action_view/layouts.rb:392>}
    [2] pry(#<Statistics::General>)> args.class
    # => Hash

This commit fixes this by allowing any arguments to be passed to
`Statistics::General#to_json` and better mirrors the framework behaviour
in the controller test.

Bug introduced in 71c57ad8d6d863c626b135eb150e68277436b48b.

Fixes https://github.com/mysociety/alaveteli/issues/6414

[1] https://github.com/rails/rails/blob/v5.2.4/actionpack/lib/action_controller/metal/renderers.rb#L157

app/models/statistics/general.rb
@@ -20,7 +20,7 @@ module Statistics
       }
     end
 
-    def to_json
+    def to_json(*)
       to_h.to_json
     end
 

spec/controllers/general_controller_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe GeneralController do
 
     before do
       expect(Statistics::General).to receive(:new).and_return(mock_stats)
+      expect(mock_stats).to receive(:to_json).with(kind_of(Hash))
     end
 
     it 'renders json stats about the install' do

spec/models/statistics/general_spec.rb
@@ -72,7 +72,15 @@ RSpec.describe Statistics::General do
   end
 
   describe '#to_json' do
-    subject { statistics.to_json }
-    it { is_expected.to eq(expected.to_json) }
+    context 'with no arguments' do
+      subject { statistics.to_json }
+      it { is_expected.to eq(expected.to_json) }
+    end
+
+    context 'with arguments' do
+      subject { statistics.to_json(args) }
+      let(:args) { { not: 'used' } }
+      it { is_expected.to eq(expected.to_json) }
+    end
   end
 end