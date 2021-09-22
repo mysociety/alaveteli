c5e3482a71d7cf61d865636678edb4ed033f263c
Gareth Rees <gareth@garethrees.co.uk>
2021-09-22 16:51:57 +0100

    Add Statistics::Leaderboard
    Extract `Statistics::Leaderboard` from `User` class methods to remove
    complexity from `User`.
    
    `Statistics::Leaderboard` isn't perfect in implementation or use. It has
    hard dependencies and doesn't encapsulate anything, but at least we now
    have instance methods that can be more easily stubbed and refactored.
    It's use in `Statistics` isn't great, but that's due to `Statistics`
    being a module rather than an instance that encapsulates an instantiated
    `Leaderboard`.
    
    That said, the main concern here was extracting stuff from `User` that
    doesn't need to be there.
    
    Also fixes some minor style issues.

---
+++ spec/models/statistics/leaderboard_spec.rb
@@ -0,0 +1,75 @@
+require 'spec_helper'
+
+RSpec.describe Statistics::Leaderboard do
+  let(:statistics) { described_class.new }
+
+  describe '#all_time_requesters' do
+    it 'gets most frequent requesters' do
+      User.destroy_all
+
+      user1 = FactoryBot.create(:user)
+      user2 = FactoryBot.create(:user)
+      user3 = FactoryBot.create(:user)
+
+      travel_to(6.months.ago) do
+        5.times { FactoryBot.create(:info_request, user: user1) }
+        2.times { FactoryBot.create(:info_request, user: user2) }
+        FactoryBot.create(:info_request, user: user3)
+      end
+
+      expect(statistics.all_time_requesters).
+        to eq({ user1 => 5,
+                user2 => 2,
+                user3 => 1 })
+    end
+  end
+
+  describe '#last_28_day_requesters' do
+    it 'gets recent frequent requesters' do
+      user_with_3_requests = FactoryBot.create(:user)
+      3.times { FactoryBot.create(:info_request, user: user_with_3_requests) }
+      user_with_2_requests = FactoryBot.create(:user)
+      2.times { FactoryBot.create(:info_request, user: user_with_2_requests) }
+      user_with_1_request = FactoryBot.create(:user)
+      FactoryBot.create(:info_request, user: user_with_1_request)
+      user_with_an_old_request = FactoryBot.create(:user)
+      FactoryBot.create(:info_request,
+                        user: user_with_an_old_request,
+                        created_at: 2.months.ago)
+
+      expect(statistics.last_28_day_requesters).
+        to eql({ user_with_3_requests => 3,
+                 user_with_2_requests => 2,
+                 user_with_1_request => 1 })
+    end
+  end
+
+  describe '#all_time_commenters' do
+    it 'gets most frequent commenters' do
+      # FIXME: This uses fixtures. Change it to use factories when we can.
+      expect(statistics.all_time_commenters).
+        to eql({ users(:bob_smith_user) => 1,
+                 users(:silly_name_user) => 1 })
+    end
+  end
+
+  describe '#last_28_day_commenters' do
+    it 'gets recent frequent commenters' do
+      user_with_3_comments = FactoryBot.create(:user)
+      3.times { FactoryBot.create(:comment, user: user_with_3_comments) }
+      user_with_2_comments = FactoryBot.create(:user)
+      2.times { FactoryBot.create(:comment, user: user_with_2_comments) }
+      user_with_1_comment = FactoryBot.create(:user)
+      FactoryBot.create(:comment, user: user_with_1_comment)
+      user_with_an_old_comment = FactoryBot.create(:user)
+      FactoryBot.create(:comment,
+                        user: user_with_an_old_comment,
+                        created_at: 2.months.ago)
+
+      expect(statistics.last_28_day_commenters).
+        to eql({ user_with_3_comments => 3,
+                 user_with_2_comments => 2,
+                 user_with_1_comment => 1 })
+    end
+  end
+end
