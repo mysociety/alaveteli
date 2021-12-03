767bc48034ffca227a3f4f705ca2dd917b6d26cb
Gareth Rees <gareth@garethrees.co.uk>
2021-12-03 17:44:25 +0000

    Add canned CensorRule replacements
    We frequently write the same text into the `CensorRule#replacement` form
    field, so this commit adds a set of defaults that can be selected
    through a `<datalist>` element.
    
    Reusers can add to the defaults or set their own in the theme:
    
        # Add a reason
        CensorRule.canned_replacements << '[A new reason]'
    
        # Use a custom set of replacements
        CensorRule.canned_replacements = [
          '[A new reason]',
          '[Another reason]'
        ]
    
    Fixes https://github.com/mysociety/alaveteli/issues/6487.

--- app/models/censor_rule.rb
+++ app/models/censor_rule.rb
@@ -23,7 +23,16 @@
 # Email: hello@mysociety.org; WWW: http://www.mysociety.org/
 
 class CensorRule < ApplicationRecord
+  DEFAULT_CANNED_REPLACEMENTS = [
+    _('[Personally Identifiable Information removed]'),
+    _('[Name removed]'),
+    _('[Extraneous material removed]'),
+    _('[Potentially defamatory material removed]'),
+    _('[Extraneous and potentially defamatory material removed]')
+  ].freeze
+
   include AdminColumn
+
   belongs_to :info_request,
              :inverse_of => :censor_rules
   belongs_to :user,
@@ -43,6 +52,10 @@ class CensorRule < ApplicationRecord
           user_id: nil,
           public_body_id: nil)
   }
+
+  cattr_accessor :canned_replacements,
+                 instance_writer: false,
+                 default: DEFAULT_CANNED_REPLACEMENTS.dup
 
   def apply_to_text(text_to_censor)
     return nil if text_to_censor.nil?
