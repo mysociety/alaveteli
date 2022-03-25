901890d920e521450fcaff7613194c22b73db1d1
Gareth Rees <gareth@garethrees.co.uk>
2022-03-25 11:01:13 +0000

    fixup! WIP Remove external requests

--- db/migrate/20220323165941_remove_external_url_external_user_name_from_info_request.rb
+++ db/migrate/20220323165941_remove_external_url_external_user_name_from_info_request.rb
@@ -1,6 +1,6 @@
 class RemoveExternalUrlExternalUserNameFromInfoRequest < ActiveRecord::Migration[6.1]
   def change
-    remove_column :info_requests, :external_url
-    remove_column :info_requests, :external_user_name
+    remove_column :info_requests, :external_url, :string, null: true
+    remove_column :info_requests, :external_user_name, :string, null: true
   end
 end
