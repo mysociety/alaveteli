1904e8b1f1545db7d4e50d84688ff5fd45519a34
Gareth Rees <gareth@garethrees.co.uk>
2022-05-06 16:22:07 +0100

    Minor cleanup of PublicBodyCategoryLink
    * Hash style
    * Line length
    * Use `self.class` over explicit class name
    * Use conditional assignment instead of `unless`

--- app/models/public_body_category_link.rb
+++ app/models/public_body_category_link.rb
@@ -13,27 +13,33 @@
 
 class PublicBodyCategoryLink < ApplicationRecord
   belongs_to :public_body_category,
-             :inverse_of => :public_body_category_links
+             inverse_of: :public_body_category_links
+
   belongs_to :public_body_heading,
-             :inverse_of => :public_body_category_links
+             inverse_of: :public_body_category_links
 
   validates_presence_of :public_body_category
   validates_presence_of :public_body_heading
-  validates :category_display_order, :numericality => { :only_integer => true,
-                                                        :message => 'Display order must be a number' }
 
-  before_validation :on => :create do
-    unless self.category_display_order
-      self.category_display_order = PublicBodyCategoryLink.next_display_order(public_body_heading_id)
-    end
+  validates :category_display_order, numericality: {
+    only_integer: true, message: 'Display order must be a number'
+  }
+
+  before_validation on: :create do
+    self.category_display_order ||=
+      self.class.next_display_order(public_body_heading_id)
   end
 
   def self.next_display_order(heading_id)
-    if last = where(:public_body_heading_id => heading_id).order(:category_display_order).last
-      last.category_display_order + 1
+    last_record =
+      where(public_body_heading_id: heading_id).
+      order(:category_display_order).
+      last
+
+    if last_record
+      last_record.category_display_order + 1
     else
       0
     end
   end
-
 end
