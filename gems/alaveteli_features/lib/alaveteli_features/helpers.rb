22611f66d052e2dc774a240d43f3af0227dda829
Graeme Porteous <graeme@rgbp.co.uk>
2022-05-11 13:32:59 +0100

    Add #disable_actor feature flag helper method
    This will be useful when removing a revoking a feature flag from a user.

--- gems/alaveteli_features/lib/alaveteli_features/helpers.rb
+++ gems/alaveteli_features/lib/alaveteli_features/helpers.rb
@@ -9,5 +9,11 @@ module AlaveteliFeatures
       return true if feature_enabled?(feature, user)
       AlaveteliFeatures.backend.enable_actor(feature, user)
     end
+
+    def disable_actor(feature, user)
+      # check feature isn't already been disabled for the user
+      return true unless feature_enabled?(feature, user)
+      AlaveteliFeatures.backend.disable_actor(feature, user)
+    end
   end
 end
