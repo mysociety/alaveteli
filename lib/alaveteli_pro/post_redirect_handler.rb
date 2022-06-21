d8b182ad3ec4a90ad22a1aa1acf28db21193bb42
Graeme Porteous <graeme@rgbp.co.uk>
2022-06-21 10:25:03 +0100

    Minor cleanup
    - Add module documentation
    - String quotes
    - Closing brace position
    - Remove unneeded return

--- lib/alaveteli_pro/post_redirect_handler.rb
+++ lib/alaveteli_pro/post_redirect_handler.rb
@@ -1,9 +1,12 @@
 module AlaveteliPro
+  ##
+  # Handle post redirects for pro users
+  #
+  # A hook for us to override certain post redirects for pro users, e.g. if they
+  # start making a request, then we realise they're a pro when they log in, so
+  # we want to send them into the pro system
+  #
   module PostRedirectHandler
-
-    # A hook for us to override certain post redirects for pro users, e.g.
-    # if they start making a request, then we realise they're a pro when they
-    # log in, so we want to send them into the pro system
     def override_post_redirect_for_pro(uri, post_redirect, user)
       # We could have a locale in the url, or we could not, e.g. /en/new or /new
       if uri =~ /^(\/[a-z]{2})?\/new$/
@@ -12,9 +15,10 @@ module AlaveteliPro
         params = post_redirect.post_params
         draft = DraftInfoRequest.create(
           user: user,
-          title: params["info_request"]["title"],
-          body: params["outgoing_message"]["body"],
-          public_body_id: params["info_request"]["public_body_id"])
+          title: params['info_request']['title'],
+          body: params['outgoing_message']['body'],
+          public_body_id: params['info_request']['public_body_id']
+        )
         # Clear out the post_redirect, so that we don't get a lot of other
         # params put into our URL later on
         post_redirect.post_params = {}
@@ -26,8 +30,7 @@ module AlaveteliPro
                            "using the form below.")
         return "#{new_alaveteli_pro_info_request_path}?draft_id=#{draft.id}"
       end
-      return uri
+      uri
     end
-
   end
 end
