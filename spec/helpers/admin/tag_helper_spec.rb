69e217d7e0e578e841d1a4b8a03d73c99b8ca54d
Graeme Porteous <graeme@rgbp.co.uk>
2022-08-02 17:38:48 +0100

    Refactor admin tag helper
    With the introduction of an admin tag controller we will be able to link
    to an admin view. This refactors the existing helpers for rendering tags
    so that all tags rendered in the admin are link.
    
    This removes the custom `search_target` for public body tags, linking to
    the public page with handled via the upcoming admin `tags#show` view.
    
    At present all links are targeting an anchor on the current page. This
    will be updated when we add an admin `tags#show` action.

--- spec/helpers/admin/tag_helper_spec.rb
+++ spec/helpers/admin/tag_helper_spec.rb
@@ -14,48 +14,26 @@ RSpec.describe Admin::TagHelper, type: :helper do
 
     context 'tag with no value' do
       let(:record_tag) { tag_without_value }
-      let(:search_target) { nil }
 
-      it 'renders the tag' do
-        expect(helper.render_tag(record_tag, search_target: search_target)).
-          to eq(%q(<span class="label label-info tag">foo</span>))
+      it 'renders the tag with a link' do
+        expected = '<span class="label label-info tag">' \
+                   '<a href="#foo">foo</a>' \
+                   '</span>'
+        expect(helper.render_tag(record_tag)).
+          to eq(expected)
       end
     end
 
     context 'tag with a value' do
       let(:record_tag) { tag_with_value }
-      let(:search_target) { nil }
 
-      it 'renders the tag with its value' do
-        expect(helper.render_tag(record_tag, search_target: search_target)).
-          to eq(%q(<span class="label label-info tag">foo:bar</span>))
-      end
-    end
-
-    context 'tag with no value and a search_target' do
-      let(:record_tag) { tag_without_value }
-      let(:search_target) { '/admin/some-index' }
-
-      it 'renders the tag with a link to the search_target' do
+      it 'renders the tag and its value with links' do
         expected = '<span class="label label-info tag">' \
-                   '<a href="/admin/some-index?tag=foo">foo</a>' \
-                   '</span>'
-        expect(helper.render_tag(record_tag, search_target: search_target)).
-          to eq(expected)
-      end
-    end
-
-    context 'tag with a value and a search_target' do
-      let(:record_tag) { tag_with_value }
-      let(:search_target) { '/admin/some-index' }
-
-      it 'renders the tag and its value with links to the search_target' do
-        expected = '<span class="label label-info tag">' \
-                   '<a href="/admin/some-index?tag=foo">foo</a>:' \
-                   '<a href="/admin/some-index?tag=foo%3Abar">bar</a>' \
+                   '<a href="#foo">foo</a>:' \
+                   '<a href="#foo:bar">bar</a>' \
                    '</span>'
 
-        expect(helper.render_tag(record_tag, search_target: search_target)).
+        expect(helper.render_tag(record_tag)).
           to eq(expected)
       end
     end
