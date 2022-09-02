cd739d2cde027bea49aeb4a04d7db5c7e3c3f88a
Graeme Porteous <graeme@rgbp.co.uk>
2022-09-02 11:19:40 +0100

    Fix rendering of notes for Pro authority search
    After selecting an authority we should render the notes as HTML using
    the `render_notes` helper.
    
    This is matches how the notes are render when click the "Make a Freedom
    of Information request to this authority" button on the public authority
    page.
    
    Fixes #7265

--- spec/helpers/alaveteli_pro/public_bodies_helper_spec.rb
+++ spec/helpers/alaveteli_pro/public_bodies_helper_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe AlaveteliPro::PublicBodiesHelper, type: :helper do
 
   describe '#public_body_search_attributes' do
     let(:html) { double(:html) }
+    let(:notes_as_html) { double(:notes_as_html) }
     let(:expected) do
       {
         id: public_body.id,
@@ -14,7 +15,8 @@ RSpec.describe AlaveteliPro::PublicBodiesHelper, type: :helper do
         info_requests_visible_count: public_body.info_requests_visible_count,
         about: _('About {{public_body_name}}',
                  public_body_name: public_body.name),
-        html: html
+        html: html,
+        notes_as_html: notes_as_html
       }
     end
 
@@ -30,7 +32,14 @@ RSpec.describe AlaveteliPro::PublicBodiesHelper, type: :helper do
       let(:in_controller) { true }
 
       it 'returns hash with applicable search attribute' do
-        expect(helper).to receive(:render_to_string).and_return(html)
+        expect(helper).to receive(:render_to_string).with(
+          hash_including(partial: 'alaveteli_pro/public_bodies/search_result')
+        ).and_return(html)
+
+        expect(helper).to receive(:render_to_string).with(
+          hash_including(partial: 'alaveteli_pro/public_bodies/notes')
+        ).and_return(notes_as_html)
+
         expect(helper.public_body_search_attributes(public_body)).to eq expected
       end
     end
@@ -39,7 +48,14 @@ RSpec.describe AlaveteliPro::PublicBodiesHelper, type: :helper do
       let(:in_controller) { false }
 
       it 'returns hash with applicable search attribute' do
-        expect(helper).to receive(:render).and_return(html)
+        expect(helper).to receive(:render).with(
+          hash_including(partial: 'alaveteli_pro/public_bodies/search_result')
+        ).and_return(html)
+
+        expect(helper).to receive(:render).with(
+          hash_including(partial: 'alaveteli_pro/public_bodies/notes')
+        ).and_return(notes_as_html)
+
         expect(helper.public_body_search_attributes(public_body)).to eq expected
       end
     end
