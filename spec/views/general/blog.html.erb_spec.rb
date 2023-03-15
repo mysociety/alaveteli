1cd4fbe9dfda7ad1a397256a48a2807062d515fe
Graeme Porteous <graeme@rgbp.co.uk>
2023-03-15 07:21:34 +0000

    Update blog rendering
    Render blog items from the database.

--- spec/views/general/blog.html.erb_spec.rb
+++ spec/views/general/blog.html.erb_spec.rb
@@ -4,39 +4,51 @@ RSpec.describe 'general/blog' do
   subject { rendered }
 
   before do
-    assign :blog, double(posts: blog_posts)
+    assign :blog, double(posts: [double(blog_post_attributes)])
     assign :twitter_user, double.as_null_object
     assign :facebook_user, double.as_null_object
     render
   end
 
   context 'with a creator and date' do
-    let(:blog_posts) do
-      [{ 'title' => 'foo',
-         'link' => 'https://www.example.com/foo',
-         'creator' => ['Bob'],
-         'pubDate' => ['Mon, 01 Apr 2013 19:26:08 +0000'] }]
+    let(:blog_post_attributes) do
+      {
+        title: 'foo',
+        url: 'https://www.example.com/foo',
+        data: {
+          'creator' => ['Bob'],
+          'pubDate' => ['Mon, 01 Apr 2013 19:26:08 +0000']
+        }
+      }
     end
 
     it { is_expected.to match(/Posted on.*April 01, 2013.*by Bob/) }
   end
 
   context 'with an author and date' do
-    let(:blog_posts) do
-      [{ 'title' => 'foo',
-         'link' => 'https://www.example.com/foo',
-         'author' => ['Bob'],
-         'pubDate' => ['Mon, 01 Apr 2013 19:26:08 +0000'] }]
+    let(:blog_post_attributes) do
+      {
+        title: 'foo',
+        url: 'https://www.example.com/foo',
+        data: {
+          'author' => ['Bob'],
+          'pubDate' => ['Mon, 01 Apr 2013 19:26:08 +0000']
+        }
+      }
     end
 
     it { is_expected.to match(/Posted on.*April 01, 2013.*by Bob/) }
   end
 
   context 'with a date and no author or creator' do
-    let(:blog_posts) do
-      [{ 'title' => 'foo',
-         'link' => 'https://www.example.com/foo',
-         'pubDate' => ['Mon, 01 Apr 2013 19:26:08 +0000'] }]
+    let(:blog_post_attributes) do
+      {
+        title: 'foo',
+        url: 'https://www.example.com/foo',
+        data: {
+          'pubDate' => ['Mon, 01 Apr 2013 19:26:08 +0000']
+        }
+      }
     end
 
     it { is_expected.to match(/Posted on.*April 01, 2013/) }
