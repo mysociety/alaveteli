635bd8fd49236f5e2cda275de8bc817c80951435
Alexander Griffen <alexander@mysociety.org>
2023-05-05 19:22:00 +0100

    View & suspensions controller use new controllers

--- spec/controllers/admin_users_account_suspensions_controller_spec.rb
+++ spec/controllers/admin_users_account_suspensions_controller_spec.rb
@@ -32,30 +32,6 @@ RSpec.describe AdminUsersAccountSuspensionsController do
       end
     end
 
-    context 'with valid params for closing' do
-      let(:valid_params) do
-        { user_id: user.id, close_and_anonymise: true }
-      end
-
-      before { post :create, params: valid_params }
-
-      it 'finds the user to suspend' do
-        expect(assigns[:suspended_user]).to eq(user)
-      end
-
-      it 'closes the user' do
-        expect { user.reload }.to change(user, :closed?).to(true)
-      end
-
-      it 'tells the admin that the user was banned' do
-        expect(flash[:notice]).to eq('The user was suspended.')
-      end
-
-      it 'redirects to the user page' do
-        expect(response).to redirect_to(admin_user_path(user))
-      end
-    end
-
     context 'with invalid params' do
       it 'renders a 404' do
         expect {
