762ef91e3cf59e20795d4d0ecc80187acedfdc20
Graeme Porteous <graeme@rgbp.co.uk>
2023-06-19 18:17:34 +0100

    Update User#can_make_comments?
    Allow active users with `no_limit` set to always make comments and
    bypass the exceeded limit/creation rate checks.
    
    Fixes #7780

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2125,6 +2125,23 @@ RSpec.describe User do
       it { is_expected.to eq(true) }
     end
 
+    context 'when the user has no limit set' do
+      let(:user) { FactoryBot.create(:user, no_limit: true) }
+
+      # Irrespective of how many comments they've made
+      before do
+        allow(user).
+          to receive(:exceeded_limit?).with(:comments).and_return(true)
+
+        allow(Comment).
+          to receive(:exceeded_creation_rate?).
+          with(user.comments).
+          and_return(true)
+      end
+
+      it { is_expected.to eq(true) }
+    end
+
     context 'when the user has reached their daily limit' do
       let(:user) { FactoryBot.build(:user) }
 
