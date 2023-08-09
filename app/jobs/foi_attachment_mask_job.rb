7b84549e9d1a9da032add051f637141998deb06c
Graeme Porteous <graeme@rgbp.co.uk>
2023-08-09 09:44:39 +0100

    Revert part of 92123ec
    Rescuing the mismatched hexdigest exception in this way seem cause a
    cascade of additional jobs.
    
    We wont to keep the exception as this is more useful than the current
    errors, EG:
      undefined method `gsub' for nil:NilClass

--- app/jobs/foi_attachment_mask_job.rb
+++ app/jobs/foi_attachment_mask_job.rb
@@ -31,10 +31,6 @@ class FoiAttachmentMaskJob < ApplicationJob
     end
 
     attachment.update(body: body, masked_at: Time.zone.now)
-
-  rescue MailHandler::MismatchedAttachmentHexdigest
-    incoming_message.parse_raw_email!(true)
-    retry_job
   end
 
   private
