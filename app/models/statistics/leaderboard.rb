9a97886bc632059500cb6f968db187ec219ba1b8
Chris Mytton <mail@chrismytton.com>
2023-11-14 10:34:41 +0000

    Have leaderboard comment methods return user instances directly
    Changed `.group('comments.user_id')` to `.group(:user)` to match the
    InfoRequest-based methods above.

--- app/models/statistics/leaderboard.rb
+++ app/models/statistics/leaderboard.rb
@@ -24,35 +24,25 @@ module Statistics
     end
 
     def all_time_commenters
-      commenters = Comment.visible.
-                           joins(:user).
-                           merge(User.not_banned).
-                           group('comments.user_id').
-                           order(count_all: :desc).
-                           limit(10).
-                           count
-      # TODO: Have user objects automatically instantiated like the InfoRequest
-      # queries above
-      result = {}
-      commenters.each { |user_id, count| result[User.find(user_id)] = count }
-      result
+      Comment.visible.
+              joins(:user).
+              merge(User.not_banned).
+              group(:user).
+              order(count_all: :desc).
+              limit(10).
+              count
     end
 
     def last_28_day_commenters
       # TODO: Refactor as it's basically the same as all_time_commenters
-      commenters = Comment.visible.
-                           where('comments.created_at >= ?', 28.days.ago).
-                           joins(:user).
-                           merge(User.not_banned).
-                           group('comments.user_id').
-                           order(count_all: :desc).
-                           limit(10).
-                           count
-      # TODO: Have user objects automatically instantiated like the InfoRequest
-      # queries above
-      result = {}
-      commenters.each { |user_id, count| result[User.find(user_id)] = count }
-      result
+      Comment.visible.
+              where('comments.created_at >= ?', 28.days.ago).
+              joins(:user).
+              merge(User.not_banned).
+              group(:user).
+              order(count_all: :desc).
+              limit(10).
+              count
     end
   end
 end
