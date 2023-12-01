d46170bab22947f6d156c1d4eef7f796b0519473
Gareth Rees <gareth@garethrees.co.uk>
2023-12-01 10:52:27 +0000

    Add line breaks to FoiAttachment factory
    Better readability of larger blocks

--- spec/factories/foi_attchments.rb
+++ spec/factories/foi_attchments.rb
@@ -29,16 +29,19 @@ FactoryBot.define do
       body { 'hereisthetext' }
       filename { 'attachment.txt' }
     end
+
     factory :pdf_attachment do
       content_type { 'application/pdf' }
       filename { 'interesting.pdf' }
       body { load_file_fixture('interesting.pdf') }
     end
+
     factory :rtf_attachment do
       content_type { 'application/rtf' }
       filename { 'interesting.rtf' }
       body { load_file_fixture('interesting.rtf') }
     end
+
     factory :html_attachment do
       content_type { 'text/html' }
       filename { 'interesting.html' }
@@ -51,11 +54,13 @@ FactoryBot.define do
         Mail::Utilities.to_crlf(load_file_fixture('interesting.html'))
       }
     end
+
     factory :jpeg_attachment do
       content_type { 'image/jpeg' }
       filename { 'interesting.jpg' }
       body { 'someimage' }
     end
+
     factory :unknown_attachment do
       content_type { 'application/unknown' }
       filename { 'interesting.spc' }
