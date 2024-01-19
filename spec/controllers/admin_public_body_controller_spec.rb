8c606666866cb16bdcfa2fa952ecc30898260be1
Alexander Griffen <alexander@mysociety.org>
2024-01-19 10:56:44 +0000

    Add locale URL redirection
    When processing requests for URLs where the locale is a prefix, we now:
    1. Redirect to the same URL with the locale as a parameter, this is done
       via routing redirects.
    
    2. Set the locale in the session. This is done in the existing
       `set_gettext_locale` before action callback.
    
    3. Finally, we redirect to the same URL without the locale at all as
       this information is retrieved from the session or cookies.
    
    The redirect is skipped for the `PublicBodyController#show` action as
    there is already redirection in place there to redirect to localised
    version of the `PublicBody#url_name`.

--- spec/controllers/admin_public_body_controller_spec.rb
+++ spec/controllers/admin_public_body_controller_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe AdminPublicBodyController do
     end
 
     it "searches for 'humpa' in another locale" do
-      get :index, params: { query: "humpa", locale: "es" }
+      get :index, params: { query: "humpa" }, session: { locale: "es" }
       expect(assigns[:public_bodies]).
         to eq([public_bodies(:humpadink_public_body)])
     end
@@ -45,7 +45,7 @@ RSpec.describe AdminPublicBodyController do
         public_body.save!
       end
       sign_in admin_user
-      get :show, params: { id: public_body.id, locale: "es" }
+      get :show, params: { id: public_body.id }, session: { locale: 'es' }
       expect(assigns[:public_body].name).to eq 'El Public Body'
     end
 
@@ -329,7 +329,7 @@ RSpec.describe AdminPublicBodyController do
     end
 
     it "edits a public body in another locale" do
-      get :edit, params: { id: 3, locale: :en }
+      get :edit, params: { id: 3 }, session: { locale: 'en' }
 
       # When editing a body, the controller returns all available translations
       expect(assigns[:public_body].find_translation_by_locale("es").name).
