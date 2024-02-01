c150a291a012148fa0d26f7d5bf700df916b4866
Graeme Porteous <graeme@rgbp.co.uk>
2024-02-01 10:21:47 +0000

    Update to configuration defaults for Rails 5.0

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module Alaveteli
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.load_defaults = 5.0
     config.autoloader = :zeitwerk
     config.active_record.legacy_connection_handling = false
     config.active_support.use_rfc4122_namespaced_uuids = true
