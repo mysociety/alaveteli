c189791169c3969c0bf4141107e1e2873914438e
Graeme Porteous <graeme@rgbp.co.uk>
2024-05-16 10:04:42 +0100

    Add nofollow to pagination links over page 20

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -1,7 +1,25 @@
 ##
-# Helper to translate (via gettext) the strings provided by will_paginate.
+# Helper for rendering pagination
 #
 module PaginationHelper
+  def will_paginate(collection, options = {})
+    super(collection, options.merge(renderer: CustomRenderer))
+  end
+
+  ##
+  # Custom pagination link renderer which injects nofollow for pages over 20
+  #
+  class CustomRenderer < WillPaginate::ActionView::LinkRenderer
+    def rel_value(page)
+      rel = [*super]
+      rel << 'nofollow' if page > 20
+      rel.join(' ')
+    end
+  end
+
+  ##
+  # Helper to translate (via gettext) the strings provided by will_paginate.
+  #
   def will_paginate_translate(key, options = {})
     case key
     when :previous_label then _("&#8592; Previous")
