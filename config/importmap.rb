eea5c5efb41f64b13a46e4d185a3d32c37f8492a
Graeme Porteous <graeme@rgbp.co.uk>
2024-08-16 13:04:45 +0100

    Add ActionCable

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,8 +3,10 @@
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@rails/actioncable", to: "actioncable.esm.js"
 pin "sortablejs" # @1.15.2
 
 pin "modern_application"
+pin_all_from "app/javascript/channels", under: "channels"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/helpers", under: "helpers"
