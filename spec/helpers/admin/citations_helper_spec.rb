27ba74805913fcd0c37e03b475d050e7c76261ba
Graeme Porteous <graeme@rgbp.co.uk>
2024-10-07 10:43:12 +0100

    Add citation_title helper
    Use `title` over the `source_url` but fallback if not present.

--- spec/helpers/admin/citations_helper_spec.rb
+++ spec/helpers/admin/citations_helper_spec.rb
@@ -3,6 +3,23 @@ require 'spec_helper'
 RSpec.describe Admin::CitationsHelper do
   include Admin::CitationsHelper
 
+  describe '#citation_title' do
+    subject { citation_title(citation) }
+
+    context 'with a citation that has a title' do
+      let(:citation) { FactoryBot.build(:citation, title: 'Example Title') }
+      it { is_expected.to eq('Example Title') }
+    end
+
+    context 'with a citation that has a blank title' do
+      let(:citation) do
+        FactoryBot.build(:citation, title: '', source_url: 'http://example.com')
+      end
+
+      it { is_expected.to eq('http://example.com') }
+    end
+  end
+
   describe '#citation_icon' do
     subject { citation_icon(citation) }
 
