1e4379f441bf765a0e419366968be3a3626a21be
Graeme Porteous <graeme@rgbp.co.uk>
2024-10-11 17:02:38 +0100

    Update billing helper methods
    Improve billing frequency helper method:
    1. accept Stripe Plan as and argument
    2. take into account plans interval count
    3. work with all interval types
    
    Add billing interval helper method to output `per user, per month` or
    `per user, every 2 months` depending on how the Stripe Plan has been
    configured.

--- app/helpers/alaveteli_pro/plan_helper.rb
+++ app/helpers/alaveteli_pro/plan_helper.rb
@@ -3,16 +3,43 @@
 # in the Alaveteli Pro interface
 #
 module AlaveteliPro::PlanHelper
-  def billing_frequency(billing_unit)
-    case billing_unit
-    when 'day'
+  def billing_frequency(plan)
+    if interval(plan) == 'day' && interval_count(plan) == 1
       _('Billed: Daily')
-    when 'week'
+    elsif interval(plan) == 'week' && interval_count(plan) == 1
       _('Billed: Weekly')
-    when 'month'
+    elsif interval(plan) == 'month' && interval_count(plan) == 1
       _('Billed: Monthly')
-    when 'year'
+    elsif interval(plan) == 'year' && interval_count(plan) == 1
       _('Billed: Annually')
+    else
+      _('Billed: every {{interval}}', interval: interval(plan))
     end
+  end
+
+  def billing_interval(plan)
+    if interval_count(plan) == 1
+      _('per user, per {{interval}}', interval: pluralize_interval(plan))
+    else
+      _('per user, every {{interval}}', interval: pluralize_interval(plan))
+    end
+  end
+
+  private
+
+  def pluralize_interval(plan)
+    count = interval_count(plan)
+    interval = interval(plan)
+    return interval if count == 1
+
+    pluralize(count, interval)
+  end
+
+  def interval(plan)
+    plan.interval
+  end
+
+  def interval_count(plan)
+    plan.interval_count
   end
 end
