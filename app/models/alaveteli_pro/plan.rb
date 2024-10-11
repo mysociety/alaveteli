56423c24bd432e2b2d14538e1e24704ad62a7c25
Graeme Porteous <graeme@rgbp.co.uk>
2024-10-11 17:02:38 +0100

    Add Pro Plan list and retrieve methods
    Allowing us to simplify the controller actions so we're always dealing
    with Plan objects.

--- app/models/alaveteli_pro/plan.rb
+++ app/models/alaveteli_pro/plan.rb
@@ -2,7 +2,31 @@
 # A wrapper for a Stripe::Plan
 #
 class AlaveteliPro::Plan < SimpleDelegator
+  extend AlaveteliPro::StripeNamespace
+  include AlaveteliPro::StripeNamespace
   include Taxable
 
   tax :amount
+
+  def self.list
+    [retrieve('pro')]
+  end
+
+  def self.retrieve(id)
+    new(Stripe::Plan.retrieve(add_stripe_namespace(id)))
+  rescue Stripe::InvalidRequestError
+    nil
+  end
+
+  def to_param
+    remove_stripe_namespace(id)
+  end
+
+  # product
+  def product
+    @product ||= (
+      product_id = __getobj__.product
+      Stripe::Product.retrieve(product_id) if product_id
+    )
+  end
 end
