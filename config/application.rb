14578243e76d7ab390a4001f80bcbfe9032f5f50
Graeme Porteous <graeme@rgbp.co.uk>
2024-12-17 06:17:40 +0000

    Add comment to new framework config overrides
    So there is better visibility why these have been set without going into
    the git commit history.

--- config/application.rb
+++ config/application.rb
@@ -33,9 +33,12 @@ module Alaveteli
     # config.eager_load_paths << Rails.root.join("extras")
     config.load_defaults 6.0
     config.autoloader = :zeitwerk
-    config.active_record.legacy_connection_handling = false
-    config.active_support.use_rfc4122_namespaced_uuids = true
-    config.active_storage.replace_on_assign_to_many = true
+
+    # Enable new framework defaults configurations for later Rails versions
+    # preventing deprecation warnings
+    config.active_record.legacy_connection_handling = false # 7.0
+    config.active_support.use_rfc4122_namespaced_uuids = true # 7.0
+    config.active_storage.replace_on_assign_to_many = true # 7.1
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
