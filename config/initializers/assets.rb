8b49ca12a4d6eb8f8324c3f50140b31c6c7a622f
Graeme Porteous <graeme@rgbp.co.uk>
2025-01-10 15:08:40 +0000

    Update application to Rails 8.0
    Changes from running `rails app:update`.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,8 +6,4 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w[ admin.js admin.css ]
 Rails.application.config.assets.precompile += %w(manifest.js)
