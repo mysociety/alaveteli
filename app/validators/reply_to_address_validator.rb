2e81691016efb2b857bf42f1878ef83ce4c4ca29
Graeme Porteous <graeme@rgbp.co.uk>
2025-12-01 16:22:25 +0000

    Extract RawEmail#valid_to_reply_to? validation
    As the TODO suggested move to ReplyToAddressValidator#valid? this will
    make future refactoring easier.

--- app/validators/reply_to_address_validator.rb
+++ app/validators/reply_to_address_validator.rb
@@ -20,4 +20,33 @@ class ReplyToAddressValidator
   def self.invalid_reply_addresses=(addresses)
     @invalid_reply_addresses = addresses.map(&:downcase)
   end
+
+  # Return false if for some reason this is a message that we shouldn't let them
+  # reply to
+  def self.valid?(from_email)
+    email = from_email.try(:downcase)
+
+    # check validity of email
+    return false if email.nil? || !MySociety::Validate.is_valid_email(email)
+
+    # Check whether the email is a known invalid reply address
+    if ReplyToAddressValidator.invalid_reply_addresses.include?(email)
+      return false
+    end
+
+    prefix = email
+    prefix =~ /^(.*)@/
+    prefix = $1
+
+    return false unless prefix
+
+    no_reply_regexp = ReplyToAddressValidator.no_reply_regexp
+
+    # reject postmaster - authorities seem to nearly always not respond to
+    # email to postmaster, and it tends to only happen after delivery failure.
+    # likewise Mailer-Daemon, Auto_Reply...
+    return false if prefix.match(no_reply_regexp)
+
+    true
+  end
 end
