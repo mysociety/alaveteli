a6948437c37a2e1912089b41a621a09cc9f3c922
Gareth Rees <gareth@garethrees.co.uk>
2026-01-13 16:05:16 +0000

    Extract IncomingMessage::MainBody
    Organise the majority of main body related methods in a concern to help
    make `IncomingMessage` more readable.

---
+++ app/models/incoming_message/main_body.rb
@@ -0,0 +1,181 @@
+# Various methods for handling the main body part of `IncomingMessage` records.
+module IncomingMessage::MainBody
+  extend ActiveSupport::Concern
+
+  # Internal function to cache two sorts of main body text.
+  # Cached as loading raw_email can be quite huge, and need this for just
+  # search results
+  def _cache_main_body_text
+    text = get_main_body_text_internal
+    # Strip the uudecode parts from main text
+    # - this also effectively does a .dup as well, so text mods don't alter original
+    text = text.split(/^begin.+^`\n^end\n/m).join(" ")
+
+    if text.size > 1_000_000 # 1 MB ish
+      raise "main body text more than 1 MB, need to implement clipping like for attachment text, or there is some other MIME decoding problem or similar"
+    end
+
+    # apply masks for this message
+    text = apply_masks(text, 'text/html') unless get_main_body_text_part&.locked?
+
+    # Remove existing quoted sections
+    folded_quoted_text = remove_lotus_quoting(text, 'FOLDED_QUOTED_SECTION')
+    folded_quoted_text = IncomingMessage.remove_quoted_sections(folded_quoted_text, "FOLDED_QUOTED_SECTION")
+
+    self.cached_main_body_text_unfolded = text.delete("\0")
+    self.cached_main_body_text_folded = folded_quoted_text.delete("\0")
+    save!
+  end
+
+  # Returns body text from main text part of email, converted to UTF-8, with uudecode removed,
+  # emails and privacy sensitive things remove, censored, and folded to remove excess quoted text
+  # (marked with FOLDED_QUOTED_SECTION)
+  # TODO: returns a .dup of the text, so calling functions can in place modify it
+  def get_main_body_text_folded
+    _cache_main_body_text if cached_main_body_text_folded.nil?
+    cached_main_body_text_folded
+  end
+
+  def get_main_body_text_unfolded
+    _cache_main_body_text if cached_main_body_text_unfolded.nil?
+    cached_main_body_text_unfolded
+  end
+
+  # Returns body text from main text part of email, converted to UTF-8
+  def get_main_body_text_internal
+    parse_raw_email
+    main_part = get_main_body_text_part
+    _convert_part_body_to_text(main_part)
+
+  rescue FoiAttachment::MissingAttachment
+    # occasionally the main body part gets rebuilt while being masked, we should
+    # be able to just retry to get the new main body part instance from the db.
+    retry
+  end
+
+  # Given a main text part, converts it to text
+  def _convert_part_body_to_text(part)
+    if part.nil?
+      text = "[ Email has no body, please see attachments ]"
+    else
+      # whatever kind of attachment it is, get the UTF-8 encoded text
+      text = part.body_as_text.string
+      if part.content_type == 'text/html'
+        # e.g. http://www.whatdotheyknow.com/request/35/response/177
+        # TODO: This is a bit of a hack as it is calling a
+        # convert to text routine.  Could instead call a
+        # sanitize HTML one.
+        text = MailHandler.get_attachment_text_one_file(part.content_type, text, "UTF-8")
+      end
+    end
+
+    # Add an annotation if the text had to be scrubbed
+    if part && part.body_as_text.scrubbed?
+      text += _("\n\n[ {{site_name}} note: The above text was badly encoded, and has had strange characters removed. ]",
+                site_name: site_name)
+    end
+    # Fix DOS style linefeeds to Unix style ones (or other later regexps won't work)
+    text = text.gsub(/\r\n/, "\n")
+
+    # Compress extra spaces down to save space, and to stop regular expressions
+    # breaking in strange extreme cases. e.g. for
+    # http://www.whatdotheyknow.com/request/spending_on_consultants
+    text.gsub(/ +/, " ")
+  end
+
+  # Returns part which contains main body text, or nil if there isn't one,
+  # from a set of foi_attachments. If the leaves parameter is empty or not
+  # supplied, uses its own foi_attachments.
+  def get_main_body_text_part(leaves=[])
+    leaves = foi_attachments if leaves.empty?
+
+    # Find first part which is text/plain or text/html
+    # (We have to include HTML, as increasingly there are mail clients that
+    # include no text alternative for the main part, and we don't want to
+    # instead use the first text attachment
+    # e.g. http://www.whatdotheyknow.com/request/list_of_public_authorties)
+    leaves.each do |p|
+      if (p.content_type == 'text/plain') || (p.content_type == 'text/html')
+        return p
+      end
+    end
+
+    # Otherwise first part which is any sort of text
+    leaves.each do |p|
+      return p if p.content_type.match(/^text/)
+    end
+
+    # ... or if none, consider first part
+    p = leaves[0]
+    # if it is a known type then don't use it, return no body (nil)
+    if !p.nil? && AlaveteliFileTypes.mimetype_to_extension(p.content_type)
+      # this is guess of case where there are only attachments, no body text
+      # e.g. http://www.whatdotheyknow.com/request/cost_benefit_analysis_for_real_n
+      return nil
+    end
+
+    # otherwise return it assuming it is text (sometimes you get things
+    # like binary/octet-stream, or the like, which are really text - TODO: if
+    # you find an example, put URL here - perhaps we should be always returning
+    # nil in this case)
+    p
+  end
+
+  # Returns body text as HTML with quotes flattened, and emails removed.
+  def get_body_for_html_display(collapse_quoted_sections = true)
+    # Find the body text and remove emails for privacy/anti-spam reasons
+    text = get_main_body_text_unfolded
+    folded_quoted_text = get_main_body_text_folded
+
+    # Remove quoted sections, adding HTML. TODO: The FOLDED_QUOTED_SECTION is
+    # a nasty hack so we can escape other HTML before adding the unfold
+    # links, without escaping them. Rather than using some proper parser
+    # making a tree structure (I don't know of one that is to hand, that
+    # works well in this kind of situation, such as with regexps).
+    text = folded_quoted_text if collapse_quoted_sections
+    text = MySociety::Format.simplify_angle_bracketed_urls(text)
+    text = CGI.escapeHTML(text)
+    text = MySociety::Format.make_clickable(text, contract: 1)
+
+    # add a helpful link to email addresses and mobile numbers removed
+    # by apply_masks
+    email_pattern = Regexp.escape(_("email address"))
+    mobile_pattern = Regexp.escape(_("mobile number"))
+    text.gsub!(/\[(#{email_pattern}|#{mobile_pattern})\]/,
+               '[<a href="/help/officers#mobiles">\1</a>]')
+
+    if collapse_quoted_sections
+      text = text.gsub(/(\s*FOLDED_QUOTED_SECTION\s*)+/m, "FOLDED_QUOTED_SECTION")
+      text.strip!
+      # if there is nothing but quoted stuff, then show the subject
+      if text == "FOLDED_QUOTED_SECTION"
+        text = "[Subject only] " + CGI.escapeHTML(subject || '') + text
+      end
+      # and display link for quoted stuff
+      text = text.gsub(/FOLDED_QUOTED_SECTION/, "\n\n" + '<span class="unfold_link"><a href="?unfold=1#incoming-'+id.to_s+'">'+_("show quoted sections")+'</a></span>' + "\n\n")
+    elsif folded_quoted_text.include?('FOLDED_QUOTED_SECTION')
+      text = text + "\n\n" + '<span class="unfold_link"><a href="?#incoming-'+id.to_s+'">'+_("hide quoted sections")+'</a></span>'
+    end
+    text.strip!
+
+    text = ActionController::Base.helpers.simple_format(text)
+    text.html_safe
+  end
+
+  def get_body_for_indexing # rubocop:disable Naming/AccessorMethodName
+    return '' if Ability.guest.cannot?(:read, get_main_body_text_part)
+
+    get_body_for_quoting
+  end
+
+  # Returns text of email for using in quoted section when replying
+  def get_body_for_quoting
+    # Get the body text with emails and quoted sections removed
+    text = get_main_body_text_folded.dup
+    text.gsub!("FOLDED_QUOTED_SECTION", " ")
+    text.strip!
+    raise "internal error" if text.nil?
+
+    text
+  end
+end
