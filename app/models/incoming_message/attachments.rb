7c89407200916f3cdf044978113c5d91c5701470
Graeme Porteous <graeme@rgbp.co.uk>
2026-01-19 10:38:22 +0000

    fixup! Don't allow parsing of erased raw emails

--- app/models/incoming_message/attachments.rb
+++ app/models/incoming_message/attachments.rb
@@ -175,7 +175,7 @@ module IncomingMessage::Attachments
 
   # rubocop:disable Lint::UnderscorePrefixedVariableName
   def extract_attachments
-    raise RawEmailErasedError if raw_email_erased?
+    raise IncomingMessage::RawEmailErasedError if raw_email_erased?
 
     _mail = raw_email.mail!
     attachment_attributes = MailHandler.get_attachment_attributes(_mail)
