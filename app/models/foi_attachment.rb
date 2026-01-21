96866ddcebc0a12733001e90fd6b58a63b495a8a
Gareth Rees <gareth@garethrees.co.uk>
2026-01-21 17:31:20 +0000

    Add FoiAttachment#update_and_log_event!
    Add a bang version of `FoiAttachment#update_and_log_event` that raises
    on failure. This will allow us to simplify some controller actions that
    don't deal with unstructured user input.

--- app/models/foi_attachment.rb
+++ app/models/foi_attachment.rb
@@ -295,6 +295,33 @@ class FoiAttachment < ApplicationRecord
     )
   end
 
+  def update_and_log_event!(event: {}, **params)
+    transaction do
+      update!(params)
+
+      replaced = locked? && (
+                 replacement_body_previously_changed? ||
+                 replacement_file_previously_changed?)
+
+      log_event(
+        'edit_attachment',
+        event.merge(
+          attachment_id: id,
+          old_locked: locked_previously_was,
+          locked: locked,
+          replaced: replaced,
+          replaced_at: replaced ? replaced_at : nil,
+          replaced_filename: replaced ? filename : nil,
+          replaced_reason: replaced ? replaced_reason : nil,
+          old_prominence: prominence_previously_was,
+          prominence: prominence,
+          old_prominence_reason: prominence_reason_previously_was,
+          prominence_reason: prominence_reason
+        )
+      )
+    end
+  end
+
   def storage_key
     file.blob.key if file&.attached?
   end
