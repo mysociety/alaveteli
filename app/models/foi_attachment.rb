45a924c0e93b2de0899db35ec01f967af299b3fb
Gareth Rees <gareth@garethrees.co.uk>
2026-01-21 17:35:07 +0000

    Extract FoiAttachment::Eventable
    Organises event-related functionality into an isolated concern.

--- app/models/foi_attachment.rb
+++ app/models/foi_attachment.rb
@@ -36,6 +36,7 @@ class FoiAttachment < ApplicationRecord
 
   include MessageProminence
 
+  include Eventable
   include Lockable
   include Maskable
   include Replaceable
@@ -57,7 +58,7 @@ class FoiAttachment < ApplicationRecord
 
   scope :binary, -> { where.not(content_type: AlaveteliTextMasker::TextMask) }
 
-  delegate :expire, :log_event, to: :info_request
+  delegate :expire, to: :info_request
   delegate :metadata, to: :file_blob, allow_nil: true
 
   admin_columns exclude: %i[url_part_number within_rfc822_subject hexdigest],
@@ -268,58 +269,6 @@ class FoiAttachment < ApplicationRecord
       url_part_number,
       display_filename
     )
-  end
-
-  def update_and_log_event(event: {}, **params)
-    return false unless update(params)
-
-    replaced = locked? && (
-               replacement_body_previously_changed? ||
-               replacement_file_previously_changed?)
-
-    log_event(
-      'edit_attachment',
-      event.merge(
-        attachment_id: id,
-        old_locked: locked_previously_was,
-        locked: locked,
-        replaced: replaced,
-        replaced_at: replaced ? replaced_at : nil,
-        replaced_filename: replaced ? filename : nil,
-        replaced_reason: replaced ? replaced_reason : nil,
-        old_prominence: prominence_previously_was,
-        prominence: prominence,
-        old_prominence_reason: prominence_reason_previously_was,
-        prominence_reason: prominence_reason
-      )
-    )
-  end
-
-  def update_and_log_event!(event: {}, **params)
-    transaction do
-      update!(params)
-
-      replaced = locked? && (
-                 replacement_body_previously_changed? ||
-                 replacement_file_previously_changed?)
-
-      log_event(
-        'edit_attachment',
-        event.merge(
-          attachment_id: id,
-          old_locked: locked_previously_was,
-          locked: locked,
-          replaced: replaced,
-          replaced_at: replaced ? replaced_at : nil,
-          replaced_filename: replaced ? filename : nil,
-          replaced_reason: replaced ? replaced_reason : nil,
-          old_prominence: prominence_previously_was,
-          prominence: prominence,
-          old_prominence_reason: prominence_reason_previously_was,
-          prominence_reason: prominence_reason
-        )
-      )
-    end
   end
 
   def storage_key
