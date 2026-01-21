ee5da769e28e1ec6e3fe2b36e011c551a5679bdf
Gareth Rees <gareth@garethrees.co.uk>
2026-01-21 14:52:18 +0000

    Extract Admin::FoiAttachments::ReplacementsController
    Handling multiple behaviours (prominence, locking, replacing) in a
    single controller action (`Admin::FoiAttachments#update`) is becoming
    too unwieldy.
    
    This extracts a replacement-specific action.
    
    Currently this uses `FoiAttachment#update`, but we'll later separate it
    out into three actions with associated model methods:
    
    * `#create` -> `FoiAttachment#replace`
    * `#destroy` -> `FoiAttachment#clear_replacement`
    * `#update` -> `FoiAttachment#update_replacement` (possibly – it might
      be better to just clear and then re-replace)

---
+++ app/controllers/admin/foi_attachments/replacements_controller.rb
@@ -0,0 +1,63 @@
+# Allow admins to upload replacement data for an FoiAttachment.
+class Admin::FoiAttachments::ReplacementsController < AdminController
+  before_action :set_foi_attachment, :set_incoming_message, :set_info_request
+  before_action :check_info_request
+
+  def create
+    if @foi_attachment.update_and_log_event(
+        **foi_attachment_params,
+        event: { editor: admin_current_user }
+      )
+      @foi_attachment.expire
+
+      if @foi_attachment.locked? && !@foi_attachment.masked?
+        flash[:notice] = <<~TXT.squish
+          Attachment successfully updated and locked. Please wait for masking to
+          complete before adding additional censor rules.
+        TXT
+      else
+        flash[:notice] = 'Attachment successfully updated.'
+      end
+      redirect_to edit_admin_foi_attachment_path(@foi_attachment)
+
+    else
+      flash.now[:error] = @foi_attachment.errors.full_messages.to_sentence
+      render 'admin/foi_attachments/edit'
+    end
+  end
+
+  private
+
+  def replacement_params
+    { replacement_body: foi_attachment_params[:replacement_body],
+      replacement_file: foi_attachment_params[:replacement_file],
+      replaced_filename: foi_attachment_params[:replaced_filename],
+      replaced_reason: foi_attachment_params[:replaced_reason] }
+  end
+
+  def foi_attachment_params
+    params.require(:foi_attachment).permit(
+      :replacement_body, :replacement_file,
+      :replaced_filename, :replaced_reason
+    )
+  end
+
+  def set_foi_attachment
+    @foi_attachment = FoiAttachment.find(params[:foi_attachment_id])
+  end
+
+  def set_incoming_message
+    @incoming_message = @foi_attachment&.incoming_message
+  end
+
+  def set_info_request
+    @info_request = @incoming_message&.info_request
+  end
+
+  def check_info_request
+    return if can? :admin, @info_request
+
+    raise ActiveRecord::RecordNotFound
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -745,6 +745,8 @@ Rails.application.routes.draw do
                                 only: [:edit, :update] do
       resource :prominence, controller: 'foi_attachments/prominence',
                             only: [:update]
+      resource :replacements, controller: 'foi_attachments/replacements',
+                              only: [:create]
     end
   end
   ####
