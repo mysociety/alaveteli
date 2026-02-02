eb59dfe6038559fef21b9832c9aa2b367735fd1f
Graeme Porteous <graeme@rgbp.co.uk>
2026-02-02 12:25:16 +0000

    Add prod mailer retriever method options
    Include options for ActionMailbox supported services in the retriever
    method and set the ingress setting.

--- config/initializers/action_mailbox.rb
+++ config/initializers/action_mailbox.rb
@@ -1,3 +1,13 @@
 Rails.application.configure do
+  retriever = AlaveteliConfiguration.production_mailer_retriever_method.to_sym
+  case retriever
+  when :passive, :postfix, :exim, :qmail
+    config.action_mailbox.ingress = :relay
+  when :mailgun, :mandrill, :postmark, :sendgrid
+    config.action_mailbox.ingress = retriever
+  else
+    config.action_mailbox.ingress = nil
+  end
+
   config.action_mailbox.storage_service = :inbound_emails
 end
