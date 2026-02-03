2b419e0129b9dd73b74d143cb01742b3e364b293
Lucas <hello@lucascm.dev>
2026-02-03 09:09:26 +0000

    Added web-share.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "sortablejs" # @1.15.2
 
 pin "public", to: "public/index.js"
+pin "web_share", to: "public/web_share.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/public/controllers", under: "controllers", to: "public/controllers"
 pin_all_from "app/javascript/public/helpers", under: "helpers", to: "public/helpers"
