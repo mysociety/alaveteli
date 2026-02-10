084634eab4933d1be4ac5762529fda85e95979d9
Graeme Porteous <graeme@rgbp.co.uk>
2026-02-10 17:43:02 +0000

    Update RSpec email helpers
    Use consistent method argument names.

--- spec/support/email_helpers.rb
+++ spec/support/email_helpers.rb
@@ -1,7 +1,7 @@
-def receive_incoming_mail(email_name_or_string, **kargs)
-  kargs[:email_from] ||= 'geraldinequango@localhost'
-  content = load_file_fixture(email_name_or_string) || email_name_or_string
-  content = gsub_addresses(content.dup, **kargs)
+def receive_incoming_mail(filename_or_string, **kwargs)
+  kwargs[:email_from] ||= 'geraldinequango@localhost'
+  content = load_file_fixture(filename_or_string) || filename_or_string
+  content = gsub_addresses(content.dup, **kwargs)
   content = ::Mail::Utilities.binary_unsafe_to_crlf(content)
 
   ActionMailbox::InboundEmail.create_and_extract_message_id!(
@@ -9,8 +9,8 @@ def receive_incoming_mail(email_name_or_string, **kargs)
   )&.route
 end
 
-def get_fixture_mail(filename, email_to = nil, email_from = nil)
-  content = load_file_fixture(filename)
+def get_fixture_mail(filename_or_string, email_to = nil, email_from = nil)
+  content = load_file_fixture(filename_or_string) || filename_or_string
   content = gsub_addresses(content, email_from: email_from, email_to: email_to)
   Mail.from_source(content)
 end
@@ -33,13 +33,13 @@ def load_mail_server_logs(log)
   end
 end
 
-def gsub_addresses(content, **kargs)
-  content.gsub!('EMAIL_TO', kargs[:email_to]) if kargs[:email_to]
-  content.gsub!('EMAIL_FROM', kargs[:email_from]) if kargs[:email_from]
-  content.gsub!('EMAIL_CC', kargs[:email_cc]) if kargs[:email_cc]
-  content.gsub!('EMAIL_BCC', kargs[:email_bcc]) if kargs[:email_bcc]
-  if kargs[:email_envelope_to]
-    content.gsub!('EMAIL_ENVELOPE_TO', kargs[:email_envelope_to])
+def gsub_addresses(content, **kwargs)
+  content.gsub!('EMAIL_TO', kwargs[:email_to]) if kwargs[:email_to]
+  content.gsub!('EMAIL_FROM', kwargs[:email_from]) if kwargs[:email_from]
+  content.gsub!('EMAIL_CC', kwargs[:email_cc]) if kwargs[:email_cc]
+  content.gsub!('EMAIL_BCC', kwargs[:email_bcc]) if kwargs[:email_bcc]
+  if kwargs[:email_envelope_to]
+    content.gsub!('EMAIL_ENVELOPE_TO', kwargs[:email_envelope_to])
   end
   content
 end
