6b49e82cd7e13fb9ab75e523d69c14e0a0ef3948
Gareth Rees <gareth@garethrees.co.uk>
2026-02-25 16:42:28 +0000

    Extract User::Erasable
    Encapsulate erasure-related code

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,6 +50,7 @@ class User < ApplicationRecord
   include AlaveteliPro::PhaseCounts
 
   include User::Authentication
+  include User::Erasable
   include User::InternalAdmin
   include User::LimitedProfile
   include User::LoginToken
@@ -434,37 +435,6 @@ class User < ApplicationRecord
 
   def closed?
     closed_at.present?
-  end
-
-  def erase
-    erase!
-  rescue ActiveRecord::RecordInvalid
-    false
-  end
-
-  def erase!
-    raise ActiveRecord::RecordInvalid unless closed?
-
-    sha = Digest::SHA1.hexdigest(rand.to_s)
-
-    transaction do
-      slugs.destroy_all
-      sign_ins.destroy_all
-      email_histories.destroy_all
-      profile_photo&.destroy!
-
-      outgoing_messages.update!(
-        from_name: _('[Name Removed]')
-      )
-
-      update!(
-        name: _('[Name Removed]'),
-        email: "#{sha}@invalid",
-        url_name: sha,
-        about_me: '',
-        password: MySociety::Util.generate_token
-      )
-    end
   end
 
   def anonymise!
