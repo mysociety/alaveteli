757f881aaa5c731fd9c139d24fc8e9962db52ee7
Gareth Rees <gareth@garethrees.co.uk>
2026-02-25 16:48:17 +0000

    Extract methods from User::Erasable#erase!
    We'll be adding more to this so extract existing constituent parts into
    named methods.

--- app/models/user/erasable.rb
+++ app/models/user/erasable.rb
@@ -10,25 +10,35 @@ module User::Erasable
   def erase!
     raise ActiveRecord::RecordInvalid unless closed?
 
-    sha = Digest::SHA1.hexdigest(rand.to_s)
-
     transaction do
-      slugs.destroy_all
-      sign_ins.destroy_all
-      email_histories.destroy_all
-      profile_photo&.destroy!
+      destroy_identifying_associations
 
       outgoing_messages.update!(
         from_name: _('[Name Removed]')
       )
 
-      update!(
-        name: _('[Name Removed]'),
-        email: "#{sha}@invalid",
-        url_name: sha,
-        about_me: '',
-        password: MySociety::Util.generate_token
-      )
+      erase_account
     end
+  end
+
+  private
+
+  def destroy_identifying_associations
+    slugs.destroy_all
+    sign_ins.destroy_all
+    email_histories.destroy_all
+    profile_photo&.destroy!
+  end
+
+  def erase_account
+    sha = Digest::SHA1.hexdigest(rand.to_s)
+
+    update!(
+      name: _('[Name Removed]'),
+      email: "#{sha}@invalid",
+      url_name: sha,
+      about_me: '',
+      password: MySociety::Util.generate_token
+    )
   end
 end
