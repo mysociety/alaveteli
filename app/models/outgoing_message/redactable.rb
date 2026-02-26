b5f2ef45bc77ef401c240c2c22b20ee95eced82b
Gareth Rees <gareth@garethrees.co.uk>
2026-02-26 17:46:12 +0000

    Add OutgoingMessage::Redactable#make_redactions_permanent

--- app/models/outgoing_message/redactable.rb
+++ app/models/outgoing_message/redactable.rb
@@ -5,6 +5,23 @@ module OutgoingMessage::Redactable
     from_name_redacted? || body_redacted?
   end
 
+  def make_redactions_permanent(editor:, reason: 'OutgoingMessage#make_redactions_permanent')
+    return unless redacted?
+
+    self.from_name = safe_from_name if from_name_redacted?
+    self.body = body if body_redacted?
+    save!
+
+    log_event(
+      'edit_outgoing',
+      editor: editor,
+      reason: reason,
+      outgoing_message_id: id,
+      body_changed: body_previously_changed?,
+      from_name_changed: from_name_previously_changed?
+    )
+  end
+
   private
 
   def from_name_redacted?
