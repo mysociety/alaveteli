847685ce9b7f830be407ed8289f4d7ee1518bc70
Gareth Rees <gareth@garethrees.co.uk>
2026-02-27 11:52:10 +0000

    Add IncomingMessage::Redactable#make_redactions_permanent
    Adds basic ability to persist redactions. There are two things we need
    to ensure:
    
    1. That we don't retain PII in cached attributes
    2. That we don't retain PII in attachments
    
    Since completing 2 requires raw email erasure, we must first re-cache
    any data from non-attachment content (i.e. mail headers like From); then
    we can lock the attachments and erase the raw email to preserve them in
    the redacted state. This will then automatically repopulate the cached
    attributes that are generated from multipart attachment content with the
    redactions applied.

--- app/models/incoming_message/redactable.rb
+++ app/models/incoming_message/redactable.rb
@@ -7,6 +7,30 @@ module IncomingMessage::Redactable
       attachments_redacted?
   end
 
+  def make_redactions_permanent(editor:, reason: 'IncomingMessage#make_redactions_permanent')
+    return unless redacted?
+
+    raise RawEmail::UnmaskedAttachmentsError unless all_attachments_masked?
+
+    # Commit cached attribute redactions
+    # TODO: In future we should ensure all cached attributes can be redacted.
+    # Currently some aren't (e.g. from_email)
+    update!(from_name: safe_from_name)
+
+    log_event(
+      'edit_incoming',
+      editor: editor,
+      reason: reason,
+      incoming_message_id: id,
+      from_name_changed: from_name_previously_changed?
+    )
+
+    # Commit attachment redactions
+    clear_in_database_caches! # These get regenerated from the locked attachment
+    lock_all_attachments(editor: editor, reason: reason)
+    raw_email.erase(editor: editor, reason: reason) unless raw_email.erased?
+  end
+
   private
 
   def from_name_redacted?
