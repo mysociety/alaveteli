a91baa89c639995f18e4c8e4200e0f4b1b194eee
Gareth Rees <gareth@garethrees.co.uk>
2026-03-04 16:59:09 +0000

    Add InfoRequest::Redactable#make_redactions_permanent
    Just delegates to the redactable content.

--- app/models/info_request.rb
+++ app/models/info_request.rb
@@ -58,6 +58,7 @@ class InfoRequest < ApplicationRecord
   include InfoRequest::HoldingPen
   include InfoRequest::MagicEmail
   include InfoRequest::PublicToken
+  include InfoRequest::Redactable
   include InfoRequest::Sluggable
   include InfoRequest::TitleValidation
 
