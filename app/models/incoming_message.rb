f2e651bf2790d61d479395c5a0e38b707329f0d2
Graeme Porteous <graeme@rgbp.co.uk>
2026-03-10 13:15:20 +0000

    Remove IncomingMessage::RawEmailErasedError
    Use RawEmail::ErasedError instead.

--- app/models/incoming_message.rb
+++ app/models/incoming_message.rb
@@ -36,8 +36,6 @@ require 'rexml/document'
 require 'zip'
 
 class IncomingMessage < ApplicationRecord
-  class RawEmailErasedError < StandardError; end
-
   include MessageProminence
   include Taggable
 
@@ -95,7 +93,7 @@ class IncomingMessage < ApplicationRecord
     # values in case we want to regenerate them (due to mail
     # parsing bugs, etc).
     raise "Incoming message id=#{id} has no raw_email" if raw_email.nil?
-    raise RawEmailErasedError if raw_email_erased?
+    raise RawEmail::ErasedError if raw_email_erased?
 
     ActiveRecord::Base.transaction do
       extract_attachments
