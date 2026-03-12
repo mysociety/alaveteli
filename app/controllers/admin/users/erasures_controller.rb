74580c9dd4ff62fcedcbfa40ec452215e9c93e2e
Gareth Rees <gareth@garethrees.co.uk>
2026-03-12 16:17:15 +0000

    Process user erasures asynchronously
    Queues a `User::ErasureJob` rather than processing within the request
    cycle in preparation for making redactions permanent on request content.

--- app/controllers/admin/users/erasures_controller.rb
+++ app/controllers/admin/users/erasures_controller.rb
@@ -2,22 +2,24 @@
 # Controller for erasing accounts
 #
 class Admin::Users::ErasuresController < AdminController
-  before_action :set_erased_user
+  before_action :set_erased_user, :ensure_erasable
 
   def create
-    if @erased_user.erase(editor: admin_current_user, reason: reason)
-      flash[:notice] = 'The user was erased.'
-    else
-      flash[:error] = 'Something went wrong. The user could not be erased.'
-    end
-
-    redirect_to admin_user_path(@erased_user)
+    @erased_user.erase_later(editor: admin_current_user, reason: reason)
+    redirect_to admin_user_path(@erased_user), notice: 'User erasure queued.'
   end
 
   private
 
   def set_erased_user
     @erased_user = User.find(params[:user_id])
+  end
+
+  def ensure_erasable
+    return if @erased_user.closed?
+
+    flash[:error] = 'User accounts must be closed before erasing.'
+    redirect_to admin_user_path(@erased_user)
   end
 
   def reason
