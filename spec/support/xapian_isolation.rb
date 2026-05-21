d90243847186e9fecdba99a762562678a0961408
Graeme Porteous <graeme@rgbp.co.uk>
2026-05-21 09:42:16 +0100

    Add Xapian access warning for test suite
    Prepend hooks on `ActsAsXapian.readable_init` and `.writable_init` to
    emit warnings when called during tests. This makes it easy to identify
    specs that depend on a live Xapian database.
    
    The warning is output-only and does not change test behaviour.

---
+++ spec/support/xapian_isolation.rb
@@ -0,0 +1,26 @@
+##
+# Detect and warn when Xapian database is accessed during tests.
+#
+# Prepends hooks on ActsAsXapian.readable_init and .writable_init to
+# emit warnings with a backtrace snippet, making it easy to find specs
+# that depend on a live Xapian index.
+#
+module XapianIsolation
+  module Warning
+    def readable_init
+      XapianIsolation.warn_xapian_access('readable_init')
+      super
+    end
+
+    def writable_init(_suffix = "")
+      XapianIsolation.warn_xapian_access('writable_init')
+      super
+    end
+  end
+
+  def self.warn_xapian_access(method)
+    warn "[XAPIAN] #{method} called during test"
+  end
+end
+
+ActsAsXapian.singleton_class.prepend(XapianIsolation::Warning)
