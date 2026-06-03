04035981a9744c530566e14b81c7257f85bb84fc
Graeme Porteous <graeme@rgbp.co.uk>
2026-06-03 13:44:33 +0100

    Move InfoRequest search methods to Search module
    Move request_list, recent_requests, and search_events from InfoRequest
    to the Search module.
    
    - Search.request_list: paginated request list for RequestController#list
    - Search.recent_requests: front page recent successful/sent requests
    - Search.search_events: private helper converting offset/limit to
      page/per_page for the Search interface
    
    Callers in RequestController and GeneralController already updated to
    use Search.request_list and Search.recent_requests.

--- spec/models/search/context/info_request_spec.rb
+++ spec/models/search/context/info_request_spec.rb
@@ -17,4 +17,25 @@ RSpec.describe Search::Context::InfoRequest do
       expect(described_class.new(request).similar_requests).to eq(searcher)
     end
   end
+
+  describe '#request_list' do
+    it 'delegates to Search::RequestList' do
+      filters = { latest_status: 'all' }
+      list = instance_double(Search::RequestList, call: :list_result)
+      expect(Search::RequestList).to receive(:new).
+        with(filters, 1, 25, 50).and_return(list)
+
+      result = described_class.new(request).request_list(filters, 1, 25, 50)
+      expect(result).to eq(:list_result)
+    end
+  end
+
+  describe '#recent_requests' do
+    it 'delegates to Search::RecentRequests' do
+      recent = instance_double(Search::RecentRequests, call: :recent_result)
+      expect(Search::RecentRequests).to receive(:new).and_return(recent)
+
+      expect(described_class.new(request).recent_requests).to eq(:recent_result)
+    end
+  end
 end
