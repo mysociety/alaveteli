fa8000f3594662d58a94a359131a60926306c5a7
Graeme Porteous <graeme@rgbp.co.uk>
2026-06-23 10:21:14 +0100

    Add shared a search backend contract for adapters
    Add a shared example group covering composable search_scope, search
    results, reindex_later and queued_jobs_count, and apply it to the
    Xapian adapter.
    
    A future PostgreSQL adapter will includes the same shared examples.

--- spec/search/adapters/xapian_spec.rb
+++ spec/search/adapters/xapian_spec.rb
@@ -1,29 +1,7 @@
 require 'spec_helper'
+require_relative '../shared_examples/backend_contract'
 
 RSpec.describe Search::Adapters::Xapian::Adapter, :xapian do
   subject(:adapter) { described_class.new }
-
-  describe '#search_scope' do
-    it 'returns an ActiveRecord::Relation' do
-      scope = adapter.search_scope('bob', User.all)
-      expect(scope).to be_a(ActiveRecord::Relation)
-    end
-
-    it 'constrains the relation to matching records' do
-      scope = adapter.search_scope('bob', User.all)
-      expect(scope).to include(users(:bob_smith_user))
-    end
-
-    it 'keeps the relation chainable with further conditions' do
-      bob = users(:bob_smith_user)
-      scope = adapter.search_scope('bob', User.all).where.not(id: bob.id)
-      expect(scope).not_to include(bob)
-    end
-
-    it 'respects conditions already applied to the relation' do
-      bob = users(:bob_smith_user)
-      scope = adapter.search_scope('bob', User.where.not(id: bob.id))
-      expect(scope).not_to include(bob)
-    end
-  end
+  it_behaves_like 'a search backend'
 end
