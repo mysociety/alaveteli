564ca236afe275f00528291b7c47cbbf3d5a2a0d
Graeme Porteous <graeme@rgbp.co.uk>
2026-08-11 17:04:29 +0100

    Make request_updates alerts query the database

--- app/models/search/track_events/request_updates.rb
+++ app/models/search/track_events/request_updates.rb
@@ -1,6 +1,15 @@
 module Search
   class TrackEvents
-    class RequestUpdates < Index
+    ##
+    # Resolved TrackThing query:
+    #   request:info_request.url_title
+    #
+    class RequestUpdates < Database
+      private
+
+      def events_scope
+        InfoRequestEvent.where(info_request: track_thing.info_request)
+      end
     end
   end
 end
