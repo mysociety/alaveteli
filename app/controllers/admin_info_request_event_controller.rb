a7a8be062166f31cb861a741083435bb042e09ef
Laurent Savaëte <laurent@where.tf>
2026-08-14 14:50:33 +0200

    Add UI to search and list info request events

--- app/controllers/admin_info_request_event_controller.rb
+++ app/controllers/admin_info_request_event_controller.rb
@@ -5,7 +5,26 @@
 # Email: hello@mysociety.org; WWW: http://www.mysociety.org/
 
 class AdminInfoRequestEventController < AdminController
-  before_action :set_info_request_event, only: [:update]
+  include Admin::Searchable
+  include Admin::Sortable
+
+  before_action :set_info_request_event, only: [:edit, :update]
+
+  sortable default: :updated_at_desc, relevance: :indexed_search?,
+           only: [:index]
+
+  def index
+    @page = get_search_page_from_params
+    @query = params[:query] || ""
+
+    @info_request_events = measure_search(
+      sorted(
+        info_request_event_scope.
+          order(id: :desc).
+          paginate(page: @page, per_page: 100)
+      )
+    )
+  end
 
   # used so due dates get fixed
   def update
@@ -28,5 +47,16 @@ class AdminInfoRequestEventController < AdminController
 
   def set_info_request_event
     @info_request_event = InfoRequestEvent.find(params[:id])
+  end
+
+  def info_request_event_scope
+    return base_scope if @query.blank?
+
+    base_scope.search_scope(@query, backend: :postgresql, admin_mode: true)
+  end
+
+  def base_scope
+    scope = InfoRequestEvent
+    cannot?(:admin, AlaveteliPro::Embargo) ? scope.not_embargoed : scope
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -730,7 +730,7 @@ Rails.application.routes.draw do
   scope '/admin', :as => 'admin' do
     resources :info_request_events,
       :controller => 'admin_info_request_event',
-      :only => [:update]
+      :only => [:index, :update]
   end
 
   #### AdminIncomingMessage controller
