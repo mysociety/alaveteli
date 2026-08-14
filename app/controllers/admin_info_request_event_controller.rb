226553f235ee5fbf48a8b21d01c85bf93539afdf
Laurent Savaëte <laurent@where.tf>
2026-08-14 17:01:54 +0200

    Add edit form for InfoRequestEvent params
    This only allows modifying the params attribute, as the anticipated use
    case is removal of personal data, which can only happen in this
    particular attribute. This limits the risk of causing data
    inconsistency.

--- app/controllers/admin_info_request_event_controller.rb
+++ app/controllers/admin_info_request_event_controller.rb
@@ -5,7 +5,7 @@
 # Email: hello@mysociety.org; WWW: http://www.mysociety.org/
 
 class AdminInfoRequestEventController < AdminController
-  before_action :set_info_request_event, only: [:update]
+  before_action :set_info_request_event, only: [:edit, :update]
 
   def index
     @page = params[:page] || 1
@@ -22,21 +22,46 @@ class AdminInfoRequestEventController < AdminController
     end
   end
 
+  def edit
+    @params_to_show = JSON.pretty_generate(@info_request_event.params)
+  end
+
   # used so due dates get fixed
   def update
-    if @info_request_event.event_type != 'response'
-      raise "can only mark responses as requires clarification"
+    @was_clarification = params[:commit] == "Was clarification request"
+
+    if @was_clarification
+      if @info_request_event.event_type != 'response'
+        raise "can only mark responses as requires clarification"
+      end
+
+      @info_request_event.described_state = 'waiting_clarification'
+      @info_request_event.calculated_state = 'waiting_clarification'
+      # TODO: deliberately don't update described_at so doesn't reenter search?
+      @info_request_event.save!
+      # Reset the due dates for the request if necessary
+      @info_request_event.recheck_due_dates
+
+      flash[:notice] = "Old response marked as having been a request for clarification"
+      redirect_to admin_request_url(@info_request_event.info_request)
+    else
+      @params_to_show = params.dig(:info_request_event, :params_to_show)
+
+      if @params_to_show.strip.empty?
+        raise "Event params cannot be empty"
+      end
+      begin
+        @info_request_event.params = JSON.parse(@params_to_show)
+
+        @info_request_event.save!
+        flash[:notice] = "InfoRequestEvent params updated"
+        redirect_to admin_info_request_events_path
+      rescue JSON::ParserError => e
+        flash[:error] = "Invalid JSON content: #{e.message}"
+        render :edit, status: :unprocessable_entity
+      end
+
     end
-
-    @info_request_event.described_state = 'waiting_clarification'
-    @info_request_event.calculated_state = 'waiting_clarification'
-    # TODO: deliberately don't update described_at so doesn't reenter search?
-    @info_request_event.save!
-    # Reset the due dates for the request if necessary
-    @info_request_event.recheck_due_dates
-
-    flash[:notice] = "Old response marked as having been a request for clarification"
-    redirect_to admin_request_url(@info_request_event.info_request)
   end
 
   private
